f2279ee49b9bb18d501b85f7547f59d5e77d7c10
Alex de Borba <mail@alexdeborba.com>
2022-09-19 13:42:13 -0500

    Initial commit

---
+++ inc/controls/class-aeon-react-control.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The React Customizer control.
+ *
+ * @package Aeon
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+if ( class_exists( 'WP_Customize_Control' ) && ! class_exists( 'Aeon_React_Control' ) ) {
+	/**
+	 * Control class.
+	 */
+	class Aeon_React_Control extends WP_Customize_Control {
+		/**
+		 * The control type.
+		 *
+		 * @access public
+		 * @var string
+		 */
+		public $type = 'aeon-react-control';
+
+		/**
+		 * Additional arguments passed to JavaScript.
+		 *
+		 * @var array
+		 */
+		public $content;
+
+		/**
+		 * Custom control class.
+		 *
+		 * @access public
+		 * @var string
+		 */
+		public $custom_class = '';
+	
+		/**
+		 * Refresh the parameters passed to the JavaScript via JSON.
+		 *
+		 * @since 3.4.0
+		 * @uses WP_Customize_Control::to_json()
+		 */
+		public function to_json() {
+			parent::to_json();
+			$this->json['content'] = $this->content;
+			$this->json['choices'] = $this->choices;
+			$this->json['custom_class'] = $this->custom_class;
+		}
+	}
+}
