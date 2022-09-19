88fd496e8c2ad55b8c162a358b65145abb72b3a9
Alex de Borba <mail@alexdeborba.com>
2022-09-19 14:29:59 -0500

    Initial commit

---
+++ inc/controls/js/upsell-control.js
@@ -0,0 +1,12 @@
+( function( api ) {
+	api.sectionConstructor['aeon-upsell'] = api.Section.extend( {
+
+		// No events for this type of section.
+		attachEvents: function () {},
+
+		// Always make the section active.
+		isContextuallyActive: function () {
+			return true;
+		}
+	} );
+} )( wp.customize );
