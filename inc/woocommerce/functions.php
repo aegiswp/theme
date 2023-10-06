eda9e1fa97ad98cdc0df86c390ff839e5f56cf0f
Atmostfear Entertainment <mail@alexdeborba.com>
2023-10-06 17:02:04 -0500

    Update functions.php

--- inc/woocommerce/functions.php
+++ inc/woocommerce/functions.php
@@ -6,24 +6,51 @@
  * @since 1.0.0
  */
 
-add_theme_support( 'woocommerce');
+// Enable WooCommerce support.
+add_theme_support('woocommerce');
 
-// Register WooCommerce theme features.
-add_theme_support( 'wc-product-gallery-zoom' );
-add_theme_support( 'wc-product-gallery-lightbox' );
-add_theme_support( 'wc-product-gallery-slider' );
-add_theme_support('woocommerce', array(
-	'thumbnail_image_width' => 600,
-	'single_image_width'    => 1400,
+// Enable WooCommerce product gallery features.
+add_theme_support('wc-product-gallery-zoom');
+add_theme_support('wc-product-gallery-lightbox');
+add_theme_support('wc-product-gallery-slider');
+
+// Configure WooCommerce support.
+add_theme_support(
+	'woocommerce',
+	array(
+		'thumbnail_image_width' => 600,
+		'single_image_width'    => 1400,
 	)
 );
 
+
 /**
- * Change number or products per row to default to 3 columns.
+ * Woocommerce functions
+ *
+ * @package Aegis
+ * @since 1.0.0
+ */
+
+/**
+ * Filter the number of columns in the shop loop.
+ * 
+ * @param int $columns The default number of columns.
+ * @return int The modified number of columns.
  */
 add_filter('loop_shop_columns', 'loop_columns', 999);
+
+/**
+ * Check if the function loop_columns does not already exist.
+ * If it does not, define the function.
+ */
 if (!function_exists('loop_columns')) {
-	function loop_columns() {
-		return 3; // 3 products per row
+	/**
+	 * Get the number of products per row in the shop loop.
+	 *
+	 * @return int The number of products per row.
+	 */
+	function loop_columns()
+	{
+		return 3; // 3 products per row.
 	}
 }
