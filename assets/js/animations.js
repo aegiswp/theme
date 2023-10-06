ef49f0ba7c6a90063a28c7564b1ca677ad788354
Atmostfear Entertainment <mail@alexdeborba.com>
2023-10-06 16:41:42 -0500

    Update animations.js

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,18 +1,18 @@
 // Calculate clients viewport
 const w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName('body')[0];
+  d = document,
+  e = d.documentElement,
+  g = d.getElementsByTagName('body')[0];
 
 let x = w.innerWidth || e.clientWidth || g.clientWidth, // Viewport Width
-    y = w.innerHeight || e.clientHeight || g.clientHeight; // Viewport Height
+  y = w.innerHeight || e.clientHeight || g.clientHeight; // Viewport Height
 
 // Global vars
 const htmlEl = document.documentElement;
 const body = document.body;
 
 // On scroll
-const fnOnScroll = function() {
+const fnOnScroll = function () {
   const animateBlock = document.querySelectorAll('.image-animation-from-bottom, .image-animation-from-top, .image-animation-from-left, .image-animation-from-right, .animate-from-top, .animate-from-bottom, .animate-from-left, .animate-from-right');
 
   animateBlock.forEach(element => {
@@ -38,6 +38,6 @@ const fnOnScroll = function() {
 
 fnOnScroll();
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   setTimeout(fnOnScroll, 300);
 });
