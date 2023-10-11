eed29cb8f1f1f23769e3a5b0991a096884150a93
Atmostfear Entertainment <5738223+alexdeborba@users.noreply.github.com>
2023-10-11 01:11:20 -0500

    Update block-patterns.php

--- inc/block-patterns.php
+++ inc/block-patterns.php
@@ -57,8 +57,6 @@ function aegis_register_block_patterns() {
 		'header/header-4',
 		'general/hero-1',
 		'general/hero-2',
-		'general/hero-3',
-		'general/hero-4',
 		'general/about-1',
 		'general/about-2',
 		'general/team-1',
