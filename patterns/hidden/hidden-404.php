59a9dce28a33f772fd01a79e68da0fff09de9e67
Atmostfear Entertainment <labs@atmostfear-entertainment.com>
2023-10-12 09:11:07 -0500

    Update hidden-404.php

--- patterns/hidden/hidden-404.php
+++ patterns/hidden/hidden-404.php
@@ -5,11 +5,12 @@
 return array(
 	'title'    => __( '404 content', 'aegis' ),
 	'inserter' => false,
-	'content'  => '<!-- wp:heading {"style":{"typography":{"fontSize":"clamp(4rem, 40vw, 20rem)","fontWeight":"200","lineHeight":"1"}},"className":"has-text-align-center"} -->
-					<h2 class="has-text-align-center" style="font-size:clamp(4rem, 40vw, 20rem);font-weight:200;line-height:1">' . esc_html( _x( '404', 'Error code for a webpage that is not found.', 'aegis' ) ) . '</h2>
-					<!-- /wp:heading -->
-					<!-- wp:paragraph {"align":"center"} -->
-					<p class="has-text-align-center">' . esc_html__( 'This page could not be found. Maybe try a search?', 'aegis' ) . '</p>
-					<!-- /wp:paragraph -->
-					<!-- wp:search {"label":"Search","showLabel":false,"width":100,"widthUnit":"%","buttonText":"Search","buttonUseIcon":true,"align":"center","backgroundColor":"foreground"} /-->',
+	'content'  => '
+	<!-- wp:heading {"style":{"typography":{"fontSize":"clamp(4rem, 40vw, 20rem)","fontWeight":"200","lineHeight":"1"}},"className":"has-text-align-center"} -->
+	<h2 class="has-text-align-center" style="font-size:clamp(4rem, 40vw, 20rem);font-weight:200;line-height:1">' . esc_html( _x( '404', 'Error code for a webpage that is not found.', 'aegis' ) ) . '</h2>
+	<!-- /wp:heading -->
+	<!-- wp:paragraph {"align":"center"} -->
+	<p class="has-text-align-center">' . esc_html__( 'This page could not be found. Maybe try a search?', 'aegis' ) . '</p>
+	<!-- /wp:paragraph -->
+	<!-- wp:search {"label":"Search","showLabel":false,"width":100,"widthUnit":"%","buttonText":"Search","buttonUseIcon":true,"align":"center","backgroundColor":"foreground"} /-->',
 );
