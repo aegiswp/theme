23c0d1547538f3a1b35fdf26dae229e0ea468533
Atmostfear Entertainment <5738223+alexdeborba@users.noreply.github.com>
2023-10-13 03:33:43 -0500

    Update block-patterns.php

--- inc/block-patterns.php
+++ inc/block-patterns.php
@@ -1,8 +1,7 @@
 <?php
 /**
- * Block Patterns
+ * Aegis: Block Patterns
  *
- * @package Aegis
  * @since 1.0.0
  */
 
@@ -12,38 +11,36 @@
  * @since 1.0.0
  */
 function aegis_register_block_patterns() {
-	// Define an array of block pattern categories.
 	$block_pattern_categories = array(
-		'aegis-header'           => array('label' => __('Header', 'aegis')),
-		'aegis-hero'             => array('label' => __('Hero', 'aegis')),
-		'aegis-about'            => array('label' => __('About', 'aegis')),
-		'aegis-team'             => array('label' => __('Team', 'aegis')),
-		'aegis-query'            => array('label' => __('Blog', 'aegis')),
-		'aegis-commerce'         => array('label' => __('Commerce', 'aegis')),
-		'aegis-pages'            => array('label' => __('Pages', 'aegis')),
-		'aegis-pricing'          => array('label' => __('Pricing', 'aegis')),
-		'aegis-testimonials'     => array('label' => __('Testimonials', 'aegis')),
-		'aegis-faq'              => array('label' => __('FAQ', 'aegis')),
-		'aegis-contact'          => array('label' => __('Contact', 'aegis')),
-		'aegis-footer'           => array('label' => __('Footer', 'aegis')),
+		'aegis-header'			=> array( 'label' => __( 'Header', 'aegis' ) ),
+		'aegis-footer'			=> array( 'label' => __( 'Footer', 'aegis' ) ),
+		'aegis-hero'			=> array( 'label' => __( 'Hero', 'aegis' ) ),
+		'aegis-about'			=> array( 'label' => __( 'About', 'aegis' ) ),
+		'aegis-team'			=> array( 'label' => __( 'Team', 'aegis' ) ),
+		'aegis-testimonials'	=> array( 'label' => __( 'Testimonials', 'aegis' ) ),
+		'aegis-faq'				=> array( 'label' => __( 'FAQ', 'aegis' ) ),
+		'aegis-pricing'			=> array( 'label' => __( 'Pricing', 'aegis' ) ),
+		'aegis-contact'			=> array( 'label' => __( 'Contact', 'aegis' ) ),
+		'aegis-commerce'		=> array( 'label' => __( 'eCommerce', 'aegis' ) ),
+		'aegis-pages'			=> array( 'label' => __( 'Pages', 'aegis' ) ),
+		'aegis-query'			=> array( 'label' => __( 'Blog', 'aegis' ) ),
 	);
-}
 
-/**
- * Filters the theme block pattern categories.
- *
- * @since 1.0.0
- *
- * @param array[] $block_pattern_categories {
- *	 An associative array of block pattern categories, keyed by category name.
- *
- *	 @type array[] $properties {
- *		 An array of block category properties.
- *
- *		 @type string $label A human-readable label for the pattern category.
- *	 }
- * }
- */
+	/**
+	 * Filters the theme block pattern categories.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array[] $block_pattern_categories {
+	 *	 An associative array of block pattern categories, keyed by category name.
+	 *
+	 *	 @type array[] $properties {
+	 *		 An array of block category properties.
+	 *
+	 *		 @type string $label A human-readable label for the pattern category.
+	 *	 }
+	 * }
+	 */
 	$block_pattern_categories = apply_filters( 'aegis_block_pattern_categories', $block_pattern_categories );
 
 	foreach ( $block_pattern_categories as $name => $properties ) {
@@ -51,59 +48,47 @@ function aegis_register_block_patterns() {
 	}
 
 	$block_patterns = array(
-		'header/header-1',
-		'header/header-2',
-		'header/header-3',
-		'header/header-4',
-		'general/hero-1',
-		'general/hero-2',
-		'general/about-1',
-		'general/about-2',
-		'general/team-1',
-		'general/testimonials-1',
-		'general/pricing-1',
-		'general/faq-1',
-		'general/contact-1',
-		'commerce/commerce-1',
-		'commerce/commerce-2',
-		'commerce/commerce-3',
-		'commerce/commerce-4',
-		'pages/home',
-		'pages/home-1',
-		'pages/home-2',
-		'pages/home-3',
-		'pages/home-4',
-		'pages/about',
-		'pages/shop',
-		'pages/shop-1',
-		'pages/shop-2',
-		'query/query-1',
-		'query/query-2',
-		'query/query-3',
-		'query/query-4',
-	    'footer/footer-1',
-	    'footer/footer-2',
-	    'footer/footer-3',
-	    'footer/footer-4',
-		'hidden/hidden-404',
+	'header/header-1',
+	'header/header-2',
+	'header/header-3',
+	'header/header-4',
+	'footer/footer-1',
+	'footer/footer-2',
+	'footer/footer-3',
+	'footer/footer-4',
+	'general/hero-1',
+	'general/hero-2',
+	'general/about-1',
+	'general/about-2',
+	'general/team-1',
+	'general/testimonials-1',
+	'general/faq-1',
+	'general/contact-1',
+	'ecommerce/ecommerce-1',
+	'ecommerce/ecommerce-2',
+	'ecommerce/ecommerce-3',
+	'ecommerce/ecommerce-4',
+	'query/query-1',
+	'query/query-2',
+	'query/query-3',
+	'query/query-4',
+	'hidden/hidden-404',
 	);
 
-/**
- * Registers block patterns in the theme.
- *
- * @since 1.0.0
- */
-function aegis_register_block_patterns() {
+	/**
+	 * Filters the theme block patterns.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param $block_patterns array List of block patterns by name.
+	 */
 	$block_patterns = apply_filters( 'aegis_block_patterns', $block_patterns );
 
 	foreach ( $block_patterns as $block_pattern ) {
-		// Register each block pattern
 		register_block_pattern(
 			'aegis/' . $block_pattern,
 			require get_template_directory() . '/patterns/' . $block_pattern . '.php'
 		);
 	}
 }
-
-// Add the 'aegis_register_block_patterns' function to the 'init' action with priority 9
 add_action( 'init', 'aegis_register_block_patterns', 9 );
