eba900ddd7922a72eefa236a65672f4103549f69
Atmostfear Entertainment <labs@atmostfear-entertainment.com>
2023-10-14 11:57:58 -0500

    Update animations.js
    Recoded version.

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,43 +1,50 @@
-// Calculate clients viewport
-const w = window,
-  d = document,
-  e = d.documentElement,
-  g = d.getElementsByTagName('body')[0];
+(function() {
+  'use strict';
 
-let x = w.innerWidth || e.clientWidth || g.clientWidth, // Viewport Width
-  y = w.innerHeight || e.clientHeight || g.clientHeight; // Viewport Height
+  // Calculate clients viewport
+  var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0];
 
-// Global vars
-const htmlEl = document.documentElement;
-const body = document.body;
+  var x = w.innerWidth || e.clientWidth || g.clientWidth; // Viewport Width
+  var y = w.innerHeight || e.clientHeight || g.clientHeight; // Viewport Height
 
-// On scroll
-const fnOnScroll = function () {
-  const animateBlock = document.querySelectorAll('.image-animation-from-bottom, .image-animation-from-top, .image-animation-from-left, .image-animation-from-right, .animate-from-top, .animate-from-bottom, .animate-from-left, .animate-from-right');
+  // Global vars
+  var htmlEl = document.documentElement;
+  var body = document.body;
 
-  animateBlock.forEach(element => {
-    element.classList.remove('is-loading');
+  document.addEventListener('DOMContentLoaded', function() {
+    // On scroll
+    var fnOnScroll = function() {
+      var animateBlock = document.querySelectorAll('.image-animation-from-bottom, .image-animation-from-top, .image-animation-from-left, .image-animation-from-right, .animate-from-top, .animate-from-bottom, .animate-from-left, .animate-from-right');
 
-    const animateBlockOffsetTop = element.offsetTop;
-    let activationOffset;
+      animateBlock.forEach(function(el) {
+        el.classList.remove('is-loading');
 
-    // Determinate distance to initiate animation relative to viewport height
-    if (element.dataset.offset) {
-      activationOffset = element.dataset.offset;
-    } else if (element.classList.contains('image-animation-from-bottom')) {
-      activationOffset = 1.2;
-    } else {
-      activationOffset = 1.1;
-    }
+        var animateBlockOffsetTop = el.offsetTop;
+        var activationOffset;
 
-    if (window.pageYOffset > animateBlockOffsetTop - y / activationOffset) {
-      element.classList.add('scrolled-to');
-    }
+        if (el.dataset.offset) {
+          activationOffset = el.dataset.offset;
+        } else if (el.classList.contains('image-animation-from-bottom')) {
+          activationOffset = 1.2;
+        } else {
+          activationOffset = 1.1;
+        }
+
+        if ((window.pageYOffset > animateBlockOffsetTop - y / activationOffset)) {
+          el.classList.add('scrolled-to');
+        }
+      });
+    };
+
+    fnOnScroll();
+
+    window.addEventListener('scroll', function() {
+      setTimeout(function() {
+        fnOnScroll();
+      }, 300);
+    });
   });
-};
-
-fnOnScroll();
-
-window.addEventListener('scroll', function () {
-  setTimeout(fnOnScroll, 300);
-});
+})();
