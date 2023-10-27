d01d3542f710c9e98608c4777f1ae50e39cc170b
Atmostfear Entertainment <5738223+alexdeborba@users.noreply.github.com>
2023-10-27 10:53:17 -0500

    Update ecommerce-3.php
    - Changed: Refined pattern title and description.
    - Updated: Images filename for testimonials.

--- patterns/ecommerce/ecommerce-3.php
+++ patterns/ecommerce/ecommerce-3.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * eCommerce 3 Block Pattern
+ * 03. eCommerce Block Pattern
  */
 return array(
-	'title'	  => __( 'eCommerce 3', 'aegis' ),
-	'description' => __( 'Testimonials grid block pattern', 'aegis' ),
+	'title'	  => __( '03. eCommerce', 'aegis' ),
+	'description' => __( 'eCommerce Block Pattern', 'aegis' ),
 	'categories' => array( 'aegis-ecommerce' ),
-	'content' => '<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"30px","left":"30px","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
-	<div class="wp-block-group alignfull"
-		style="padding-top:var(--wp--preset--spacing--50);padding-right:30px;padding-bottom:var(--wp--preset--spacing--50);padding-left:30px">
+	'content' => '
+	<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"30px","left":"30px","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--50);padding-right:30px;padding-bottom:var(--wp--preset--spacing--50);padding-left:30px">
 		<!-- wp:columns {"align":"wide"} -->
 		<div class="wp-block-columns alignwide">
 			<!-- wp:column -->
@@ -54,7 +54,7 @@ return array(
 					<!-- wp:group {"layout":{"type":"flex","allowOrientation":false,"justifyContent":"left"}} -->
 					<div class="wp-block-group">
 						<!-- wp:image {"sizeSlug":"full","linkDestination":"none","className":"is-resized is-style-rounded"} -->
-						<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="' . esc_url( get_template_directory_uri() ) . '/assets/images/placeholder_80x80_darkcolor.webp" alt="' . esc_attr__( 'Describe the main elements of the image and its context.', 'aegis' ) . '" /></figure>
+						<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="' . esc_url( get_template_directory_uri() ) . '/assets/images/thumb_80x80_dark.webp" alt="' . esc_attr__( 'Describe the main elements of the image and its context.', 'aegis' ) . '" /></figure>
 						<!-- /wp:image -->
 
 						<!-- wp:paragraph {"fontSize":"tiny"} -->
@@ -70,8 +70,7 @@ return array(
 			<!-- wp:column -->
 			<div class="wp-block-column">
 				<!-- wp:group {"style":{"spacing":{"padding":{"top":"10%","right":"10%","bottom":"10%","left":"10%"}}},"backgroundColor":"background","className":"is-style-aegis-border"} -->
-				<div class="wp-block-group is-style-aegis-border has-background-background-color has-background"
-					style="padding-top:10%;padding-right:10%;padding-bottom:10%;padding-left:10%">
+				<div class="wp-block-group is-style-aegis-border has-background-background-color has-background" style="padding-top:10%;padding-right:10%;padding-bottom:10%;padding-left:10%">
 					<!-- wp:columns -->
 					<div class="wp-block-columns">
 						<!-- wp:column {"width":"100%"} -->
@@ -81,8 +80,7 @@ return array(
 							<!-- /wp:paragraph -->
 
 							<!-- wp:paragraph {"style":{"typography":{"letterSpacing":"2px"}},"textColor":"foreground","className":"aegis-rating","fontSize":"tiny"} -->
-							<p class="aegis-rating has-foreground-color has-text-color has-tiny-font-size"
-								style="letter-spacing:2px">' . esc_html__( '★★★★★', 'aegis' ) . '</p>
+							<p class="aegis-rating has-foreground-color has-text-color has-tiny-font-size" style="letter-spacing:2px">' . esc_html__( '★★★★★', 'aegis' ) . '</p>
 							<!-- /wp:paragraph -->
 
 							<!-- wp:paragraph {"textColor":"foreground","fontSize":"small"} -->
@@ -100,7 +98,7 @@ return array(
 					<!-- wp:group {"layout":{"type":"flex","allowOrientation":false,"justifyContent":"left"}} -->
 					<div class="wp-block-group">
 						<!-- wp:image {"className":"size-full is-resized is-style-rounded"} -->
-						<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="' . esc_url( get_template_directory_uri() ) . '/assets/images/placeholder_80x80_darkcolor.webp" alt="' . esc_attr__( 'Describe the main elements of the image and its context.', 'aegis' ) . '" /></figure>
+						<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="' . esc_url( get_template_directory_uri() ) . '/assets/images/thumb_80x80_dark.webp" alt="' . esc_attr__( 'Describe the main elements of the image and its context.', 'aegis' ) . '" /></figure>
 						<!-- /wp:image -->
 
 						<!-- wp:paragraph {"fontSize":"tiny"} -->
@@ -144,7 +142,7 @@ return array(
 					<!-- wp:group {"layout":{"type":"flex","allowOrientation":false,"justifyContent":"left"}} -->
 					<div class="wp-block-group">
 						<!-- wp:image {"sizeSlug":"full","linkDestination":"none","className":"is-resized is-style-rounded"} -->
-						<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="' . esc_url( get_template_directory_uri() ) . '/assets/images/placeholder_80x80_darkcolor.webp" alt="' . esc_attr__( 'Describe the main elements of the image and its context.', 'aegis' ) . '" /></figure>
+						<figure class="wp-block-image size-full is-resized is-style-rounded"><img src="' . esc_url( get_template_directory_uri() ) . '/assets/images/thumb_80x80_dark.webp" alt="' . esc_attr__( 'Describe the main elements of the image and its context.', 'aegis' ) . '" /></figure>
 						<!-- /wp:image -->
 
 						<!-- wp:paragraph {"fontSize":"tiny"} -->
