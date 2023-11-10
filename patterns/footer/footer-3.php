5f5ded293bb6106e4112ef7e0dad4098e9747bd2
Atmostfear Entertainment <labs@atmostfear-entertainment.com>
2023-11-10 08:30:34 -0500

    Update footer-3.php

--- patterns/footer/footer-3.php
+++ patterns/footer/footer-3.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Footer 3 Block Pattern
+ * 03. Footer Block Pattern
  */
 return array(
-	'title'	      => __( 'Footer 3', 'aegis' ),
-	'description' => __( 'Footer block pattern', 'aegis' ),
+	'title'	      => __( '03. Footer', 'aegis' ),
+	'description' => __( 'Footer Block Pattern', 'aegis' ),
 	'categories'  => array( 'aegis-footer' ),
 	'blockTypes' => array( 'core/template-part/footer' ),
 	'content' => '
@@ -144,13 +144,13 @@ return array(
 
 				<!-- wp:social-links {"iconColor":"foreground","iconColorValue":"#1c1c1e","size":"has-small-icon-size","style":{"spacing":{"blockGap":"20px","margin":{"top":"30px","bottom":"24px"}}},"className":"is-style-logos-only"} -->
 				<ul class="wp-block-social-links has-small-icon-size has-icon-color is-style-logos-only" style="margin-top:30px;margin-bottom:24px">
-					<!-- wp:social-link {"url":"https://facebook.com/","service":"facebook","label":"Facebook"} /-->
+				<!-- wp:social-link {"url":"#","service":"facebook","label":"Facebook"} /-->
 
-					<!-- wp:social-link {"url":"https://linkedin.com/","service":"linkedin","rel":"LinkedIn"} /-->
+				<!-- wp:social-link {"url":"#","service":"linkedin","label":"LinkedIn"} /-->
 
-					<!-- wp:social-link {"url":"https://tiktok.com/","service":"tiktok","rel":"TikTok"} /-->
+				<!-- wp:social-link {"url":"#","service":"instagram","label":"Instagram"} /-->
 
-					<!-- wp:social-link {"url":"https://instagram.com/","service":"instagram","rel":"Instagram"} /-->
+				<!-- wp:social-link {"url":"#","service":"wordpress","label":"WordPress"} /-->
 				</ul>
 				<!-- /wp:social-links -->
 			</div>
