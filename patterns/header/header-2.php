870873c770e421678a29c2c6e5baa8d72e831263
Atmostfear Entertainment <labs@atmostfear-entertainment.com>
2023-11-16 10:05:44 -0500

    Update header-2.php
    - Enhanced the vertical alignment of columns within the 'socials-cart' group for improved visual consistency.
    - Simplified the border styling of the search bar in the 'banner-info' group by removing the border radius, focusing on a cleaner and more minimalistic design.
    - Adjusted the 'navigation' block within the wide-aligned columns to ensure centralized alignment and consistency in design across different devices.
    - Retained other elements and overall functionality from the previous version to ensure continuity in the header block pattern.

--- patterns/header/header-2.php
+++ patterns/header/header-2.php
@@ -12,73 +12,72 @@ return array(
 	<div class="wp-block-group alignfull header-2" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
 		<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"var:preset|spacing|30","bottom":"0","left":"var:preset|spacing|30"}}},"backgroundColor":"secondary","layout":{"type":"constrained"}} -->
 		<div class="wp-block-group has-secondary-background-color has-background" style="padding-top:0;padding-right:var(--wp--preset--spacing--30);padding-bottom:0;padding-left:var(--wp--preset--spacing--30)">
-			<!-- wp:columns {"align":"wide"} -->
-			<div class="wp-block-columns alignwide">
-				<!-- wp:column {"style":{"spacing":{"padding":{"top":"10px","bottom":"10px","right":"0","left":"0"}}}} -->
-				<div class="wp-block-column" style="padding-top:10px;padding-right:0;padding-bottom:10px;padding-left:0">
-					<!-- wp:paragraph {"align":"center","fontSize":"tiny"} -->
-					<p class="has-text-align-center has-tiny-font-size">' . esc_html__( 'Offer Highlight (52 chars): [Announce a special deal or limited-time opportunity.]', 'aegis' ) . '</p>
-					<!-- /wp:paragraph -->
-				</div>
-				<!-- /wp:column -->
-			</div>
-			<!-- /wp:columns -->
+		<!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
+		<div class="wp-block-columns alignwide are-vertically-aligned-center">
+		<!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"10px","bottom":"10px","right":"0","left":"0"}}}} -->
+		<div class="wp-block-column is-vertically-aligned-center" style="padding-top:10px;padding-right:0;padding-bottom:10px;padding-left:0">
+		<!-- wp:paragraph {"align":"center","fontSize":"tiny"} -->
+		<p class="has-text-align-center has-tiny-font-size">' . esc_html__( 'Offer Highlight (52 chars): [Announce a special deal or limited-time opportunity.]', 'aegis' ) . '</p>
+		<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+		</div>
+		<!-- /wp:columns -->
 		</div>
 		<!-- /wp:group -->
 
 		<!-- wp:group {"style":{"spacing":{"padding":{"top":"10px","right":"var:preset|spacing|30","left":"var:preset|spacing|30","bottom":"10px"},"margin":{"top":"0","bottom":"0"}}},"className":"socials-cart","layout":{"type":"constrained"}} -->
 		<div class="wp-block-group socials-cart" style="margin-top:0;margin-bottom:0;padding-top:10px;padding-right:var(--wp--preset--spacing--30);padding-bottom:10px;padding-left:var(--wp--preset--spacing--30)">
-			<!-- wp:columns {"verticalAlignment":null,"align":"wide"} -->
-			<div class="wp-block-columns alignwide">
-				<!-- wp:column {"verticalAlignment":"center"} -->
-				<div class="wp-block-column is-vertically-aligned-center">
-					<!-- wp:social-links {"iconColor":"foreground","iconColorValue":"#1c1c1e","openInNewTab":true,"size":"has-small-icon-size","className":"is-style-logos-only"} -->
-					<ul class="wp-block-social-links has-small-icon-size has-icon-color is-style-logos-only">
-						<!-- wp:social-link {"url":"#","service":"facebook","label":"Facebook"} /-->
-
-						<!-- wp:social-link {"url":"#","service":"linkedin","label":"LinkedIn"} /-->
-
-						<!-- wp:social-link {"url":"#","service":"instagram","label":"Instagram"} /-->
-
-						<!-- wp:social-link {"url":"#","service":"wordpress","label":"WordPress"} /-->
-					</ul>
-					<!-- /wp:social-links -->
-				</div>
-				<!-- /wp:column -->
-
-				<!-- wp:column {"verticalAlignment":"top"} -->
-				<div class="wp-block-column is-vertically-aligned-top">
-					<!-- wp:site-title {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|foreground"}}}},"fontSize":"large"} /-->
-				</div>
-				<!-- /wp:column -->
-
-				<!-- wp:column -->
-				<div class="wp-block-column">
-					<!-- wp:group {"align":"wide","className":"banner-info","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
-					<div class="wp-block-group alignwide banner-info">
-						<!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search products…","width":350,"widthUnit":"px","buttonText":"Search","buttonPosition":"no-button","query":{"post_type":"product"},"style":{"border":{"width":"1px","color":"#1c1c1e","radius":{"bottomLeft":"0px","bottomRight":"0px"}}},"className":"hide-mobile"} /-->
-
-						<!-- wp:woocommerce/mini-cart {"hasHiddenPrice":true,"style":{"typography":{"fontSize":"12px"}}} /-->
-					</div>
-					<!-- /wp:group -->
-				</div>
-				<!-- /wp:column -->
-			</div>
-			<!-- /wp:columns -->
+		<!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
+		<div class="wp-block-columns alignwide are-vertically-aligned-center">
+		<!-- wp:column {"verticalAlignment":"center"} -->
+		<div class="wp-block-column is-vertically-aligned-center">
+		<!-- wp:social-links {"iconColor":"foreground","iconColorValue":"#1c1c1e","openInNewTab":true,"size":"has-small-icon-size","className":"is-style-logos-only"} -->
+		<ul class="wp-block-social-links has-small-icon-size has-icon-color is-style-logos-only">
+		<!-- wp:social-link {"url":"#","service":"facebook","label":"Facebook"} /-->
+		
+		<!-- wp:social-link {"url":"#","service":"linkedin","label":"LinkedIn"} /-->
+		
+		<!-- wp:social-link {"url":"#","service":"instagram","label":"Instagram"} /-->
+		
+		<!-- wp:social-link {"url":"#","service":"wordpress","label":"WordPress"} /-->
+		</ul>
+		<!-- /wp:social-links -->
+		</div>
+		<!-- /wp:column -->
+		
+		<!-- wp:column {"verticalAlignment":"center"} -->
+		<div class="wp-block-column is-vertically-aligned-center">
+		<!-- wp:site-title {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|foreground"}}}},"fontSize":"large"} /-->
+		</div>
+		<!-- /wp:column -->
+		
+		<!-- wp:column {"verticalAlignment":"center"} -->
+		<div class="wp-block-column is-vertically-aligned-center">
+		<!-- wp:group {"align":"wide","className":"banner-info","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
+		<div class="wp-block-group alignwide banner-info">
+		<!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search products…","width":350,"widthUnit":"px","buttonText":"Search","buttonPosition":"no-button","query":{"post_type":"product"},"style":{"border":{"width":"1px","color":"#1c1c1e"}},"className":"hide-mobile"} /-->
+		
+		<!-- wp:woocommerce/mini-cart {"hasHiddenPrice":true,"style":{"typography":{"fontSize":"12px"}}} /-->
+		</div>
+		<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+		</div>
+		<!-- /wp:columns -->
 		</div>
 		<!-- /wp:group -->
 
 		<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"10px","right":"var:preset|spacing|30","left":"var:preset|spacing|30","bottom":"10px"},"margin":{"top":"0","bottom":"0"}},"border":{"top":{"color":"var:preset|color|septenary","width":"1px"},"bottom":{"color":"var:preset|color|septenary","width":"1px"}}},"layout":{"type":"constrained"}} -->
 		<div class="wp-block-group alignwide" style="border-top-color:var(--wp--preset--color--septenary);border-top-width:1px;border-bottom-color:var(--wp--preset--color--septenary);border-bottom-width:1px;margin-top:0;margin-bottom:0;padding-top:10px;padding-right:var(--wp--preset--spacing--30);padding-bottom:10px;padding-left:var(--wp--preset--spacing--30)">
-			<!-- wp:columns {"isStackedOnMobile":false,"align":"wide","style":{"spacing":{"padding":{"right":"0","left":"0"}}}} -->
-			<div class="wp-block-columns alignwide is-not-stacked-on-mobile" style="padding-right:0;padding-left:0">
-				<!-- wp:column {"verticalAlignment":"center","width":""} -->
-				<div class="wp-block-column is-vertically-aligned-center">
-					<!-- wp:navigation {"icon":"menu","overlayBackgroundColor":"background","className":"is-style-default","layout":{"type":"flex","setCascadingProperties":"true","justifyContent":"center","orientation":"horizontal","flexWrap":"wrap"},"fontSize":"tiny"} /-->
-				</div>
-				<!-- /wp:column -->
-			</div>
-			<!-- /wp:columns -->
+		<!-- wp:columns {"verticalAlignment":"center","isStackedOnMobile":false,"align":"wide","style":{"spacing":{"padding":{"right":"0","left":"0"}}}} -->
+		<div class="wp-block-columns alignwide are-vertically-aligned-center is-not-stacked-on-mobile" style="padding-right:0;padding-left:0">
+		<!-- wp:column {"verticalAlignment":"center","width":""} -->
+		<div class="wp-block-column is-vertically-aligned-center"><!-- wp:navigation {"ref":4,"icon":"menu","overlayBackgroundColor":"background","className":"is-style-default","layout":{"type":"flex","setCascadingProperties":"true","justifyContent":"center","orientation":"horizontal","flexWrap":"wrap"},"fontSize":"tiny"} /-->
+		</div>
+		<!-- /wp:column -->
+		</div>
+		<!-- /wp:columns -->
 		</div>
 		<!-- /wp:group -->
 	</div>
