116080239308384c8dbf72f9e82ade95de82cb34
Atmostfear Entertainment <labs@atmostfear-entertainment.com>
2023-12-08 09:52:56 -0500

    Update about-2.php
    - Adjusted padding values: Updated padding to use variables ('var:preset|spacing|30') for right and left, ensuring consistency with the top and bottom padding values. This enhances the overall symmetry and balance of the block pattern.
    - Simplified description: Changed the description field from 'About Us Block Pattern' to 'About Us' for a more straightforward and concise representation.
    - Class name addition: Added 'about' class to the main group for better CSS targeting and styling flexibility.
    - Consistent padding in image columns: Updated padding for the top of the second image column to align with the new variable-based padding system, maintaining visual consistency across the block.
    - Preserved key elements: Kept essential elements like the tagline, heading, paragraph, and call-to-action button unchanged, focusing on refinement rather than overhaul.

--- patterns/general/about-2.php
+++ patterns/general/about-2.php
@@ -4,31 +4,31 @@
  */
 return array(
 	'title'	      => __( '02. About Us', 'aegis' ),
-	'description' => __( 'About Us Block Pattern', 'aegis' ),
+	'description' => __( 'About Us', 'aegis' ),
 	'categories'  => array( 'aegis-about' ),
 	'content' => '
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","right":"30px","left":"30px"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--50);padding-right:30px;padding-bottom:var(--wp--preset--spacing--50);padding-left:30px">
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","right":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"className":"about","layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull about" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--30)">
 	<!-- wp:columns {"align":"wide"} -->
 	<div class="wp-block-columns alignwide">
 		<!-- wp:column {"verticalAlignment":"center","width":"40%"} -->
 		<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:40%">
 			<!-- wp:paragraph {"align":"left","style":{"typography":{"textTransform":"uppercase","letterSpacing":"3px","fontStyle":"normal","fontWeight":"400"}},"className":"tagline","fontSize":"tiny"} -->
-			<p class="has-text-align-left tagline has-tiny-font-size" style="font-style:normal;font-weight:400;letter-spacing:3px;text-transform:uppercase">' . esc_html__( 'About Us' , 'aegis' ). '</p>
+			<p class="has-text-align-left tagline has-tiny-font-size" style="font-style:normal;font-weight:400;letter-spacing:3px;text-transform:uppercase">' . esc_html__('About Us', 'aegis') . '</p>
 			<!-- /wp:paragraph -->
 
 			<!-- wp:heading {"style":{"spacing":{"margin":{"top":"0"}}}} -->
-			<h2 class="wp-block-heading" style="margin-top:0">' . esc_html__( 'Inspiration Statement (45 chars): [Express core values or motivations.]' , 'aegis' ). '</h2>
+			<h2 class="wp-block-heading" style="margin-top:0">' . esc_html__('Inspiration Statement (45 chars): [Express core values or motivations.]', 'aegis') . '</h2>
 			<!-- /wp:heading -->
 
 			<!-- wp:paragraph -->
-			<p>' . esc_html__( 'About Us Content (205 chars): [Detail the core principles, values, or characteristics of the organization or subject.]' , 'aegis' ). '</p>
+			<p>' . esc_html__('About Us Content (205 chars): [Detail the core principles, values, or characteristics of the organization or subject.]', 'aegis') . '</p>
 			<!-- /wp:paragraph -->
 
 			<!-- wp:buttons -->
 			<div class="wp-block-buttons">
 				<!-- wp:button {"className":"is-style-aegis-button-shadow is-style-aegis-button-shadow"} -->
-				<div class="wp-block-button is-style-aegis-button-shadow"><a class="wp-block-button__link wp-element-button" href="#">' . esc_html__( 'Call to Action' , 'aegis' ). '</a></div>
+				<div class="wp-block-button is-style-aegis-button-shadow"><a class="wp-block-button__link wp-element-button" href="#">' . esc_html__('Call to Action', 'aegis') . '</a></div>
 				<!-- /wp:button -->
 			</div>
 			<!-- /wp:buttons -->
@@ -42,15 +42,15 @@ return array(
 				<!-- wp:column -->
 				<div class="wp-block-column">
 					<!-- wp:image {"sizeSlug":"full","linkDestination":"none","className":"is-style-aegis-effect-2-image"} -->
-					<figure class="wp-block-image size-full is-style-aegis-effect-2-image"><img src="' . esc_url( get_template_directory_uri() ) . '/assets/images/thumb_420x600_dark.webp" alt="' . esc_attr__( 'Describe the main elements of the image and its context.', 'aegis' ) . '" /></figure>
+					<figure class="wp-block-image size-full is-style-aegis-effect-2-image"><img src="' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_420x600_dark.webp" alt="' . esc_attr__('Describe the main elements of the image and its context.', 'aegis') . '" /></figure>
 					<!-- /wp:image -->
 				</div>
 				<!-- /wp:column -->
 
-				<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60"}}}} -->
-				<div class="wp-block-column" style="padding-top:var(--wp--preset--spacing--60)">
+				<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50"}}}} -->
+				<div class="wp-block-column" style="padding-top:var(--wp--preset--spacing--50)">
 					<!-- wp:image {"sizeSlug":"full","linkDestination":"none","className":"is-style-aegis-effect-2-image"} -->
-					<figure class="wp-block-image size-full is-style-aegis-effect-2-image"><img src="' . esc_url( get_template_directory_uri() ) . '/assets/images/thumb_420x600_dark.webp" alt="' . esc_attr__( 'Describe the main elements of the image and its context.', 'aegis' ) . '" /></figure>
+					<figure class="wp-block-image size-full is-style-aegis-effect-2-image"><img src="' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_420x600_dark.webp" alt="' . esc_attr__('Describe the main elements of the image and its context.', 'aegis') . '" /></figure>
 					<!-- /wp:image -->
 				</div>
 				<!-- /wp:column -->
