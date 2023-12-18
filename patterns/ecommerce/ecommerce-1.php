c9aefd451eb5c6aad3638c7e3c424cd11aa3c080
Atmostfear Entertainment <labs@atmostfear-entertainment.com>
2023-12-18 15:02:45 -0500

    Update ecommerce-1.php
    - Modified the 'description' field to a more concise version.
    - Adjusted padding and margin settings in the main group block for better spacing consistency.
    - Changed padding values to use variables for easier maintenance and uniformity across the pattern.
    - Updated class names and styles for columns to reflect a more default and consistent styling approach.
    - Simplified the image block code for clarity and maintainability.
    - Altered button styles to include a new 'is-style-aegis-button-shadow-outline' class for enhanced visual appeal.
    - Streamlined the overall structure of the code for better readability and efficiency.

--- patterns/ecommerce/ecommerce-1.php
+++ patterns/ecommerce/ecommerce-1.php
@@ -4,129 +4,123 @@
  */
 return array(
 	'title'	  => __( '01. eCommerce', 'aegis' ),
-	'description' => __( 'eCommerce Block Pattern', 'aegis' ),
+	'description' => __( 'eCommerce', 'aegis' ),
     'categories' => array( 'aegis-ecommerce' ),
 	'content' => '
-	<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"30px","bottom":"var:preset|spacing|40","left":"30px"}}},"layout":{"type":"constrained"}} -->
-	<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--40);padding-right:30px;padding-bottom:var(--wp--preset--spacing--40);padding-left:30px">
-		<!-- wp:columns {"align":"wide"} -->
-		<div class="wp-block-columns alignwide">
-			<!-- wp:column {"style":{"spacing":{"padding":{"top":"30px","right":"30px","bottom":"30px","left":"30px"}}},"backgroundColor":"secondary","className":"product-card is-style-aegis-hover-shadow"} -->
-			<div class="wp-block-column product-card is-style-aegis-hover-shadow has-secondary-background-color has-background" style="padding-top:30px;padding-right:30px;padding-bottom:30px;padding-left:30px">
-				<!-- wp:image {"align":"center","className":"size-large is-style-aegis-effect-2-image"} -->
-				<figure class="wp-block-image aligncenter size-large is-style-aegis-effect-2-image"><img src="' . esc_url( get_template_directory_uri() ) . '/assets/images/thumb_420x625_dark.webp" alt="' . esc_attr__( 'Describe the main elements of the image and its context.', 'aegis' ) . '" /></figure>
-				<!-- /wp:image -->
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|30","bottom":"var:preset|spacing|40","left":"var:preset|spacing|30"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--30)">
+	<!-- wp:columns {"align":"wide"} -->
+	<div class="wp-block-columns alignwide">
+		<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"backgroundColor":"secondary","className":"is-style-default"} -->
+		<div class="wp-block-column is-style-default has-secondary-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
+			<!-- wp:image {"align":"center","className":"size-large is-style-default"} -->
+			<figure class="wp-block-image aligncenter size-large is-style-default"><img src="' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_420x625_dark.webp" alt="' . esc_attr__('Describe the main elements of the image and its context.', 'aegis') . '" /></figure>
+			<!-- /wp:image -->
 
-				<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-				<div class="wp-block-group">
-					<!-- wp:paragraph {"style":{"typography":{"letterSpacing":"2px"}},"textColor":"foreground","fontSize":"tiny"} -->
-					<p class="has-foreground-color has-text-color has-tiny-font-size" style="letter-spacing:2px">' . esc_html__( '★★★★★', 'aegis' ) . '</p>
-					<!-- /wp:paragraph -->
-
-					<!-- wp:paragraph -->
-					<p><strong>' . esc_html__( '$00.00', 'aegis' ) . '</strong></p>
-					<!-- /wp:paragraph -->
-				</div>
-				<!-- /wp:group -->
-
-				<!-- wp:heading {"className":"has-large-font-size"} -->
-				<h2 class="wp-block-heading has-large-font-size">' . esc_html__( 'Product Name', 'aegis' ) . '</h2>
-				<!-- /wp:heading -->
-
-				<!-- wp:paragraph -->
-				<p>' . esc_html__( 'A range of 50-150 characters is common for grid view descriptions. This is just enough to give a brief overview of the product without overwhelming the viewer.', 'aegis' ) . '</p>
+			<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+			<div class="wp-block-group">
+				<!-- wp:paragraph {"style":{"typography":{"letterSpacing":"2px"}},"textColor":"foreground","fontSize":"tiny"} -->
+				<p class="has-foreground-color has-text-color has-tiny-font-size" style="letter-spacing:2px">' . esc_html__('★★★★★', 'aegis') . '</p>
 				<!-- /wp:paragraph -->
 
-				<!-- wp:buttons -->
-				<div class="wp-block-buttons">
-					<!-- wp:button {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"className":"is-style-aegis-button-shadow","fontSize":"small"} -->
-					<div class="wp-block-button has-custom-font-size is-style-aegis-button-shadow has-small-font-size" style="font-style:normal;font-weight:600">
-						<a class="wp-block-button__link wp-element-button">' . esc_html__( 'Call to Action', 'aegis' ) . '</a>
-					</div>
-					<!-- /wp:button -->
-				</div>
-				<!-- /wp:buttons -->
-			</div>
-			<!-- /wp:column -->
-
-			<!-- wp:column {"style":{"spacing":{"padding":{"top":"30px","right":"30px","bottom":"30px","left":"30px"}}},"backgroundColor":"foreground","textColor":"background","className":"product-card is-style-aegis-hover-shadow"} -->
-			<div class="wp-block-column product-card is-style-aegis-hover-shadow has-background-color has-foreground-background-color has-text-color has-background" style="padding-top:30px;padding-right:30px;padding-bottom:30px;padding-left:30px">
-				<!-- wp:image {"align":"center","className":"size-large is-style-aegis-effect-2-image"} -->
-				<figure class="wp-block-image aligncenter size-large is-style-aegis-effect-2-image"><img src="' . esc_url( get_template_directory_uri() ) . '/assets/images/thumb_420x625_light.webp" alt="' . esc_attr__( 'Describe the main elements of the image and its context.', 'aegis' ) . '" /></figure>
-				<!-- /wp:image -->
-
-				<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-				<div class="wp-block-group">
-					<!-- wp:paragraph {"style":{"typography":{"letterSpacing":"2px"}},"textColor":"secondary","fontSize":"tiny"} -->
-					<p class="has-secondary-color has-text-color has-tiny-font-size" style="letter-spacing:2px">' .
-						esc_html__( '★★★★★', 'aegis' ) . '</p>
-					<!-- /wp:paragraph -->
-
-					<!-- wp:paragraph -->
-					<p><strong>' . esc_html__( '$00.00', 'aegis' ) . '</strong></p>
-					<!-- /wp:paragraph -->
-				</div>
-				<!-- /wp:group -->
-
-				<!-- wp:heading {"className":"has-large-font-size"} -->
-				<h2 class="wp-block-heading has-large-font-size">' . esc_html__( 'Product Name', 'aegis' ) . '</h2>
-				<!-- /wp:heading -->
-
 				<!-- wp:paragraph -->
-				<p>' . esc_html__( 'A range of 50-150 characters is common for grid view descriptions. This is just enough to give a brief overview of the product without overwhelming the viewer.', 'aegis' ) . '</p>
+				<p><strong>' . esc_html__('$00.00', 'aegis') . '</strong></p>
 				<!-- /wp:paragraph -->
-
-				<!-- wp:buttons -->
-				<div class="wp-block-buttons">
-					<!-- wp:button {"backgroundColor":"secondary","textColor":"foreground","style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"className":"is-style-aegis-button-shadow","fontSize":"small"} -->
-					<div class="wp-block-button has-custom-font-size is-style-aegis-button-shadow has-small-font-size" style="font-style:normal;font-weight:600">
-						<a class="wp-block-button__link has-foreground-color has-secondary-background-color has-text-color has-background wp-element-button">' . esc_html__( 'Call to Action', 'aegis' ) . '</a>
-					</div>
-					<!-- /wp:button -->
-				</div>
-				<!-- /wp:buttons -->
 			</div>
-			<!-- /wp:column -->
+			<!-- /wp:group -->
 
-			<!-- wp:column {"style":{"spacing":{"padding":{"top":"30px","right":"30px","bottom":"30px","left":"30px"}}},"backgroundColor":"secondary","className":"product-card is-style-aegis-hover-shadow"} -->
-			<div class="wp-block-column product-card is-style-aegis-hover-shadow has-secondary-background-color has-background" style="padding-top:30px;padding-right:30px;padding-bottom:30px;padding-left:30px">
-				<!-- wp:image {"align":"center",className":"size-large is-style-aegis-effect-2-image"} -->
-				<figure class="wp-block-image aligncenter size-large is-style-aegis-effect-2-image"><img src="' . esc_url( get_template_directory_uri() ) . '/assets/images/thumb_420x625_dark.webp" alt="' . esc_attr__( 'Describe the main elements of the image and its context.', 'aegis' ) . '" /></figure>
-				<!-- /wp:image -->
+			<!-- wp:heading {"fontSize":"large"} -->
+			<h3 class="wp-block-heading has-large-font-size">' . esc_html__('Product Name', 'aegis') . '</h3>
+			<!-- /wp:heading -->
 
-				<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-				<div class="wp-block-group">
-					<!-- wp:paragraph {"style":{"typography":{"letterSpacing":"2px"}},"textColor":"foreground","fontSize":"tiny"} -->
-					<p class="has-foreground-color has-text-color has-tiny-font-size" style="letter-spacing:2px">' . esc_html__( '★★★★★', 'aegis' ) . '</p>
-					<!-- /wp:paragraph -->
+			<!-- wp:paragraph -->
+			<p>' . esc_html__('A range of 50-150 characters is common for grid view descriptions. This is just enough to give a brief overview of the product without overwhelming the viewer.', 'aegis') . '</p>
+			<!-- /wp:paragraph -->
 
-					<!-- wp:paragraph -->
-					<p><strong>' . esc_html__( '$00.00', 'aegis' ) . '</strong></p>
-					<!-- /wp:paragraph -->
-				</div>
-				<!-- /wp:group -->
-
-				<!-- wp:heading {"className":"has-large-font-size"} -->
-				<h2 class="wp-block-heading has-large-font-size">' . esc_html__( 'Product Name', 'aegis' ) . '</h2>
-				<!-- /wp:heading -->
-
-				<!-- wp:paragraph -->
-				<p>' . esc_html__( 'A range of 50-150 characters is common for grid view descriptions. This is just enough to give a brief overview of the product without overwhelming the viewer.', 'aegis' ) . '</p>
-				<!-- /wp:paragraph -->
-
-				<!-- wp:buttons -->
-				<div class="wp-block-buttons">
-					<!-- wp:button {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"className":"is-style-aegis-button-shadow","fontSize":"small"} -->
-					<div class="wp-block-button has-custom-font-size is-style-aegis-button-shadow has-small-font-size" style="font-style:normal;font-weight:600">
-						<a class="wp-block-button__link wp-element-button">' . esc_html__( 'Call to Action', 'aegis' ) . '</a>
-					</div>
-					<!-- /wp:button -->
-				</div>
-				<!-- /wp:buttons -->
+			<!-- wp:buttons {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+			<div class="wp-block-buttons">
+				<!-- wp:button {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"className":"is-style-aegis-button-shadow","fontSize":"small"} -->
+				<div class="wp-block-button has-custom-font-size is-style-aegis-button-shadow has-small-font-size" style="font-style:normal;font-weight:600"><a class="wp-block-button__link wp-element-button">' . esc_html__('Call to Action', 'aegis') . '</a></div>
+				<!-- /wp:button -->
 			</div>
-			<!-- /wp:column -->
+			<!-- /wp:buttons -->
 		</div>
-		<!-- /wp:columns -->
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"backgroundColor":"senary","className":"is-style-default"} -->
+		<div class="wp-block-column is-style-default has-senary-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
+			<!-- wp:image {"align":"center","className":"size-large is-style-default"} -->
+			<figure class="wp-block-image aligncenter size-large is-style-default"><img src="' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_420x625_light.webp" alt="' . esc_attr__('Describe the main elements of the image and its context.', 'aegis') . '" /></figure>
+			<!-- /wp:image -->
+
+			<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+			<div class="wp-block-group">
+				<!-- wp:paragraph {"style":{"typography":{"letterSpacing":"2px"}},"textColor":"foreground","fontSize":"tiny"} -->
+				<p class="has-foreground-color has-text-color has-tiny-font-size" style="letter-spacing:2px">' . esc_html__('★★★★★', 'aegis') . '</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:paragraph -->
+				<p><strong>' . esc_html__('$00.00', 'aegis') . '</strong></p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+
+			<!-- wp:heading {"fontSize":"large"} -->
+			<h3 class="wp-block-heading has-large-font-size">' . esc_html__('Product Name', 'aegis') . '</h3>
+			<!-- /wp:heading -->
+
+			<!-- wp:paragraph -->
+			<p>' . esc_html__('A range of 50-150 characters is common for grid view descriptions. This is just enough to give a brief overview of the product without overwhelming the viewer.', 'aegis') . '</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:buttons {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+			<div class="wp-block-buttons">
+				<!-- wp:button {"backgroundColor":"foreground","textColor":"background","style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"className":"is-style-aegis-button-shadow-outline","fontSize":"small"} -->
+				<div class="wp-block-button has-custom-font-size is-style-aegis-button-shadow-outline has-small-font-size" style="font-style:normal;font-weight:600"><a class="wp-block-button__link has-background-color has-foreground-background-color has-text-color has-background has-link-color wp-element-button">' . esc_html__('Call to Action', 'aegis') . '</a></div>
+				<!-- /wp:button -->
+			</div>
+			<!-- /wp:buttons -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}},"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"backgroundColor":"foreground","textColor":"background","className":"is-style-default"} -->
+		<div class="wp-block-column is-style-default has-background-color has-foreground-background-color has-text-color has-background has-link-color"
+			style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
+			<!-- wp:image {"align":"center","id":2060,"sizeSlug":"full","linkDestination":"none","className":"size-large is-style-default"} -->
+			<figure class="wp-block-image aligncenter size-full size-large is-style-default"><img src="' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_420x625_dark.webp" alt="' . esc_attr__('Describe the main elements of the image and its context.', 'aegis') . '" /></figure>
+			<!-- /wp:image -->
+
+			<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+			<div class="wp-block-group">
+				<!-- wp:paragraph {"style":{"typography":{"letterSpacing":"2px"}},"fontSize":"tiny"} -->
+				<p class="has-tiny-font-size" style="letter-spacing:2px">' . esc_html__('★★★★★', 'aegis') . '</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:paragraph -->
+				<p><strong>' . esc_html__('$00.00', 'aegis') . '</strong></p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+
+			<!-- wp:heading {"fontSize":"large"} -->
+			<h3 class="wp-block-heading has-large-font-size">' . esc_html__('Product Name', 'aegis') . '</h3>
+			<!-- /wp:heading -->
+
+			<!-- wp:paragraph -->
+			<p>' . esc_html__('A range of 50-150 characters is common for grid view descriptions. This is just enough to give a brief overview of the product without overwhelming the viewer.', 'aegis') . '</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:buttons {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+			<div class="wp-block-buttons">
+				<!-- wp:button {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"className":"is-style-aegis-button-shadow-outline","fontSize":"small"} -->
+				<div class="wp-block-button has-custom-font-size is-style-aegis-button-shadow-outline has-small-font-size" style="font-style:normal;font-weight:600"><a class="wp-block-button__link wp-element-button">' . esc_html__('Call to Action', 'aegis') . '</a></div>
+				<!-- /wp:button -->
+			</div>
+			<!-- /wp:buttons -->
+		</div>
+		<!-- /wp:column -->
 	</div>
-	<!-- /wp:group -->',
+	<!-- /wp:columns -->
+</div>
+<!-- /wp:group -->',
 );
