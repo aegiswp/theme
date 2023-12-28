b187b1fe73d37e9b6131833b54974195e3650749
Atmostfear Entertainment <labs@atmostfear-entertainment.com>
2023-12-28 14:08:43 -0500

    Update hero-3.php
    - New block pattern, replaces older version position.

--- patterns/hero/hero-3.php
+++ patterns/hero/hero-3.php
@@ -4,51 +4,107 @@
  */
 return array(
     'title'      => __('03. Hero', 'aegis'),
-    'description' => __('Hero', 'aegis'),
+    'description' => __('Hero with Icons, Heading, and Text', 'aegis'),
     'categories' => array('aegis-hero'),
     'content' => '
-<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60","right":"var:preset|spacing|30","left":"var:preset|spacing|30"},"margin":{"top":"0","bottom":"0"}}},"gradient":"vertical-background-to-foreground","className":"about gradient","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide about gradient has-vertical-background-to-foreground-gradient-background has-background"
-	style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--30)">
-	<!-- wp:columns {"align":"wide"} -->
-	<div class="wp-block-columns alignwide">
-		<!-- wp:column {"verticalAlignment":"center","width":""} -->
-		<div class="wp-block-column is-vertically-aligned-center">
-			<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"300","fontSize":"3.6rem"}}} -->
-			<h2 class="wp-block-heading" style="font-size:3.6rem;font-style:normal;font-weight:300"><strong>' . esc_html__('Main', 'aegis') . '</strong> ' . esc_html__('Heading', 'aegis') . '</h2>
-			<!-- /wp:heading -->
+<!-- wp:group {"tagName":"section","align":"wide","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"right":"0","left":"0","top":"0","bottom":"0"}}},"layout":{"type":"default"},"metadata":{"name":"' . esc_html__('Hero Section', 'aegis') . '"}} -->
+<section class="wp-block-group alignwide" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
+    <!-- wp:cover {"url":"' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_1800x1200_dark.webp","dimRatio":70,"overlayColor":"foreground","minHeight":100,"minHeightUnit":"vh","align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}}}} -->
+    <div class="wp-block-cover alignwide" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50);min-height:100vh">
+        <span aria-hidden="true" class="wp-block-cover__background has-foreground-background-color has-background-dim-70 has-background-dim"></span>
+        <img class="wp-block-cover__image-background" src="' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_1800x1200_dark.webp" alt="' . esc_attr__('Describe the main elements of the image and its context.', 'aegis') . '" data-object-fit="cover" />
+        <div class="wp-block-cover__inner-container">
+            <!-- wp:columns {"verticalAlignment":"center"} -->
+            <div class="wp-block-columns are-vertically-aligned-center">
+                <!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"0","bottom":"var:preset|spacing|30","left":"0"}}}} -->
+                <div class="wp-block-column is-vertically-aligned-center"
+                    style="padding-top:var(--wp--preset--spacing--30);padding-right:0;padding-bottom:var(--wp--preset--spacing--30);padding-left:0">
+                    <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"className":"is-style-default","layout":{"type":"flex","allowOrientation":false,"justifyContent":"space-between","flexWrap":"nowrap"}} -->
+                    <div class="wp-block-group is-style-default">
+                        <!-- wp:image {"width":"40px","sizeSlug":"full","linkDestination":"none"} -->
+                        <figure class="wp-block-image size-full is-resized"><img src="' . esc_url(get_template_directory_uri()) . '/assets/icons/laurel_left.svg" alt="' . esc_attr__('Describe the main elements of the image and its context.', 'aegis') . '" style="width:40px" /></figure>
+                        <!-- /wp:image -->
 
-			<!-- wp:paragraph -->
-			<p>' . esc_html_x('Product Description (88 chars): [Detail key aspects or selling points of a product.]', 'Placeholder for product description', 'aegis') . '</p>
-			<!-- /wp:paragraph -->
+                        <!-- wp:group -->
+                        <div class="wp-block-group">
+                            <!-- wp:paragraph {"align":"left","style":{"typography":{"textTransform":"uppercase","letterSpacing":"3px","fontStyle":"normal","fontWeight":"500","lineHeight":"1"}},"fontSize":"medium"} -->
+                            <p class="has-text-align-left has-medium-font-size" style="font-style:normal;font-weight:500;letter-spacing:3px;line-height:1;text-transform:uppercase">' . esc_attr__('[Category]', 'aegis') . '</p>
+                            <!-- /wp:paragraph -->
 
-			<!-- wp:buttons {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-			<div class="wp-block-buttons">
-				<!-- wp:button {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"className":"is-style-aegis-button-shadow","fontSize":"small"} -->
-				<div class="wp-block-button has-custom-font-size is-style-aegis-button-shadow has-small-font-size" style="font-style:normal;font-weight:600"><a class="wp-block-button__link wp-element-button" href="#">' . esc_html__('Call to Action', 'aegis') . '</a></div>
-				<!-- /wp:button -->
-			</div>
-			<!-- /wp:buttons -->
-		</div>
-		<!-- /wp:column -->
+                            <!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"small"} -->
+                            <p class="has-text-align-center has-small-font-size" style="margin-top:0">' . esc_html__('[Nominee]', 'aegis') . '</p>
+                            <!-- /wp:paragraph -->
+                        </div>
+                        <!-- /wp:group -->
 
-		<!-- wp:column {"width":"60%","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"className":"floating-image"} -->
-		<div class="wp-block-column floating-image" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);flex-basis:60%">
-			<!-- wp:image {"sizeSlug":"full","linkDestination":"none","className":"is-style-aegis-shadow-image image-one"} -->
-			<figure class="wp-block-image size-full is-style-aegis-shadow-image image-one"><img src="' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_450x680_dark.webp" alt="' . esc_attr_x('Default product image one for the section', 'Description of the first default product image', 'aegis') . '" /></figure>
-			<!-- /wp:image -->
+                        <!-- wp:image {"width":"40px","sizeSlug":"full","linkDestination":"none"} -->
+                        <figure class="wp-block-image size-full is-resized"><img src="' . esc_url(get_template_directory_uri()) . '/assets/icons/laurel_right.svg" alt="' . esc_attr__('Describe the main elements of the image and its context.', 'aegis') . '" style="width:40px" /></figure>
+                        <!-- /wp:image -->
+                    </div>
+                    <!-- /wp:group -->
+                </div>
+                <!-- /wp:column -->
 
-			<!-- wp:image {"sizeSlug":"large","linkDestination":"none","className":"is-style-aegis-shadow-image image-two"} -->
-			<figure class="wp-block-image size-large is-style-aegis-shadow-image image-two"><img src="' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_680x1025_light.webp" alt="' . esc_attr_x('Default product image two for the section', 'Description of the second default product image', 'aegis') . '" /></figure>
-			<!-- /wp:image -->
+                <!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"0","bottom":"var:preset|spacing|30","left":"0"}}}} -->
+                <div class="wp-block-column is-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--30);padding-right:0;padding-bottom:var(--wp--preset--spacing--30);padding-left:0">
+                    <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"className":"is-style-default","layout":{"type":"flex","allowOrientation":false,"justifyContent":"space-between","flexWrap":"nowrap"}} -->
+                    <div class="wp-block-group is-style-default">
+                        <!-- wp:image {"width":"40px","sizeSlug":"full","linkDestination":"none"} -->
+                        <figure class="wp-block-image size-full is-resized"><img src="' . esc_url(get_template_directory_uri()) . '/assets/icons/laurel_left.svg" alt="' . esc_attr__('Describe the main elements of the image and its context.', 'aegis') . '" style="width:40px" /></figure>
+                        <!-- /wp:image -->
 
-			<!-- wp:image {"sizeSlug":"full","linkDestination":"none","className":"is-style-aegis-shadow-image image-three"} -->
-			<figure class="wp-block-image size-full is-style-aegis-shadow-image image-three"><img src="' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_450x680_light.webp" alt="' . esc_attr_x('Default product image three for the section', 'Description of the third default product image', 'aegis') . '" /></figure>
-			<!-- /wp:image -->
-		</div>
-		<!-- /wp:column -->
-	</div>
-	<!-- /wp:columns -->
-</div>
+                        <!-- wp:group -->
+                        <div class="wp-block-group">
+                            <!-- wp:paragraph {"align":"left","style":{"typography":{"textTransform":"uppercase","letterSpacing":"3px","fontStyle":"normal","fontWeight":"500","lineHeight":"1"}},"fontSize":"medium"} -->
+                            <p class="has-text-align-left has-medium-font-size" style="font-style:normal;font-weight:500;letter-spacing:3px;line-height:1;text-transform:uppercase">' . esc_html__('[Event Title]', 'aegis') . '</p>
+                            <!-- /wp:paragraph -->
+
+                            <!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0"}},"typography":{"textTransform":"uppercase"}},"fontSize":"small"} -->
+                            <p class="has-text-align-center has-small-font-size" style="margin-top:0;text-transform:uppercase">' . esc_html__('[Date of Event]', 'aegis') . '</p>
+                            <!-- /wp:paragraph -->
+                        </div>
+                        <!-- /wp:group -->
+
+                        <!-- wp:image {"width":"40px","sizeSlug":"full","linkDestination":"none"} -->
+                        <figure class="wp-block-image size-full is-resized"><img src="' . esc_url(get_template_directory_uri()) . '/assets/icons/laurel_right.svg" alt="' . esc_attr__('Describe the main elements of the image and its context.', 'aegis') . '" style="width:40px" /></figure>
+                        <!-- /wp:image -->
+                    </div>
+                    <!-- /wp:group -->
+                </div>
+                <!-- /wp:column -->
+
+                <!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"0","bottom":"var:preset|spacing|30","left":"0"}}}} -->
+                <div class="wp-block-column is-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--30);padding-right:0;padding-bottom:var(--wp--preset--spacing--30);padding-left:0">
+                    <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"className":"is-style-default","layout":{"type":"flex","allowOrientation":false,"justifyContent":"space-between","flexWrap":"nowrap"}} -->
+                    <div class="wp-block-group is-style-default">
+                        <!-- wp:image {"width":"40px","sizeSlug":"full","linkDestination":"none"} -->
+                        <figure class="wp-block-image size-full is-resized"><img src="' . esc_url(get_template_directory_uri()) . '/assets/icons/laurel_left.svg" alt="' . esc_attr__('Describe the main elements of the image and its context.', 'aegis') . '" style="width:40px" /></figure>
+                        <!-- /wp:image -->
+
+                        <!-- wp:group -->
+                        <div class="wp-block-group">
+                            <!-- wp:paragraph {"align":"left","style":{"typography":{"textTransform":"uppercase","letterSpacing":"3px","fontStyle":"normal","fontWeight":"500","lineHeight":"1"}},"fontSize":"medium"} -->
+                            <p class="has-text-align-left has-medium-font-size" style="font-style:normal;font-weight:500;letter-spacing:3px;line-height:1;text-transform:uppercase">' . esc_attr__('[Category]', 'aegis') . '</p>
+                            <!-- /wp:paragraph -->
+
+                            <!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"small"} -->
+                            <p class="has-text-align-center has-small-font-size" style="margin-top:0">' . esc_html__('[Nominee]', 'aegis') . '</p>
+                            <!-- /wp:paragraph -->
+                        </div>
+                        <!-- /wp:group -->
+
+                        <!-- wp:image {"width":"40px","sizeSlug":"full","linkDestination":"none"} -->
+                        <figure class="wp-block-image size-full is-resized"><img src="' . esc_url(get_template_directory_uri()) . '/assets/icons/laurel_right.svg" alt="' . esc_attr__('Describe the main elements of the image and its context.', 'aegis') . '" style="width:40px" /></figure>
+                        <!-- /wp:image -->
+                    </div>
+                    <!-- /wp:group -->
+                </div>
+                <!-- /wp:column -->
+            </div>
+            <!-- /wp:columns -->
+        </div>
+    </div>
+    <!-- /wp:cover -->
+</section>
 <!-- /wp:group -->',
 );
