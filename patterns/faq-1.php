b49b8c9243680085b2e24760a23f0d2711fe2922
Atmostfear Entertainment <labs@atmostfear-entertainment.com>
2024-01-06 18:31:18 -0500

    Move faq-1.php
    - Moved: FAQ 1 Block Pattern from '/patterns/faq' to '/patterns' following Theme Handbook Block Patterns guidelines.

--- patterns/faq-1.php
+++ patterns/faq-1.php
@@ -4,17 +4,16 @@
  */
 return array(
 	'title'	      => __( '01. FAQ', 'aegis' ),
-	'description' => __( 'FAQ', 'aegis' ),
+	'description' => __( 'Block Pattern with Headings, Paragraps, Icon, and According', 'aegis' ),
 	'categories'  => array( 'aegis-faq' ),
-	'content' => '
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","right":"var:preset|spacing|30","left":"var:preset|spacing|30"},"margin":{"top":"0","bottom":"0"}}},"gradient":"vertical-background-to-foreground","className":"about gradient","layout":{"type":"default"}} -->
+	'content' => '<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","right":"var:preset|spacing|30","left":"var:preset|spacing|30"},"margin":{"top":"0","bottom":"0"}}},"gradient":"vertical-background-to-foreground","className":"about gradient","layout":{"type":"default"},"metadata":{"name":"' . esc_html__('03. FAQ Pattern', 'aegis') . '"}} -->
 <div class="wp-block-group alignfull about gradient has-vertical-background-to-foreground-gradient-background has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--30)">
 	<!-- wp:columns {"align":"wide"} -->
 	<div class="wp-block-columns alignwide">
 		<!-- wp:column {"width":"56.66%","style":{"spacing":{"padding":{"left":"0","right":"var:preset|spacing|50"}}}} -->
 		<div class="wp-block-column" style="padding-right:var(--wp--preset--spacing--50);padding-left:0;flex-basis:56.66%">
-			<!-- wp:heading {"level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"extra-large"} -->
-			<h4 class="wp-block-heading has-extra-large-font-size" style="font-style:normal;font-weight:400">' . esc_html__('Heading Format (45 chars): [Present a ', 'aegis') . ' <strong>' . esc_html__('welcoming tone', 'aegis') . '</strong> ' . esc_html__('for users queries.]', 'aegis') . '</h4>
+			<!-- wp:heading {"level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"fontSize":"extra-large"} -->
+			<h4 class="wp-block-heading has-extra-large-font-size" style="font-style:normal;font-weight:500">' . esc_html__('[Heading (45 characters): Present a welcoming tone for users queries.]', 'aegis') . '</h4>
 			<!-- /wp:heading -->
 
 			<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"0px","right":"0px","left":"0px"}}},"className":"faq","layout":{"type":"default"}} -->
@@ -22,11 +21,12 @@ return array(
 				<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"var:preset|spacing|30","left":"0"}},"border":{"bottom":{"color":"var:preset|color|foreground","width":"1px"},"top":[],"right":[],"left":[]}},"className":"trigger is-style-default","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 				<div class="wp-block-group trigger is-style-default" style="border-bottom-color:var(--wp--preset--color--foreground);border-bottom-width:1px;padding-top:0;padding-right:0;padding-bottom:var(--wp--preset--spacing--30);padding-left:0">
 					<!-- wp:heading {"level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"medium"} -->
-					<h4 class="wp-block-heading has-medium-font-size" style="font-style:normal;font-weight:400"><strong>' . esc_html__('01.', 'aegis') . '</strong> ' . esc_html__('FAQ Question Format (57 chars): [Frame a frequently asked question about a feature or service.]', 'aegis') . '</h4>
+					<h4 class="wp-block-heading has-medium-font-size" style="font-style:normal;font-weight:400">
+						<strong>' . esc_html__('01.', 'aegis') . '</strong> ' . esc_html__('[Question (57 characters): Frame a frequently asked question about a feature or service.]', 'aegis') . '</h4>
 					<!-- /wp:heading -->
 
 					<!-- wp:paragraph {"fontSize":"large"} -->
-					<p class="has-large-font-size">' . esc_html__('+', 'aegis') . '</p>
+					<p class="has-large-font-size"><span class="dashicons dashicons-plus-alt2"></span></p>
 					<!-- /wp:paragraph -->
 				</div>
 				<!-- /wp:group -->
@@ -34,32 +34,7 @@ return array(
 				<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"backgroundColor":"secondary","className":"content","layout":{"type":"constrained"}} -->
 				<div class="wp-block-group content has-secondary-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
 					<!-- wp:paragraph {"fontSize":"small"} -->
-					<p class="has-small-font-size">' . esc_html__('FAQ Answer Format (150 chars): [Provide a clear response to a common user query.]', 'aegis') . '</p>
-					<!-- /wp:paragraph -->
-				</div>
-				<!-- /wp:group -->
-			</div>
-			<!-- /wp:group -->
-
-			<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"0px","right":"0px","left":"0px"}}},"className":"faq","layout":{"type":"default"}} -->
-			<div class="wp-block-group alignfull faq"
-				style="padding-top:var(--wp--preset--spacing--30);padding-right:0px;padding-bottom:0px;padding-left:0px">
-				<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"var:preset|spacing|30","left":"0"}},"border":{"bottom":{"color":"var:preset|color|foreground","width":"1px"},"top":[],"right":[],"left":[]}},"className":"trigger is-style-default","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-				<div class="wp-block-group trigger is-style-default" style="border-bottom-color:var(--wp--preset--color--foreground);border-bottom-width:1px;padding-top:0;padding-right:0;padding-bottom:var(--wp--preset--spacing--30);padding-left:0">
-					<!-- wp:heading {"level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"medium"} -->
-					<h4 class="wp-block-heading has-medium-font-size" style="font-style:normal;font-weight:400"><strong>' . esc_html__('02.', 'aegis') . '</strong> ' . esc_html__('FAQ Question Format (57 chars): [Frame a frequently asked question about a feature or service.]', 'aegis') . '</h4>
-					<!-- /wp:heading -->
-
-					<!-- wp:paragraph {"fontSize":"large"} -->
-					<p class="has-large-font-size">' . esc_html__('+', 'aegis') . '</p>
-					<!-- /wp:paragraph -->
-				</div>
-				<!-- /wp:group -->
-
-				<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"backgroundColor":"secondary","className":"content","layout":{"type":"constrained"}} -->
-				<div class="wp-block-group content has-secondary-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
-					<!-- wp:paragraph {"fontSize":"small"} -->
-					<p class="has-small-font-size">' . esc_html__('FAQ Answer Format (150 chars): [Provide a clear response to a common user query.]', 'aegis') . '</p>
+					<p class="has-small-font-size">' . esc_html__('[Answer (150 characters): [Provide a clear response to a common query.]', 'aegis') . '</p>
 					<!-- /wp:paragraph -->
 				</div>
 				<!-- /wp:group -->
@@ -71,11 +46,12 @@ return array(
 				<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"var:preset|spacing|30","left":"0"}},"border":{"bottom":{"color":"var:preset|color|foreground","width":"1px"},"top":[],"right":[],"left":[]}},"className":"trigger is-style-default","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 				<div class="wp-block-group trigger is-style-default" style="border-bottom-color:var(--wp--preset--color--foreground);border-bottom-width:1px;padding-top:0;padding-right:0;padding-bottom:var(--wp--preset--spacing--30);padding-left:0">
 					<!-- wp:heading {"level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"medium"} -->
-					<h4 class="wp-block-heading has-medium-font-size" style="font-style:normal;font-weight:400"><strong>' . esc_html__('03.', 'aegis') . '</strong> ' . esc_html__('FAQ Question Format (57 chars): [Frame a frequently asked question about a feature or service.]', 'aegis') . '</h4>
+					<h4 class="wp-block-heading has-medium-font-size" style="font-style:normal;font-weight:400">
+						<strong>' . esc_html__('02.', 'aegis') . '</strong> ' . esc_html__('[Question (57 characters): Frame a frequently asked question about a feature or service.]', 'aegis') . '</h4>
 					<!-- /wp:heading -->
 
 					<!-- wp:paragraph {"fontSize":"large"} -->
-					<p class="has-large-font-size">' . esc_html__('+', 'aegis') . '</p>
+					<p class="has-large-font-size"><span class="dashicons dashicons-plus-alt2"></span></p>
 					<!-- /wp:paragraph -->
 				</div>
 				<!-- /wp:group -->
@@ -83,7 +59,58 @@ return array(
 				<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"backgroundColor":"secondary","className":"content","layout":{"type":"constrained"}} -->
 				<div class="wp-block-group content has-secondary-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
 					<!-- wp:paragraph {"fontSize":"small"} -->
-					<p class="has-small-font-size">' . esc_html__('FAQ Answer Format (150 chars): [Provide a clear response to a common user query.]', 'aegis') . '</p>
+					<p class="has-small-font-size">' . esc_html__('[Answer (150 characters): [Provide a clear response to a common query.]', 'aegis') . '</p>
+					<!-- /wp:paragraph -->
+				</div>
+				<!-- /wp:group -->
+			</div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"0px","right":"0px","left":"0px"}}},"className":"faq","layout":{"type":"default"}} -->
+			<div class="wp-block-group alignfull faq" style="padding-top:var(--wp--preset--spacing--30);padding-right:0px;padding-bottom:0px;padding-left:0px">
+				<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"var:preset|spacing|30","left":"0"}},"border":{"bottom":{"color":"var:preset|color|foreground","width":"1px"},"top":[],"right":[],"left":[]}},"className":"trigger is-style-default","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+				<div class="wp-block-group trigger is-style-default" style="border-bottom-color:var(--wp--preset--color--foreground);border-bottom-width:1px;padding-top:0;padding-right:0;padding-bottom:var(--wp--preset--spacing--30);padding-left:0">
+					<!-- wp:heading {"level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"medium"} -->
+					<h4 class="wp-block-heading has-medium-font-size" style="font-style:normal;font-weight:400">
+						<strong>' . esc_html__('03.', 'aegis') . '</strong> ' . esc_html__('[Question (57 characters): Frame a frequently asked question about a feature or service.]', 'aegis') . '</h4>
+					<!-- /wp:heading -->
+
+					<!-- wp:paragraph {"fontSize":"large"} -->
+					<p class="has-large-font-size"><span class="dashicons dashicons-plus-alt2"></span></p>
+					<!-- /wp:paragraph -->
+				</div>
+				<!-- /wp:group -->
+
+				<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"backgroundColor":"secondary","className":"content","layout":{"type":"constrained"}} -->
+				<div class="wp-block-group content has-secondary-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
+					<!-- wp:paragraph {"fontSize":"small"} -->
+					<p class="has-small-font-size">' . esc_html__('[Answer (150 characters): [Provide a clear response to a common query.]', 'aegis') . '</p>
+					<!-- /wp:paragraph -->
+				</div>
+				<!-- /wp:group -->
+			</div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"0px","right":"0px","left":"0px"}}},"className":"faq","layout":{"type":"default"}} -->
+			<div class="wp-block-group alignfull faq" style="padding-top:var(--wp--preset--spacing--30);padding-right:0px;padding-bottom:0px;padding-left:0px">
+				<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"var:preset|spacing|30","left":"0"}},"border":{"bottom":{"color":"var:preset|color|foreground","width":"1px"},"top":[],"right":[],"left":[]}},"className":"trigger is-style-default","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+				<div class="wp-block-group trigger is-style-default" style="border-bottom-color:var(--wp--preset--color--foreground);border-bottom-width:1px;padding-top:0;padding-right:0;padding-bottom:var(--wp--preset--spacing--30);padding-left:0">
+					<!-- wp:heading {"level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"medium"} -->
+					<h4 class="wp-block-heading has-medium-font-size" style="font-style:normal;font-weight:400">
+						<strong>' . esc_html__('03.', 'aegis') . '</strong> ' . esc_html__('[Question (57 characters): Frame a frequently asked question about a feature or service.]', 'aegis') . '</h4>
+					<!-- /wp:heading -->
+
+					<!-- wp:paragraph {"fontSize":"large"} -->
+					<p class="has-large-font-size"><span class="dashicons dashicons-plus-alt2"></span></p>
+					<!-- /wp:paragraph -->
+				</div>
+				<!-- /wp:group -->
+
+				<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"backgroundColor":"secondary","className":"content","layout":{"type":"constrained"}} -->
+				<div class="wp-block-group content has-secondary-background-color has-background"
+					style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
+					<!-- wp:paragraph {"fontSize":"small"} -->
+					<p class="has-small-font-size">' . esc_html__('[Answer (150 characters): [Provide a clear response to a common query.]', 'aegis') . '</p>
 					<!-- /wp:paragraph -->
 				</div>
 				<!-- /wp:group -->
@@ -99,23 +126,23 @@ return array(
 				<!-- wp:group {"style":{"spacing":{"padding":{"top":"15px","right":"15px","bottom":"15px","left":"15px"},"blockGap":"0"},"border":{"radius":"100px"}},"backgroundColor":"foreground","className":"icon","layout":{"type":"constrained"}} -->
 				<div class="wp-block-group icon has-foreground-background-color has-background" style="border-radius:100px;padding-top:15px;padding-right:15px;padding-bottom:15px;padding-left:15px">
 					<!-- wp:image {"width":"40px","height":"undefinedpx","sizeSlug":"large","linkDestination":"none","style":{"color":{"duotone":["#f6f5f2","#f6f5f2"]}}} -->
-					<figure class="wp-block-image size-large is-resized"><img src="' . esc_url(get_template_directory_uri()) . '/assets/icons/questions.svg" alt="' . esc_attr__('Describe the icon context.', 'aegis') . '" style="width:40px;height:undefinedpx" /></figure>
+					<figure class="wp-block-image size-large is-resized"><img src="' . esc_url(get_template_directory_uri()) . '/assets/icons/questions.svg" alt="' . esc_html__('Describe the icon context', 'aegis') . '" style="width:40px;height:undefinedpx" /></figure>
 					<!-- /wp:image -->
 				</div>
 				<!-- /wp:group -->
 
 				<!-- wp:heading {"level":3} -->
-				<h3 class="wp-block-heading">' . esc_html__('Heading Format (25 chars): [Encourage users to seek more information.]', 'aegis') . '</h3>
+				<h3 class="wp-block-heading">' . esc_html__('[Heading (25 characters): Encourage users to seek more information.]', 'aegis') . '</h3>
 				<!-- /wp:heading -->
 
 				<!-- wp:paragraph -->
-				<p>' . esc_html__('Response Assurance Format (50 chars): [Reassure users of timely feedback or support.]', 'aegis') . '</p>
+				<p>' . esc_html__('[Paragraph (50 characters): Reassure timely feedback or support.]', 'aegis') . '</p>
 				<!-- /wp:paragraph -->
 
 				<!-- wp:buttons {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 				<div class="wp-block-buttons">
 					<!-- wp:button {"backgroundColor":"background","textColor":"foreground","className":"is-style-aegis-button-shadow"} -->
-					<div class="wp-block-button is-style-aegis-button-shadow"><a class="wp-block-button__link has-foreground-color has-background-background-color has-text-color has-background wp-element-button" href="#">' . esc_html__('Call to Action', 'aegis') . '</a></div>
+					<div class="wp-block-button is-style-aegis-button-shadow"><a class="wp-block-button__link has-foreground-color has-background-background-color has-text-color has-background wp-element-button" href="#">' . esc_html__('[Call to Action]', 'aegis') . '</a></div>
 					<!-- /wp:button -->
 				</div>
 				<!-- /wp:buttons -->
