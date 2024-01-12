afd5c966d99269623c4336277c8e84beb56e0e76
Atmostfear Entertainment <labs@atmostfear-entertainment.com>
2024-01-12 11:47:09 -0500

    Update gallery-2.php
    - Changed: Block Pattern Description to make its purpose more clear.
    - Changed: Thumbnail from old to new design for visual enhancement and consistency.
    - Changed: Improved Content placeholders.
    - Added: Group Block Name.

--- patterns/gallery/gallery-2.php
+++ patterns/gallery/gallery-2.php
@@ -4,23 +4,22 @@
  */
 return array(
     'title'      => __('02. Video Gallery', 'aegis'),
-    'description' => __('Gallery with tagline, headings, paragraphs, and grig videos', 'aegis'),
+    'description' => __('Gallery with Tagline, Headings, Paragraphs, and four Columns of Videos', 'aegis'),
     'categories' => array('aegis-gallery'),
-    'content' => '
-<!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"backgroundColor":"foreground","textColor":"background","className":"gallery","layout":{"type":"default"},"metadata":{"name":"' . esc_html__('Gallery Section', 'aegis') . '"}} -->
-<div class="wp-block-group alignwide gallery has-background-color has-foreground-background-color has-text-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--30)">
-    <!-- wp:group {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|30"}}},"layout":{"type":"default"}} -->
-    <div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--30)">
+    'content' => '<!-- wp:group {"metadata":{"name":"' . esc_html__('02. Gallery Pattern', 'aegis') . '"},"align":"wide","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"backgroundColor":"foreground","textColor":"background","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide has-background-color has-foreground-background-color has-text-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--30)">
+    <!-- wp:group {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|40"},"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"default"}} -->
+    <div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--40);padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
         <!-- wp:paragraph {"align":"left","style":{"typography":{"textTransform":"uppercase","letterSpacing":"3px","fontStyle":"normal","fontWeight":"400"}},"className":"tagline","fontSize":"tiny"} -->
-        <p class="has-text-align-left tagline has-tiny-font-size" style="font-style:normal;font-weight:400;letter-spacing:3px;text-transform:uppercase">' . esc_html__('Gallery', 'aegis') . '</p>
+        <p class="has-text-align-left tagline has-tiny-font-size" style="font-style:normal;font-weight:400;letter-spacing:3px;text-transform:uppercase">' . esc_html__('[Tagline]', 'aegis') . '</p>
         <!-- /wp:paragraph -->
 
         <!-- wp:heading {"level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"spacing":{"margin":{"top":"5px","bottom":"10px"}}},"fontSize":"huge"} -->
-        <h4 class="wp-block-heading has-huge-font-size" style="margin-top:5px;margin-bottom:10px;font-style:normal;font-weight:600">' . esc_html__('[Gallery Title]', 'aegis') . '</h4>
+        <h4 class="wp-block-heading has-huge-font-size" style="margin-top:5px;margin-bottom:10px;font-style:normal;font-weight:600">' . esc_html__('[Heading]', 'aegis') . '</h4>
         <!-- /wp:heading -->
 
         <!-- wp:paragraph {"fontSize":"small"} -->
-        <p class="has-small-font-size">' . esc_html__('Gallery Description (333 characters): [Provide details about the videos, including themes, or special highlights.]', 'aegis') . '</p>
+        <p class="has-small-font-size">' . esc_html__('[Paragraph (333 characters): Provide details about the videos, including themes, or special highlights.]', 'aegis') . '</p>
         <!-- /wp:paragraph -->
     </div>
     <!-- /wp:group -->
@@ -34,11 +33,11 @@ return array(
             <!-- /wp:video -->
 
             <!-- wp:heading {"level":4,"style":{"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"0"}},"typography":{"fontStyle":"normal","fontWeight":"300"}},"fontSize":"large"} -->
-            <h4 class="wp-block-heading has-large-font-size" style="margin-top:var(--wp--preset--spacing--30);margin-bottom:0;font-style:normal;font-weight:300"><strong>' . esc_html__('[Video Title]', 'aegis') . '</strong></h4>
+            <h4 class="wp-block-heading has-large-font-size" style="margin-top:var(--wp--preset--spacing--30);margin-bottom:0;font-style:normal;font-weight:300"><strong>' . esc_html__('[Heading]', 'aegis') . '</strong></h4>
             <!-- /wp:heading -->
 
             <!-- wp:paragraph {"fontSize":"small"} -->
-            <p class="has-small-font-size">' . esc_html__('Video Description (60 characters): [Provide a brief synopsis of the video.]', 'aegis') . '</p>
+            <p class="has-small-font-size">' . esc_html__('[Paragraph (65 characters): Provide a brief synopsis of the video.]', 'aegis') . '</p>
             <!-- /wp:paragraph -->
         </div>
         <!-- /wp:column -->
@@ -50,11 +49,11 @@ return array(
             <!-- /wp:video -->
 
             <!-- wp:heading {"level":4,"style":{"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"0"}},"typography":{"fontStyle":"normal","fontWeight":"300"}},"fontSize":"large"} -->
-            <h4 class="wp-block-heading has-large-font-size" style="margin-top:var(--wp--preset--spacing--30);margin-bottom:0;font-style:normal;font-weight:300"><strong>' . esc_html__('[Video Title]', 'aegis') . '</strong></h4>
+            <h4 class="wp-block-heading has-large-font-size" style="margin-top:var(--wp--preset--spacing--30);margin-bottom:0;font-style:normal;font-weight:300"><strong>' . esc_html__('[Heading]', 'aegis') . '</strong></h4>
             <!-- /wp:heading -->
 
             <!-- wp:paragraph {"fontSize":"small"} -->
-            <p class="has-small-font-size">' . esc_html__('Video Description (60 characters): [Provide a brief synopsis of the video.]', 'aegis') . '</p>
+            <p class="has-small-font-size">' . esc_html__('[Paragraph (65 characters): Provide a brief synopsis of the video.]', 'aegis') . '</p>
             <!-- /wp:paragraph -->
         </div>
         <!-- /wp:column -->
@@ -66,11 +65,11 @@ return array(
             <!-- /wp:video -->
 
             <!-- wp:heading {"level":4,"style":{"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"0"}},"typography":{"fontStyle":"normal","fontWeight":"300"}},"fontSize":"large"} -->
-            <h4 class="wp-block-heading has-large-font-size" style="margin-top:var(--wp--preset--spacing--30);margin-bottom:0;font-style:normal;font-weight:300"><strong>' . esc_html__('[Video Title]', 'aegis') . '</strong></h4>
+            <h4 class="wp-block-heading has-large-font-size" style="margin-top:var(--wp--preset--spacing--30);margin-bottom:0;font-style:normal;font-weight:300"><strong>' . esc_html__('[Heading]', 'aegis') . '</strong></h4>
             <!-- /wp:heading -->
 
             <!-- wp:paragraph {"fontSize":"small"} -->
-            <p class="has-small-font-size">' . esc_html__('Video Description (60 characters): [Provide a brief synopsis of the video.]', 'aegis') . '</p>
+            <p class="has-small-font-size">' . esc_html__('[Paragraph (65 characters): Provide a brief synopsis of the video.]', 'aegis') . '</p>
             <!-- /wp:paragraph -->
         </div>
         <!-- /wp:column -->
@@ -82,11 +81,11 @@ return array(
             <!-- /wp:video -->
 
             <!-- wp:heading {"level":4,"style":{"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"0"}},"typography":{"fontStyle":"normal","fontWeight":"300"}},"fontSize":"large"} -->
-            <h4 class="wp-block-heading has-large-font-size" style="margin-top:var(--wp--preset--spacing--30);margin-bottom:0;font-style:normal;font-weight:300"><strong>' . esc_html__('[Video Title]', 'aegis') . '</strong></h4>
+            <h4 class="wp-block-heading has-large-font-size" style="margin-top:var(--wp--preset--spacing--30);margin-bottom:0;font-style:normal;font-weight:300"><strong>' . esc_html__('[Heading]', 'aegis') . '</strong></h4>
             <!-- /wp:heading -->
 
             <!-- wp:paragraph {"fontSize":"small"} -->
-            <p class="has-small-font-size">' . esc_html__('Video Description (60 characters): [Provide a brief synopsis of the video.]', 'aegis') . '</p>
+            <p class="has-small-font-size">' . esc_html__('[Paragraph (65 characters): Provide a brief synopsis of the video.]', 'aegis') . '</p>
             <!-- /wp:paragraph -->
         </div>
         <!-- /wp:column -->
