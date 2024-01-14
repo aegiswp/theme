f68478ad1930e8e88e271ff569cd5baf7c6c956a
Atmostfear Entertainment <labs@atmostfear-entertainment.com>
2024-01-14 10:53:55 -0500

    Update hero-1.php
    - Changed: Block Pattern Description to make its purpose more clear.
    - Changed: Improved Content placeholders.
    - Added: Group Block Name.
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/hero/hero-1.php
+++ patterns/hero/hero-1.php
@@ -3,34 +3,38 @@
  * 01. Hero Block Pattern
  */
 return array(
-    'title'      => __('01. Hero', 'aegis'),
-    'description' => __('Hero', 'aegis'),
-    'categories' => array('aegis-hero'),
-    'content' => '
-<!-- wp:cover {"url":"' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_1800x1200_dark.webp","dimRatio":70,"overlayColor":"foreground","focalPoint":{"x":0.5,"y":0.5},"minHeight":800,"contentPosition":"center center","align":"full","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"margin":{"top":"0","bottom":"0"}}},"className":"is-style-default header-hero additional"} -->
-<div class="wp-block-cover alignfull is-style-default header-hero additional" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;min-height:800px">
-	<span aria-hidden="true" class="wp-block-cover__background has-foreground-background-color has-background-dim-70 has-background-dim"></span><img class="wp-block-cover__image-background" alt="' . esc_attr_x('Default background image for the hero', 'Description of the default image', 'aegis') . '" src="' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_1800x1200_dark.webp" style="object-position:50% 50%" data-object-fit="cover" data-object-position="50% 50%" />
-	<div class="wp-block-cover__inner-container">
-		<!-- wp:group {"style":{"spacing":{"padding":{"top":"240px","right":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"layout":{"type":"constrained"}} -->
-		<div class="wp-block-group" style="padding-top:240px;padding-right:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
-			<!-- wp:heading {"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"gigantic"} -->
-			<h2 class="wp-block-heading has-text-align-center has-gigantic-font-size" style="font-style:normal;font-weight:400"><strong>' . esc_html__('Main', 'aegis') . '</strong> ' . esc_html__('Heading', 'aegis') . '</h2>
-			<!-- /wp:heading -->
+	'title'      => __('01. Hero', 'aegis'),
+	'description' => __('Full-screen hero with cover background image, dark overlay, and light centered heading, paragraph, and a call-to-action button', 'aegis'),
+	'categories' => array('aegis-hero'),
+	'content' => '<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"default"},"metadata":{"name":"' . esc_html__('01. Hero Pattern', 'aegis') . '"}} -->
+    <div class="wp-block-group alignfull" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
+	<!-- wp:cover {"url":"' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_1920x1200_dark.webp","dimRatio":70,"overlayColor":"foreground","minHeight":100,"minHeightUnit":"vh","contentPosition":"center center","align":"full","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"margin":{"top":"0","bottom":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"textColor":"background"} -->
+	<div class="wp-block-cover alignfull has-background-color has-text-color has-link-color" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;min-height:100vh">
+		<span aria-hidden="true" class="wp-block-cover__background has-foreground-background-color has-background-dim-70 has-background-dim"></span>
+		<img class="wp-block-cover__image-background" src="' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_1920x1200_dark.webp" alt="' . esc_html__('Describe the main elements of the image and its context.', 'aegis') . '" data-object-fit="cover" />
+		<div class="wp-block-cover__inner-container">
+			<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
+				<!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"gigantic"} -->
+				<h1 class="wp-block-heading has-text-align-center has-gigantic-font-size" style="font-style:normal;font-weight:400">' . wp_kses_post(_x('<strong>Main</strong> Headline', 'Sample descriptive heading', 'aegis')) . '</h1>
+				<!-- /wp:heading -->
 
-			<!-- wp:paragraph {"align":"center"} -->
-			<p class="has-text-align-center">' . esc_html_x('About Content (333 chars): [Provide a detailed overview of a specific topic, product, or service.]', 'Placeholder content for the about section', 'aegis') . '</p>
-			<!-- /wp:paragraph -->
+				<!-- wp:paragraph {"align":"center"} -->
+				<p class="has-text-align-center">' . esc_html__('[Paragraph (155 characters): Promote an offer, summarize an article, or share a news update.]', 'aegis') . '</p>
+				<!-- /wp:paragraph -->
 
-			<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center","flexWrap":"nowrap"}} -->
-			<div class="wp-block-buttons">
-				<!-- wp:button {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"className":"is-style-aegis-button-shadow","fontSize":"small"} -->
-				<div class="wp-block-button has-custom-font-size is-style-aegis-button-shadow has-small-font-size" style="font-style:normal;font-weight:600"><a class="wp-block-button__link wp-element-button" href="#">' . esc_html__('Call to Action', 'aegis') . '</a></div>
-				<!-- /wp:button -->
+				<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center","flexWrap":"wrap"}} -->
+				<div class="wp-block-buttons">
+					<!-- wp:button {"className":"is-style-aegis-button-shadow"} -->
+					<div class="wp-block-button is-style-aegis-button-shadow"><a class="wp-block-button__link wp-element-button" href="#">' . esc_html__('[Call to Action]', 'aegis') . '</a></div>
+					<!-- /wp:button -->
+				</div>
+				<!-- /wp:buttons -->
 			</div>
-			<!-- /wp:buttons -->
+			<!-- /wp:group -->
 		</div>
-		<!-- /wp:group -->
 	</div>
+	<!-- /wp:cover -->
 </div>
-<!-- /wp:cover -->',
+<!-- /wp:group -->',
 );
