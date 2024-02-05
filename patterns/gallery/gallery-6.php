f933a52ea97b1c29259d339885b09aef80b3324b
Atmostfear Entertainment <labs@atmostfear-entertainment.com>
2024-02-05 00:52:27 -0500

    Update gallery-6.php
    - Changed: Block Pattern Description to make its purpose more clear.
    - Changed: Improved Content placeholders.

--- patterns/gallery/gallery-6.php
+++ patterns/gallery/gallery-6.php
@@ -4,34 +4,29 @@
  */
 return array(
     'title'      => __('06. Gallery', 'aegis'),
-    'description' => __('Grid Gallery with Heading, Paragraph, and buttons', 'aegis'),
+    'description' => __('Grid Gallery with Tagline, Heading, and Paragraph', 'aegis'),
     'categories' => array('aegis-gallery'),
-    'content' => '<!-- wp:group {"metadata":{"name":"' . esc_html__('06. Gallery Pattern', 'aegis') . '"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|30","right":"var:preset|spacing|30"},"margin":{"top":"0","bottom":"0"}}},"gradient":"diagonal-secondary-to-background","layout":{"type":"default"}} -->
-    <div class="wp-block-group alignfull has-diagonal-secondary-to-background-gradient-background has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--30)">
+    'content' => '<!-- wp:group {"metadata":{"name":"06. Gallery Pattern"},"gradient":"diagonal-secondary-to-background","layout":{"type":"constrained"}} -->
+    <div class="wp-block-group has-diagonal-secondary-to-background-gradient-background has-background">
     <!-- wp:columns {"verticalAlignment":"center","align":"wide","style":{"spacing":{"blockGap":"40px","padding":{"top":"0","bottom":"0","left":"0","right":"0"}}}} -->
     <div class="wp-block-columns alignwide are-vertically-aligned-center" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
         <!-- wp:column {"verticalAlignment":"center","width":"40%"} -->
         <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:40%">
-            <!-- wp:heading {"className":"is-style-default","fontSize":"huge"} -->
-            <h2 class="wp-block-heading is-style-default has-huge-font-size"><mark style="background-color:#1c1c1e" class="has-inline-color has-background-color">' . esc_html__('[Heading]', 'aegis') . '</mark></h2>
-            <!-- /wp:heading -->
+            <!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"blockGap":"0","margin":{"bottom":"var:preset|spacing|30"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
+            <div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--30);padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
+                <!-- wp:paragraph {"align":"left","style":{"typography":{"textTransform":"uppercase","letterSpacing":"3px","fontStyle":"normal","fontWeight":"400"}},"className":"is-tagline","fontSize":"tiny"} -->
+                <p class="has-text-align-left is-tagline has-tiny-font-size" style="font-style:normal;font-weight:400;letter-spacing:3px;text-transform:uppercase">' . esc_html__('Gallery', 'aegis') . '</p>
+                <!-- /wp:paragraph -->
 
-            <!-- wp:paragraph -->
-            <p>' . esc_html__( '[Paragraph (133 characters): Provide details about upcoming events, including themes, or special highlights.]', 'aegis') . '</p>
-            <!-- /wp:paragraph -->
+                <!-- wp:heading {"style":{"typography":{"fontSize":"6rem","textTransform":"uppercase"},"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+                <h2 class="wp-block-heading" style="margin-top:0;margin-bottom:0;font-size:6rem;text-transform:uppercase">' . esc_html__('Photos', 'aegis') . '</h2>
+                <!-- /wp:heading -->
 
-            <!-- wp:buttons -->
-            <div class="wp-block-buttons">
-                <!-- wp:button -->
-                <div class="wp-block-button"><a class="wp-block-button__link wp-element-button">' . esc_html__('[Call to Action]', 'aegis') . '</a></div>
-                <!-- /wp:button -->
-
-                <!-- wp:button -->
-                <div class="wp-block-button"><a class="wp-block-button__link wp-element-button">' . esc_html__('[Call to Action]', 'aegis') . '</a>
-                </div>
-                <!-- /wp:button -->
+                <!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0"}}}} -->
+                <p style="margin-top:0">' . esc_html__('Description (333 characters): Provide details about the photographs, including themes, or special highlights.', 'aegis') . '</p>
+                <!-- /wp:paragraph -->
             </div>
-            <!-- /wp:buttons -->
+            <!-- /wp:group -->
         </div>
         <!-- /wp:column -->
 
