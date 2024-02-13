b0b754854fd3224e29a879e7317b4ff6008fa4b2
Atmostfear Entertainment <labs@atmostfear-entertainment.com>
2024-02-13 14:08:44 -0500

    Update about-1.php
    - Changed: Block Pattern Description to make its purpose more clear.
    - Changed: Typography size for better Accessibility.
    - Changed: Color pallet for more consistent visual design.
    - Removed: Unnecessary groups, columns, and other nested blocks.
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/about/about-1.php
+++ patterns/about/about-1.php
@@ -3,34 +3,22 @@
  * 01. About Block Pattern
  */
 return array(
-	'title'	      => __( '01. About', 'aegis' ),
+	'title'	      => __( '01. About Pattern', 'aegis' ),
 	'description' => __( 'Block Pattern with Tagline, Heading, Paragraph, Media, and Button', 'aegis' ),
 	'categories'  => array( 'aegis-about' ),
-	'content' => '<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"0","right":"0"}}},"gradient":"diagonal-secondary-to-background","layout":{"type":"constrained"},"metadata":{"name":"' . esc_html__('01. About Pattern', 'aegis') . '"}} -->
-<div class="wp-block-group alignfull has-diagonal-secondary-to-background-gradient-background has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--30);padding-right:0;padding-bottom:var(--wp--preset--spacing--30);padding-left:0">
-	<!-- wp:columns {"align":"wide","style":{"spacing":{"padding":{"bottom":"0","top":"0"}}}} -->
-	<div class="wp-block-columns alignwide" style="padding-top:0;padding-bottom:0">
-		<!-- wp:column {"verticalAlignment":"center","width":""} -->
-		<div class="wp-block-column is-vertically-aligned-center">
-			<!-- wp:group {"gradient":"horizontal-primary-to-background"} -->
-			<div class="wp-block-group has-horizontal-primary-to-background-gradient-background has-background">
-				<!-- wp:image {"aspectRatio":"4/3","scale":"cover","sizeSlug":"full","linkDestination":"none","className":"size-full is-style-aegis-shadow-image"} -->
-				<figure class="wp-block-image size-full is-style-aegis-shadow-image"><img src="' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_1920x1200_dark.webp" alt="' . esc_attr__('Describe the main elements of the image and its context.', 'aegis') . '" style="aspect-ratio:4/3;object-fit:cover" /></figure>
-				<!-- /wp:image -->
-			</div>
-			<!-- /wp:group -->
-		</div>
-		<!-- /wp:column -->
-
-		<!-- wp:column {"verticalAlignment":"center","width":"","style":{"spacing":{"padding":{"top":"0","right":"var:preset|spacing|30","bottom":"var:preset|spacing|40","left":"var:preset|spacing|30"}}},"layout":{"type":"constrained"}} -->
-		<div class="wp-block-column is-vertically-aligned-center" style="padding-top:0;padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--30)">
+	'content' => '<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"gradient":"diagonal-secondary-to-background","layout":{"type":"constrained"},"metadata":{"name":"' . esc_html__('01. About Pattern', 'aegis') . '"}} -->
+    <div class="wp-block-group has-diagonal-secondary-to-background-gradient-background has-background" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
+	<!-- wp:media-text {"align":"wide","mediaId":1410,"mediaLink":"' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_1920x1200_dark.webp","mediaType":"image","imageFill":false} -->
+	<div class="wp-block-media-text alignwide is-stacked-on-mobile">
+		<figure class="wp-block-media-text__media"><img src="' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_1920x1200_dark.webp" alt="' . esc_attr__('Describe the main elements of the image and its context.', 'aegis') . '" class="wp-image-1410 size-full" /></figure>
+		<div class="wp-block-media-text__content">
 			<!-- wp:paragraph {"align":"left","style":{"typography":{"textTransform":"uppercase","letterSpacing":"3px","fontStyle":"normal","fontWeight":"400"}},"className":"is-tagline","fontSize":"tiny"} -->
 			<p class="has-text-align-left is-tagline has-tiny-font-size" style="font-style:normal;font-weight:400;letter-spacing:3px;text-transform:uppercase">' . esc_html__('[Tagline]', 'aegis') . '</p>
 			<!-- /wp:paragraph -->
 
 			<!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|30","left":"0","right":"0"}},"typography":{"fontStyle":"normal","fontWeight":"300"}},"fontSize":"gigantic"} -->
-            <h3 class="wp-block-heading has-gigantic-font-size" style="margin-top:0;margin-right:0;margin-bottom:var(--wp--preset--spacing--30);margin-left:0;font-style:normal;font-weight:300"><strong>' . esc_html__('[Heading]', 'aegis') . '</strong></h3>
-            <!-- /wp:heading -->
+			<h3 class="wp-block-heading has-gigantic-font-size" style="margin-top:0;margin-right:0;margin-bottom:var(--wp--preset--spacing--30);margin-left:0;font-style:normal;font-weight:300"><strong>' . esc_html__('[Heading]', 'aegis') . '</strong></h3>
+			<!-- /wp:heading -->
 
 			<!-- wp:paragraph -->
 			<p>' . esc_html__('[Paragraph (333 characters): Detail the core principles, values, or characteristics of the organization or subject.]', 'aegis') . '</p>
@@ -44,9 +32,8 @@ return array(
 			</div>
 			<!-- /wp:buttons -->
 		</div>
-		<!-- /wp:column -->
 	</div>
-	<!-- /wp:columns -->
+	<!-- /wp:media-text -->
 </div>
 <!-- /wp:group -->',
 );
