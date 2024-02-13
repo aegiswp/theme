92c0060f232980580f5334f30bb3f4639add5ca3
Atmostfear Entertainment <labs@atmostfear-entertainment.com>
2024-02-13 15:28:22 -0500

    Update about-7.php
    - Added: Image aspect ratio.
    - Changed: Block Pattern Description to make its purpose more clear.
    - Changed: Typography size for better Accessibility.
    - Changed: Color pallet for more consistent visual design.
    - Removed: Unnecessary groups, columns, and other nested blocks.
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/about/about-7.php
+++ patterns/about/about-7.php
@@ -3,13 +3,13 @@
  * 07. About Block Pattern
  */
 return array(
-    'title'      => __('07. About', 'aegis'),
+    'title'      => __('07. About Pattern', 'aegis'),
     'description' => __('Grid Gallery with Tagline, Heading, both vertical and horizontal Paragraphs', 'aegis'),
     'categories' => array('aegis-about'),
-    'content' => '<!-- wp:group {"metadata":{"name":"' . esc_html__('07. About Pattern', 'aegis') . '"},"gradient":"vertical-background-to-secondary","layout":{"type":"default"}} -->
-    <div class="wp-block-group has-vertical-background-to-secondary-gradient-background has-background">
-    <!-- wp:columns {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}}} -->
-    <div class="wp-block-columns" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
+    'content' => '<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"gradient":"vertical-background-to-secondary","layout":{"type":"constrained"},"metadata":{"name":"' . esc_html__('07. About Pattern', 'aegis') . '"}} -->
+    <div class="wp-block-group has-vertical-background-to-secondary-gradient-background has-background" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
+    <!-- wp:columns {"align":"wide","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}}} -->
+    <div class="wp-block-columns alignwide" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
         <!-- wp:column {"verticalAlignment":"center","width":"5%","className":"is-hidden-on-mobile"} -->
         <div class="wp-block-column is-vertically-aligned-center is-hidden-on-mobile" style="flex-basis:5%">
             <!-- wp:paragraph {"align":"right","style":{"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}},"typography":{"fontSize":"6.9rem","lineHeight":"1","writingMode":"vertical-rl","textTransform":"uppercase","fontStyle":"normal","fontWeight":"700","letterSpacing":"3px"}},"textColor":"secondary"} -->
@@ -20,10 +20,8 @@ return array(
 
         <!-- wp:column {"width":"19%"} -->
         <div class="wp-block-column" style="flex-basis:19%">
-            <!-- wp:cover {"url":"' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_1200x1920_dark.webp","dimRatio":70,"overlayColor":"foreground","isUserOverlayColor":true,"minHeight":75,"minHeightUnit":"vh","contentPosition":"bottom center","style":{"border":{"width":"1px"}},"borderColor":"background","className":"is-style-aegis-shadow","layout":{"type":"default"}} -->
-            <div class="wp-block-cover has-custom-content-position is-position-bottom-center is-style-aegis-shadow has-border-color has-background-border-color" style="border-width:1px;min-height:75vh">
-            <span aria-hidden="true" class="wp-block-cover__background has-foreground-background-color has-background-dim-70 has-background-dim"></span>
-            <img class="wp-block-cover__image-background" alt="' . esc_attr__('Describe the main elements of the image and its context.', 'aegis') . '" src="' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_1200x1920_dark.webp" data-object-fit="cover" />
+            <!-- wp:cover {"url":"' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_1200x1920_dark.webp","dimRatio":70,"overlayColor":"foreground","minHeight":75,"minHeightUnit":"vh","contentPosition":"bottom center","style":{"border":{"width":"1px"}},"borderColor":"background","className":"is-style-aegis-shadow","layout":{"type":"default"}} -->
+            <div class="wp-block-cover has-custom-content-position is-position-bottom-center is-style-aegis-shadow has-border-color has-background-border-color" style="border-width:1px;min-height:75vh"><span aria-hidden="true" class="wp-block-cover__background has-foreground-background-color has-background-dim-70 has-background-dim"></span><img class="wp-block-cover__image-background" alt="' . esc_attr__('Describe the main elements of the image and its context.', 'aegis') . '" src="' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_1200x1920_dark.webp" data-object-fit="cover" />
                 <div class="wp-block-cover__inner-container">
                     <!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0"},"blockGap":"5px"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center","verticalAlignment":"bottom","orientation":"vertical"}} -->
                     <div class="wp-block-group" style="padding-top:0;padding-bottom:0">
@@ -42,12 +40,10 @@ return array(
         </div>
         <!-- /wp:column -->
 
-        <!-- wp:column {"verticalAlignment":"center","width":"19%"} -->
-        <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:19%">
-            <!-- wp:cover {"url":"' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_1200x1920_dark.webp","dimRatio":70,"overlayColor":"foreground","isUserOverlayColor":true,"focalPoint":{"x":0.76,"y":0.72},"minHeight":65,"minHeightUnit":"vh","contentPosition":"bottom center","style":{"border":{"width":"1px"}},"borderColor":"background","className":"is-style-aegis-shadow"} -->
-            <div class="wp-block-cover has-custom-content-position is-position-bottom-center is-style-aegis-shadow has-border-color has-background-border-color" style="border-width:1px;min-height:65vh">
-            <span aria-hidden="true" class="wp-block-cover__background has-foreground-background-color has-background-dim-70 has-background-dim"></span>
-            <img class="wp-block-cover__image-background" alt="' . esc_attr__('Describe the main elements of the image and its context.', 'aegis') . '" src="' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_1200x1920_dark.webp" style="object-position:76% 72%" data-object-fit="cover" data-object-position="76% 72%" />
+        <!-- wp:column {"verticalAlignment":"center","width":"19%","className":"is-hidden-on-mobile"} -->
+        <div class="wp-block-column is-vertically-aligned-center is-hidden-on-mobile" style="flex-basis:19%">
+            <!-- wp:cover {"url":"' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_1200x1920_dark.webp","dimRatio":70,"overlayColor":"foreground","focalPoint":{"x":0.76,"y":0.72},"minHeight":65,"minHeightUnit":"vh","contentPosition":"bottom center","style":{"border":{"width":"1px"}},"borderColor":"background","className":"is-style-aegis-shadow"} -->
+            <div class="wp-block-cover has-custom-content-position is-position-bottom-center is-style-aegis-shadow has-border-color has-background-border-color" style="border-width:1px;min-height:65vh"><span aria-hidden="true" class="wp-block-cover__background has-foreground-background-color has-background-dim-70 has-background-dim"></span><img class="wp-block-cover__image-background" alt="' . esc_attr__('Describe the main elements of the image and its context.', 'aegis') . '" src="' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_1200x1920_dark.webp" style="object-position:76% 72%" data-object-fit="cover" data-object-position="76% 72%" />
                 <div class="wp-block-cover__inner-container">
                     <!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0"},"blockGap":"5px"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center","verticalAlignment":"bottom","orientation":"vertical"}} -->
                     <div class="wp-block-group" style="padding-top:0;padding-bottom:0">
@@ -56,7 +52,8 @@ return array(
                         <!-- /wp:paragraph -->
 
                         <!-- wp:paragraph {"align":"center","fontSize":"small"} -->
-                        <p class="has-text-align-center has-small-font-size">' . esc_html__('[Position]', 'aegis') . '</p>
+                        <p class="has-text-align-center has-small-font-size">' . esc_html__('[Position]', 'aegis') . '
+                        </p>
                         <!-- /wp:paragraph -->
                     </div>
                     <!-- /wp:group -->
@@ -66,12 +63,10 @@ return array(
         </div>
         <!-- /wp:column -->
 
-        <!-- wp:column {"verticalAlignment":"center","width":"19%"} -->
-        <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:19%">
-            <!-- wp:cover {"url":"' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_1200x1920_dark.webp","dimRatio":70,"overlayColor":"foreground","isUserOverlayColor":true,"minHeight":55,"minHeightUnit":"vh","contentPosition":"bottom center","style":{"border":{"width":"1px"}},"borderColor":"background","className":"is-style-aegis-shadow"} -->
-            <div class="wp-block-cover has-custom-content-position is-position-bottom-center is-style-aegis-shadow has-border-color has-background-border-color" style="border-width:1px;min-height:55vh">
-            <span aria-hidden="true" class="wp-block-cover__background has-foreground-background-color has-background-dim-70 has-background-dim"></span>
-            <img class="wp-block-cover__image-background" alt="' . esc_attr__('Describe the main elements of the image and its context.', 'aegis') . '" src="' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_1200x1920_dark.webp" data-object-fit="cover" />
+        <!-- wp:column {"verticalAlignment":"center","width":"19%","className":"is-hidden-on-mobile"} -->
+        <div class="wp-block-column is-vertically-aligned-center is-hidden-on-mobile" style="flex-basis:19%">
+            <!-- wp:cover {"url":"' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_1200x1920_dark.webp","dimRatio":70,"overlayColor":"foreground","minHeight":55,"minHeightUnit":"vh","contentPosition":"bottom center","style":{"border":{"width":"1px"}},"borderColor":"background","className":"is-style-aegis-shadow"} -->
+            <div class="wp-block-cover has-custom-content-position is-position-bottom-center is-style-aegis-shadow has-border-color has-background-border-color" style="border-width:1px;min-height:55vh"><span aria-hidden="true" class="wp-block-cover__background has-foreground-background-color has-background-dim-70 has-background-dim"></span><img class="wp-block-cover__image-background" alt="' . esc_attr__('Describe the main elements of the image and its context.', 'aegis') . '" src="' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_1200x1920_dark.webp" data-object-fit="cover" />
                 <div class="wp-block-cover__inner-container">
                     <!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0"},"blockGap":"5px"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center","verticalAlignment":"bottom","orientation":"vertical"}} -->
                     <div class="wp-block-group" style="padding-top:0;padding-bottom:0">
