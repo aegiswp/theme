3580e8561227cd430519660d66a41e3e6fa16aac
Atmostfear Entertainment <labs@atmostfear-entertainment.com>
2024-02-19 06:05:04 -0500

    Create event-feature-video-posts-socials.php
    - Moved: event-1.php block pattern from '/patterns/event' to '/patterns' following the Theme Handbook Block Patterns guidelines.
    - Changed: Rewrote event-1.php as event-feature-video-posts-socials.php following the WordPress Theme Handbook Block Patterns structure guidelines.
    - Initial commit.
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/event-feature-video-posts-socials.php
+++ patterns/event-feature-video-posts-socials.php
@@ -1,22 +1,38 @@
 <?php
 /**
- * 01. Event Block Pattern
+ * Title: Event with featured video, posts, and socials
+ * Slug: aegis/event-feature-video-posts-socials
+ * Categories: event, featured, media, social, video
+ * Description: A full-width event feature pattern with gradient background, centered headings, video showcase, query posts, and centralized social links.
+ * Keywords: event, media, video, posts, social links
+ * Viewport Width: 1400
+ * Block Types: core/group, core/heading, core/paragraph, core/video, core/query, core/post-template, core/post-date, core/post-title, core/buttons, core/button, core/social-links, core/social-link
+ * Inserter: true
+ * 
+ * @package aegis
+ * @since 1.0.0
  */
-return array(
-    'title'      => __('01. Event', 'aegis'),
-    'description' => __('Block Pattern with Video, Heading, Date, Paragraph, Button, and Social Icons', 'aegis'),
-    'categories' => array('aegis-event'),
-    'content' => '<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"gradient":"diagonal-secondary-to-background","layout":{"type":"constrained"},"metadata":{"name":"' . esc_html__('01. Event Pattern', 'aegis') . '"}} -->
-    <div class="wp-block-group alignfull has-diagonal-secondary-to-background-gradient-background has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
-    <!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|40"},"padding":{"bottom":"0","top":"0"}}},"layout":{"type":"constrained"}} -->
-    <div class="wp-block-group alignwide" style="margin-top:0;margin-bottom:var(--wp--preset--spacing--40);padding-top:0;padding-bottom:0">
-        <!-- wp:heading {"textAlign":"center","level":3,"align":"wide","style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}},"fontSize":"huge"} -->
-        <h3 class="wp-block-heading alignwide has-text-align-center has-huge-font-size" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0">' . esc_html__('[Heading]', 'aegis') . '</h3>
-        <!-- /wp:heading -->
+?>
 
-        <!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
-        <p class="has-text-align-center" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0">' . esc_html__('[Paragraph (133 characters): Provide details about upcoming events, including themes, or special highlights.]', 'aegis') . '</p>
-        <!-- /wp:paragraph -->
+<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"gradient":"diagonal-secondary-to-background","layout":{"type":"constrained"},"metadata":{"name":"<?php echo esc_html_x('01. Event Pattern', 'Name of the pattern', 'aegis'); ?>"}} -->
+<div class="wp-block-group alignfull has-diagonal-secondary-to-background-gradient-background has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--30)">
+    <!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|40"},"padding":{"bottom":"0","top":"0"}}},"layout":{"type":"default"}} -->
+    <div class="wp-block-group alignwide" style="margin-top:0;margin-bottom:var(--wp--preset--spacing--40);padding-top:0;padding-bottom:0">
+        <!-- wp:group {"style":{"spacing":{"blockGap":"0","padding":{"right":"var:preset|spacing|30"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+        <div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--30)">
+            <!-- wp:paragraph {"align":"center","style":{"typography":{"textTransform":"uppercase","letterSpacing":"3px","fontStyle":"normal","fontWeight":"400"}},"fontSize":"tiny"} -->
+            <p class="has-text-align-center has-tiny-font-size" style="font-style:normal;font-weight:400;letter-spacing:3px;text-transform:uppercase"><?php echo esc_html_e('Upcoming Events', 'aegis'); ?></p>
+            <!-- /wp:paragraph -->
+
+            <!-- wp:heading {"textAlign":"center","level":3,"style":{"spacing":{"margin":{"right":"0px","left":"0px","top":"0px","bottom":"0px"},"padding":{"top":"0","bottom":"0"}},"typography":{"textTransform":"none"}},"fontSize":"gigantic"} -->
+            <h3 class="wp-block-heading has-text-align-center has-gigantic-font-size" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px;padding-top:0;padding-bottom:0;text-transform:none"><?php echo esc_html_x('Events Agenda', 'Events call to action title', 'aegis'); ?></h3>
+            <!-- /wp:heading -->
+
+            <!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0"}}}} -->
+            <p class="has-text-align-center" style="margin-top:0"><?php echo esc_html_x('Description (up to 160 characters): Enter a brief summary of your events here.', 'Events call to action description', 'aegis'); ?></p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
 
@@ -25,7 +41,7 @@ return array(
         <!-- wp:column {"verticalAlignment":"top","width":"","style":{"spacing":{"padding":{"top":"0","bottom":"0"}}},"className":"is-style-default"} -->
         <div class="wp-block-column is-vertically-aligned-top is-style-default" style="padding-top:0;padding-bottom:0">
             <!-- wp:video -->
-            <figure class="wp-block-video"><video controls poster= "' . esc_url(get_template_directory_uri()) . '/assets/images/thumb_1920x1200_dark.webp" src= "' . esc_url(get_template_directory_uri()) . '/assets/videos/sample.mp4" playsinline></video></figure>
+            <figure class="wp-block-video"><video controls poster="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/thumb_1200x1920_dark.webp" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/videos/sample.mp4" playsinline></video></figure>
             <!-- /wp:video -->
         </div>
         <!-- /wp:column -->
@@ -60,7 +76,7 @@ return array(
                 <div class="wp-block-buttons">
                     <!-- wp:button {"backgroundColor":"foreground","width":100,"className":"is-style-aegis-3d-button-dark"} -->
                     <div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-aegis-3d-button-dark">
-                        <a class="wp-block-button__link has-foreground-background-color has-background wp-element-button" href="#">' . esc_html__('[Call to Action]', 'aegis') . '</a>
+                        <a class="wp-block-button__link has-foreground-background-color has-background wp-element-button" href="#"><?php echo esc_html_x( 'Call to Action', 'Call to action button text', 'aegis' ); ?></a>
                     </div>
                     <!-- /wp:button -->
                 </div>
@@ -72,18 +88,20 @@ return array(
     </div>
     <!-- /wp:columns -->
 
-    <!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"type":"default"}} -->
-    <div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);padding-top:0;padding-bottom:0">
-        <!-- wp:social-links {"iconColor":"foreground","iconColorValue":"#1c1c1e","openInNewTab":false,"size":"has-small-icon-size","style":{"spacing":{"blockGap":"20px","margin":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"center"}} -->
+    <!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+    <div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
+        <!-- wp:social-links {"iconColor":"foreground","iconColorValue":"#1c1c1e","size":"has-small-icon-size","style":{"spacing":{"blockGap":"20px","margin":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"center"}} -->
         <ul class="wp-block-social-links has-small-icon-size has-icon-color is-style-logos-only" style="margin-top:var(--wp--preset--spacing--30);margin-bottom:var(--wp--preset--spacing--30)">
             <!-- wp:social-link {"url":"#","service":"facebook","label":"Facebook"} /-->
+
             <!-- wp:social-link {"url":"#","service":"meetup","label":"Meetup"} /-->
+
             <!-- wp:social-link {"url":"#","service":"patreon","label":"Patreon"} /-->
+
             <!-- wp:social-link {"url":"#","service":"google","label":"Google"} /-->
         </ul>
         <!-- /wp:social-links -->
     </div>
     <!-- /wp:group -->
 </div>
-<!-- /wp:group -->',
-);
+<!-- /wp:group -->
