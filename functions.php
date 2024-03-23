48f868b0050d74f65c813d6a2afb340057d1caa1
Atmostfear Entertainment <labs@atmostfear-entertainment.com>
2024-03-23 11:00:07 -0500

    Update functions.php
    - Changed: GPL license version.
    - Fixed: Minor capitalization.
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- functions.php
+++ functions.php
@@ -1,26 +1,30 @@
 <?php
 /**
- * This file adds actions, filters, and functions to the Aegis theme.
+ * This file adds actions, filters, and functions to the Aegis block theme.
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
  * @package aegis
  * @author  Atmostfear Entertainment
- * @license GNU General Public License v2 or later
+ * @license GNU General Public License v3 or later
  * @link    https://github.com/atmostfear-entertainment/aegis
  * @since   1.0.0
  */
+
 
 /**
  * Sets up theme defaults and registers support for various WordPress features.
  *
  * @since 1.0.0
- *
  * @return void
  */
+
 function aegis_support() 
 {
-    // Remove WordPress Block Patterns.
+    // Disable WooCommerce Block Patterns.
+    remove_theme_support( 'woocommerce-blocks-patterns' );
+
+    // Disable WordPress Block Patterns.
     remove_theme_support('core-block-patterns');
 
     // Enqueue editor styles.
@@ -28,9 +32,17 @@ function aegis_support()
 }
 add_action('after_setup_theme', 'aegis_support');
 
-/*
+// Disable loading of remote block patterns:
+add_filter( 'should_load_remote_block_patterns', '__return_false' );
+
+
+/**
  * Query whether WooCommerce is activated.
+ * 
+ * @since 1.0.0
+ * @return void
  */
+
 function aegis_is_woocommerce_activated()
 {
     if (class_exists('woocommerce')) {
@@ -40,13 +52,14 @@ function aegis_is_woocommerce_activated()
     }
 }
 
+
 /**
  * Enqueue styles.
  *
  * @since 1.0.0
- *
  * @return void
  */
+
 function aegis_styles()
 {
     wp_enqueue_style('aegis-styles', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get('Version'));
@@ -54,32 +67,88 @@ function aegis_styles()
     // Animations CSS.
     wp_enqueue_style('aegis-animations-styles', get_template_directory_uri() . '/assets/css/animations.css', array(), wp_get_theme()->get('Version'));
 
-    // Global script.
+    // Global Script.
     wp_enqueue_script('aegis-global-script', get_template_directory_uri() . '/assets/js/index.js', array('jquery'), wp_get_theme()->get('Version'), true);
 
-    // Animations script.
+    // Animations Script.
     wp_enqueue_script('aegis-animations-script', get_template_directory_uri() . '/assets/js/animations.js', array('jquery'), wp_get_theme()->get('Version'), true);
 }
+
 add_action('wp_enqueue_scripts', 'aegis_styles');
 
-    // Add block pattern.
+    // Add Block Patterns.
     require get_template_directory() . '/inc/block-patterns.php';
 
-    // Block styles.
+    // Add Block Styles.
     require get_template_directory() . '/inc/block-styles.php';
+
 
 /**
  * Include Woocommerce support.
+ * 
+ * @since 1.0.0
+ * @return void
  */
+
 if (class_exists('Woocommerce')) {
     require get_template_directory() . '/inc/woocommerce/functions.php';
 }
 
-/*
- * Enqueue Dashicons for use with block styles.
+
+/**
+ * Register pattern categories.
+ *
+ * @since 1.0.0
+ * @return void
  */
+
+if ( ! function_exists( 'aegis_pattern_categories' ) ) :
+
+    function aegis_pattern_categories() {
+
+        // Adding events category
+        register_block_pattern_category(
+            'events',
+            array(
+                'label'       => _x( 'Events', 'Block pattern category' ),
+                'description' => __( 'A collection of events patterns.' ),
+            ),
+        );
+
+        // Adding hero category
+        register_block_pattern_category(
+            'hero',
+            array(
+                'label'       => _x( 'Hero', 'Block pattern category' ),
+                'description' => __( 'A collection of hero patterns.' ),
+            ),
+        );
+
+        // Adding video category
+        register_block_pattern_category(
+            'video',
+            array(
+                'label'       => _x( 'Video', 'Block pattern category' ),
+                'description' => __( 'A collection of video patterns.' ),
+            ),
+        );
+    }
+endif;
+
+add_action( 'init', 'aegis_pattern_categories' );
+
+
+/**
+ * Enqueue Dashicons for use with block styles.
+ * 
+ * @since 1.0.0
+ * @return void
+ */
+
 function aegis_enqueue_block_dashicons()
-{
-    wp_enqueue_style('dashicons');
-}
+
+    {
+        wp_enqueue_style('dashicons');
+    }
+
 add_action('enqueue_block_assets', 'aegis_enqueue_block_dashicons');
