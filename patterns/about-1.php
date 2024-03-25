2898e7677def77c4449cc88de03967eb952fa4c6
Atmostfear Entertainment <labs@atmostfear-entertainment.com>
2024-03-25 15:12:44 -0500

    Update about-1.php
    - Changed: Keywords for optimal pattern discovery in search.
    - Changed: Block Pattern Description to make its purpose more clear.
    - Fixed: Minor issues.
    
    Co-Authored-By: Elín Ólafsdóttir <115507350+eelinolafsdottir@users.noreply.github.com>

--- patterns/about-1.php
+++ patterns/about-1.php
@@ -3,8 +3,8 @@
  * Title: 01. About Pattern
  * Slug: aegis/about-1
  * Categories: about
- * Description: This pattern features a tagline, heading, paragraph, media, and button, arranged in a compelling layout to introduce an organization or subject. It employs a diagonal gradient background and wide alignment for media-text blocks, enhancing visual appeal and focus.
- * Keywords: about, tagline, heading, paragraph, media, button, diagonal gradient, wide alignment
+ * Description: Two-column block pattern with media on the left and tagline, headline, paragraph, and button on the right
+ * Keywords: about, buttons, columns, heading, image, media
  * Viewport Width: 1400
  * Block Types: core/group, core/media-text, core/paragraph, core/heading, core/button
  * Inserter: true
@@ -12,16 +12,16 @@
  * @package aegis
  * @since 1.0.0
  */
-?>	
+?>
 
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"gradient":"diagonal-secondary-to-background","layout":{"type":"constrained"},"metadata":{"name":"<?php echo esc_html_x('01. About Pattern', 'Name of the pattern', 'aegis'); ?>"}} -->
 <div class="wp-block-group has-diagonal-secondary-to-background-gradient-background has-background" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
     <!-- wp:media-text {"align":"wide","mediaId":1410,"mediaLink":"<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/thumb_1920x1200_dark.webp","mediaType":"image","imageFill":false} -->
     <div class="wp-block-media-text alignwide is-stacked-on-mobile">
-        <figure class="wp-block-media-text__media"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/thumb_1920x1200_dark.webp" alt="<?php echo esc_attr__('Placeholder image - Replace with an image, photograph, or artwork.', 'aegis'); ?>" class="wp-image-1410 size-full" /></figure>
+        <figure class="wp-block-media-text__media"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/thumb_1920x1200_dark.webp" alt="<?php echo esc_attr__('Placeholder image: Brief description of the image and its context, non-text content for screen readers.', 'aegis'); ?>" class="wp-image-1410 size-full" /></figure>
         <div class="wp-block-media-text__content">
             <!-- wp:paragraph {"align":"left","style":{"typography":{"textTransform":"uppercase","letterSpacing":"3px","fontStyle":"normal","fontWeight":"400"},"spacing":{"margin":{"bottom":"0"}}},"fontSize":"tiny"} -->
-            <p class="has-text-align-left has-tiny-font-size" style="margin-bottom:0;font-style:normal;font-weight:400;letter-spacing:3px;text-transform:uppercase"><?php echo esc_html_e('[Tagline]', 'Replace with a descriptive tagline', 'aegis'); ?></p>
+            <p class="has-text-align-left has-tiny-font-size" style="margin-bottom:0;font-style:normal;font-weight:400;letter-spacing:3px;text-transform:uppercase"><?php echo esc_html_x('[Tagline]', 'Replace with a descriptive tagline', 'aegis'); ?></p>
             <!-- /wp:paragraph -->
 
             <!-- wp:heading {"style":{"typography":{"textTransform":"uppercase"},"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0"}}},"fontSize":"gigantic"} -->
