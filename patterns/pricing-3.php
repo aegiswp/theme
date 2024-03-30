d25f9ba9d5699b65f1b8f3015dd97cd8b8b0ba21
Atmostfear Entertainment <labs@atmostfear-entertainment.com>
2024-03-30 17:36:52 -0500

    Update pricing-3.php
    - Added: Keywords for easier pattern discovery in search.
    - Added: Viewport width for optimal display on responsive devices.
    - Added: Block types to clearly define the pattern's structure.
    - Added: Inserter functionality to control whether the pattern appears in the inserter.
    - Added: Internationalization language strings for broader accessibility.
    - Changed: Renamed aegis-pricing category to pricing for reduced dependency.
    - Refactored: Recoded the pattern to follow the Theme Handbook Block Patterns guidelines.
    - Fixed: Addressed minor inconsistencies in padding, margin, and typography sizes for improved visual consistency.
    - Fixed: Resolved various issues to adhere to Theme Handbook Block Patterns guidelines and best practices.
    - Moved: Relocated pricing-3.php block pattern from /patterns/pricing to /patterns for a more streamlined structure.
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/pricing-3.php
+++ patterns/pricing-3.php
@@ -1,23 +1,31 @@
 <?php
 /**
- * 03. Pricing Block Pattern
+ * Title: 03. Pricing Pattern
+ * Slug: aegis/pricing-3
+ * Categories: pricing
+ * Description: Three-columns pricing columns, with headings, paragraphs, separators and call to action buttons
+ * Keywords: call to action, pricing, plans
+ * Viewport Width: 1400
+ * Block Types: core/group, core/columns, core/column, core/heading, core/paragraph, core/separator, core/button
+ * Inserter: true
+ * 
+ * @package aegis
+ * @since 1.0.0
  */
-return array(
-	'title'	      => __( '03. Pricing Table', 'aegis' ),
-	'description' => __( 'Pricing three-columns, with headings, paragraphs, and call to action buttons', 'aegis' ),
-	'categories'  => array( 'aegis-pricing' ),
-	'content' => '<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"},"metadata":{"name":"' . esc_html__('03. Pricing Pattern', 'aegis') . '"}} -->
-    <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
+?>
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"},"metadata":{"name":"<?php echo esc_html_x('03. Pricing Pattern', 'Name of the pattern', 'aegis'); ?>"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
     <!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
     <div class="wp-block-group alignwide">
         <!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
         <div class="wp-block-group">
             <!-- wp:heading {"textAlign":"center","className":"wp-block-heading","fontSize":"gigantic"} -->
-            <h2 class="wp-block-heading has-text-align-center has-gigantic-font-size">' . esc_html__('Heading', 'aegis') . '</h2>
+            <h2 class="wp-block-heading has-text-align-center has-gigantic-font-size"><?php echo esc_html_x('[Heading]', 'Replace with a descriptive title', 'aegis'); ?></h2>
             <!-- /wp:heading -->
 
             <!-- wp:paragraph {"align":"center"} -->
-            <p class="has-text-align-center">' . esc_html__('Description (133 characters): Introduce the range or essence of the available plans.', 'aegis') . '</p>
+            <p class="has-text-align-center"><?php echo esc_html_x('[Description (133 characters): Provide a brief overview of the selling points of the plans.]', 'Replace with a description of the section', 'aegis'); ?></p>
             <!-- /wp:paragraph -->
         </div>
         <!-- /wp:group -->
@@ -29,11 +37,11 @@ return array(
                 <!-- wp:group {"style":{"spacing":{"blockGap":"4px"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
                 <div class="wp-block-group">
                     <!-- wp:heading {"level":4,"fontSize":"gigantic"} -->
-                    <h4 class="wp-block-heading has-gigantic-font-size">' . esc_html__('Basic', 'aegis') . '</h4>
+                    <h4 class="wp-block-heading has-gigantic-font-size"><?php echo esc_html_x('[Plan]', 'Replace with a descriptive title', 'aegis'); ?></h4>
                     <!-- /wp:heading -->
 
                     <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"tiny"} -->
-                    <p class="has-tiny-font-size" style="font-style:normal;font-weight:400">' . esc_html__('Description (26 characters): Briefly highlight key features or value.', 'aegis') . '</p>
+                    <p class="has-tiny-font-size" style="font-style:normal;font-weight:400"><?php echo esc_html_x('[Description (26 characters): Briefly highlight key features or value.]', 'Replace with a description of the section', 'aegis'); ?></p>
                     <!-- /wp:paragraph -->
                 </div>
                 <!-- /wp:group -->
@@ -45,11 +53,11 @@ return array(
                 <!-- wp:group {"style":{"spacing":{"blockGap":"8px"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
                 <div class="wp-block-group">
                     <!-- wp:heading {"level":5,"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"contrast","fontSize":"gigantic"} -->
-                    <h5 class="wp-block-heading has-contrast-color has-text-color has-gigantic-font-size" style="font-style:normal;font-weight:600">' . esc_html__('$00.00', 'aegis') . '</h5>
+                    <h5 class="wp-block-heading has-contrast-color has-text-color has-gigantic-font-size" style="font-style:normal;font-weight:600"><?php echo esc_html_x('[$00.00]', 'Replace with plan pricing', 'aegis'); ?></h5>
                     <!-- /wp:heading -->
 
                     <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"tiny"} -->
-                    <p class="has-tiny-font-size" style="font-style:normal;font-weight:400">' . esc_html__('or $00.00 [monthly / yearly]', 'aegis') . '</p>
+                    <p class="has-tiny-font-size" style="font-style:normal;font-weight:400"><?php echo esc_html_x('[or $00.00 monthly / yearly]', 'Replace with plan pricing discount', 'aegis'); ?></p>
                     <!-- /wp:paragraph -->
                 </div>
                 <!-- /wp:group -->
@@ -58,7 +66,8 @@ return array(
                 <div class="wp-block-buttons">
                     <!-- wp:button {"backgroundColor":"contrast","textColor":"base","width":100,"className":"is-style-aegis-button-shadow-outline"} -->
                     <div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-aegis-button-shadow-outline">
-                        <a class="wp-block-button__link has-base-color has-contrast-background-color has-text-color has-background wp-element-button">' . esc_html__('[Call to Action]', 'aegis') . '</a></div>
+                        <a class="wp-block-button__link has-base-color has-contrast-background-color has-text-color has-background wp-element-button"><?php echo esc_html_x( '[Call to Action]', 'Call to action button text', 'aegis' ); ?></a>
+                    </div>
                     <!-- /wp:button -->
                 </div>
                 <!-- /wp:buttons -->
@@ -70,11 +79,11 @@ return array(
                 <!-- wp:group {"style":{"spacing":{"blockGap":"14px"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
                 <div class="wp-block-group">
                     <!-- wp:paragraph {"align":"center","style":{"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"tiny"} -->
-                    <p class="has-text-align-center has-tiny-font-size">' . esc_html__('Feature Highlight (33 characters):', 'aegis') . '</p>
+                    <p class="has-text-align-center has-tiny-font-size"><?php echo esc_html_x('[Feature Highlight (33 characters):]', 'Replace with a descriptive call-to-action', 'aegis'); ?></p>
                     <!-- /wp:paragraph -->
 
                     <!-- wp:paragraph {"style":{"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"tiny"} -->
-                    <p class="has-tiny-font-size">' . esc_html__('Describe a specific feature or set of features.', 'aegis') . '</p>
+                    <p class="has-tiny-font-size"><?php echo esc_html_x('[Describe a specific feature or set of features.]', 'Replace with a set of features.', 'aegis'); ?></p>
                     <!-- /wp:paragraph -->
                 </div>
                 <!-- /wp:group -->
@@ -86,7 +95,7 @@ return array(
                 <!-- wp:group {"style":{"border":{"radius":"0px"},"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"spacing":{"padding":{"top":"10px","bottom":"10px"}}},"backgroundColor":"foreground","textColor":"background","layout":{"type":"default"}} -->
                 <div class="wp-block-group has-background-color has-foreground-background-color has-text-color has-background has-link-color" style="border-radius:0px;padding-top:10px;padding-bottom:10px">
                     <!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"base"} -->
-                    <p class="has-text-align-center has-base-color has-text-color" style="font-style:normal;font-weight:600">' . esc_html__('Best Value Plan', 'aegis') . '</p>
+                    <p class="has-text-align-center has-base-color has-text-color" style="font-style:normal;font-weight:600"><?php echo esc_html_x('[Heading]', 'Replace with a descriptive title', 'aegis'); ?></p>
                     <!-- /wp:paragraph -->
                 </div>
                 <!-- /wp:group -->
@@ -96,11 +105,11 @@ return array(
                     <!-- wp:group {"style":{"spacing":{"blockGap":"4px"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
                     <div class="wp-block-group">
                         <!-- wp:heading {"level":4,"fontSize":"gigantic"} -->
-                        <h4 class="wp-block-heading has-gigantic-font-size">' . esc_html__('Expert', 'aegis') . '</h4>
+                        <h4 class="wp-block-heading has-gigantic-font-size"><?php echo esc_html_x('[Plan]', 'Replace with a descriptive title', 'aegis'); ?></h4>
                         <!-- /wp:heading -->
 
                         <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"tiny"} -->
-                        <p class="has-tiny-font-size" style="font-style:normal;font-weight:400">' . esc_html__('Description (26 characters): Briefly highlight key features or value.', 'aegis') . '</p>
+                        <p class="has-tiny-font-size" style="font-style:normal;font-weight:400"><?php echo esc_html_x('[Description (26 characters): Briefly highlight key features or value.]', 'Replace with a description of the section', 'aegis'); ?></p>
                         <!-- /wp:paragraph -->
                     </div>
                     <!-- /wp:group -->
@@ -112,11 +121,11 @@ return array(
                     <!-- wp:group {"style":{"spacing":{"blockGap":"8px"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
                     <div class="wp-block-group">
                         <!-- wp:heading {"level":5,"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"contrast","fontSize":"gigantic"} -->
-                        <h5 class="wp-block-heading has-contrast-color has-text-color has-gigantic-font-size" style="font-style:normal;font-weight:600">' . esc_html__('$00.00', 'aegis') . '</h5>
+                        <h5 class="wp-block-heading has-contrast-color has-text-color has-gigantic-font-size" style="font-style:normal;font-weight:600"><?php echo esc_html_x('[$00.00]', 'Replace with plan pricing', 'aegis'); ?></h5>
                         <!-- /wp:heading -->
 
                         <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"tiny"} -->
-                        <p class="has-tiny-font-size" style="font-style:normal;font-weight:400">' . esc_html__('or $00.00 [monthly / yearly]', 'aegis') . '</p>
+                        <p class="has-tiny-font-size" style="font-style:normal;font-weight:400"><?php echo esc_html_x('[or $00.00 monthly / yearly]', 'Replace with plan pricing discount', 'aegis'); ?></p>
                         <!-- /wp:paragraph -->
                     </div>
                     <!-- /wp:group -->
@@ -125,7 +134,8 @@ return array(
                     <div class="wp-block-buttons">
                         <!-- wp:button {"backgroundColor":"contrast","textColor":"base","width":100,"className":"is-style-aegis-button-shadow"} -->
                         <div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-aegis-button-shadow">
-                            <a class="wp-block-button__link has-base-color has-contrast-background-color has-text-color has-background wp-element-button">' . esc_html__('[Call to Action]', 'aegis') . '</a></div>
+                            <a class="wp-block-button__link has-base-color has-contrast-background-color has-text-color has-background wp-element-button"><?php echo esc_html_x( '[Call to Action]', 'Call to action button text', 'aegis' ); ?></a>
+                        </div>
                         <!-- /wp:button -->
                     </div>
                     <!-- /wp:buttons -->
@@ -137,11 +147,11 @@ return array(
                     <!-- wp:group {"style":{"spacing":{"blockGap":"14px"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
                     <div class="wp-block-group">
                         <!-- wp:paragraph {"align":"center","style":{"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"tiny"} -->
-                        <p class="has-text-align-center has-tiny-font-size">' . esc_html__('Feature Highlight (33 characters):', 'aegis') . '</p>
+                        <p class="has-text-align-center has-tiny-font-size"><?php echo esc_html_x('[Feature Highlight (33 characters):]', 'Replace with a descriptive call-to-action', 'aegis'); ?></p>
                         <!-- /wp:paragraph -->
 
                         <!-- wp:paragraph {"style":{"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"tiny"} -->
-                        <p class="has-tiny-font-size">' . esc_html__('Describe a specific feature or set of features.', 'aegis') . '</p>
+                        <p class="has-tiny-font-size"><?php echo esc_html_x('[Describe a specific feature or set of features.]', 'Replace with a set of features.', 'aegis'); ?></p>
                         <!-- /wp:paragraph -->
                     </div>
                     <!-- /wp:group -->
@@ -155,11 +165,11 @@ return array(
                 <!-- wp:group {"style":{"spacing":{"blockGap":"4px"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
                 <div class="wp-block-group">
                     <!-- wp:heading {"level":4,"fontSize":"gigantic"} -->
-                    <h4 class="wp-block-heading has-gigantic-font-size">' . esc_html__('Starter', 'aegis') . '</h4>
+                    <h4 class="wp-block-heading has-gigantic-font-size"><?php echo esc_html_x('[Plan]', 'Replace with a descriptive title', 'aegis'); ?></h4>
                     <!-- /wp:heading -->
 
                     <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"tiny"} -->
-                    <p class="has-tiny-font-size" style="font-style:normal;font-weight:400">' . esc_html__('Description (26 characters): Briefly highlight key features or value.', 'aegis') . '</p>
+                    <p class="has-tiny-font-size" style="font-style:normal;font-weight:400"><?php echo esc_html_x('[Description (26 characters): Briefly highlight key features or value.]', 'Replace with a description of the section', 'aegis'); ?></p>
                     <!-- /wp:paragraph -->
                 </div>
                 <!-- /wp:group -->
@@ -171,11 +181,11 @@ return array(
                 <!-- wp:group {"style":{"spacing":{"blockGap":"8px"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
                 <div class="wp-block-group">
                     <!-- wp:heading {"level":5,"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"contrast","fontSize":"gigantic"} -->
-                    <h5 class="wp-block-heading has-contrast-color has-text-color has-gigantic-font-size" style="font-style:normal;font-weight:600">' . esc_html__('$00.00', 'aegis') . '</h5>
+                    <h5 class="wp-block-heading has-contrast-color has-text-color has-gigantic-font-size" style="font-style:normal;font-weight:600"><?php echo esc_html_x('[$00.00]', 'Replace with plan pricing', 'aegis'); ?></h5>
                     <!-- /wp:heading -->
 
                     <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}},"fontSize":"tiny"} -->
-                    <p class="has-tiny-font-size" style="font-style:normal;font-weight:400">' . esc_html__('or $00.00 [monthly / yearly]', 'aegis') . '</p>
+                    <p class="has-tiny-font-size" style="font-style:normal;font-weight:400"><?php echo esc_html_x('[or $00.00 monthly / yearly]', 'Replace with plan pricing discount', 'aegis'); ?></p>
                     <!-- /wp:paragraph -->
                 </div>
                 <!-- /wp:group -->
@@ -184,7 +194,8 @@ return array(
                 <div class="wp-block-buttons">
                     <!-- wp:button {"backgroundColor":"contrast","textColor":"base","width":100,"className":"is-style-aegis-button-shadow-outline"} -->
                     <div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-aegis-button-shadow-outline">
-                        <a class="wp-block-button__link has-base-color has-contrast-background-color has-text-color has-background wp-element-button">' . esc_html__('[Call to Action]', 'aegis') . '</a></div>
+                        <a class="wp-block-button__link has-base-color has-contrast-background-color has-text-color has-background wp-element-button"><?php echo esc_html_x( '[Call to Action]', 'Call to action button text', 'aegis' ); ?></a>
+                    </div>
                     <!-- /wp:button -->
                 </div>
                 <!-- /wp:buttons -->
@@ -196,11 +207,11 @@ return array(
                 <!-- wp:group {"style":{"spacing":{"blockGap":"14px"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
                 <div class="wp-block-group">
                     <!-- wp:paragraph {"align":"center","style":{"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"tiny"} -->
-                    <p class="has-text-align-center has-tiny-font-size">' . esc_html__('Feature Highlight (33 characters):', 'aegis') . '</p>
+                    <p class="has-text-align-center has-tiny-font-size"><?php echo esc_html_x('[Feature Highlight (33 characters):]', 'Replace with a descriptive call-to-action', 'aegis'); ?></p>
                     <!-- /wp:paragraph -->
 
                     <!-- wp:paragraph {"style":{"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"tiny"} -->
-                    <p class="has-tiny-font-size">' . esc_html__('Describe a specific feature or set of features.', 'aegis') . '</p>
+                    <p class="has-tiny-font-size"><?php echo esc_html_x('[Describe a specific feature or set of features.]', 'Replace with a set of features.', 'aegis'); ?></p>
                     <!-- /wp:paragraph -->
                 </div>
                 <!-- /wp:group -->
@@ -211,5 +222,4 @@ return array(
     </div>
     <!-- /wp:group -->
 </div>
-<!-- /wp:group -->',
-);
+<!-- /wp:group -->
