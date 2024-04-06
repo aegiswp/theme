8a75f744481622c532e5adfae9cf8b34dbe1b7a3
Atmostfear Entertainment <labs@atmostfear-entertainment.com>
2024-04-06 08:47:13 -0500

    Update query-8.php
    - Added: Keywords for easier pattern discovery in search.
    - Added: Viewport width for optimal display on responsive devices.
    - Added: Block types to clearly define the pattern's structure.
    - Added: Inserter functionality to control whether the pattern appears in the inserter.
    - Added: Internationalization language strings for broader accessibility.
    - Changed: Renamed query-8.php to blog-grid-3.php for better understanding.
    - Changed: Renamed aegis-query category to blog for reduced dependency.
    - Changed: Recoded the pattern to follow the Theme Handbook Block Patterns guidelines.
    - Fixed: Addressed minor inconsistencies in padding, margin, and typography sizes for improved visual consistency.
    - Fixed: Resolved various issues to adhere to Theme Handbook Block Patterns guidelines and best practices.
    - Moved: Relocated query-8.php block pattern from /patterns/query to /patterns for a more streamlined structure.
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/blog-grid-3.php
+++ patterns/blog-grid-3.php
@@ -1,21 +1,29 @@
 <?php
 /**
- * 08. Blog Block Pattern
+ * Title: 03. Blog Grid Pattern
+ * Slug: aegis/blog-grid-3
+ * Categories: blog
+ * Description: Displays a three-column grid of blog postings with cover images, titles, excerpts, and dates
+ * Keywords: blog, featured, grid, posts
+ * Viewport Width: 1400
+ * Block Types: core/group, core/query, core/post-template, core/post-featured-image, core/post-date, core/post-title, core/post-excerpt
+ * Inserter: true
+ * 
+ * @package aegis
+ * @since Aegis 1.0.0
  */
-return array(
-	'title'	  => __( '08. Blog Pattern', 'aegis' ),
-	'description' => __( 'Blog Posts Grid', 'aegis' ),
-	'categories' => array( 'aegis-query' ),
-	'content' => '<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"},"metadata":{"name":"' . esc_html__('08. Blog Pattern', 'aegis') . '"}} -->
-    <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
+?>
+
+<!-- wp:group {"metadata":{"name":"<?php echo esc_html_x('03. Blog Grid Pattern', 'Name of the pattern', 'aegis'); ?>"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
     <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"default"}} -->
     <div class="wp-block-group alignfull" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
-        <!-- wp:query {"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"layout":{"inherit":false}} -->
+        <!-- wp:query {"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"className":"alignwide","layout":{"inherit":false}} -->
         <div class="wp-block-query alignwide">
             <!-- wp:post-template {"align":"wide","layout":{"type":"grid","columnCount":3}} -->
-            <!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"minHeight":270,"style":{"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"className":"is-style-default","layout":{"type":"constrained"}} -->
+            <!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"customOverlayColor":"#222222","isUserOverlayColor":true,"minHeight":270,"style":{"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"className":"is-style-default","layout":{"type":"constrained"}} -->
             <div class="wp-block-cover is-style-default" style="padding-right:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30);min-height:270px">
-                <span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span>
+                <span aria-hidden="true" class="wp-block-cover__background has-background-dim" style="background-color:#222222"></span>
                 <div class="wp-block-cover__inner-container">
                     <!-- wp:post-title {"isLink":true,"style":{"elements":{"link":{"color":{"text":"var:preset|color|background"},":hover":{"color":{"text":"var:preset|color|secondary"}}}},"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"background","className":"is-style-aegis-post-title-border","fontSize":"large"} /-->
                 </div>
@@ -35,5 +43,4 @@ return array(
     </div>
     <!-- /wp:group -->
 </div>
-<!-- /wp:group -->',
-);
+<!-- /wp:group -->
