c52915079151291fe19fa5ca190a99ced6d5ad41
Atmostfear Entertainment <labs@atmostfear-entertainment.com>
2024-04-06 09:11:20 -0500

    Update query-9.php
    - Added: Keywords for easier pattern discovery in search.
    - Added: Viewport width for optimal display on responsive devices.
    - Added: Block types to clearly define the pattern's structure.
    - Added: Inserter functionality to control whether the pattern appears in the inserter.
    - Added: Internationalization language strings for broader accessibility.
    - Changed: Renamed query-9.php to blog-grid-4.php for better understanding.
    - Changed: Renamed aegis-query category to blog for reduced dependency.
    - Changed: Recoded the pattern to follow the Theme Handbook Block Patterns guidelines.
    - Fixed: Addressed minor inconsistencies in padding, margin, and typography sizes for improved visual consistency.
    - Fixed: Resolved various issues to adhere to Theme Handbook Block Patterns guidelines and best practices.
    - Moved: Relocated query-9.php block pattern from /patterns/query to /patterns for a more streamlined structure.
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/blog-grid-4.php
+++ patterns/blog-grid-4.php
@@ -1,16 +1,24 @@
 <?php
 /**
- * 09. Blog Block Pattern
+ * Title: 04. Blog Grid Pattern
+ * Slug: aegis/blog-grid-4
+ * Categories: blog
+ * Description: Displays a three-column grid of blog postings with borders, titles, featurede images, excerpts, and call to action buttons
+ * Keywords: blog, featured, grid, posts
+ * Viewport Width: 1400
+ * Block Types: core/group, core/query, core/post-template, core/post-featured-image, core/post-date, core/post-title, core/post-excerpt
+ * Inserter: true
+ * 
+ * @package aegis
+ * @since Aegis 1.0.0
  */
-return array(
-    'title'      => __('09. Blog Pattern', 'aegis'),
-    'description' => __('Blog Posts Grid', 'aegis'),
-    'categories' => array('aegis-query'),
-    'content' => '<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"},"metadata":{"name":"' . esc_html__('09. Blog Pattern', 'aegis') . '"}} -->
-    <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
+?>
+
+<!-- wp:group {"metadata":{"name":"<?php echo esc_html_x('04. Blog Grid Pattern', 'Name of the pattern', 'aegis'); ?>"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
     <!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
     <div class="wp-block-group alignwide">
-        <!-- wp:query {"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"layout":{"inherit":false}} -->
+        <!-- wp:query {"query":{"perPage":10,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"className":"alignwide","layout":{"inherit":false}} -->
         <div class="wp-block-query alignwide">
             <!-- wp:post-template {"align":"wide","layout":{"type":"grid","columnCount":3}} -->
             <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"},"margin":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}},"border":{"width":"1px"}},"borderColor":"foreground","backgroundColor":"secondary","className":"negative-margin is-style-default","layout":{"type":"default"}} -->
@@ -28,5 +36,4 @@ return array(
     </div>
     <!-- /wp:group -->
 </div>
-<!-- /wp:group -->',
-);
+<!-- /wp:group -->
