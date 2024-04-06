a5a869c51591e89e9845ea3d754fc3af06dd4702
Atmostfear Entertainment <labs@atmostfear-entertainment.com>
2024-04-06 12:01:55 -0500

    Rename blog-4.php to blog-archive-sidebar-2.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/blog-archive-sidebar-2.php
+++ patterns/blog-archive-sidebar-2.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Title: 04. Blog Pattern
- * Slug: aegis/blog-4
+ * Title: 02. Blog Archive Sidebar Pattern
+ * Slug: aegis/blog-archive-sidebar-2
  * Categories: blog
  * Description: Display a list of blog entries with a left sidebar that includes titles, featured images, excerpts with shadow, and pagination on the right
  * Keywords: blog, posts, pagination, featured image, excerpt
@@ -14,7 +14,7 @@
  */
 ?>
 
-<!-- wp:group {"metadata":{"name":"<?php echo esc_html_x('04. Blog Pattern', 'Name of the pattern', 'aegis'); ?>"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
+<!-- wp:group {"metadata":{"name":"<?php echo esc_html_x('02. Blog Archive Sidebar Pattern', 'Name of the pattern', 'aegis'); ?>"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
     <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"right":"0","left":"0"}}},"layout":{"inherit":true,"type":"constrained"}} -->
     <div class="wp-block-group alignwide" style="padding-right:0;padding-left:0">
