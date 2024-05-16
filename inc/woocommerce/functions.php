39cda06ea4b451ffdd9b8814bd8e0546cd4344af
Atmostfear Entertainment <labs@atmostfear-entertainment.com>
2024-05-16 11:17:53 -0500

    Update functions.php
    - Modified: Changed the number of products per row in the shop loop:
      - Old: `return 3; // 3 products per row.`
      - New: `return 4; // 4 products per row.`

--- inc/woocommerce/functions.php
+++ inc/woocommerce/functions.php
@@ -17,12 +17,13 @@ add_theme_support('woocommerce', array(
 	'single_image_width'    => 1400,
 	)
 );
+
 /**
- * Change number or products per row to 3.
+ * Change number or products per row to 4.
  */
 add_filter('loop_shop_columns', 'loop_columns', 999);
 if (!function_exists('loop_columns')) {
 	function loop_columns() {
-		return 3; // 3 products per row
+		return 4; // 4 products per row.
 	}
 }
