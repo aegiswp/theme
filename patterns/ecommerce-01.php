64c067201c8da8afa4ecce753f2252c87235938e
alexdeborba <labs@atmostfear-entertainment.com>
2024-09-17 07:36:08 -0500

    Update ecommerce-01.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/ecommerce-01.php
+++ patterns/ecommerce-01.php
@@ -18,42 +18,6 @@
 <div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--30)">
     <!-- wp:columns {"align":"wide"} -->
     <div class="wp-block-columns alignwide">
-        <!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"backgroundColor":"secondary","className":"is-style-default"} -->
-        <div class="wp-block-column is-style-default has-secondary-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
-            <!-- wp:image {"align":"center","aspectRatio":"3/4","scale":"cover","className":"size-large is-style-default"} -->
-            <figure class="wp-block-image aligncenter size-large is-style-default"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/thumb_1200x1920_dark.webp" alt="<?php echo esc_attr__('Add a brief description of the placeholder image and its context, non-text content for screen readers.', 'aegis'); ?>" style="aspect-ratio:3/4;object-fit:cover" /></figure>
-            <!-- /wp:image -->
-
-            <!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-            <div class="wp-block-group" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
-                <!-- wp:paragraph {"style":{"typography":{"letterSpacing":"2px"}},"fontSize":"tiny"} -->
-                <p class="has-tiny-font-size" style="letter-spacing:2px"><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-half"></span><span class="dashicons dashicons-star-empty"></span></p>
-                <!-- /wp:paragraph -->
-
-                <!-- wp:paragraph -->
-                <p><strong><?php echo esc_html_e('$00.00', 'aegis'); ?></strong></p>
-                <!-- /wp:paragraph -->
-            </div>
-            <!-- /wp:group -->
-
-            <!-- wp:heading {"level":3,"className":"has-large-font-size"} -->
-            <h3 class="wp-block-heading has-large-font-size"><?php echo esc_html_e('[Product]', 'aegis'); ?></h3>
-            <!-- /wp:heading -->
-
-            <!-- wp:paragraph -->
-            <p><?php echo esc_html_x('[Description (50-150 characters): Enter a brief overview of a product.]', 'Replace with a product description.', 'aegis'); ?></p>
-            <!-- /wp:paragraph -->
-
-            <!-- wp:buttons {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-            <div class="wp-block-buttons">
-                <!-- wp:button {"className":"is-style-aegis-button-shadow"} -->
-                <div class="wp-block-button is-style-aegis-button-shadow"><a class="wp-block-button__link wp-element-button"><?php echo esc_html_x( '[Call to Action]', 'Call to action button text.', 'aegis' ); ?></a></div>
-                <!-- /wp:button -->
-            </div>
-            <!-- /wp:buttons -->
-        </div>
-        <!-- /wp:column -->
-
         <!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"backgroundColor":"tertiary","className":"is-style-default"} -->
         <div class="wp-block-column is-style-default has-tertiary-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
             <!-- wp:image {"align":"center","aspectRatio":"3/4","scale":"cover","className":"size-large is-style-default"} -->
@@ -82,8 +46,44 @@
 
             <!-- wp:buttons {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
             <div class="wp-block-buttons">
-                <!-- wp:button {"backgroundColor":"foreground","textColor":"background","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"className":"is-style-aegis-button-shadow-outline"} -->
-                <div class="wp-block-button is-style-aegis-button-shadow-outline"><a class="wp-block-button__link has-background-color has-foreground-background-color has-text-color has-background has-link-color wp-element-button"><?php echo esc_html_x( '[Call to Action]', 'Call to action button text.', 'aegis' ); ?></a></div>
+                <!-- wp:button {"className":"is-style-dense-shadow"} -->
+                <div class="wp-block-button is-style-dense-shadow"><a class="wp-block-button__link wp-element-button"><?php echo esc_html_x( '[Call to Action]', 'Call to action button text.', 'aegis' ); ?></a></div>
+                <!-- /wp:button -->
+            </div>
+            <!-- /wp:buttons -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"backgroundColor":"quinary","className":"is-style-default"} -->
+        <div class="wp-block-column is-style-default has-quinary-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
+            <!-- wp:image {"align":"center","aspectRatio":"3/4","scale":"cover","className":"size-large is-style-default"} -->
+            <figure class="wp-block-image aligncenter size-large is-style-default"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/thumb_1200x1920_dark.webp" alt="<?php echo esc_attr__('Add a brief description of the placeholder image and its context, non-text content for screen readers.', 'aegis'); ?>" style="aspect-ratio:3/4;object-fit:cover" /></figure>
+            <!-- /wp:image -->
+
+            <!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+            <div class="wp-block-group" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
+                <!-- wp:paragraph {"style":{"typography":{"letterSpacing":"2px"}},"fontSize":"tiny"} -->
+                <p class="has-tiny-font-size" style="letter-spacing:2px"><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-filled"></span><span class="dashicons dashicons-star-half"></span><span class="dashicons dashicons-star-empty"></span></p>
+                <!-- /wp:paragraph -->
+
+                <!-- wp:paragraph -->
+                <p><strong><?php echo esc_html_e('$00.00', 'aegis'); ?></strong></p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+
+            <!-- wp:heading {"level":3,"className":"has-large-font-size"} -->
+            <h3 class="wp-block-heading has-large-font-size"><?php echo esc_html_e('[Product]', 'aegis'); ?></h3>
+            <!-- /wp:heading -->
+
+            <!-- wp:paragraph -->
+            <p><?php echo esc_html_x('[Description (50-150 characters): Enter a brief overview of a product.]', 'Replace with a product description.', 'aegis'); ?></p>
+            <!-- /wp:paragraph -->
+
+            <!-- wp:buttons {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+            <div class="wp-block-buttons">
+                <!-- wp:button {"backgroundColor":"foreground","textColor":"background","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"className":"is-style-dense-shadow-outline"} -->
+                <div class="wp-block-button is-style-dense-shadow-outline"><a class="wp-block-button__link has-background-color has-foreground-background-color has-text-color has-background has-link-color wp-element-button"><?php echo esc_html_x( '[Call to Action]', 'Call to action button text.', 'aegis' ); ?></a></div>
                 <!-- /wp:button -->
             </div>
             <!-- /wp:buttons -->
@@ -118,8 +118,8 @@
 
             <!-- wp:buttons {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
             <div class="wp-block-buttons">
-                <!-- wp:button {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"className":"is-style-aegis-button-shadow-outline","fontSize":"small"} -->
-                <div class="wp-block-button has-custom-font-size is-style-aegis-button-shadow-outline has-small-font-size" style="font-style:normal;font-weight:600">
+                <!-- wp:button {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"className":"is-style-dense-shadow-outline","fontSize":"small"} -->
+                <div class="wp-block-button has-custom-font-size is-style-dense-shadow-outline has-small-font-size" style="font-style:normal;font-weight:600">
                 <a class="wp-block-button__link wp-element-button"><?php echo esc_html_x( '[Call to Action]', 'Call to action button text.', 'aegis' ); ?></a>
                 </div>
                 <!-- /wp:button -->
