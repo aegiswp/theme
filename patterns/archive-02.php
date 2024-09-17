acaa3facdf83373679797e7c7a3cfd7d0d4aa8c6
alexdeborba <labs@atmostfear-entertainment.com>
2024-09-17 04:40:17 -0500

    Update archive-02.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/archive-02.php
+++ patterns/archive-02.php
@@ -14,7 +14,7 @@
  */
 ?>
 
-<!-- wp:group {"metadata":{"name":"<?php echo esc_html_x('01. Archive Pattern', 'Name of the pattern', 'aegis'); ?>","categories":["blog"],"patternName":"aegis/archive-02"},"layout":{"type":"constrained"}} -->
+<!-- wp:group {"metadata":{"name":"<?php echo esc_html_x('02. Archive Pattern', 'Name of the pattern', 'aegis'); ?>","categories":["blog"],"patternName":"aegis/archive-02"},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group">
     <!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left","orientation":"vertical"}} -->
     <div class="wp-block-group alignwide">
