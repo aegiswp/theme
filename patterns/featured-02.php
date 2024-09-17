29807fbb9eea14be3f63a06b8ee6bb78e98c9e8f
alexdeborba <labs@atmostfear-entertainment.com>
2024-09-17 14:21:49 -0500

    Update featured-02.php
    - Fixed: Minor styling adjustments.
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/featured-02.php
+++ patterns/featured-02.php
@@ -14,32 +14,32 @@
  */
 ?>
 
-<!-- wp:group {"metadata":{"name":"<?php echo esc_html_x('02. Featured Pattern', 'Name of the pattern', 'aegis'); ?>"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","right":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"gradient":"vertical-background-to-foreground","layout":{"type":"constrained","contentSize":""}} -->
-<div class="wp-block-group has-vertical-background-to-foreground-gradient-background has-background" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--30)">
+<!-- wp:group {"metadata":{"name":"<?php echo esc_html_x('02. Featured Pattern', 'Name of the pattern', 'aegis'); ?>"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","right":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"gradient":"vertical-large-background-to-foreground","layout":{"type":"constrained","contentSize":""}} -->
+<div class="wp-block-group has-vertical-large-background-to-foreground-gradient-background has-background" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--30)">
     <!-- wp:columns {"verticalAlignment":"center","align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|30","left":"var:preset|spacing|30"}}}} -->
     <div class="wp-block-columns alignwide are-vertically-aligned-center">
         <!-- wp:column {"verticalAlignment":"center"} -->
         <div class="wp-block-column is-vertically-aligned-center">
             <!-- wp:columns -->
             <div class="wp-block-columns">
-                <!-- wp:column {"width":"7%","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"backgroundColor":"foreground","textColor":"background","className":"is-hidden-on-mobile"} -->
-                <div class="wp-block-column is-hidden-on-mobile has-background-color has-foreground-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30);flex-basis:7%">
+                <!-- wp:column {"width":"7%","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"backgroundColor":"foreground","textColor":"background","className":"is-style-hide-mobile"} -->
+                <div class="wp-block-column is-style-hide-mobile has-background-color has-foreground-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30);flex-basis:7%">
                     <!-- wp:paragraph {"align":"left","style":{"elements":{"link":{"color":{"text":"var:preset|color|foregound-alt"}}},"typography":{"fontStyle":"normal","fontWeight":"600","writingMode":"vertical-rl"}},"textColor":"foregound-alt","fontSize":"huge"} -->
                     <p class="has-text-align-left has-foregound-alt-color has-text-color has-link-color has-huge-font-size" style="font-style:normal;font-weight:600;writing-mode:vertical-rl"><?php echo esc_html_x('[#]', 'Replace with the number of the feature', 'aegis'); ?></p>
                     <!-- /wp:paragraph -->
                 </div>
                 <!-- /wp:column -->
 
-                <!-- wp:column {"width":"77.77%","style":{"spacing":{"blockGap":"0","padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"backgroundColor":"secondary"} -->
-                <div class="wp-block-column has-secondary-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30);flex-basis:77.77%">
+                <!-- wp:column {"width":"77.77%","style":{"spacing":{"blockGap":"0","padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"backgroundColor":"tertiary"} -->
+                <div class="wp-block-column has-tertiary-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30);flex-basis:77.77%">
                     <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical"}} -->
                     <div class="wp-block-group">
                         <!-- wp:heading {"level":4,"style":{"elements":{"link":{"color":{"text":"var:preset|color|foregound-alt"}}}},"textColor":"foregound-alt"} -->
                         <h4 class="wp-block-heading has-foregound-alt-color has-text-color has-link-color"><?php echo esc_html_x('[Heading]', 'Replace with a descriptive section title.', 'aegis'); ?></h4>
                         <!-- /wp:heading -->
 
-                        <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|tertiary-light"}}}},"textColor":"tertiary-light"} -->
-                        <p class="has-tertiary-light-color has-text-color has-link-color"><?php echo esc_html_x('[Description (55 characters): Enter a brief overview of a feature.]', 'Call to action section text', 'aegis'); ?></p>
+                        <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|quinary-light"}}}},"textColor":"quinary-light"} -->
+                        <p class="has-quinary-light-color has-text-color has-link-color"><?php echo esc_html_x('[Description (55 characters): Enter a brief overview of a feature.]', 'Call to action section text', 'aegis'); ?></p>
                         <!-- /wp:paragraph -->
                     </div>
                     <!-- /wp:group -->
@@ -50,8 +50,8 @@
 
             <!-- wp:columns -->
             <div class="wp-block-columns">
-                <!-- wp:column {"width":"7%","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"backgroundColor":"secondary","className":"is-hidden-on-mobile"} -->
-                <div class="wp-block-column is-hidden-on-mobile has-secondary-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30);flex-basis:7%">
+                <!-- wp:column {"width":"7%","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"backgroundColor":"tertiary","className":"is-style-hide-mobile"} -->
+                <div class="wp-block-column is-style-hide-mobile has-tertiary-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30);flex-basis:7%">
                     <!-- wp:paragraph {"align":"left","style":{"elements":{"link":{"color":{"text":"var:preset|color|foregound-alt"}}},"typography":{"fontStyle":"normal","fontWeight":"600","writingMode":"vertical-rl"}},"textColor":"foregound-alt","fontSize":"huge"} -->
                     <p class="has-text-align-left has-foregound-alt-color has-text-color has-link-color has-huge-font-size" style="font-style:normal;font-weight:600;writing-mode:vertical-rl"><?php echo esc_html_x('[#]', 'Replace with the number of the feature', 'aegis'); ?></p>
                     <!-- /wp:paragraph -->
@@ -66,8 +66,8 @@
                         <h4 class="wp-block-heading has-foregound-alt-color has-text-color has-link-color"><?php echo esc_html_x('[Heading]', 'Replace with a descriptive section title.', 'aegis'); ?></h4>
                         <!-- /wp:heading -->
 
-                        <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|tertiary-light"}}}},"textColor":"tertiary-light"} -->
-                        <p class="has-tertiary-light-color has-text-color has-link-color"><?php echo esc_html_x('[Description (55 characters): Enter a brief overview of a feature.]', 'Call to action section text', 'aegis'); ?></p>
+                        <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|quinary-light"}}}},"textColor":"quinary-light"} -->
+                        <p class="has-quinary-light-color has-text-color has-link-color"><?php echo esc_html_x('[Description (55 characters): Enter a brief overview of a feature.]', 'Call to action section text', 'aegis'); ?></p>
                         <!-- /wp:paragraph -->
                     </div>
                     <!-- /wp:group -->
@@ -78,24 +78,24 @@
 
             <!-- wp:columns -->
             <div class="wp-block-columns">
-                <!-- wp:column {"width":"7%","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"backgroundColor":"tertiary","className":"is-hidden-on-mobile"} -->
-                <div class="wp-block-column is-hidden-on-mobile has-tertiary-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30);flex-basis:7%">
+                <!-- wp:column {"width":"7%","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"backgroundColor":"quinary","className":"is-style-hide-mobile"} -->
+                <div class="wp-block-column is-style-hide-mobile has-quinary-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30);flex-basis:7%">
                     <!-- wp:paragraph {"align":"left","style":{"elements":{"link":{"color":{"text":"var:preset|color|foregound-alt"}}},"typography":{"fontStyle":"normal","fontWeight":"600","writingMode":"vertical-rl"}},"textColor":"foregound-alt","fontSize":"huge"} -->
                     <p class="has-text-align-left has-foregound-alt-color has-text-color has-link-color has-huge-font-size" style="font-style:normal;font-weight:600;writing-mode:vertical-rl"><?php echo esc_html_x('[#]', 'Replace with the number of the feature', 'aegis'); ?></p>
                     <!-- /wp:paragraph -->
                 </div>
                 <!-- /wp:column -->
 
-                <!-- wp:column {"width":"77.77%","style":{"spacing":{"blockGap":"0","padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"backgroundColor":"secondary"} -->
-                <div class="wp-block-column has-secondary-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30);flex-basis:77.77%">
+                <!-- wp:column {"width":"77.77%","style":{"spacing":{"blockGap":"0","padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"backgroundColor":"tertiary"} -->
+                <div class="wp-block-column has-tertiary-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30);flex-basis:77.77%">
                     <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical"}} -->
                     <div class="wp-block-group">
                         <!-- wp:heading {"level":4,"style":{"elements":{"link":{"color":{"text":"var:preset|color|foregound-alt"}}}},"textColor":"foregound-alt"} -->
                         <h4 class="wp-block-heading has-foregound-alt-color has-text-color has-link-color"><?php echo esc_html_x('[Heading]', 'Replace with a descriptive section title.', 'aegis'); ?></h4>
                         <!-- /wp:heading -->
 
-                        <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|tertiary-light"}}}},"textColor":"tertiary-light"} -->
-                        <p class="has-tertiary-light-color has-text-color has-link-color"><?php echo esc_html_x('[Description (55 characters): Enter a brief overview of a feature.]', 'Call to action section text', 'aegis'); ?></p>
+                        <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|quinary-light"}}}},"textColor":"quinary-light"} -->
+                        <p class="has-quinary-light-color has-text-color has-link-color"><?php echo esc_html_x('[Description (55 characters): Enter a brief overview of a feature.]', 'Call to action section text', 'aegis'); ?></p>
                         <!-- /wp:paragraph -->
                     </div>
                     <!-- /wp:group -->
@@ -109,7 +109,7 @@
         <!-- wp:column {"verticalAlignment":"center"} -->
         <div class="wp-block-column is-vertically-aligned-center">
             <!-- wp:image {"aspectRatio":"3/4","scale":"cover","sizeSlug":"large","linkDestination":"none"} -->
-            <figure class="wp-block-image size-large"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/thumb_1200x1920_dark.webp" alt="<?php echo esc_attr__('Add a brief description of the placeholder image and its context, non-text content for screen readers.', 'aegis'); ?>" style="aspect-ratio:3/4;object-fit:cover" /></figure>
+            <figure class="wp-block-image size-large"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/thumb_1200x1920_dark.webp" alt="<?php echo esc_attr__('Add a brief description of the placeholder image and its context, non-text content for screen readers', 'aegis'); ?>" style="aspect-ratio:3/4;object-fit:cover" /></figure>
             <!-- /wp:image -->
         </div>
         <!-- /wp:column -->
