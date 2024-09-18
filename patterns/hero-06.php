c406108eadfe9cffb3758c593aa0adfdc3ab3e11
alexdeborba <labs@atmostfear-entertainment.com>
2024-09-18 10:23:19 -0500

    Update hero-06.php
    - Fixed: Code improvements.
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/hero-06.php
+++ patterns/hero-06.php
@@ -14,8 +14,8 @@
  */
 ?>
 
-<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","right":"var:preset|spacing|30","left":"var:preset|spacing|30"},"margin":{"top":"0","bottom":"0"}}},"gradient":"vertical-background-to-foreground","className":"has-no-gradient-on-mobile","layout":{"type":"default"},"metadata":{"name":"<?php echo esc_html_x('06. Hero Pattern', 'Name of the pattern', 'aegis'); ?>"}} -->
-<div class="wp-block-group alignwide has-no-gradient-on-mobile has-vertical-background-to-foreground-gradient-background has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--30)">
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","right":"var:preset|spacing|30","left":"var:preset|spacing|30"},"margin":{"top":"0","bottom":"0"}}},"gradient":"vertical-large-background-to-foreground","className":"has-no-gradient-on-mobile","layout":{"type":"default"},"metadata":{"name":"<?php echo esc_html_x('06. Hero Pattern', 'Name of the pattern', 'aegis'); ?>"}} -->
+<div class="wp-block-group alignwide has-no-gradient-on-mobile has-vertical-large-background-to-foreground-gradient-background has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--30)">
     <!-- wp:columns {"align":"wide"} -->
     <div class="wp-block-columns alignwide">
         <!-- wp:column {"verticalAlignment":"center","width":""} -->
@@ -30,8 +30,8 @@
 
             <!-- wp:buttons {"layout":{"type":"flex","flexWrap":"wrap"}} -->
             <div class="wp-block-buttons">
-                <!-- wp:button {"className":"is-style-aegis-button-shadow"} -->
-                <div class="wp-block-button is-style-aegis-button-shadow"><a class="wp-block-button__link wp-element-button" href="#"><?php echo esc_html_x( '[Call to Action]', 'Call to action button text.', 'aegis' ); ?></a></div>
+                <!-- wp:button {"className":"is-style-dense-shadow"} -->
+                <div class="wp-block-button is-style-dense-shadow"><a class="wp-block-button__link wp-element-button" href="#"><?php echo esc_html_x( '[Call to Action]', 'Call to action button text.', 'aegis' ); ?></a></div>
                 <!-- /wp:button -->
             </div>
             <!-- /wp:buttons -->
@@ -40,8 +40,8 @@
 
         <!-- wp:column {"width":"60%","style":{"spacing":{"padding":{"top":"0","bottom":"0"}}}} -->
         <div class="wp-block-column" style="padding-top:0;padding-bottom:0;flex-basis:60%">
-            <!-- wp:video {"className":"is-style-aegis-shadow"} -->
-            <figure class="wp-block-video is-style-aegis-shadow"><video controls poster="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/thumb_1920x1200_dark.webp" preload="auto" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/videos/sample.mp4" playsinline></video></figure>
+            <!-- wp:video {"className":"is-style-dark-shadow"} -->
+            <figure class="wp-block-video is-style-dark-shadow"><video controls poster="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/thumb_1920x1200_dark.webp" preload="auto" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/videos/sample.mp4" playsinline></video></figure>
             <!-- /wp:video -->
         </div>
         <!-- /wp:column -->
