f128d9dde2f88f56d22cb665fde254798ca87dbb
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2025-02-24 02:28:59 -0500

    Update hero-03.php
    Fixed: Minor code formatting.
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/hero-03.php
+++ patterns/hero-03.php
@@ -23,11 +23,15 @@
             <!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
             <div class="wp-block-group">
                 <!-- wp:paragraph {"align":"left","metadata":{"name":"Tagline"},"style":{"typography":{"textTransform":"uppercase","letterSpacing":"3px","fontStyle":"normal","fontWeight":"400"},"spacing":{"margin":{"bottom":"-30px"}}},"fontSize":"tiny"} -->
-                <p class="has-text-align-left has-tiny-font-size" style="margin-bottom:-30px;font-style:normal;font-weight:400;letter-spacing:3px;text-transform:uppercase"><?php echo esc_html_x( 'Tagline', 'Enter a brief and descriptive tagline here.', 'aegis' ); ?></p>
+                <p class="has-text-align-left has-tiny-font-size" style="margin-bottom:-30px;font-style:normal;font-weight:400;letter-spacing:3px;text-transform:uppercase">
+                    <?php echo esc_html_x( 'Tagline', 'Enter a brief and descriptive tagline here.', 'aegis' ); ?>
+                </p>
                 <!-- /wp:paragraph -->
 
                 <!-- wp:heading {"style":{"typography":{"textTransform":"uppercase"},"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0"}}},"fontSize":"gigantic"} -->
-                <h2 class="wp-block-heading has-gigantic-font-size" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;text-transform:uppercase"><?php echo esc_html_x( 'Heading', 'Enter a compelling headline for this section.', 'aegis' ); ?></h2>
+                <h2 class="wp-block-heading has-gigantic-font-size" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;text-transform:uppercase">
+                    <?php echo esc_html_x( 'Heading', 'Enter a compelling headline for this section.', 'aegis' ); ?>
+                </h2>
                 <!-- /wp:heading -->
 
                 <!-- wp:paragraph {"align":"center","style":{"layout":{"selfStretch":"fixed","flexSize":"50%"},"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"0","bottom":"0"}}}} -->
@@ -47,38 +51,9 @@
                         <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","allowOrientation":false,"justifyContent":"space-between","flexWrap":"wrap"}} -->
                         <div class="wp-block-group">
                             <!-- wp:image {"width":"40px","sizeSlug":"full","linkDestination":"none","isDecorative":true} -->
-                            <figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/icons/laurel_left.svg" alt="<?php echo esc_attr__('This image is decorative', 'aegis'); ?>" style="width:40px" /></figure>
-                            <!-- /wp:image -->
-
-                            <!-- wp:group -->
-                            <div class="wp-block-group">
-                                <!-- wp:paragraph {"align":"left","style":{"typography":{"textTransform":"uppercase","letterSpacing":"3px","fontStyle":"normal","fontWeight":"600","lineHeight":"1"}},"fontSize":"small"} -->
-                                <p class="has-text-align-left has-small-font-size" style="font-style:normal;font-weight:600;letter-spacing:3px;line-height:1;text-transform:uppercase"><?php echo esc_html_x('Category', 'Replace with a descriptive category.', 'aegis'); ?>
-                                </p>
-                                <!-- /wp:paragraph -->
-
-                                <!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"small"} -->
-                                <p class="has-text-align-center has-small-font-size" style="margin-top:0">
-                                    <?php echo esc_html_x('Nominee', 'Replace with the name of a nominee.', 'aegis'); ?>
-                                </p>
-                                <!-- /wp:paragraph -->
-                            </div>
-                            <!-- /wp:group -->
-
-                            <!-- wp:image {"width":"40px","sizeSlug":"full","linkDestination":"none","isDecorative":true} -->
-                            <figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/icons/laurel_right.svg" alt="<?php echo esc_attr__('This image is decorative', 'aegis'); ?>" style="width:40px" /></figure>
-                            <!-- /wp:image -->
-                        </div>
-                        <!-- /wp:group -->
-                    </div>
-                    <!-- /wp:column -->
-
-                    <!-- wp:column {"verticalAlignment":"center","width":"22%","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
-                    <div class="wp-block-column is-vertically-aligned-center" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;flex-basis:22%">
-                        <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","allowOrientation":false,"justifyContent":"space-between","flexWrap":"wrap"}} -->
-                        <div class="wp-block-group">
-                            <!-- wp:image {"width":"40px","sizeSlug":"full","linkDestination":"none","isDecorative":true} -->
-                            <figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/icons/laurel_left.svg" alt="<?php echo esc_attr__('This image is decorative', 'aegis'); ?>" style="width:40px" /></figure>
+                            <figure class="wp-block-image size-full is-resized">
+                                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/icons/laurel_left.svg" alt="<?php echo esc_attr__('This image is decorative', 'aegis'); ?>" style="width:40px" />
+                            </figure>
                             <!-- /wp:image -->
 
                             <!-- wp:group -->
@@ -98,7 +73,9 @@
                             <!-- /wp:group -->
 
                             <!-- wp:image {"width":"40px","sizeSlug":"full","linkDestination":"none","isDecorative":true} -->
-                            <figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/icons/laurel_right.svg" alt="<?php echo esc_attr__('This image is decorative', 'aegis'); ?>" style="width:40px" /></figure>
+                            <figure class="wp-block-image size-full is-resized">
+                                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/icons/laurel_right.svg" alt="<?php echo esc_attr__('This image is decorative', 'aegis'); ?>" style="width:40px" />
+                            </figure>
                             <!-- /wp:image -->
                         </div>
                         <!-- /wp:group -->
@@ -110,7 +87,9 @@
                         <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","allowOrientation":false,"justifyContent":"space-between","flexWrap":"wrap"}} -->
                         <div class="wp-block-group">
                             <!-- wp:image {"width":"40px","sizeSlug":"full","linkDestination":"none","isDecorative":true} -->
-                            <figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/icons/laurel_left.svg" alt="<?php echo esc_attr__('This image is decorative', 'aegis'); ?>" style="width:40px" /></figure>
+                            <figure class="wp-block-image size-full is-resized">
+                                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/icons/laurel_left.svg" alt="<?php echo esc_attr__('This image is decorative', 'aegis'); ?>" style="width:40px" />
+                            </figure>
                             <!-- /wp:image -->
 
                             <!-- wp:group -->
@@ -130,7 +109,45 @@
                             <!-- /wp:group -->
 
                             <!-- wp:image {"width":"40px","sizeSlug":"full","linkDestination":"none","isDecorative":true} -->
-                            <figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/icons/laurel_right.svg" alt="<?php echo esc_attr__('This image is decorative', 'aegis'); ?>" style="width:40px" /></figure>
+                            <figure class="wp-block-image size-full is-resized">
+                                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/icons/laurel_right.svg" alt="<?php echo esc_attr__('This image is decorative', 'aegis'); ?>" style="width:40px" />
+                            </figure>
+                            <!-- /wp:image -->
+                        </div>
+                        <!-- /wp:group -->
+                    </div>
+                    <!-- /wp:column -->
+
+                    <!-- wp:column {"verticalAlignment":"center","width":"22%","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+                    <div class="wp-block-column is-vertically-aligned-center" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;flex-basis:22%">
+                        <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","allowOrientation":false,"justifyContent":"space-between","flexWrap":"wrap"}} -->
+                        <div class="wp-block-group">
+                            <!-- wp:image {"width":"40px","sizeSlug":"full","linkDestination":"none","isDecorative":true} -->
+                            <figure class="wp-block-image size-full is-resized">
+                                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/icons/laurel_left.svg" alt="<?php echo esc_attr__('This image is decorative', 'aegis'); ?>" style="width:40px" />
+                            </figure>
+                            <!-- /wp:image -->
+
+                            <!-- wp:group -->
+                            <div class="wp-block-group">
+                                <!-- wp:paragraph {"align":"left","style":{"typography":{"textTransform":"uppercase","letterSpacing":"3px","fontStyle":"normal","fontWeight":"600","lineHeight":"1"}},"fontSize":"small"} -->
+                                <p class="has-text-align-left has-small-font-size" style="font-style:normal;font-weight:600;letter-spacing:3px;line-height:1;text-transform:uppercase">
+                                    <?php echo esc_html_x('Category', 'Replace with a descriptive category.', 'aegis'); ?>
+                                </p>
+                                <!-- /wp:paragraph -->
+
+                                <!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0"}}},"fontSize":"small"} -->
+                                <p class="has-text-align-center has-small-font-size" style="margin-top:0">
+                                    <?php echo esc_html_x('Nominee', 'Replace with the name of a nominee.', 'aegis'); ?>
+                                </p>
+                                <!-- /wp:paragraph -->
+                            </div>
+                            <!-- /wp:group -->
+
+                            <!-- wp:image {"width":"40px","sizeSlug":"full","linkDestination":"none","isDecorative":true} -->
+                            <figure class="wp-block-image size-full is-resized">
+                                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/icons/laurel_right.svg" alt="<?php echo esc_attr__('This image is decorative', 'aegis'); ?>" style="width:40px" />
+                            </figure>
                             <!-- /wp:image -->
                         </div>
                         <!-- /wp:group -->
