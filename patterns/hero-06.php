aa00b7bc1f463bcc3417997d0866cc54178a121b
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2025-02-24 05:07:54 -0500

    Update hero-06.php
    Fixed: Minor code formatting.
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/hero-06.php
+++ patterns/hero-06.php
@@ -26,17 +26,19 @@
 				<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--70);margin-bottom:var(--wp--preset--spacing--70);padding-top:0;padding-bottom:0">
 					<!-- wp:paragraph {"align":"left","metadata":{"name":"Tagline"},"style":{"typography":{"textTransform":"uppercase","letterSpacing":"3px","fontStyle":"normal","fontWeight":"400"},"spacing":{"margin":{"bottom":"-20px"}}},"fontSize":"tiny"} -->
 					<p class="has-text-align-left has-tiny-font-size" style="margin-bottom:-20px;font-style:normal;font-weight:400;letter-spacing:3px;text-transform:uppercase">
-					<?php echo esc_html_x( 'Tagline', 'Enter a brief and descriptive tagline here.', 'aegis' ); ?></p>
+					    <?php echo esc_html_x( 'Tagline', 'Enter a brief and descriptive tagline here.', 'aegis' ); ?>
+					</p>
 					<!-- /wp:paragraph -->
 
 					<!-- wp:heading {"style":{"typography":{"textTransform":"uppercase","fontSize":"6.5rem"},"spacing":{"margin":{"top":"0","bottom":"0","left":"0","right":"0"}}}} -->
 					<h2 class="wp-block-heading" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;font-size:6.5rem;text-transform:uppercase">
-					<?php echo esc_html_x( 'Heading', 'Enter a compelling headline for this section.', 'aegis' ); ?></h2>
+					    <?php echo esc_html_x( 'Heading', 'Enter a compelling headline for this section.', 'aegis' ); ?>
+				    </h2>
 					<!-- /wp:heading -->
 
 					<!-- wp:paragraph {"align":"center","style":{"layout":{"selfStretch":"fixed","flexSize":"50%"},"spacing":{"padding":{"top":"0","bottom":"0"},"margin":{"top":"0","bottom":"0"}}}} -->
 					<p class="has-text-align-center" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0">
-					<?php echo esc_html_x('Provide a concise description, up to 155 characters, summarizing the key points of an offer, article, or news update.', 'Replace with a description of the section.', 'aegis'); ?>
+					    <?php echo esc_html_x('Provide a concise description, up to 155 characters, summarizing the key points of an offer, article, or news update.', 'Replace with a description of the section.', 'aegis'); ?>
 					</p>
 					<!-- /wp:paragraph -->
 				</div>
@@ -56,7 +58,8 @@
 
 						<!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"textTransform":"uppercase","letterSpacing":"3px","fontStyle":"normal","fontWeight":"400"},"spacing":{"margin":{"bottom":"-20px"}}},"fontSize":"medium"} -->
 						<h3 class="wp-block-heading has-text-align-center has-medium-font-size" style="margin-bottom:-20px;font-style:normal;font-weight:400;letter-spacing:3px;text-transform:uppercase">
-						<?php echo esc_html_x( 'Heading', 'Enter a compelling headline for this section.', 'aegis' ); ?></h3>
+						    <?php echo esc_html_x( 'Heading', 'Enter a compelling headline for this section.', 'aegis' ); ?>
+					    </h3>
 						<!-- /wp:heading -->
 
 						<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
@@ -75,12 +78,13 @@
 
 						<!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"textTransform":"uppercase","letterSpacing":"3px","fontStyle":"normal","fontWeight":"400"},"spacing":{"margin":{"bottom":"-20px"}}},"fontSize":"medium"} -->
 						<h3 class="wp-block-heading has-text-align-center has-medium-font-size" style="margin-bottom:-20px;font-style:normal;font-weight:400;letter-spacing:3px;text-transform:uppercase">
-						<?php echo esc_html_x( 'Heading', 'Enter a compelling headline for this section.', 'aegis' ); ?></h3>
+						    <?php echo esc_html_x( 'Heading', 'Enter a compelling headline for this section.', 'aegis' ); ?>
+						</h3>
 						<!-- /wp:heading -->
 
 						<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
 						<p class="has-text-align-center has-small-font-size">
-						<?php echo esc_html_x('Provide a concise description, up to 65 characters, summarizing a service of product brand.', 'Replace with a description of the section.', 'aegis'); ?>
+						    <?php echo esc_html_x('Provide a concise description, up to 65 characters, summarizing a service of product brand.', 'Replace with a description of the section.', 'aegis'); ?>
 						</p>
 						<!-- /wp:paragraph -->
 					</div>
@@ -89,17 +93,20 @@
 					<!-- wp:column {"verticalAlignment":"bottom","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}},"color":{"background":"#1c1c1f66"}}} -->
 					<div class="wp-block-column is-vertically-aligned-bottom has-background" style="background-color:#1c1c1f66;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)">
 						<!-- wp:image {"width":"70px","sizeSlug":"thumbnail","linkDestination":"none","align":"center","isDecorative":true} -->
-						<figure class="wp-block-image aligncenter size-thumbnail is-resized"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/logo_aegis_light.svg" alt="<?php echo esc_attr__('This image is decorative', 'aegis'); ?>" style="width:70px" /></figure>
+						<figure class="wp-block-image aligncenter size-thumbnail is-resized">
+							<img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/logo_aegis_light.svg" alt="<?php echo esc_attr__('This image is decorative', 'aegis'); ?>" style="width:70px" />
+						</figure>
 						<!-- /wp:image -->
 
 						<!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"textTransform":"uppercase","letterSpacing":"3px","fontStyle":"normal","fontWeight":"400"},"spacing":{"margin":{"bottom":"-20px"}}},"fontSize":"medium"} -->
 						<h3 class="wp-block-heading has-text-align-center has-medium-font-size" style="margin-bottom:-20px;font-style:normal;font-weight:400;letter-spacing:3px;text-transform:uppercase">
-						<?php echo esc_html_x( 'Heading', 'Enter a compelling headline for this section.', 'aegis' ); ?></h3>
+						    <?php echo esc_html_x( 'Heading', 'Enter a compelling headline for this section.', 'aegis' ); ?>
+						</h3>
 						<!-- /wp:heading -->
 
 						<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
 						<p class="has-text-align-center has-small-font-size">
-						<?php echo esc_html_x('Provide a concise description, up to 65 characters, summarizing a service of product brand.', 'Replace with a description of the section.', 'aegis'); ?>
+						    <?php echo esc_html_x('Provide a concise description, up to 65 characters, summarizing a service of product brand.', 'Replace with a description of the section.', 'aegis'); ?>
 						</p>
 						<!-- /wp:paragraph -->
 					</div>
@@ -108,17 +115,20 @@
 					<!-- wp:column {"verticalAlignment":"bottom","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}},"color":{"background":"#1c1c1fad"}}} -->
 					<div class="wp-block-column is-vertically-aligned-bottom has-background" style="background-color:#1c1c1fad;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)">
 						<!-- wp:image {"width":"70px","sizeSlug":"thumbnail","linkDestination":"none","align":"center","isDecorative":true} -->
-						<figure class="wp-block-image aligncenter size-thumbnail is-resized"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/logo_aegis_light.svg" alt="<?php echo esc_attr__('This image is decorative', 'aegis'); ?>" style="width:70px" /></figure>
+						<figure class="wp-block-image aligncenter size-thumbnail is-resized">
+							<img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/logo_aegis_light.svg" alt="<?php echo esc_attr__('This image is decorative', 'aegis'); ?>" style="width:70px" />
+						</figure>
 						<!-- /wp:image -->
 
 						<!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"textTransform":"uppercase","letterSpacing":"3px","fontStyle":"normal","fontWeight":"400"},"spacing":{"margin":{"bottom":"-20px"}}},"fontSize":"medium"} -->
 						<h3 class="wp-block-heading has-text-align-center has-medium-font-size" style="margin-bottom:-20px;font-style:normal;font-weight:400;letter-spacing:3px;text-transform:uppercase">
-						<?php echo esc_html_x( 'Heading', 'Enter a compelling headline for this section.', 'aegis' ); ?></h3>
+						    <?php echo esc_html_x( 'Heading', 'Enter a compelling headline for this section.', 'aegis' ); ?>
+						</h3>
 						<!-- /wp:heading -->
 
 						<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
 						<p class="has-text-align-center has-small-font-size">
-						<?php echo esc_html_x('Provide a concise description, up to 65 characters, summarizing a service of product brand.', 'Replace with a description of the section.', 'aegis'); ?>
+						    <?php echo esc_html_x('Provide a concise description, up to 65 characters, summarizing a service of product brand.', 'Replace with a description of the section.', 'aegis'); ?>
 						</p>
 						<!-- /wp:paragraph -->
 					</div>
