28298b19fe1db0f079d6cbf9c06b6748303b8d3d
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2025-04-02 17:31:56 -0500

    Update files

--- patterns/about-03.php
+++ patterns/about-03.php
@@ -23,10 +23,7 @@
     <div class="wp-block-column is-vertically-aligned-center">
       <!-- wp:image {"aspectRatio":"16/9","scale":"cover","sizeSlug":"large","linkDestination":"none","className":"is-style-default","style":{"shadow":"var:preset|shadow|primary-solid-shadow-right-bottom","border":{"width":"2px"}},"borderColor":"base"} -->
       <figure class="wp-block-image size-large has-custom-border is-style-default">
-        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/thumb_1920x1200_light.webp"
-             alt="<?php echo esc_attr__( 'Abstract illustration featuring the theme’s logo', 'aegis' ); ?>"
-             class="has-border-color has-base-border-color"
-             style="border-width:2px;box-shadow:var(--wp--preset--shadow--primary-solid-shadow-right-bottom);aspect-ratio:16/9;object-fit:cover" />
+        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/thumb_1920x1200_light.webp" alt="<?php echo esc_attr__( 'Abstract illustration featuring the theme’s logo', 'aegis' ); ?>" class="has-border-color has-base-border-color" style="border-width:2px;box-shadow:var(--wp--preset--shadow--primary-solid-shadow-right-bottom);aspect-ratio:16/9;object-fit:cover" />
       </figure>
       <!-- /wp:image -->
     </div>
