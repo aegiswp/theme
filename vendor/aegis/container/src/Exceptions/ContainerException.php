21cdd4bfde24f454b77135b69a5fa0c1955e3e02
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2025-08-05 07:16:08 -0500

    Update ContainerException.php

--- vendor/aegis/container/src/Exceptions/ContainerException.php
+++ vendor/aegis/container/src/Exceptions/ContainerException.php
@@ -1,20 +1,14 @@
 <?php
 /**
- * Container Exception
+ * Container Exception.
  *
- * Defines the exception class for the dependency injection container.
+ * Exception thrown for general errors in the dependency injection container.
  *
- * Responsibilities:
- * - Handles errors and exceptions thrown by the container
- *
- * @package    Aegis\Container\Exceptions
- * @since      1.0.0
- * @author     Atmostfear Entertainment
- * @link       https://github.com/aegiswp/theme
- *
- * For developer documentation and onboarding. No logic changes in this doc update.
+ * @package   Aegis\Container\Exceptions
+ * @since     1.0.0
  */
 
+// Ensures strict type checking for all code in this file.
 declare( strict_types=1 );
 
 namespace Aegis\Container\Exceptions;
@@ -23,7 +17,10 @@ use Exception;
 use Psr\Container\ContainerExceptionInterface;
 
 /**
- * Container Exception.
+ * Base exception for the container.
+ *
+ * This exception is thrown when an error occurs within the container. It implements the
+ * PSR-11 ContainerExceptionInterface, allowing for interoperability.
  *
  * @since 1.0.0
  */
@@ -32,11 +29,9 @@ class ContainerException extends Exception implements ContainerExceptionInterfac
 	/**
 	 * ContainerException constructor.
 	 *
-	 * @param string     $message  Error message.
-	 * @param ?int       $code     Optional. Error code. Defaults to 0.
-	 * @param ?Exception $previous Optional. Previous exception used for the exception chaining. Defaults to null.
-	 *
-	 * @return void
+	 * @param string          $message  The error message.
+	 * @param int             $code     Optional. The error code. Defaults to 0.
+	 * @param Exception|null  $previous Optional. The previous exception used for chaining. Defaults to null.
 	 */
 	public function __construct( string $message, ?int $code = 0, ?Exception $previous = null ) {
 		parent::__construct( $message, $code, $previous );
