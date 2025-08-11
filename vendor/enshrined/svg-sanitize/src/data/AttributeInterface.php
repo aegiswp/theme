bbf100b95d727d0f5fd2dc6f6272effe77aa7631
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2025-08-11 12:36:21 -0500

    Update AttributeInterface.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- vendor/enshrined/svg-sanitize/src/data/AttributeInterface.php
+++ vendor/enshrined/svg-sanitize/src/data/AttributeInterface.php
@@ -1,24 +1,4 @@
 <?php
-/**
- * SVG Attribute Interface
- *
- * This file is used by the SVG-Sanitize library (enshrined/svg-sanitize) as the interface for allowed SVG attributes.
- *
- * Purpose:
- * - Defines the contract for attribute lists used in SVG sanitization.
- * - Used by the Aegis theme as part of its SVG sanitization process.
- *
- * @package    enshrined/svg-sanitize
- * @since      1.0.0
- * @author     enshrined
- * @link       https://github.com/darylldoyle/svg-sanitizer
- *
- * IMPORTANT:
- * - This file is part of a third-party library and may be overwritten on library updates.
- * - For custom interface changes, fork the library or use hooks/filters provided by the Aegis theme or WordPress.
- *
- * For developer documentation and onboarding. No logic or interface changes in this doc update.
- */
 namespace enshrined\svgSanitize\data;
 
 /**
