67c35fe0019e2ec63f2bcd9fa3ca3d7c6f469f3a
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2025-08-16 11:38:31 -0500

    Update index.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <?php
 /**
- * There is nothing output here because Block Themes such as Aegis do not use PHP templates.
+ * Main Template File
  *
- * @package aegis
+ * This file is required by WordPress for theme validation, but it does not
+ * output any content in a block theme like Aegis. All templating is handled
+ * by HTML files in the `templates` and `parts` directories.
  *
- * There is a core ticket discussing removing this requirement for block themes:
- * https://core.trac.wordpress.org/ticket/54272.
+ * Responsibilities:
+ * - Fulfill the WordPress requirement for an `index.php` file.
+ * - Remain empty to ensure that block templates take precedence.
+ *
+ * @package    Aegis
+ * @since      1.0.0
+ * @author     @atmostfear-entertainment
+ * @link       https://github.com/aegiswp/theme
+ *
+ * For more context on why this file is necessary but empty, see the related
+ * WordPress core ticket: https://core.trac.wordpress.org/ticket/54272.
  */
