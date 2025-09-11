43d9709f2beb5fafa47f49f3d417af033607b854
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2025-09-11 03:48:11 -0500

    Update PostContent.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- vendor/aegis/framework/src/CoreBlocks/PostContent.php
+++ vendor/aegis/framework/src/CoreBlocks/PostContent.php
@@ -10,7 +10,7 @@
  *
  * @package    Aegis\Framework\CoreBlocks
  * @since      1.0.0
- * @author     @atmostfear-entertainment
+ * @author     Atmostfear Entertainment
  * @link       https://github.com/aegiswp/theme
  *
  * For developer documentation and onboarding. No logic changes in this
@@ -117,4 +117,5 @@ class PostContent implements Renderable {
 
 		return $dom->saveHTML();
 	}
+
 }
