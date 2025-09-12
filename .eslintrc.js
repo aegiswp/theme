c4a8033889186762a436932da6ea4f74bf513055
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2025-09-12 16:54:08 -0500

    Update .estlintrc.js
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,23 +2,23 @@ module.exports = {
     extends: [
         'plugin:@wordpress/eslint-plugin/recommended',
         'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended'
     ],
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
     root: true,
     env: {
         browser: true,
-        es6: true,
+        es6: true
     },
     settings: {
         react: {
-            version: 'detect',
-        },
+            version: 'detect'
+        }
     },
     rules: {
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
-        'react/react-in-jsx-scope': 'off',
-    },
+        'react/react-in-jsx-scope': 'off'
+    }
 };
