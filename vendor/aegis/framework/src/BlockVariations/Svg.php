10819ed3ddb37add1cccf249cff3f3611a28949b
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-01-15 01:42:11 -0500

    Update Svg.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- vendor/aegis/framework/src/BlockVariations/Svg.php
+++ vendor/aegis/framework/src/BlockVariations/Svg.php
@@ -18,7 +18,7 @@
  */
 
 // Enforces strict type checking for all code in this file, ensuring type safety for blocks variations.
-declare( strict_types=1 );
+declare(strict_types=1);
 
 // Declares the namespace for block variations within the Aegis Framework.
 namespace Aegis\Framework\BlockVariations;
@@ -53,7 +53,8 @@ use function trim;
  * @package Aegis\Framework\BlockVariations
  * @since   1.0.0
  */
-class Svg implements Renderable {
+class Svg implements Renderable
+{
 
 	/**
 	 * The Onclick service instance.
@@ -69,7 +70,8 @@ class Svg implements Renderable {
 	 *
 	 * @param Onclick $onclick The Onclick service instance.
 	 */
-	public function __construct( Onclick $onclick ) {
+	public function __construct(Onclick $onclick)
+	{
 		$this->onclick = $onclick;
 	}
 
@@ -90,69 +92,70 @@ class Svg implements Renderable {
 	 *
 	 * @return string The modified block content containing the SVG.
 	 */
-	public function render( string $block_content, array $block, WP_Block $instance ): string {
-		$attrs      = $block['attrs'] ?? [];
-		$svg_string = Icon::sanitize_svg( $attrs['style']['svgString'] ?? '' );
+	public function render(string $block_content, array $block, WP_Block $instance): string
+	{
+		$attrs = $block['attrs'] ?? [];
+		$svg_string = Icon::sanitize_svg($attrs['style']['svgString'] ?? '');
 
 		// Only run if an SVG string is provided and the block has the correct style variation.
-		if ( ! $svg_string || ! str_contains( $block_content, 'is-style-svg' ) ) {
+		if (!$svg_string || !str_contains($block_content, 'is-style-svg')) {
 			return $block_content;
 		}
 
-		$dom      = DOM::create( $block_content );
-		$figure   = DOM::get_element( 'figure', $dom );
-		$link     = DOM::get_element( 'a', $figure );
-		$img      = DOM::get_element( 'img', $link ?? $figure );
-		$svg      = DOM::get_element( 'svg', $link ?? $figure );
-		$width    = esc_attr( $attrs['width'] ?? '' );
-		$height   = esc_attr( $attrs['height'] ?? '' );
-		$mask     = (bool) ( $attrs['style']['maskSvg'] ?? false );
+		$dom = DOM::create($block_content);
+		$figure = DOM::get_element('figure', $dom);
+		$link = DOM::get_element('a', $figure);
+		$img = DOM::get_element('img', $link ?? $figure);
+		$svg = DOM::get_element('svg', $link ?? $figure);
+		$width = esc_attr($attrs['width'] ?? '');
+		$height = esc_attr($attrs['height'] ?? '');
+		$mask = (bool) ($attrs['style']['maskSvg'] ?? false);
 		$on_click = $attrs['onclick'] ?? '';
 
 		// The "mask" render path is currently disabled.
-		if ( $mask ) {
+		if ($mask) {
 			//return $this->render_mask( $img, $svg_string, $dom, $width, $height );
 		}
 
 		// Apply onclick attribute if it exists.
-		if ( $on_click ) {
-			( $link ?? $figure ?? $img )->setAttribute( 'onclick', $on_click );
+		if ($on_click) {
+			($link ?? $figure ?? $img)->setAttribute('onclick', $on_click);
 			$block_content = $dom->saveHTML();
 		}
 
 		// If there is already an SVG, do not re-render.
-		if ( $svg ) {
+		if ($svg) {
 			return $block_content;
 		}
 
 		// Remove the original `<img>` tag to make way for the new SVG.
-		if ( $img ) {
-			$img->parentNode->removeChild( $img );
+		if ($img) {
+			$img->parentNode->removeChild($img);
 		}
 
 		// Create a new SVG element from the provided string.
-		$svg_dom     = DOM::create( $svg_string );
-		$svg_element = DOM::get_element( 'svg', $svg_dom );
-		if ( ! $svg_element ) {
+		$svg_dom = DOM::create($svg_string);
+		$svg_element = DOM::get_element('svg', $svg_dom);
+		if (!$svg_element) {
 			return $block_content;
 		}
 
 		// Import the new SVG into the main document.
-		$imported = DOM::node_to_element( $dom->importNode( $svg_element, true ) );
+		$imported = DOM::node_to_element($dom->importNode($svg_element, true));
 
 		// Apply width and height attributes.
-		if ( $width ) {
-			$imported->setAttribute( 'width', $width );
+		if ($width) {
+			$imported->setAttribute('width', $width);
 		}
-		if ( $height ) {
-			$imported->setAttribute( 'height', $height );
+		if ($height) {
+			$imported->setAttribute('height', $height);
 		}
 
 		// Append the new SVG to the link if it exists, otherwise to the figure.
-		if ( $link ) {
-			$link->appendChild( $imported );
+		if ($link) {
+			$link->appendChild($imported);
 		} else {
-			$figure->appendChild( $imported );
+			$figure->appendChild($imported);
 		}
 
 		return $dom->saveHTML();
@@ -176,42 +179,42 @@ class Svg implements Renderable {
 	 *
 	 * @return string The HTML for the new `<span>` element with the SVG mask.
 	 */
-	public function render_mask( DOMElement $img, string $svg_string, DOMDocument $dom, string $width, string $height ): string {
-		$span   = DOM::change_tag_name( 'span', $img );
-		$styles = CSS::string_to_array( $span->getAttribute( 'style' ) );
+	public function render_mask(DOMElement $img, string $svg_string, DOMDocument $dom, string $width, string $height): string
+	{
+		$span = DOM::change_tag_name('span', $img);
+		$styles = CSS::string_to_array($span->getAttribute('style'));
 
 		// URL-encode the SVG and set it as the mask image.
-		$encoded                      = rawurlencode( str_replace( '"', "'", trim( $svg_string ) ) );
+		$encoded = rawurlencode(str_replace('"', "'", trim($svg_string)));
 		$styles['-webkit-mask-image'] = 'url("data:image/svg+xml;utf8,' . $encoded . '")';
 
 		// Apply width and height.
-		if ( $width ) {
-			$unit            = Str::contains_any( $width, 'px', 'em', 'rem', 'vh', 'vw', '%' ) ? '' : 'px';
+		if ($width) {
+			$unit = Str::contains_any($width, 'px', 'em', 'rem', 'vh', 'vw', '%') ? '' : 'px';
 			$styles['width'] = $width . $unit;
-			$span->removeAttribute( 'width' );
+			$span->removeAttribute('width');
 		}
-		if ( $height ) {
-			$unit             = Str::contains_any( $height, 'px', 'em', 'rem', 'vh', 'vw', '%' ) ? '' : 'px';
+		if ($height) {
+			$unit = Str::contains_any($height, 'px', 'em', 'rem', 'vh', 'vw', '%') ? '' : 'px';
 			$styles['height'] = $height . $unit;
-			$span->removeAttribute( 'height' );
+			$span->removeAttribute('height');
 		}
 
-		// Transfer alt text to an aria-label for accessibility.
-		if ( $alt = $img->getAttribute( 'alt' ) ) {
-			$span->setAttribute( 'aria-label', esc_attr( $alt ) );
-			$span->removeAttribute( 'alt' );
+		// Transfer alternative text to an aria-label for accessibility.
+		if ($alt = $img->getAttribute('alt')) {
+			$span->setAttribute('aria-label', esc_attr($alt));
+			$span->removeAttribute('alt');
 		}
 
 		// Clean up and set final attributes.
-		$classes   = explode( ' ', $span->getAttribute( 'class' ) );
+		$classes = explode(' ', $span->getAttribute('class'));
 		$classes[] = 'wp-block-image__svg';
-		$span->setAttribute( 'class', implode( ' ', $classes ) );
-		$span->setAttribute( 'role', 'img' );
-		$span->removeAttribute( 'style' );
-		$span->setAttribute( 'style', CSS::array_to_string( $styles ) );
-		$span->removeAttribute( 'src' );
+		$span->setAttribute('class', implode(' ', $classes));
+		$span->setAttribute('role', 'img');
+		$span->removeAttribute('style');
+		$span->setAttribute('style', CSS::array_to_string($styles));
+		$span->removeAttribute('src');
 
 		return $dom->saveHTML();
 	}
-
 }
