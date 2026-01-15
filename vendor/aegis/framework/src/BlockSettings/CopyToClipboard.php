9a432bf94e69e965b3476e6e9363c3cbb3c963a6
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-01-14 23:32:15 -0500

    Update CopyToClipboard.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- vendor/aegis/framework/src/BlockSettings/CopyToClipboard.php
+++ vendor/aegis/framework/src/BlockSettings/CopyToClipboard.php
@@ -18,7 +18,7 @@
  */
 
 // Enforces strict type checking for all code in this file, ensuring type safety for block settings.
-declare( strict_types=1 );
+declare(strict_types=1);
 
 // Declares the namespace for block settings within the Aegis Framework.
 namespace Aegis\Framework\BlockSettings;
@@ -49,7 +49,8 @@ use function wp_strip_all_tags;
  * @package Aegis\Framework\BlockSettings
  * @since   1.0.0
  */
-class CopyToClipboard implements Renderable, Scriptable {
+class CopyToClipboard implements Renderable, Scriptable
+{
 
 	/**
 	 * Renders the code block with a "Copy to Clipboard" button.
@@ -68,24 +69,25 @@ class CopyToClipboard implements Renderable, Scriptable {
 	 *
 	 * @return string The modified block content with the copy button HTML.
 	 */
-	public function render( string $block_content, array $block, WP_Block $instance ): string {
+	public function render(string $block_content, array $block, WP_Block $instance): string
+	{
 		// This functionality is for the front-end only.
-		if ( is_admin() ) {
+		if (is_admin()) {
 			return $block_content;
 		}
 
 		// Only run if the copyToClipboard attribute is enabled.
 		$enabled = $block['attrs']['copyToClipboard'] ?? true;
-		if ( ! $enabled ) {
+		if (!$enabled) {
 			return $block_content;
 		}
 
 		// --- HTML Construction for the Copy Button ---
 		// Get the raw, unformatted code content. The hidden textarea makes it easy for JS to grab.
-		$content = trim( html_entity_decode( wp_strip_all_tags( $block['innerHTML'] ) ) );
-		$label   = esc_html__( 'Copy to clipboard', 'aegis' );
-		$copied  = esc_html__( 'Copied!', 'aegis' );
-		$svg     = <<<SVG
+		$content = trim(html_entity_decode(wp_strip_all_tags($block['innerHTML'])));
+		$label = esc_html__('Copy to clipboard', 'aegis');
+		$copied = esc_html__('Copied!', 'aegis');
+		$svg = <<<SVG
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" stroke="currentcolor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" fill="none">
   <path d="M12.9975 10.7499L11.7475 10.7499C10.6429 10.7499 9.74747 11.6453 9.74747 12.7499L9.74747 21.2499C9.74747 22.3544 10.6429 23.2499 11.7475 23.2499L20.2475 23.2499C21.352 23.2499 22.2475 22.3544 22.2475 21.2499L22.2475 12.7499C22.2475 11.6453 21.352 10.7499 20.2475 10.7499L18.9975 10.7499Z"></path>
   <path d="M17.9975 12.2499L13.9975 12.2499C13.4452 12.2499 12.9975 11.8022 12.9975 11.2499L12.9975 9.74988C12.9975 9.19759 13.4452 8.74988 13.9975 8.74988L17.9975 8.74988C18.5498 8.74988 18.9975 9.19759 18.9975 9.74988L18.9975 11.2499C18.9975 11.8022 18.5498 12.2499 17.9975 12.2499Z"></path>
@@ -96,25 +98,25 @@ SVG;
 		$copy_to_clipboard_html = "<div class='copy-to-clipboard'><span >$copied</span><button class='click-to-copy-button' title='$label'>$svg</button><textarea>$content</textarea></div>";
 
 		// --- DOM Manipulation ---
-		$dom     = DOM::create( $block_content );
-		$element = DOM::get_element( 'pre', $dom ) ?? DOM::get_element( 'code', $dom );
+		$dom = DOM::create($block_content);
+		$element = DOM::get_element('pre', $dom) ?? DOM::get_element('code', $dom);
 
-		if ( ! $element ) {
+		if (!$element) {
 			return $block_content;
 		}
 
 		// Add line number class if enabled.
-		if ( $block['attrs']['showLineNumbers'] ?? false ) {
-			$classes   = explode( ' ', $element->getAttribute( 'class' ) );
+		if ($block['attrs']['showLineNumbers'] ?? false) {
+			$classes = explode(' ', $element->getAttribute('class'));
 			$classes[] = 'show-line-numbers';
-			$element->setAttribute( 'class', implode( ' ', $classes ) );
+			$element->setAttribute('class', implode(' ', $classes));
 		}
 
 		// Inject the copy button HTML into the code block element.
-		$copy_dom = DOM::create( $copy_to_clipboard_html );
-		$div      = DOM::get_element( 'div', $copy_dom );
-		$imported = $element->ownerDocument->importNode( $div, true );
-		$element->insertBefore( $imported, $element->firstChild );
+		$copy_dom = DOM::create($copy_to_clipboard_html);
+		$div = DOM::get_element('div', $copy_dom);
+		$imported = $element->ownerDocument->importNode($div, true);
+		$element->insertBefore($imported, $element->firstChild);
 
 		return $dom->saveHTML();
 	}
@@ -129,8 +131,8 @@ SVG;
 	 *
 	 * @param Scripts $scripts The Scripts service instance.
 	 */
-	public function scripts( Scripts $scripts ): void {
-		$scripts->add_file( 'copy-to-clipboard.js', [ 'copy-to-clipboard' ] );
+	public function scripts(Scripts $scripts): void
+	{
+		$scripts->add_file('copy-to-clipboard.js', ['copy-to-clipboard']);
 	}
-
 }
