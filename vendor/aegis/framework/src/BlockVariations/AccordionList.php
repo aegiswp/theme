f7fa1b89d093e9d633ac69ed308c940e6c892d2a
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-01-15 00:37:01 -0500

    Update AccordionList.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- vendor/aegis/framework/src/BlockVariations/AccordionList.php
+++ vendor/aegis/framework/src/BlockVariations/AccordionList.php
@@ -18,7 +18,7 @@
  */
 
 // Enforces strict type checking for all code in this file, ensuring type safety for blocks variations.
-declare( strict_types=1 );
+declare(strict_types=1);
 
 // Declares the namespace for block variations within the Aegis Framework.
 namespace Aegis\Framework\BlockVariations;
@@ -44,7 +44,8 @@ use WP_Block;
  * @package Aegis\Framework\BlockVariations
  * @since   1.0.0
  */
-class AccordionList implements Renderable {
+class AccordionList implements Renderable
+{
 
 	/**
 	 * Renders a list block as a semantic accordion.
@@ -67,17 +68,18 @@ class AccordionList implements Renderable {
 	 *
 	 * @return string The modified block content, now structured as an accordion.
 	 */
-	public function render( string $block_content, array $block, WP_Block $instance ): string {
-		if ( ! str_contains( $block_content, 'is-style-accordion' ) ) {
+	public function render(string $block_content, array $block, WP_Block $instance): string
+	{
+		if (!str_contains($block_content, 'is-style-accordion')) {
 			return $block_content;
 		}
 
-		$dom = DOM::create( $block_content );
-		$ul  = DOM::get_element( 'ul', $dom );
-		$ol  = DOM::get_element( 'ol', $dom );
+		$dom = DOM::create($block_content);
+		$ul = DOM::get_element('ul', $dom);
+		$ol = DOM::get_element('ol', $dom);
 		$list = $ul ?? $ol;
 
-		if ( ! $list ) {
+		if (!$list) {
 			return $block_content;
 		}
 
@@ -86,105 +88,104 @@ class AccordionList implements Renderable {
 		$accordion_wrapper_html = '<div>';
 
 		// Iterate over each original list item.
-		foreach ( $list->getElementsByTagName( 'li' ) as $li ) {
-			if ( ! $li instanceof DOMElement ) {
+		foreach ($list->getElementsByTagName('li') as $li) {
+			if (!$li instanceof DOMElement) {
 				continue;
 			}
 
-			$inner_html = $dom->saveHTML( $li );
+			$inner_html = $dom->saveHTML($li);
 
 			// The <br> tag is used as a delimiter between the title and content.
-			if ( ! str_contains( $inner_html, '<br>' ) ) {
+			if (!str_contains($inner_html, '<br>')) {
 				continue;
 			}
 
 			// Create the new <details> and <summary> elements.
-			$details = DOM::create_element( 'details', $dom );
+			$details = DOM::create_element('details', $dom);
 			// Transfer all attributes (class, style, etc.) from the <li> to the <details>.
-			foreach ( $li->attributes as $attribute ) {
-				$details->setAttribute( esc_attr( $attribute->name ), esc_attr( $attribute->value ) );
+			foreach ($li->attributes as $attribute) {
+				$details->setAttribute(esc_attr($attribute->name), esc_attr($attribute->value));
 			}
 
-			$summary = DOM::create_element( 'summary', $dom );
-			$section = DOM::create_element( 'section', $dom );
-			$explode = explode( '<br>', $inner_html );
+			$summary = DOM::create_element('summary', $dom);
+			$section = DOM::create_element('section', $dom);
+			$explode = explode('<br>', $inner_html);
 
 			// --- Populate Title and Content ---
 			// The content before the first <br> becomes the summary (title).
-			$title_dom = DOM::create( $explode[0] );
-			$list_item = DOM::get_element( 'li', $title_dom );
-			foreach ( $list_item->childNodes as $child_node ) {
-				$summary->appendChild( $dom->importNode( $child_node, true ) );
+			$title_dom = DOM::create($explode[0]);
+			$list_item = DOM::get_element('li', $title_dom);
+			foreach ($list_item->childNodes as $child_node) {
+				$summary->appendChild($dom->importNode($child_node, true));
 			}
 
 			// The content after the <br> becomes the section (collapsible content).
 			// The strip_tags is used to clean up any leftover HTML.
-			$section->textContent = strip_tags( $explode[2] ?? $explode[1], '' );
+			$section->textContent = strip_tags($explode[2] ?? $explode[1], '');
 
 			// --- Assemble the Accordion Item ---
-			$details->appendChild( $summary );
+			$details->appendChild($summary);
 
 			// If the original list item had a border, add a visual <hr> separator.
-			$li_style   = $li->getAttribute( 'style' );
-			$has_border = Str::contains_any( $li_style, 'border-width', 'border-style', 'border-color' ) && ! str_contains( $li_style, 'border-width:0' );
-			if ( $has_border ) {
-				$details->appendChild( DOM::create_element( 'hr', $dom ) );
+			$li_style = $li->getAttribute('style');
+			$has_border = Str::contains_any($li_style, 'border-width', 'border-style', 'border-color') && !str_contains($li_style, 'border-width:0');
+			if ($has_border) {
+				$details->appendChild(DOM::create_element('hr', $dom));
 			}
 
-			$details->appendChild( $section );
+			$details->appendChild($section);
 
 			// --- Handle Padding ---
 			// Padding styles are moved from the parent <details> to the inner <summary> and <section>
 			// for more accurate visual styling.
-			$styles  = CSS::string_to_array( $details->getAttribute( 'style' ) );
+			$styles = CSS::string_to_array($details->getAttribute('style'));
 			$padding = [];
-			foreach ( $styles as $key => $value ) {
-				if ( str_contains( $key, 'padding' ) ) {
-					unset( $styles[ $key ] );
-					$padding[ $key ] = $value;
+			foreach ($styles as $key => $value) {
+				if (str_contains($key, 'padding')) {
+					unset($styles[$key]);
+					$padding[$key] = $value;
 				}
 			}
-			if ( $padding ) {
-				$summary->setAttribute( 'style', CSS::array_to_string( $padding ) );
-				if ( ! $has_border ) {
-					unset( $padding['padding-top'] );
+			if ($padding) {
+				$summary->setAttribute('style', CSS::array_to_string($padding));
+				if (!$has_border) {
+					unset($padding['padding-top']);
 				}
-				$section->setAttribute( 'style', CSS::array_to_string( $padding ) );
+				$section->setAttribute('style', CSS::array_to_string($padding));
 			}
 
 			// Re-apply the non-padding styles to the <details> element.
-			$details->setAttribute( 'style', CSS::array_to_string( $styles ) );
-			if ( ! $styles ) {
-				$details->removeAttribute( 'style' );
+			$details->setAttribute('style', CSS::array_to_string($styles));
+			if (!$styles) {
+				$details->removeAttribute('style');
 			}
 
 			// Add the expand/collapse icon.
-			$icon = DOM::create_element( 'span', $dom );
-			$icon->setAttribute( 'class', 'accordion-toggle' );
-			$summary->appendChild( $icon );
+			$icon = DOM::create_element('span', $dom);
+			$icon->setAttribute('class', 'accordion-toggle');
+			$summary->appendChild($icon);
 
 			// Append the fully constructed <details> element to our wrapper.
-			$accordion_wrapper_html .= $dom->saveHTML( $details );
+			$accordion_wrapper_html .= $dom->saveHTML($details);
 		}
 
 		$accordion_wrapper_html .= '</div>';
 
 		// --- Final DOM Replacement ---
 		// Replace the original <ul>/<ol> with the new <div> containing the accordion.
-		$div_dom  = DOM::create( $accordion_wrapper_html );
-		$imported = $dom->importNode( $div_dom->documentElement, true );
+		$div_dom = DOM::create($accordion_wrapper_html);
+		$imported = $dom->importNode($div_dom->documentElement, true);
 
 		// Transfer all attributes from the original list to the new wrapper.
-		foreach ( $list->attributes as $attribute ) {
-			if ( method_exists( $imported, 'setAttribute' ) ) {
-				$imported->setAttribute( $attribute->localName, $attribute->nodeValue );
+		foreach ($list->attributes as $attribute) {
+			if (method_exists($imported, 'setAttribute')) {
+				$imported->setAttribute($attribute->localName, $attribute->nodeValue);
 			}
 		}
 
-		$dom->removeChild( $list );
-		$dom->appendChild( $imported );
+		$dom->removeChild($list);
+		$dom->appendChild($imported);
 
 		return $dom->saveHTML();
 	}
-
 }
