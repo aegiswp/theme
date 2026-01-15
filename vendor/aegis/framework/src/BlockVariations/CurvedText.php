d47b146fe3cfcabe162c981c057d20bbc49c4da2
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-01-15 01:15:39 -0500

    Update CurvedText.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- vendor/aegis/framework/src/BlockVariations/CurvedText.php
+++ vendor/aegis/framework/src/BlockVariations/CurvedText.php
@@ -18,7 +18,7 @@
  */
 
 // Enforces strict type checking for all code in this file, ensuring type safety for blocks variations.
-declare( strict_types=1 );
+declare(strict_types=1);
 
 // Declares the namespace for block variations within the Aegis Framework.
 namespace Aegis\Framework\BlockVariations;
@@ -30,7 +30,8 @@ use WP_Block;
 
 // Implements the CurvedText class to support curved text block rendering.
 
-class CurvedText implements Renderable {
+class CurvedText implements Renderable
+{
 
 	/**
 	 * Renders curved text.
@@ -45,44 +46,44 @@ class CurvedText implements Renderable {
 	 *
 	 * @return string
 	 */
-	public function render( string $block_content, array $block, WP_Block $instance ): string {
+	public function render(string $block_content, array $block, WP_Block $instance): string
+	{
 		$svg_string = $block['attrs']['curvedText']['svgString'] ?? '';
 
-		if ( ! $svg_string ) {
+		if (!$svg_string) {
 			return $block_content;
 		}
 
-		$dom            = DOM::create( $block_content );
-		$p              = DOM::get_element( 'p', $dom );
+		$dom = DOM::create($block_content);
+		$p = DOM::get_element('p', $dom);
 		$p->textContent = '';
 
-		$svg_dom     = DOM::create( $svg_string );
-		$svg_element = DOM::get_element( 'svg', $svg_dom );
+		$svg_dom = DOM::create($svg_string);
+		$svg_element = DOM::get_element('svg', $svg_dom);
 
-		if ( ! $svg_element ) {
+		if (!$svg_element) {
 			return $block_content;
 		}
 
-		$svg_text_element = DOM::get_element( 'text', $svg_element );
+		$svg_text_element = DOM::get_element('text', $svg_element);
 
-		if ( ! $svg_text_element ) {
+		if (!$svg_text_element) {
 			return $block_content;
 		}
 
-		$text_path_element = DOM::get_element( '*', $svg_text_element );
+		$text_path_element = DOM::get_element('*', $svg_text_element);
 
-		if ( $text_path_element ) {
+		if ($text_path_element) {
 			$text_path_element->textContent = $block['attrs']['curvedText']['content'] ?? '';
 		}
 
-		$svg_string      = $svg_dom->saveHTML( $svg_element );
-		$new_svg_dom     = DOM::create( $svg_string );
-		$new_svg_element = DOM::get_element( 'svg', $new_svg_dom );
-		$imported        = $dom->importNode( $new_svg_element, true );
+		$svg_string = $svg_dom->saveHTML($svg_element);
+		$new_svg_dom = DOM::create($svg_string);
+		$new_svg_element = DOM::get_element('svg', $new_svg_dom);
+		$imported = $dom->importNode($new_svg_element, true);
 
-		$p->appendChild( $imported );
+		$p->appendChild($imported);
 
-		return $dom->saveHTML( $p );
+		return $dom->saveHTML($p);
 	}
-
 }
