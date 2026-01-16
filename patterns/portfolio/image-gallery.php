0aa27c22dfae2c4ad2a3ff5e392a1d336831e230
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-01-16 18:09:51 -0500

    Create image-gallery.php
    - Initial commit
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

---
+++ patterns/portfolio/image-gallery.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Title: Portfolio Image Gallery
+ * Slug: image-gallery
+ * Categories: Portfolio
+ * ID: 554
+ */
+?>
+
+<!-- wp:group {"metadata":{"categories":["portfolio"],"patternName":"image-gallery","name":"Portfolio Image Gallery"},"align":"full","style":{"spacing":{"blockGap":"2em","padding":{"top":"2em","bottom":"2em"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="padding-top:2em;padding-bottom:2em">
+    <!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|xs"}}},"layout":{"type":"constrained"}} -->
+    <div class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--xs)">
+        <!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|sm"}}},"layout":{"type":"constrained","contentSize":"640px"}} -->
+        <div class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--sm)">
+            <!-- wp:group {"metadata":{"name":"Heading"},"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|lg"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"},"animation":{"duration":"1","iterationCount":"1"}} -->
+            <div class="wp-block-group has-animation"
+                style="margin-bottom:var(--wp--preset--spacing--lg);animation-duration:1s;animation-iteration-count:1">
+                <!-- wp:paragraph {"align":"center","className":"is-style-sub-heading","animation":{"duration":"1","iterationCount":"1"}} -->
+                <p class="aligncenter has-text-align-center is-style-sub-heading aligncenter has-animation"
+                    style="animation-duration:1s;animation-iteration-count:1">Portfolio</p>
+                <!-- /wp:paragraph -->
+
+                <!-- wp:heading {"textAlign":"center","style":{"display":{"all":"","mobile":""}},"fontSize":"48"} -->
+                <h2 class="wp-block-heading has-text-align-center has-48-font-size">Latest Projects</h2>
+                <!-- /wp:heading -->
+
+                <!-- wp:paragraph {"align":"center","fontSize":"18","animation":{"duration":"1","iterationCount":"1"}} -->
+                <p class="aligncenter has-text-align-center has-18-font-size aligncenter has-animation"
+                    style="animation-duration:1s;animation-iteration-count:1">Manage your visual language from a single
+                    source of truth. By defining typography, colors, and spacing globally, you ensure instant
+                    consistency across the entire framework without repetitive manual adjustments.</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|sm","left":"var:preset|spacing|sm"}}}} -->
+    <div class="wp-block-columns alignwide"><!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:image -->
+            <figure class="wp-block-image"><img alt="" /></figure>
+            <!-- /wp:image -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:image -->
+            <figure class="wp-block-image"><img alt="" /></figure>
+            <!-- /wp:image -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:image -->
+            <figure class="wp-block-image"><img alt="" /></figure>
+            <!-- /wp:image -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:image -->
+            <figure class="wp-block-image"><img alt="" /></figure>
+            <!-- /wp:image -->
+        </div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+
+    <!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|sm","left":"var:preset|spacing|sm"},"margin":{"top":"var:preset|spacing|sm","bottom":"var:preset|spacing|lg"}}}} -->
+    <div class="wp-block-columns alignwide"
+        style="margin-top:var(--wp--preset--spacing--sm);margin-bottom:var(--wp--preset--spacing--lg)">
+        <!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:image -->
+            <figure class="wp-block-image"><img alt="" /></figure>
+            <!-- /wp:image -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:image -->
+            <figure class="wp-block-image"><img alt="" /></figure>
+            <!-- /wp:image -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:image -->
+            <figure class="wp-block-image"><img alt="" /></figure>
+            <!-- /wp:image -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:image -->
+            <figure class="wp-block-image"><img alt="" /></figure>
+            <!-- /wp:image -->
+        </div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
