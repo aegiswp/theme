46a4b1f7d98897df5cf1bf068c03cfa1385788f6
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-01-16 17:55:29 -0500

    Create image-below.php
    - Initial commit
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

---
+++ patterns/hero/image-below.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Title: Hero Image Below
+ * Slug: image-below
+ * Categories: hero
+ * ID: 609
+ */
+?>
+
+<!-- wp:group {"metadata":{"categories":["hero"],"patternName":"image-below","name":"Hero Image Below"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|md","bottom":"0"},"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}},"layout":{"type":"constrained"},"onclick":""} -->
+<div class="wp-block-group alignfull"
+    style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:var(--wp--preset--spacing--md);padding-bottom:0">
+    <!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"lineHeight":1.1},"spacing":{"margin":{"top":"var:preset|spacing|sm"}}},"fontSize":"52"} -->
+    <h1 class="wp-block-heading has-text-align-center has-52-font-size"
+        style="margin-top:var(--wp--preset--spacing--sm);line-height:1.1">Native Block Framework</h1>
+    <!-- /wp:heading -->
+
+    <!-- wp:group {"layout":{"type":"constrained","contentSize":"640px"},"onclick":""} -->
+    <div class="wp-block-group">
+        <!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|sm"}}}} -->
+        <p class="aligncenter has-text-align-center aligncenter" style="margin-top:var(--wp--preset--spacing--sm)">
+            Wireframe, build, and ship high-performance sites without the bloat—a zero-dependency architecture for total
+            creative freedom.</p>
+        <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:buttons {"style":{"spacing":{"blockGap":"var:preset|spacing|xs","margin":{"top":"var:preset|spacing|md","bottom":"var:preset|spacing|xl"}}},"layout":{"type":"flex","justifyContent":"center"}} -->
+    <div class="wp-block-buttons"
+        style="margin-top:var(--wp--preset--spacing--md);margin-bottom:var(--wp--preset--spacing--xl)">
+        <!-- wp:button {"onclick":"","size":"large","iconSize":"20px","iconPosition":"end"} -->
+        <div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#pricing">Get started</a>
+        </div>
+        <!-- /wp:button -->
+
+        <!-- wp:button {"className":"is-style-ghost","onclick":"","size":"large","iconSize":"20px","iconPosition":"end"} -->
+        <div class="wp-block-button is-style-ghost"><a class="wp-block-button__link wp-element-button"
+                href="#features">Learn more</a></div>
+        <!-- /wp:button -->
+    </div>
+    <!-- /wp:buttons -->
+
+    <!-- wp:image {"className":"","style":{"aspectRatio":{"all":""},"objectFit":{"all":"cover"},"objectPosition":{"all":"center"},"height":{"all":"100%"},"width":{"all":"100%"}}} -->
+    <figure class="wp-block-image"><img alt="" /></figure>
+    <!-- /wp:image -->
+</div>
+<!-- /wp:group -->
