ad145bbd32b319514657a17a85cf851ab270e49c
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-01-16 22:02:22 -0500

    Create front-page.php
    - initial commit
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

---
+++ patterns/template/front-page.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: Front Page
+ * Slug: front-page
+ * Categories: template
+ * Block Types: core/post-content
+ * Template Types: front-page, home
+ * Inserter: false
+ */
+?>
+
+<!-- wp:template-part {"slug":"header","tagName":"header","className":"site-header"} /-->
+
+<!-- wp:group {"tagName":"main","className":"site-main","metadata":{"name":"Main"},"layout":{"type":"constrained"}} -->
+<main class="wp-block-group site-main">
+	<!-- wp:pattern {"slug":"page-home"} /-->
+</main>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","tagName":"footer","className":"site-footer"} /-->
