45a68ba2479700c926092dc7b22eea248b6bae68
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-01-16 22:37:12 -0500

    Create taxomony-product_cat.php
    - Initial commit
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

---
+++ patterns/template/taxonomy-product_cat.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: Taxonomy Product Category
+ * Slug: taxonomy-product_cat
+ * Categories: template
+ * Template Types: taxonomy-product_cat
+ * Inserter: false
+ */
+?>
+<!-- wp:template-part {"slug":"header","tagName":"header","className":"site-header"} /-->
+
+<!-- wp:group {"tagName":"main","className":"site-main","metadata":{"name":"Main"},"layout":{"type":"constrained"}} -->
+<main class="wp-block-group site-main">
+	<!-- wp:woocommerce/product-collection {"query":{"inherit":true}} /-->
+</main>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","tagName":"footer","className":"site-footer"} /-->
