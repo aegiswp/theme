844cdfa536ba128ae7ad1e37482d4616398baa83
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-01-17 08:01:42 -0500

    Update centered.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/footer/centered.php
+++ patterns/footer/centered.php
@@ -16,12 +16,13 @@
         <!-- wp:image {"className":"is-style-icon","textColor":"primary-500","iconSet":"social","iconName":"aegis","iconSize":"75px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 fill=\u0022currentColor\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-696aa0b58f7c4\u0022 data-icon=\u0022social-aegis\u0022 width=\u002224\u0022 height=\u002224\u0022\u003e\u003ctitle id=\u0022icon-696aa0b58f7c4\u0022\u003eAegis Icon\u003c/title\u003e\u003cpath fill-rule=\u0022evenodd\u0022 d=\u0022M10.06 7.75 L12.02 3.87 L13.95 7.72 L16.65 9.29 L12.03 0 L7.34 9.3 L10.06 7.75 Z M18.37 12.72 L18.2 12.36 L15.5 10.79 L17.02 13.68 L20.05 18.61 L12.02 15.17 L3.98 18.62 L6.96 13.68 L8.39 10.81 L5.67 12.39 L5.5 12.71 L0 22.87 L12.01 16.87 L24 22.94 L18.37 12.72 Z\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} -->
         <figure class="wp-block-image is-style-icon has-primary-500-color has-text-color"
             style="--wp--custom--icon--color:var(--wp--preset--color--primary-500,currentColor);--wp--custom--icon--size:75px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; fill=&quot;currentColor&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-696aa0b58f7c4&quot; data-icon=&quot;social-aegis&quot; width=&quot;24&quot; height=&quot;24&quot;&gt;<title id=&quot;icon-696aa0b58f7c4&quot;&gt;Aegis Icon</title&gt;<path fill-rule=&quot;evenodd&quot; d=&quot;M10.06 7.75 L12.02 3.87 L13.95 7.72 L16.65 9.29 L12.03 0 L7.34 9.3 L10.06 7.75 Z M18.37 12.72 L18.2 12.36 L15.5 10.79 L17.02 13.68 L20.05 18.61 L12.02 15.17 L3.98 18.62 L6.96 13.68 L8.39 10.81 L5.67 12.39 L5.5 12.71 L0 22.87 L12.01 16.87 L24 22.94 L18.37 12.72 Z&quot;&gt;</path&gt;</svg&gt;')">
-            <img src="" alt="" /></figure>
+            <img src="" alt="" />
+        </figure>
         <!-- /wp:image -->
     </div>
     <!-- /wp:group -->
 
-    <!-- wp:navigation {"ref":5,"overlayMenu":"never","style":{"spacing":{"padding":{"top":"var:preset|spacing|sm","bottom":"var:preset|spacing|sm"}},"typography":{"fontStyle":"normal","fontWeight":"500"}},"layout":{"type":"flex","justifyContent":"center","flexWrap":"wrap"}} /-->
+    <!-- wp:navigation {"overlayMenu":"never","style":{"spacing":{"padding":{"top":"var:preset|spacing|sm","bottom":"var:preset|spacing|sm"}},"typography":{"fontStyle":"normal","fontWeight":"500"}},"layout":{"type":"flex","justifyContent":"center","flexWrap":"wrap"}} /-->
 
     <!-- wp:social-links {"iconColor":"neutral-400","iconColorValue":"#94a3b8","size":"has-small-icon-size","className":"is-style-logos-only","style":{"spacing":{"blockGap":{"top":"20px","left":"20px"},"padding":{"top":"var:preset|spacing|xxs","bottom":"var:preset|spacing|md"}}},"layout":{"type":"flex","justifyContent":"left","orientation":"horizontal"}} -->
     <ul class="wp-block-social-links has-small-icon-size has-icon-color is-style-logos-only"
