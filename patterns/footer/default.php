8ea0a8abb3a70526b022a4e83701ae4a83fedc6a
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-01-17 08:02:01 -0500

    Update default.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/footer/default.php
+++ patterns/footer/default.php
@@ -7,7 +7,7 @@
  */
 ?>
 
-<!-- wp:group {"metadata":{"name":"Default Footer"},"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"}},"typography":{"textDecoration":"none"}},"fontSize":"14","layout":{"inherit":true,"type":"constrained"}} -->
+<!-- wp:group {"metadata":{"name":"Footer"},"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"}},"typography":{"textDecoration":"none"}},"fontSize":"14","layout":{"inherit":true,"type":"constrained"}} -->
 <div class="wp-block-group alignfull has-14-font-size" style="margin-top:0;margin-bottom:0;text-decoration:none">
     <!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|xl"}}}} -->
     <div class="wp-block-columns alignwide"
@@ -20,7 +20,8 @@
                 <!-- wp:image {"className":"is-style-icon","style":{"elements":{"link":{"color":{"text":"var:preset|color|neutral-400"}}}},"textColor":"neutral-400","iconSet":"social","iconName":"aegis","iconSize":"35px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 fill=\u0022currentColor\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-696aa0b58f7c4\u0022 data-icon=\u0022social-aegis\u0022 width=\u002224\u0022 height=\u002224\u0022\u003e\u003ctitle id=\u0022icon-696aa0b58f7c4\u0022\u003eAegis Icon\u003c/title\u003e\u003cpath fill-rule=\u0022evenodd\u0022 d=\u0022M10.06 7.75 L12.02 3.87 L13.95 7.72 L16.65 9.29 L12.03 0 L7.34 9.3 L10.06 7.75 Z M18.37 12.72 L18.2 12.36 L15.5 10.79 L17.02 13.68 L20.05 18.61 L12.02 15.17 L3.98 18.62 L6.96 13.68 L8.39 10.81 L5.67 12.39 L5.5 12.71 L0 22.87 L12.01 16.87 L24 22.94 L18.37 12.72 Z\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} -->
                 <figure class="wp-block-image is-style-icon has-neutral-400-color has-text-color has-link-color"
                     style="--wp--custom--icon--color:var(--wp--preset--color--neutral-400,currentColor);--wp--custom--icon--size:35px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; fill=&quot;currentColor&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-696aa0b58f7c4&quot; data-icon=&quot;social-aegis&quot; width=&quot;24&quot; height=&quot;24&quot;&gt;<title id=&quot;icon-696aa0b58f7c4&quot;&gt;Aegis Icon</title&gt;<path fill-rule=&quot;evenodd&quot; d=&quot;M10.06 7.75 L12.02 3.87 L13.95 7.72 L16.65 9.29 L12.03 0 L7.34 9.3 L10.06 7.75 Z M18.37 12.72 L18.2 12.36 L15.5 10.79 L17.02 13.68 L20.05 18.61 L12.02 15.17 L3.98 18.62 L6.96 13.68 L8.39 10.81 L5.67 12.39 L5.5 12.71 L0 22.87 L12.01 16.87 L24 22.94 L18.37 12.72 Z&quot;&gt;</path&gt;</svg&gt;')">
-                    <img src="" alt="" /></figure>
+                    <img src="" alt="" />
+                </figure>
                 <!-- /wp:image -->
             </div>
             <!-- /wp:group -->
@@ -52,7 +53,7 @@
                     </h6>
                     <!-- /wp:heading -->
 
-                    <!-- wp:navigation {"ref":5,"overlayMenu":"never","style":{"spacing":{"margin":{"top":"var:preset|spacing|xs"},"blockGap":"10px"},"typography":{"fontStyle":"normal","fontWeight":"400"}},"layout":{"type":"flex","orientation":"vertical"}} /-->
+                    <!-- wp:navigation {"overlayMenu":"never","style":{"spacing":{"margin":{"top":"var:preset|spacing|xs"},"blockGap":"10px"},"typography":{"fontStyle":"normal","fontWeight":"400"}},"layout":{"type":"flex","orientation":"vertical"}} /-->
                 </div>
                 <!-- /wp:column -->
 
@@ -63,7 +64,7 @@
                     </h6>
                     <!-- /wp:heading -->
 
-                    <!-- wp:navigation {"ref":5,"overlayMenu":"never","style":{"spacing":{"margin":{"top":"var:preset|spacing|xs"},"blockGap":"10px"},"typography":{"fontStyle":"normal","fontWeight":"400"}},"layout":{"type":"flex","orientation":"vertical"}} /-->
+                    <!-- wp:navigation {"overlayMenu":"never","style":{"spacing":{"margin":{"top":"var:preset|spacing|xs"},"blockGap":"10px"},"typography":{"fontStyle":"normal","fontWeight":"400"}},"layout":{"type":"flex","orientation":"vertical"}} /-->
                 </div>
                 <!-- /wp:column -->
             </div>
@@ -81,7 +82,7 @@
                     </h6>
                     <!-- /wp:heading -->
 
-                    <!-- wp:navigation {"ref":5,"overlayMenu":"never","style":{"spacing":{"margin":{"top":"var:preset|spacing|xs"},"blockGap":"10px"},"typography":{"fontStyle":"normal","fontWeight":"400"}},"layout":{"type":"flex","orientation":"vertical"}} /-->
+                    <!-- wp:navigation {"overlayMenu":"never","style":{"spacing":{"margin":{"top":"var:preset|spacing|xs"},"blockGap":"10px"},"typography":{"fontStyle":"normal","fontWeight":"400"}},"layout":{"type":"flex","orientation":"vertical"}} /-->
                 </div>
                 <!-- /wp:column -->
 
@@ -92,7 +93,7 @@
                     </h6>
                     <!-- /wp:heading -->
 
-                    <!-- wp:navigation {"ref":5,"overlayMenu":"never","style":{"spacing":{"margin":{"top":"var:preset|spacing|xs"},"blockGap":"10px"},"typography":{"fontStyle":"normal","fontWeight":"400"}},"layout":{"type":"flex","orientation":"vertical"}} /-->
+                    <!-- wp:navigation {"overlayMenu":"never","style":{"spacing":{"margin":{"top":"var:preset|spacing|xs"},"blockGap":"10px"},"typography":{"fontStyle":"normal","fontWeight":"400"}},"layout":{"type":"flex","orientation":"vertical"}} /-->
                 </div>
                 <!-- /wp:column -->
             </div>
