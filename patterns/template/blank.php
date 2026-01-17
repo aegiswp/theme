20fa917d17c1aefd77a725ad06aaaff597050af9
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-01-16 21:59:34 -0500

    Create blank.php
    - Initial commit
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

---
+++ patterns/template/blank.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: Blank
+ * Slug: blank
+ * Categories: template
+ * Template Types: page
+ * Inserter: false
+ */
+?>
+
+<!-- wp:post-content {"layout":{"type":"default"}} /-->
