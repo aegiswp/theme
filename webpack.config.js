cfcfb460be78a535642b65ab67da886adbec6d16
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-01-23 15:10:41 -0500

    Update webpack.config.js
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
 const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const path = require('path');
 
 module.exports = {
     ...defaultConfig,
+    entry: {
+        // Block entries
+        'blocks/modal/index': path.resolve(__dirname, 'src/blocks/modal/index.tsx'),
+        'blocks/modal/view': path.resolve(__dirname, 'src/blocks/modal/view.ts'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].js',
+    },
     plugins: [
         ...defaultConfig.plugins,
         new BrowserSyncPlugin({
