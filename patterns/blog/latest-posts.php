80fb4c7285aa0e2ee60c5c974ff057d4707189f9
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-01-27 16:26:31 -0500

    Update latest-posts.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/blog/latest-posts.php
+++ patterns/blog/latest-posts.php
@@ -3,16 +3,16 @@
  * Title: Latest Posts
  * Slug: latest-posts
  * Categories: blog
- * ID: 452
+ * Keywords: blog, latest, posts, recent
+ * Description: A section displaying the latest blog posts.
+ * Viewport Width: 1280
  */
 ?>
 
 <!-- wp:group {"metadata":{"categories":["blog"],"patternName":"latest-posts","name":"Latest Posts"},"align":"full","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|sm","top":"var:preset|spacing|xs"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull"
-    style="padding-top:var(--wp--preset--spacing--xs);padding-bottom:var(--wp--preset--spacing--sm)">
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--xs);padding-bottom:var(--wp--preset--spacing--sm)">
     <!-- wp:heading {"textAlign":"center","className":"wp-block-heading","style":{"spacing":{"margin":{"bottom":"0","top":"0"},"padding":{"bottom":"var:preset|spacing|sm"}}}} -->
-    <h2 class="wp-block-heading has-text-align-center"
-        style="margin-top:0;margin-bottom:0;padding-bottom:var(--wp--preset--spacing--sm)">Latest Posts</h2>
+    <h2 class="wp-block-heading has-text-align-center" style="margin-top:0;margin-bottom:0;padding-bottom:var(--wp--preset--spacing--sm)">Latest Posts</h2>
     <!-- /wp:heading -->
 
     <!-- wp:query {"queryId":0,"query":{"perPage":3,"pages":"","offset":"","postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false},"align":"wide","layout":{"inherit":false},"style":{"spacing":{"blockGap":"1.5em"}}} -->
@@ -24,8 +24,7 @@
             <!-- wp:post-date {"metadata":{"bindings":{"datetime":{"source":"core/post-data","args":{"field":"date"}}}},"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"fontSize":"14"} /-->
 
             <!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"right":"3px","left":"3px"}}},"textColor":"neutral-200"} -->
-            <p class="has-neutral-200-color has-text-color"
-                style="margin-top:0;margin-bottom:0;padding-right:3px;padding-left:3px">|</p>
+            <p class="has-neutral-200-color has-text-color" style="margin-top:0;margin-bottom:0;padding-right:3px;padding-left:3px">|</p>
             <!-- /wp:paragraph -->
 
             <!-- wp:post-terms {"term":"category","style":{"typography":{"textDecoration":"none"},"spacing":{"margin":{"top":"0","bottom":"0"}}},"fontSize":"14"} /-->
