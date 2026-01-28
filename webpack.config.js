86b0fa516a3bd4e17119784120313a97c00bd92e
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-01-28 13:06:47 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,11 @@ module.exports = {
         'modal/index': path.resolve(__dirname, 'src/blocks/modal/index.tsx'),
         'modal/view': path.resolve(__dirname, 'src/blocks/modal/view.ts'),
         'modal/style': path.resolve(__dirname, 'src/blocks/modal/style.scss'),
+        'slider/index': path.resolve(__dirname, 'src/blocks/slider/index.tsx'),
+        'slider/view': path.resolve(__dirname, 'src/blocks/slider/view.ts'),
+        'slider/style': path.resolve(__dirname, 'src/blocks/slider/style.scss'),
+        'slide/index': path.resolve(__dirname, 'src/blocks/slide/index.tsx'),
+        'slide/style': path.resolve(__dirname, 'src/blocks/slide/style.scss'),
     },
     output: {
         path: path.resolve(__dirname, 'src/blocks'),
