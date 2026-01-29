ba4ad964e866a2f4a640afc81a6de3138fe7d909
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-01-29 04:30:10 -0500

    Update single-column.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/blog/single-column.php
+++ patterns/blog/single-column.php
@@ -3,20 +3,20 @@
  * Title: Single Column
  * Slug: single-column
  * Categories: blog
- * ID: 457
+ * Keywords: blog, single, column, posts, list
+ * Description: A single column blog post list.
+ * Viewport Width: 1280
  */
 ?>
 
-<!-- wp:group {"metadata":{"categories":["blog"],"patternName":"single-column","name":"Single Column"},"align":"full","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull">
+<!-- wp:group {"metadata":{"categories":["blog"],"patternName":"single-column","name":"Single Column"},"align":"wide","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide">
     <!-- wp:heading {"textAlign":"center","level":1,"style":{"spacing":{"margin":{"top":"var:preset|spacing|lg"}}}} -->
-    <h1 class="wp-block-heading has-text-align-center" style="margin-top:var(--wp--preset--spacing--lg)">Latest Posts
-    </h1>
+    <h1 class="wp-block-heading has-text-align-center" style="margin-top:var(--wp--preset--spacing--lg)">Latest Posts</h1>
     <!-- /wp:heading -->
 
     <!-- wp:query {"queryId":0,"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false},"layout":{"contentSize":null,"type":"constrained"},"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|lg","top":"var:preset|spacing|sm"}}}} -->
-    <div style="padding-top:var(--wp--preset--spacing--sm);padding-bottom:var(--wp--preset--spacing--lg)"
-        class="wp-block-query"><!-- wp:post-template {"layout":{"type":"default"}} -->
+    <div style="padding-top:var(--wp--preset--spacing--sm);padding-bottom:var(--wp--preset--spacing--lg)" class="wp-block-query"><!-- wp:post-template {"layout":{"type":"default"}} -->
         <!-- wp:post-featured-image {"height":"200px"} /-->
 
         <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|xxs","margin":{"top":"0","bottom":"var:preset|spacing|xxs"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
@@ -24,8 +24,7 @@
             <!-- wp:post-terms {"term":"category","style":{"typography":{"textDecoration":"none"},"spacing":{"margin":{"top":"0","bottom":"0"}}},"fontSize":"14"} /-->
 
             <!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"right":"3px","left":"3px"}}},"textColor":"neutral-200"} -->
-            <p class="has-neutral-200-color has-text-color"
-                style="margin-top:0;margin-bottom:0;padding-right:3px;padding-left:3px">|</p>
+            <p class="has-neutral-200-color has-text-color" style="margin-top:0;margin-bottom:0;padding-right:3px;padding-left:3px">|</p>
             <!-- /wp:paragraph -->
 
             <!-- wp:post-date {"metadata":{"bindings":{"datetime":{"source":"core/post-data","args":{"field":"date"}}}},"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"fontSize":"14"} /-->
