55d276d3dd58af7a1e6dac0c5998a9d48dbebf6e
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-01-29 15:20:49 -0500

    Update box.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/cta/box.php
+++ patterns/cta/box.php
@@ -3,30 +3,28 @@
  * Title: Box
  * Slug: box
  * Categories: cta
- * ID: 515
+ * Keywords: cta, box, call to action, button
+ * Description: A boxed call-to-action section.
+ * Viewport Width: 1280
  */
 ?>
 
-<!-- wp:group {"metadata":{"categories":["cta"],"patternName":"box","name":"Box"},"align":"full","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|sm","top":"var:preset|spacing|sm"}}},"layout":{"inherit":true,"type":"constrained"}} -->
-<div class="wp-block-group alignfull"
-    style="padding-top:var(--wp--preset--spacing--sm);padding-bottom:var(--wp--preset--spacing--sm)">
+<!-- wp:group {"metadata":{"categories":["cta"],"patternName":"box","name":"Box"},"align":"wide","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|sm","top":"var:preset|spacing|sm"}}},"layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--sm);padding-bottom:var(--wp--preset--spacing--sm)">
     <!-- wp:group {"align":"wide","className":"is-style-surface","style":{"spacing":{"margin":{"top":"0"},"padding":{"top":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg"}}},"backgroundColor":"neutral-50","layout":{"contentSize":"600px","type":"constrained"}} -->
-    <div class="wp-block-group alignwide is-style-surface has-neutral-50-background-color has-background"
-        style="margin-top:0;padding-top:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--lg)">
-        <!-- wp:heading {"textAlign":"center"} -->
-        <h2 class="wp-block-heading has-text-align-center">Ready to Collaborate?</h2>
+    <div class="wp-block-group alignwide is-style-surface has-neutral-50-background-color has-background" style="margin-top:0;padding-top:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--lg)">
+        <!-- wp:heading {"textAlign":"center","className":"has-48-font-size"} -->
+        <h2 class="wp-block-heading has-text-align-center has-48-font-size">Ready to Collaborate?</h2>
         <!-- /wp:heading -->
 
         <!-- wp:paragraph {"align":"center"} -->
-        <p class="aligncenter has-text-align-center aligncenter">We are currently accepting new commissions for the
-            upcoming quarter. Secure your production slot to transform your concept into reality.</p>
+        <p class="aligncenter has-text-align-center aligncenter">We are currently accepting new commissions for the upcoming quarter. Secure your production slot to transform your concept into reality.</p>
         <!-- /wp:paragraph -->
 
         <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
         <div class="wp-block-buttons">
             <!-- wp:button {"className":"is-style-transparent","iconSet":"wordpress","iconName":"calendar","iconSize":"20px","iconPosition":"end","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-696a6a1c63217\u0022 data-icon=\u0022wordpress-calendar\u0022 width=\u002224\u0022 height=\u002224\u0022 fill=\u0022currentColor\u0022\u003e\u003ctitle id=\u0022icon-696a6a1c63217\u0022\u003eCalendar Icon\u003c/title\u003e\u003cpath d=\u0022M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm.5 16c0 .3-.2.5-.5.5H5c-.3 0-.5-.2-.5-.5V7h15v12zM9 10H7v2h2v-2zm0 4H7v2h2v-2zm4-4h-2v2h2v-2zm4 0h-2v2h2v-2zm-4 4h-2v2h2v-2zm4 0h-2v2h2v-2z\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} -->
-            <div class="wp-block-button is-style-transparent"
-                style="--wp--custom--icon--size:20px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-696a6a1c63217&quot; data-icon=&quot;wordpress-calendar&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-696a6a1c63217&quot;&gt;Calendar Icon</title&gt;<path d=&quot;M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm.5 16c0 .3-.2.5-.5.5H5c-.3 0-.5-.2-.5-.5V7h15v12zM9 10H7v2h2v-2zm0 4H7v2h2v-2zm4-4h-2v2h2v-2zm4 0h-2v2h2v-2zm-4 4h-2v2h2v-2zm4 0h-2v2h2v-2z&quot;&gt;</path&gt;</svg&gt;')">
+            <div class="wp-block-button is-style-transparent" style="--wp--custom--icon--size:20px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-696a6a1c63217&quot; data-icon=&quot;wordpress-calendar&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-696a6a1c63217&quot;&gt;Calendar Icon</title&gt;<path d=&quot;M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm.5 16c0 .3-.2.5-.5.5H5c-.3 0-.5-.2-.5-.5V7h15v12zM9 10H7v2h2v-2zm0 4H7v2h2v-2zm4-4h-2v2h2v-2zm4 0h-2v2h2v-2zm-4 4h-2v2h2v-2zm4 0h-2v2h2v-2z&quot;&gt;</path&gt;</svg&gt;')">
                 <a class="wp-block-button__link wp-element-button">Book Consultation</a></div>
             <!-- /wp:button -->
         </div>
