77bd4bee7625320d27dc1f60f18cf263f233ea0e
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-01-29 13:15:46 -0500

    Update banner.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/cta/banner.php
+++ patterns/cta/banner.php
@@ -3,25 +3,23 @@
  * Title: Feature Banner
  * Slug: banner
  * Categories: cta
- * ID: 539
+ * Keywords: cta, banner, marquee, announcement
+ * Description: A scrolling banner for announcements or promotions.
+ * Viewport Width: 1280
  */
 ?>
 
-<!-- wp:group {"metadata":{"categories":["cta"],"patternName":"banner","name":"Feature Banner"},"align":"full","className":"is-style-surface","style":{"\u002d\u002dmarquee-speed-mobile":"20s","\u002d\u002dmarquee-speed-desktop":"30s","\u002d\u002dmarquee-direction":"forwards","\u002d\u002dmarquee-pause":"paused","spacing":{"blockGap":"0","padding":{"top":"var:preset|spacing|xs","bottom":"var:preset|spacing|xs","left":"var:preset|spacing|lg","right":"var:preset|spacing|lg"}},"typography":{"fontStyle":"normal","fontWeight":"500"}},"backgroundColor":"primary","textColor":"background","layout":{"type":"flex","flexWrap":"nowrap","orientation":"marquee","justifyContent":"center"},"speedMobile":"20","speedDesktop":"30","reverse":false,"pauseOnHover":true,"fadeEdges":false} -->
-<div class="wp-block-group alignfull is-style-surface has-background-color has-primary-background-color has-text-color has-background"
-    style="padding-top:var(--wp--preset--spacing--xs);padding-right:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--xs);padding-left:var(--wp--preset--spacing--lg);font-style:normal;font-weight:500;--marquee-speed-mobile:20s;--marquee-speed-desktop:30s;--marquee-direction:forwards;--marquee-pause:paused">
+<!-- wp:group {"metadata":{"categories":["cta"],"patternName":"banner","name":"Feature Banner"},"align":"wide","className":"is-style-surface","style":{"\u002d\u002dmarquee-speed-mobile":"20s","\u002d\u002dmarquee-speed-desktop":"30s","\u002d\u002dmarquee-direction":"forwards","\u002d\u002dmarquee-pause":"paused","spacing":{"blockGap":"0","padding":{"top":"var:preset|spacing|xs","bottom":"var:preset|spacing|xs","left":"var:preset|spacing|lg","right":"var:preset|spacing|lg"}},"typography":{"fontStyle":"normal","fontWeight":"500"}},"backgroundColor":"primary","textColor":"background","layout":{"type":"constrained"},"speedMobile":"20","speedDesktop":"30","reverse":false,"pauseOnHover":true,"fadeEdges":false} -->
+<div class="wp-block-group alignwide is-style-surface has-background-color has-primary-background-color has-text-color has-background"
+    style="padding-top:var(--wp--preset--spacing--xs);padding-right:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--xs);padding-left:var(--wp--preset--spacing--lg);font-style:normal;font-weight:500">
     <!-- wp:group {"style":{"spacing":{"blockGap":{"left":"0"}}},"layout":{"type":"flex","flexWrap":"wrap"}} -->
     <div class="wp-block-group"><!-- wp:paragraph -->
-        <p>Archive Event: Selected editions are now 40% off for a limited time. <a href="#" data-type="page"
-                data-id="539">Shop
-                Collection</a> </p>
+        <p>Archive Event: Selected editions are now 40% off for a limited time. <a href="#" data-type="page" data-id="539">Shop Collection</a></p>
         <!-- /wp:paragraph -->
 
         <!-- wp:image {"className":"is-style-icon","iconSet":"wordpress","iconName":"arrow-right","iconSize":"15px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-690918ef30ed4\u0022 data-icon=\u0022wordpress-arrow-right\u0022 width=\u002224\u0022 height=\u002224\u0022 fill=\u0022currentColor\u0022\u003e\u003ctitle id=\u0022icon-690918ef30ed4\u0022\u003eArrow Right Icon\u003c/title\u003e\u003cpath d=\u0022m14.5 6.5-1 1 3.7 3.7H4v1.6h13.2l-3.7 3.7 1 1 5.6-5.5z\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} -->
-        <figure class="wp-block-image is-style-icon"
-            style="--wp--custom--icon--size:15px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-690918ef30ed4&quot; data-icon=&quot;wordpress-arrow-right&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-690918ef30ed4&quot;&gt;Arrow Right Icon</title&gt;<path d=&quot;m14.5 6.5-1 1 3.7 3.7H4v1.6h13.2l-3.7 3.7 1 1 5.6-5.5z&quot;&gt;</path&gt;</svg&gt;')">
-            <img alt="" />
-        </figure>
+        <figure class="wp-block-image is-style-icon" style="--wp--custom--icon--size:15px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-690918ef30ed4&quot; data-icon=&quot;wordpress-arrow-right&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-690918ef30ed4&quot;&gt;Arrow Right Icon</title&gt;<path d=&quot;m14.5 6.5-1 1 3.7 3.7H4v1.6h13.2l-3.7 3.7 1 1 5.6-5.5z&quot;&gt;</path&gt;</svg&gt;')">
+            <img alt="" /></figure>
         <!-- /wp:image -->
     </div>
     <!-- /wp:group -->
