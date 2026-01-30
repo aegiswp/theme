ab9bb044b213060c65a508e4a4826040f5336bc4
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-01-30 10:32:15 -0500

    Update image.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/feature/image.php
+++ patterns/feature/image.php
@@ -3,16 +3,16 @@
  * Title: Image
  * Slug: image
  * Categories: feature
- * ID: 556
+ * Keywords: feature, image, content, layout
+ * Description: A feature section with image and content.
+ * Viewport Width: 1280
  */
 ?>
 
-<!-- wp:group {"lock":{"move":false,"remove":false},"metadata":{"name":"Image"},"align":"full","className":"is-style-surface","style":{"spacing":{"padding":{"top":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull is-style-surface"
-    style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--lg)">
+<!-- wp:group {"lock":{"move":false,"remove":false},"metadata":{"name":"Image","categories":["feature"],"patternName":"image"},"align":"full","className":"is-style-surface","style":{"spacing":{"padding":{"top":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-style-surface" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--lg)">
     <!-- wp:columns {"verticalAlignment":"center","align":"wide","className":"is-reverse-on-mobile is-reverse-mobile","style":{"spacing":{"padding":{"top":"var:preset|spacing|md","bottom":"var:preset|spacing|md"},"blockGap":{"top":"var:preset|spacing|lg","left":"var:preset|spacing|lg"}},"u002du002dflex-direction":"column-reverse","u002du002dflex-direction-desktop":"row"}} -->
-    <div class="wp-block-columns alignwide are-vertically-aligned-center is-reverse-on-mobile is-reverse-mobile"
-        style="padding-top:var(--wp--preset--spacing--md);padding-bottom:var(--wp--preset--spacing--md)">
+    <div class="wp-block-columns alignwide are-vertically-aligned-center is-reverse-on-mobile is-reverse-mobile" style="padding-top:var(--wp--preset--spacing--md);padding-bottom:var(--wp--preset--spacing--md)">
         <!-- wp:column {"verticalAlignment":"center","style":{"order":{"all":"","mobile":"1"}}} -->
         <div class="wp-block-column is-vertically-aligned-center">
             <!-- wp:image {"aspectRatio":"16/9","scale":"cover","sizeSlug":"full","linkDestination":"none","style":{"aspectRatio":{"all":""},"objectFit":{"all":""}}} -->
