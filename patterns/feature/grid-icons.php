e3618d67a4ea63b23f82e40f580f428d738d48c5
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-01-30 08:50:41 -0500

    Create grid-icons.php
    - Initial commit
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

---
+++ patterns/feature/grid-icons.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * Title: Feature Grid Icons
+ * Slug: grid-icons
+ * Categories: feature
+ * Keywords: feature, grid, icons, services, benefits
+ * Description: A grid of features with icons and descriptions.
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"metadata":{"categories":["feature"],"patternName":"grid-icons","name":"Feature Grid Icons"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--xl);padding-bottom:var(--wp--preset--spacing--xl)">
+    <!-- wp:group {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|lg"}}},"layout":{"type":"constrained","contentSize":"600px"}} -->
+    <div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--lg)">
+        <!-- wp:paragraph {"align":"center","className":"is-style-sub-heading"} -->
+        <p class="aligncenter has-text-align-center is-style-sub-heading aligncenter">Why Choose Us</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:heading {"textAlign":"center","fontSize":"40"} -->
+        <h2 class="wp-block-heading has-text-align-center has-40-font-size">Features That Set Us Apart</h2>
+        <!-- /wp:heading -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|md"}}}} -->
+    <div class="wp-block-columns alignwide">
+        <!-- wp:column {"className":"is-style-surface","style":{"spacing":{"padding":{"top":"var:preset|spacing|md","bottom":"var:preset|spacing|md","left":"var:preset|spacing|md","right":"var:preset|spacing|md"}}}} -->
+        <div class="wp-block-column is-style-surface" style="padding-top:var(--wp--preset--spacing--md);padding-right:var(--wp--preset--spacing--md);padding-bottom:var(--wp--preset--spacing--md);padding-left:var(--wp--preset--spacing--md)">
+            <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+            <div class="wp-block-group">
+                <!-- wp:image {"align":"left","className":"is-style-icon","iconSet":"wordpress","iconName":"arrow-up-right","iconSize":"32px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 xml:space=\u0022preserve\u0022 viewBox=\u00220 0 24 24\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-697cb40ca7384\u0022 data-icon=\u0022wordpress-arrow-up-right\u0022 width=\u002224\u0022 height=\u002224\u0022 fill=\u0022currentColor\u0022\u003e\u003ctitle id=\u0022icon-697cb40ca7384\u0022\u003eArrow Up Right Icon\u003c/title\u003e\u003cpath d=\u0022M10.1 6.1v1.4h5.2L6 16.8 7.2 18l9.3-9.3v5.2h1.4L18 6l-7.9.1z\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} -->
+                <figure class="wp-block-image alignleft is-style-icon" style="--wp--custom--icon--size:32px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; xml:space=&quot;preserve&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-697cb40ca7384&quot; data-icon=&quot;wordpress-arrow-up-right&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-697cb40ca7384&quot;&gt;Arrow Up Right Icon</title&gt;<path d=&quot;M10.1 6.1v1.4h5.2L6 16.8 7.2 18l9.3-9.3v5.2h1.4L18 6l-7.9.1z&quot;&gt;</path&gt;</svg&gt;')">
+                    <img alt="" /></figure>
+                <!-- /wp:image -->
+            </div>
+            <!-- /wp:group -->
+
+            <!-- wp:heading {"level":3,"fontSize":"20"} -->
+            <h3 class="wp-block-heading has-20-font-size">Lightning Fast</h3>
+            <!-- /wp:heading -->
+
+            <!-- wp:paragraph {"textColor":"neutral-600","fontSize":"15"} -->
+            <p class="has-neutral-600-color has-text-color has-15-font-size">Optimized for speed with zero-dependency
+                architecture. Your site loads in milliseconds, not seconds.</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"className":"is-style-surface","style":{"spacing":{"padding":{"top":"var:preset|spacing|md","bottom":"var:preset|spacing|md","left":"var:preset|spacing|md","right":"var:preset|spacing|md"}}}} -->
+        <div class="wp-block-column is-style-surface" style="padding-top:var(--wp--preset--spacing--md);padding-right:var(--wp--preset--spacing--md);padding-bottom:var(--wp--preset--spacing--md);padding-left:var(--wp--preset--spacing--md)">
+            <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+            <div class="wp-block-group">
+                <!-- wp:image {"align":"left","className":"is-style-icon","iconSet":"wordpress","iconName":"styles","iconSize":"32px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-697cb40d1e874\u0022 data-icon=\u0022wordpress-styles\u0022 width=\u002224\u0022 height=\u002224\u0022 fill=\u0022currentColor\u0022\u003e\u003ctitle id=\u0022icon-697cb40d1e874\u0022\u003eStyles Icon\u003c/title\u003e\u003cpath d=\u0022M12 4c-4.4 0-8 3.6-8 8v.1c0 4.1 3.2 7.5 7.2 7.9h.8c4.4 0 8-3.6 8-8s-3.6-8-8-8zm0 15V5c3.9 0 7 3.1 7 7s-3.1 7-7 7z\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} -->
+                <figure class="wp-block-image alignleft is-style-icon" style="--wp--custom--icon--size:32px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-697cb40d1e874&quot; data-icon=&quot;wordpress-styles&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-697cb40d1e874&quot;&gt;Styles Icon</title&gt;<path d=&quot;M12 4c-4.4 0-8 3.6-8 8v.1c0 4.1 3.2 7.5 7.2 7.9h.8c4.4 0 8-3.6 8-8s-3.6-8-8-8zm0 15V5c3.9 0 7 3.1 7 7s-3.1 7-7 7z&quot;&gt;</path&gt;</svg&gt;')">
+                    <img alt="" /></figure>
+                <!-- /wp:image -->
+            </div>
+            <!-- /wp:group -->
+
+            <!-- wp:heading {"level":3,"fontSize":"20"} -->
+            <h3 class="wp-block-heading has-20-font-size">Fully Customizable</h3>
+            <!-- /wp:heading -->
+
+            <!-- wp:paragraph {"textColor":"neutral-600","fontSize":"15"} -->
+            <p class="has-neutral-600-color has-text-color has-15-font-size">Every element is designed to be modified.
+                Create unique designs without touching code.</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"className":"is-style-surface","style":{"spacing":{"padding":{"top":"var:preset|spacing|md","bottom":"var:preset|spacing|md","left":"var:preset|spacing|md","right":"var:preset|spacing|md"}}}} -->
+        <div class="wp-block-column is-style-surface" style="padding-top:var(--wp--preset--spacing--md);padding-right:var(--wp--preset--spacing--md);padding-bottom:var(--wp--preset--spacing--md);padding-left:var(--wp--preset--spacing--md)">
+            <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+            <div class="wp-block-group">
+                <!-- wp:image {"align":"left","className":"is-style-icon","iconSet":"wordpress","iconName":"shield","iconSize":"32px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-697cb40d15160\u0022 data-icon=\u0022wordpress-shield\u0022 width=\u002224\u0022 height=\u002224\u0022 fill=\u0022currentColor\u0022\u003e\u003ctitle id=\u0022icon-697cb40d15160\u0022\u003eShield Icon\u003c/title\u003e\u003cpath fill-rule=\u0022evenodd\u0022 d=\u0022m12 3.176 6.75 3.068v4.574c0 3.9-2.504 7.59-6.035 8.755a2.283 2.283 0 0 1-1.43 0c-3.53-1.164-6.035-4.856-6.035-8.755V6.244L12 3.176zM6.75 7.21v3.608c0 3.313 2.145 6.388 5.005 7.33a.775.775 0 0 0 .49 0c2.86-.942 5.005-4.017 5.005-7.33V7.21L12 4.824 6.75 7.21z\u0022 clip-rule=\u0022evenodd\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} -->
+                <figure class="wp-block-image alignleft is-style-icon" style="--wp--custom--icon--size:32px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-697cb40d15160&quot; data-icon=&quot;wordpress-shield&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-697cb40d15160&quot;&gt;Shield Icon</title&gt;<path fill-rule=&quot;evenodd&quot; d=&quot;m12 3.176 6.75 3.068v4.574c0 3.9-2.504 7.59-6.035 8.755a2.283 2.283 0 0 1-1.43 0c-3.53-1.164-6.035-4.856-6.035-8.755V6.244L12 3.176zM6.75 7.21v3.608c0 3.313 2.145 6.388 5.005 7.33a.775.775 0 0 0 .49 0c2.86-.942 5.005-4.017 5.005-7.33V7.21L12 4.824 6.75 7.21z&quot; clip-rule=&quot;evenodd&quot;&gt;</path&gt;</svg&gt;')">
+                    <img alt="" /></figure>
+                <!-- /wp:image -->
+            </div>
+            <!-- /wp:group -->
+
+            <!-- wp:heading {"level":3,"fontSize":"20"} -->
+            <h3 class="wp-block-heading has-20-font-size">Secure by Default</h3>
+            <!-- /wp:heading -->
+
+            <!-- wp:paragraph {"textColor":"neutral-600","fontSize":"15"} -->
+            <p class="has-neutral-600-color has-text-color has-15-font-size">Built with security best practices. Regular
+                updates keep your site protected against vulnerabilities.</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+
+    <!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|md"},"margin":{"top":"var:preset|spacing|md"}}}} -->
+    <div class="wp-block-columns alignwide" style="margin-top:var(--wp--preset--spacing--md)">
+        <!-- wp:column {"className":"is-style-surface","style":{"spacing":{"padding":{"top":"var:preset|spacing|md","bottom":"var:preset|spacing|md","left":"var:preset|spacing|md","right":"var:preset|spacing|md"}}}} -->
+        <div class="wp-block-column is-style-surface" style="padding-top:var(--wp--preset--spacing--md);padding-right:var(--wp--preset--spacing--md);padding-bottom:var(--wp--preset--spacing--md);padding-left:var(--wp--preset--spacing--md)">
+            <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+            <div class="wp-block-group">
+                <!-- wp:image {"align":"left","className":"is-style-icon","iconSet":"wordpress","iconName":"mobile","iconSize":"32px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-697cb40cea6e6\u0022 data-icon=\u0022wordpress-mobile\u0022 width=\u002224\u0022 height=\u002224\u0022 fill=\u0022currentColor\u0022\u003e\u003ctitle id=\u0022icon-697cb40cea6e6\u0022\u003eMobile Icon\u003c/title\u003e\u003cpath d=\u0022M15 4H9c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h6c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm.5 14c0 .3-.2.5-.5.5H9c-.3 0-.5-.2-.5-.5V6c0-.3.2-.5.5-.5h6c.3 0 .5.2.5.5v12zm-4.5-.5h2V16h-2v1.5z\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} -->
+                <figure class="wp-block-image alignleft is-style-icon" style="--wp--custom--icon--size:32px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-697cb40cea6e6&quot; data-icon=&quot;wordpress-mobile&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-697cb40cea6e6&quot;&gt;Mobile Icon</title&gt;<path d=&quot;M15 4H9c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h6c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm.5 14c0 .3-.2.5-.5.5H9c-.3 0-.5-.2-.5-.5V6c0-.3.2-.5.5-.5h6c.3 0 .5.2.5.5v12zm-4.5-.5h2V16h-2v1.5z&quot;&gt;</path&gt;</svg&gt;')">
+                    <img alt="" /></figure>
+                <!-- /wp:image -->
+            </div>
+            <!-- /wp:group -->
+
+            <!-- wp:heading {"level":3,"fontSize":"20"} -->
+            <h3 class="wp-block-heading has-20-font-size">Responsive Design</h3>
+            <!-- /wp:heading -->
+
+            <!-- wp:paragraph {"textColor":"neutral-600","fontSize":"15"} -->
+            <p class="has-neutral-600-color has-text-color has-15-font-size">Looks perfect on every device. From mobile
+                phones to ultra-wide monitors, your content adapts seamlessly.</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"className":"is-style-surface","style":{"spacing":{"padding":{"top":"var:preset|spacing|md","bottom":"var:preset|spacing|md","left":"var:preset|spacing|md","right":"var:preset|spacing|md"}}}} -->
+        <div class="wp-block-column is-style-surface" style="padding-top:var(--wp--preset--spacing--md);padding-right:var(--wp--preset--spacing--md);padding-bottom:var(--wp--preset--spacing--md);padding-left:var(--wp--preset--spacing--md)">
+            <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+            <div class="wp-block-group">
+                <!-- wp:image {"align":"left","className":"is-style-icon","iconSet":"wordpress","iconName":"search","iconSize":"32px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-697cb40d1318e\u0022 data-icon=\u0022wordpress-search\u0022 width=\u002224\u0022 height=\u002224\u0022 fill=\u0022currentColor\u0022\u003e\u003ctitle id=\u0022icon-697cb40d1318e\u0022\u003eSearch Icon\u003c/title\u003e\u003cpath d=\u0022M13 5c-3.3 0-6 2.7-6 6 0 1.4.5 2.7 1.3 3.7l-3.8 3.8 1.1 1.1 3.8-3.8c1 .8 2.3 1.3 3.7 1.3 3.3 0 6-2.7 6-6S16.3 5 13 5zm0 10.5c-2.5 0-4.5-2-4.5-4.5s2-4.5 4.5-4.5 4.5 2 4.5 4.5-2 4.5-4.5 4.5z\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} -->
+                <figure class="wp-block-image alignleft is-style-icon" style="--wp--custom--icon--size:32px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-697cb40d1318e&quot; data-icon=&quot;wordpress-search&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-697cb40d1318e&quot;&gt;Search Icon</title&gt;<path d=&quot;M13 5c-3.3 0-6 2.7-6 6 0 1.4.5 2.7 1.3 3.7l-3.8 3.8 1.1 1.1 3.8-3.8c1 .8 2.3 1.3 3.7 1.3 3.3 0 6-2.7 6-6S16.3 5 13 5zm0 10.5c-2.5 0-4.5-2-4.5-4.5s2-4.5 4.5-4.5 4.5 2 4.5 4.5-2 4.5-4.5 4.5z&quot;&gt;</path&gt;</svg&gt;')">
+                    <img alt="" /></figure>
+                <!-- /wp:image -->
+            </div>
+            <!-- /wp:group -->
+
+            <!-- wp:heading {"level":3,"fontSize":"20"} -->
+            <h3 class="wp-block-heading has-20-font-size">SEO Optimized</h3>
+            <!-- /wp:heading -->
+
+            <!-- wp:paragraph {"textColor":"neutral-600","fontSize":"15"} -->
+            <p class="has-neutral-600-color has-text-color has-15-font-size">Clean semantic markup and optimized
+                structure help search engines understand and rank your content.</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"className":"is-style-surface","style":{"spacing":{"padding":{"top":"var:preset|spacing|md","bottom":"var:preset|spacing|md","left":"var:preset|spacing|md","right":"var:preset|spacing|md"}}}} -->
+        <div class="wp-block-column is-style-surface" style="padding-top:var(--wp--preset--spacing--md);padding-right:var(--wp--preset--spacing--md);padding-bottom:var(--wp--preset--spacing--md);padding-left:var(--wp--preset--spacing--md)">
+            <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+            <div class="wp-block-group">
+                <!-- wp:image {"align":"left","className":"is-style-icon","iconSet":"wordpress","iconName":"code","iconSize":"32px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-697cb40cb6844\u0022 data-icon=\u0022wordpress-code\u0022 width=\u002224\u0022 height=\u002224\u0022 fill=\u0022currentColor\u0022\u003e\u003ctitle id=\u0022icon-697cb40cb6844\u0022\u003eCode Icon\u003c/title\u003e\u003cpath d=\u0022m20.8 10.7-4.3-4.3-1.1 1.1 4.3 4.3c.1.1.1.3 0 .4l-4.3 4.3 1.1 1.1 4.3-4.3c.7-.8.7-1.9 0-2.6zM4.2 11.8l4.3-4.3-1-1-4.3 4.3c-.7.7-.7 1.8 0 2.5l4.3 4.3 1.1-1.1-4.3-4.3c-.2-.1-.2-.3-.1-.4z\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} -->
+                <figure class="wp-block-image alignleft is-style-icon" style="--wp--custom--icon--size:32px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-697cb40cb6844&quot; data-icon=&quot;wordpress-code&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-697cb40cb6844&quot;&gt;Code Icon</title&gt;<path d=&quot;m20.8 10.7-4.3-4.3-1.1 1.1 4.3 4.3c.1.1.1.3 0 .4l-4.3 4.3 1.1 1.1 4.3-4.3c.7-.8.7-1.9 0-2.6zM4.2 11.8l4.3-4.3-1-1-4.3 4.3c-.7.7-.7 1.8 0 2.5l4.3 4.3 1.1-1.1-4.3-4.3c-.2-.1-.2-.3-.1-.4z&quot;&gt;</path&gt;</svg&gt;')">
+                    <img alt="" /></figure>
+                <!-- /wp:image -->
+            </div>
+            <!-- /wp:group -->
+
+            <!-- wp:heading {"level":3,"fontSize":"20"} -->
+            <h3 class="wp-block-heading has-20-font-size">Developer Friendly</h3>
+            <!-- /wp:heading -->
+
+            <!-- wp:paragraph {"textColor":"neutral-600","fontSize":"15"} -->
+            <p class="has-neutral-600-color has-text-color has-15-font-size">Clean, well-documented code that developers
+                love. Extend and customize with confidence.</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
