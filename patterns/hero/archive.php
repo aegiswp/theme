018b1aba8d340dc530303e1ab0c77e2e7900cfd0
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-01-30 10:40:18 -0500

    Update archive.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/hero/archive.php
+++ patterns/hero/archive.php
@@ -3,13 +3,14 @@
  * Title: Hero Archive
  * Slug: archive
  * Categories: hero
- * ID: 603
+ * Keywords: hero, archive, header, title
+ * Description: A hero section for archive pages.
+ * Viewport Width: 1280
  */
 ?>
 
 <!-- wp:group {"metadata":{"categories":["hero"],"patternName":"archive","name":"Archive"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|md","bottom":"var:preset|spacing|md"},"blockGap":"var:preset|spacing|md"}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"
-    style="padding-top:var(--wp--preset--spacing--md);padding-bottom:var(--wp--preset--spacing--md)">
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--md);padding-bottom:var(--wp--preset--spacing--md)">
     <!-- wp:query-title {"type":"archive","textAlign":"center"} /-->
 
     <!-- wp:post-terms {"term":"category","textAlign":"center","style":{"spacing":{"blockGap":"var:preset|spacing|xs","padding":{"right":"var:preset|spacing|xs","left":"var:preset|spacing|xs"}}},"align":"center","showAll":true} /-->
