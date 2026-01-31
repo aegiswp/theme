1e1b16cf5826da25c1d5898dd376ac81a81c9b72
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-01-30 19:37:46 -0500

    Create three-columns-dark.php
    - Initial commit
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

---
+++ patterns/stats/three-columns-dark.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Title: Stats Three Columns Dark
+ * Slug: three-columns-dark
+ * Categories: stats
+ * Keywords: stats, three columns, dark, numbers, counters
+ * Description: A dark three-column statistics section.
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"metadata":{"categories":["stats"],"patternName":"three-columns-dark","name":"Stats Three Columns Dark"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl"}}},"backgroundColor":"neutral-950","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-neutral-950-background-color has-background" style="padding-top:var(--wp--preset--spacing--xl);padding-bottom:var(--wp--preset--spacing--xl)">
+    <!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|xl"}}}} -->
+    <div class="wp-block-columns alignwide">
+        <!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|xs"}}} -->
+        <div class="wp-block-column">
+            <!-- wp:paragraph {"align":"center","className":"is-style-counter is-style-heading","style":{"counter":{"start":"0","end":500,"duration":"2","delay":"0","suffix":"+"}},"textColor":"white","fontSize":"52"} -->
+            <p class="aligncenter has-text-align-center is-style-counter is-style-heading has-white-color has-text-color has-52-font-size aligncenter">500+</p>
+            <!-- /wp:paragraph -->
+
+            <!-- wp:paragraph {"align":"center","textColor":"neutral-400","fontSize":"18"} -->
+            <p class="aligncenter has-text-align-center has-neutral-400-color has-text-color has-18-font-size aligncenter">Projects Completed</p>
+            <!-- /wp:paragraph -->
+
+            <!-- wp:paragraph {"align":"center","textColor":"neutral-500","fontSize":"14"} -->
+            <p class="aligncenter has-text-align-center has-neutral-500-color has-text-color has-14-font-size aligncenter">Across 30+ industries worldwide</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|xs"}}} -->
+        <div class="wp-block-column">
+            <!-- wp:paragraph {"align":"center","className":"is-style-counter is-style-heading","style":{"counter":{"start":"0","end":98,"duration":"2","delay":"0","suffix":"%"}},"textColor":"white","fontSize":"52"} -->
+            <p class="aligncenter has-text-align-center is-style-counter is-style-heading has-white-color has-text-color has-52-font-size aligncenter">98%</p>
+            <!-- /wp:paragraph -->
+
+            <!-- wp:paragraph {"align":"center","textColor":"neutral-400","fontSize":"18"} -->
+            <p class="aligncenter has-text-align-center has-neutral-400-color has-text-color has-18-font-size aligncenter">Client Satisfaction</p>
+            <!-- /wp:paragraph -->
+
+            <!-- wp:paragraph {"align":"center","textColor":"neutral-500","fontSize":"14"} -->
+            <p class="aligncenter has-text-align-center has-neutral-500-color has-text-color has-14-font-size aligncenter">Based on post-project surveys</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|xs"}}} -->
+        <div class="wp-block-column">
+            <!-- wp:paragraph {"align":"center","className":"is-style-counter is-style-heading","style":{"counter":{"start":"0","end":24,"duration":"2","delay":"0","suffix":"/7"}},"textColor":"white","fontSize":"52"} -->
+            <p class="aligncenter has-text-align-center is-style-counter is-style-heading has-white-color has-text-color has-52-font-size aligncenter">24/7</p>
+            <!-- /wp:paragraph -->
+
+            <!-- wp:paragraph {"align":"center","textColor":"neutral-400","fontSize":"18"} -->
+            <p class="aligncenter has-text-align-center has-neutral-400-color has-text-color has-18-font-size aligncenter">Support Available</p>
+            <!-- /wp:paragraph -->
+
+            <!-- wp:paragraph {"align":"center","textColor":"neutral-500","fontSize":"14"} -->
+            <p class="aligncenter has-text-align-center has-neutral-500-color has-text-color has-14-font-size aligncenter">Dedicated team always ready to help</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
