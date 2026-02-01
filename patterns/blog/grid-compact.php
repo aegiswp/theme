01954109465fc86f6343157c021b72495f7d8cd3
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-02-01 18:16:30 -0500

    Update grid-compact.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/blog/grid-compact.php
+++ patterns/blog/grid-compact.php
@@ -9,30 +9,36 @@
  */
 ?>
 
-<!-- wp:group {"metadata":{"categories":["blog"],"patternName":"grid-compact","name":"Grid Compact"},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--xl);padding-bottom:var(--wp--preset--spacing--xl)">
+<!-- wp:group {"metadata":{"categories":["blog"],"patternName":"grid-compact","name":"Grid Compact"},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl"}}},"layout":{"type":"constrained"},"animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
+<div class="wp-block-group alignwide has-animation" style="padding-top:var(--wp--preset--spacing--xl);padding-bottom:var(--wp--preset--spacing--xl);animation-iteration-count:">
     <!-- wp:query {"queryId":0,"query":{"perPage":4,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false},"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|md"}}} -->
     <div class="wp-block-query alignwide"><!-- wp:post-template {"layout":{"type":"grid","columnCount":4}} -->
-        <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|xs"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-        <div class="wp-block-group">
-            <!-- wp:post-featured-image {"isLink":true,"aspectRatio":"1","style":{"aspectRatio":{"all":"1"},"objectFit":{"all":"cover"},"border":{"radius":"8px"}}} /-->
+        <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|xs"}},"layout":{"type":"flex","orientation":"vertical"},"animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
+        <div class="wp-block-group has-animation" style="animation-iteration-count:">
+            <!-- wp:post-featured-image {"isLink":true,"aspectRatio":"1","style":{"aspectRatio":{"all":"1"},"objectFit":{"all":"cover"}}} /-->
 
-            <!-- wp:post-terms {"term":"category","className":"is-style-default","style":{"typography":{"textDecoration":"none","fontWeight":"500","textTransform":"uppercase","letterSpacing":"0.05em"}},"fontSize":"10"} /-->
+            <!-- wp:post-terms {"term":"category","className":"is-style-sub-heading","showAll":false} /-->
 
-            <!-- wp:post-title {"level":4,"isLink":true,"style":{"spacing":{"margin":{"top":"0","bottom":"0"}},"typography":{"fontWeight":"500","lineHeight":"1.4"}},"fontSize":"16"} /-->
+            <!-- wp:post-title {"level":4,"isLink":true,"style":{"spacing":{"margin":{"top":"0","bottom":"0"}},"typography":{"fontWeight":"500","lineHeight":"1.4","fontStyle":"normal"}},"fontSize":"18"} /-->
 
             <!-- wp:post-date {"metadata":{"bindings":{"datetime":{"source":"core/post-data","args":{"field":"date"}}}},"style":{"spacing":{"margin":{"top":"var:preset|spacing|xxs","bottom":"0"}}},"textColor":"neutral-400","fontSize":"12"} /-->
         </div>
         <!-- /wp:group -->
         <!-- /wp:post-template -->
 
-        <!-- wp:query-pagination {"style":{"spacing":{"margin":{"top":"var:preset|spacing|lg"}}},"layout":{"type":"flex","justifyContent":"center"}} -->
+        <!-- wp:query-pagination {"paginationArrow":"arrow","style":{"spacing":{"margin":{"top":"var:preset|spacing|lg"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
         <!-- wp:query-pagination-previous /-->
 
         <!-- wp:query-pagination-numbers /-->
 
         <!-- wp:query-pagination-next /-->
         <!-- /wp:query-pagination -->
+
+        <!-- wp:query-no-results -->
+        <!-- wp:paragraph {"align":"center","placeholder":"Add text or blocks that will display when a query returns no results."} -->
+        <p class="aligncenter has-text-align-center aligncenter"></p>
+        <!-- /wp:paragraph -->
+        <!-- /wp:query-no-results -->
     </div>
     <!-- /wp:query -->
 </div>
