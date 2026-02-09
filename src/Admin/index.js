34e92ce4fb5493e83076a41607f820b944214868
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-02-09 07:48:54 -0500

    Create index.js
    - Initial commit
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

---
+++ src/Admin/index.js
@@ -0,0 +1,11 @@
+/**
+ * Admin Settings Entry Point
+ *
+ * Webpack entry that imports admin CSS and JS for bundling/minification.
+ *
+ * @package Aegis
+ * @since   1.0.0
+ */
+
+import './css/admin-settings.css';
+import './js/admin-settings.js';
