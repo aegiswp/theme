250de8a6a91f59d89304785bf37f24da28716f0a
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-02-13 15:55:13 -0500

    Create trust-badges.php
    - Initial commit
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

---
+++ patterns/commerce/trust-badges.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Title: Trust Badges
+ * Slug: trust-badges
+ * Categories: commerce
+ * Keywords: trust, badges, security, guarantee, shipping, support
+ * Description: A row of inline SVG trust icons for secure checkout, money-back guarantee, free shipping, and 24/7 support.
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"metadata":{"categories":["commerce"],"patternName":"commerce-trust-badges","name":"Trust Badges"},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|md","bottom":"var:preset|spacing|md"}}},"layout":{"type":"constrained"},"animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
+<div class="wp-block-group alignwide has-animation" style="padding-top:var(--wp--preset--spacing--md);padding-bottom:var(--wp--preset--spacing--md);animation-iteration-count:"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|md"}}}} -->
+	<div class="wp-block-columns alignwide"><!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|xxs"}}} -->
+		<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+			<div class="wp-block-group"><!-- wp:image {"className":"is-style-icon","iconSet":"wordpress","iconName":"lock-outline","iconSize":"40px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-698f334f9443d\u0022 data-icon=\u0022wordpress-lock-outline\u0022 width=\u002224\u0022 height=\u002224\u0022 fill=\u0022currentColor\u0022\u003e\u003ctitle id=\u0022icon-698f334f9443d\u0022\u003eLock Outline Icon\u003c/title\u003e\u003cpath d=\u0022M17 10h-1.2V7c0-2.1-1.7-3.8-3.8-3.8-2.1 0-3.8 1.7-3.8 3.8v3H7c-.6 0-1 .4-1 1v8c0 .6.4 1 1 1h10c.6 0 1-.4 1-1v-8c0-.6-.4-1-1-1zM9.8 7c0-1.2 1-2.2 2.2-2.2 1.2 0 2.2 1 2.2 2.2v3H9.8V7zm6.7 11.5h-9v-7h9v7z\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} -->
+				<figure class="wp-block-image is-style-icon" style="--wp--custom--icon--size:40px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-698f334f9443d&quot; data-icon=&quot;wordpress-lock-outline&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-698f334f9443d&quot;&gt;Lock Outline Icon</title&gt;<path d=&quot;M17 10h-1.2V7c0-2.1-1.7-3.8-3.8-3.8-2.1 0-3.8 1.7-3.8 3.8v3H7c-.6 0-1 .4-1 1v8c0 .6.4 1 1 1h10c.6 0 1-.4 1-1v-8c0-.6-.4-1-1-1zM9.8 7c0-1.2 1-2.2 2.2-2.2 1.2 0 2.2 1 2.2 2.2v3H9.8V7zm6.7 11.5h-9v-7h9v7z&quot;&gt;</path&gt;</svg&gt;')"><img alt="" /></figure>
+				<!-- /wp:image -->
+
+				<!-- wp:heading {"textAlign":"center","level":4,"fontSize":"16"} -->
+				<h4 class="wp-block-heading has-text-align-center has-16-font-size"><?php echo esc_html__( 'Secure Checkout', 'aegis' ); ?></h4>
+				<!-- /wp:heading -->
+
+				<!-- wp:paragraph {"align":"center","fontSize":"14"} -->
+				<p class="aligncenter has-text-align-center has-14-font-size aligncenter"><?php echo esc_html__( '256-bit SSL encryption', 'aegis' ); ?></p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|xxs"}}} -->
+		<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+			<div class="wp-block-group"><!-- wp:image {"className":"is-style-icon","iconSet":"wordpress","iconName":"shield","iconSize":"40px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-698f334fe1b7a\u0022 data-icon=\u0022wordpress-shield\u0022 width=\u002224\u0022 height=\u002224\u0022 fill=\u0022currentColor\u0022\u003e\u003ctitle id=\u0022icon-698f334fe1b7a\u0022\u003eShield Icon\u003c/title\u003e\u003cpath fill-rule=\u0022evenodd\u0022 d=\u0022m12 3.176 6.75 3.068v4.574c0 3.9-2.504 7.59-6.035 8.755a2.283 2.283 0 0 1-1.43 0c-3.53-1.164-6.035-4.856-6.035-8.755V6.244L12 3.176zM6.75 7.21v3.608c0 3.313 2.145 6.388 5.005 7.33a.775.775 0 0 0 .49 0c2.86-.942 5.005-4.017 5.005-7.33V7.21L12 4.824 6.75 7.21z\u0022 clip-rule=\u0022evenodd\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} -->
+				<figure class="wp-block-image is-style-icon" style="--wp--custom--icon--size:40px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-698f334fe1b7a&quot; data-icon=&quot;wordpress-shield&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-698f334fe1b7a&quot;&gt;Shield Icon</title&gt;<path fill-rule=&quot;evenodd&quot; d=&quot;m12 3.176 6.75 3.068v4.574c0 3.9-2.504 7.59-6.035 8.755a2.283 2.283 0 0 1-1.43 0c-3.53-1.164-6.035-4.856-6.035-8.755V6.244L12 3.176zM6.75 7.21v3.608c0 3.313 2.145 6.388 5.005 7.33a.775.775 0 0 0 .49 0c2.86-.942 5.005-4.017 5.005-7.33V7.21L12 4.824 6.75 7.21z&quot; clip-rule=&quot;evenodd&quot;&gt;</path&gt;</svg&gt;')"><img alt="" /></figure>
+				<!-- /wp:image -->
+
+				<!-- wp:heading {"textAlign":"center","level":4,"fontSize":"16"} -->
+				<h4 class="wp-block-heading has-text-align-center has-16-font-size"><?php echo esc_html__( 'Money-Back Guarantee', 'aegis' ); ?></h4>
+				<!-- /wp:heading -->
+
+				<!-- wp:paragraph {"align":"center","fontSize":"14"} -->
+				<p class="aligncenter has-text-align-center has-14-font-size aligncenter"><?php echo esc_html__( '30-day full refund policy', 'aegis' ); ?></p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|xxs"}}} -->
+		<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+			<div class="wp-block-group"><!-- wp:image {"className":"is-style-icon","iconSet":"wordpress","iconName":"box","iconSize":"40px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-698f334f103e8\u0022 data-icon=\u0022wordpress-box\u0022 width=\u002224\u0022 height=\u002224\u0022 fill=\u0022currentColor\u0022\u003e\u003ctitle id=\u0022icon-698f334f103e8\u0022\u003eBox Icon\u003c/title\u003e\u003cpath fill-rule=\u0022evenodd\u0022 d=\u0022M5 5.5h14a.5.5 0 0 1 .5.5v1.5a.5.5 0 0 1-.5.5H5a.5.5 0 0 1-.5-.5V6a.5.5 0 0 1 .5-.5zM4 9.232A2 2 0 0 1 3 7.5V6a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v1.5a2 2 0 0 1-1 1.732V18a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V9.232zm1.5.268V18a.5.5 0 0 0 .5.5h12a.5.5 0 0 0 .5-.5V9.5h-13z\u0022 clip-rule=\u0022evenodd\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} -->
+				<figure class="wp-block-image is-style-icon" style="--wp--custom--icon--size:40px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-698f334f103e8&quot; data-icon=&quot;wordpress-box&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-698f334f103e8&quot;&gt;Box Icon</title&gt;<path fill-rule=&quot;evenodd&quot; d=&quot;M5 5.5h14a.5.5 0 0 1 .5.5v1.5a.5.5 0 0 1-.5.5H5a.5.5 0 0 1-.5-.5V6a.5.5 0 0 1 .5-.5zM4 9.232A2 2 0 0 1 3 7.5V6a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v1.5a2 2 0 0 1-1 1.732V18a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V9.232zm1.5.268V18a.5.5 0 0 0 .5.5h12a.5.5 0 0 0 .5-.5V9.5h-13z&quot; clip-rule=&quot;evenodd&quot;&gt;</path&gt;</svg&gt;')"><img alt="" /></figure>
+				<!-- /wp:image -->
+
+				<!-- wp:heading {"textAlign":"center","level":4,"fontSize":"16"} -->
+				<h4 class="wp-block-heading has-text-align-center has-16-font-size"><?php echo esc_html__( 'Free Shipping', 'aegis' ); ?></h4>
+				<!-- /wp:heading -->
+
+				<!-- wp:paragraph {"align":"center","fontSize":"14"} -->
+				<p class="aligncenter has-text-align-center has-14-font-size aligncenter"><?php echo esc_html__( 'On orders over $50', 'aegis' ); ?></p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|xxs"}}} -->
+		<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+			<div class="wp-block-group"><!-- wp:image {"className":"is-style-icon","iconSet":"wordpress","iconName":"comment","iconSize":"40px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-698f334f60f08\u0022 data-icon=\u0022wordpress-comment\u0022 width=\u002224\u0022 height=\u002224\u0022 fill=\u0022currentColor\u0022\u003e\u003ctitle id=\u0022icon-698f334f60f08\u0022\u003eComment Icon\u003c/title\u003e\u003cpath d=\u0022M18 4H6c-1.1 0-2 .9-2 2v12.9c0 .6.5 1.1 1.1 1.1.3 0 .5-.1.8-.3L8.5 17H18c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm.5 11c0 .3-.2.5-.5.5H7.9l-2.4 2.4V6c0-.3.2-.5.5-.5h12c.3 0 .5.2.5.5v9z\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} -->
+				<figure class="wp-block-image is-style-icon" style="--wp--custom--icon--size:40px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-698f334f60f08&quot; data-icon=&quot;wordpress-comment&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-698f334f60f08&quot;&gt;Comment Icon</title&gt;<path d=&quot;M18 4H6c-1.1 0-2 .9-2 2v12.9c0 .6.5 1.1 1.1 1.1.3 0 .5-.1.8-.3L8.5 17H18c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm.5 11c0 .3-.2.5-.5.5H7.9l-2.4 2.4V6c0-.3.2-.5.5-.5h12c.3 0 .5.2.5.5v9z&quot;&gt;</path&gt;</svg&gt;')"><img alt="" /></figure>
+				<!-- /wp:image -->
+
+				<!-- wp:heading {"textAlign":"center","level":4,"fontSize":"16"} -->
+				<h4 class="wp-block-heading has-text-align-center has-16-font-size"><?php echo esc_html__( '24/7 Support', 'aegis' ); ?></h4>
+				<!-- /wp:heading -->
+
+				<!-- wp:paragraph {"align":"center","fontSize":"14"} -->
+				<p class="aligncenter has-text-align-center has-14-font-size aligncenter"><?php echo esc_html__( 'Dedicated customer care', 'aegis' ); ?></p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
