eff2661dfd880f82e372fe8b1c52f59ef0fff462
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-02-13 15:48:01 -0500

    Create shipping-info.php
    - Initial commit
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

---
+++ patterns/commerce/shipping-info.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Title: Shipping Information
+ * Slug: shipping-info
+ * Categories: commerce
+ * Keywords: shipping, delivery, returns, policy, international
+ * Description: A shipping policy and information section with SVG icons for delivery times, return policy, and international shipping.
+ * Viewport Width: 1280
+ * Block Types: core/group
+ */
+?>
+
+<!-- wp:group {"metadata":{"categories":["commerce"],"patternName":"commerce-shipping-info","name":"Shipping Information"},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg"}}},"layout":{"type":"constrained"},"animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
+<div class="wp-block-group alignwide has-animation" style="padding-top:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--lg);animation-iteration-count:"><!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|md"}}},"fontSize":"24"} -->
+	<h2 class="wp-block-heading has-text-align-center has-24-font-size" style="margin-bottom:var(--wp--preset--spacing--md)"><?php echo esc_html__( 'Shipping & Returns', 'aegis' ); ?></h2>
+	<!-- /wp:heading -->
+
+	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|md"}}}} -->
+	<div class="wp-block-columns alignwide"><!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|md","bottom":"var:preset|spacing|md","left":"var:preset|spacing|md","right":"var:preset|spacing|md"},"blockGap":"var:preset|spacing|xs"},"border":{"radius":"8px","width":"1px","color":"var:preset|color|neutral-200"}}} -->
+			<div class="wp-block-group has-border-color" style="border-color:var(--wp--preset--color--neutral-200);border-width:1px;border-radius:8px;padding-top:var(--wp--preset--spacing--md);padding-right:var(--wp--preset--spacing--md);padding-bottom:var(--wp--preset--spacing--md);padding-left:var(--wp--preset--spacing--md)"><!-- wp:image {"className":"is-style-icon","iconSet":"wordpress","iconName":"box","iconSize":"40px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-698f334f103e8\u0022 data-icon=\u0022wordpress-box\u0022 width=\u002224\u0022 height=\u002224\u0022 fill=\u0022currentColor\u0022\u003e\u003ctitle id=\u0022icon-698f334f103e8\u0022\u003eBox Icon\u003c/title\u003e\u003cpath fill-rule=\u0022evenodd\u0022 d=\u0022M5 5.5h14a.5.5 0 0 1 .5.5v1.5a.5.5 0 0 1-.5.5H5a.5.5 0 0 1-.5-.5V6a.5.5 0 0 1 .5-.5zM4 9.232A2 2 0 0 1 3 7.5V6a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v1.5a2 2 0 0 1-1 1.732V18a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V9.232zm1.5.268V18a.5.5 0 0 0 .5.5h12a.5.5 0 0 0 .5-.5V9.5h-13z\u0022 clip-rule=\u0022evenodd\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} -->
+				<figure class="wp-block-image is-style-icon" style="--wp--custom--icon--size:40px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-698f334f103e8&quot; data-icon=&quot;wordpress-box&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-698f334f103e8&quot;&gt;Box Icon</title&gt;<path fill-rule=&quot;evenodd&quot; d=&quot;M5 5.5h14a.5.5 0 0 1 .5.5v1.5a.5.5 0 0 1-.5.5H5a.5.5 0 0 1-.5-.5V6a.5.5 0 0 1 .5-.5zM4 9.232A2 2 0 0 1 3 7.5V6a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v1.5a2 2 0 0 1-1 1.732V18a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V9.232zm1.5.268V18a.5.5 0 0 0 .5.5h12a.5.5 0 0 0 .5-.5V9.5h-13z&quot; clip-rule=&quot;evenodd&quot;&gt;</path&gt;</svg&gt;')"><img alt="" /></figure>
+				<!-- /wp:image -->
+
+				<!-- wp:heading {"textAlign":"center","level":3,"fontSize":"18"} -->
+				<h3 class="wp-block-heading has-text-align-center has-18-font-size"><?php echo esc_html__( 'Fast Delivery', 'aegis' ); ?></h3>
+				<!-- /wp:heading -->
+
+				<!-- wp:paragraph {"align":"center","fontSize":"14"} -->
+				<p class="aligncenter has-text-align-center has-14-font-size aligncenter"><?php echo esc_html__( 'Standard shipping: 3–5 business days. Express shipping: 1–2 business days. Free shipping on orders over $50.', 'aegis' ); ?></p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|md","bottom":"var:preset|spacing|md","left":"var:preset|spacing|md","right":"var:preset|spacing|md"},"blockGap":"var:preset|spacing|xs"},"border":{"radius":"8px","width":"1px","color":"var:preset|color|neutral-200"}}} -->
+			<div class="wp-block-group has-border-color" style="border-color:var(--wp--preset--color--neutral-200);border-width:1px;border-radius:8px;padding-top:var(--wp--preset--spacing--md);padding-right:var(--wp--preset--spacing--md);padding-bottom:var(--wp--preset--spacing--md);padding-left:var(--wp--preset--spacing--md)"><!-- wp:image {"className":"is-style-icon","iconSet":"wordpress","iconName":"backup","iconSize":"40px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-698f334f0caef\u0022 data-icon=\u0022wordpress-backup\u0022 width=\u002224\u0022 height=\u002224\u0022 fill=\u0022currentColor\u0022\u003e\u003ctitle id=\u0022icon-698f334f0caef\u0022\u003eBackup Icon\u003c/title\u003e\u003cpath d=\u0022M5.5 12h1.75l-2.5 3-2.5-3H4a8 8 0 1 1 3.134 6.35l.907-1.194A6.5 6.5 0 1 0 5.5 12zm9.53 1.97-2.28-2.28V8.5a.75.75 0 0 0-1.5 0V12a.747.747 0 0 0 .218.529l1.282-.84-1.28.842 2.5 2.5a.75.75 0 1 0 1.06-1.061z\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} -->
+				<figure class="wp-block-image is-style-icon" style="--wp--custom--icon--size:40px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-698f334f0caef&quot; data-icon=&quot;wordpress-backup&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-698f334f0caef&quot;&gt;Backup Icon</title&gt;<path d=&quot;M5.5 12h1.75l-2.5 3-2.5-3H4a8 8 0 1 1 3.134 6.35l.907-1.194A6.5 6.5 0 1 0 5.5 12zm9.53 1.97-2.28-2.28V8.5a.75.75 0 0 0-1.5 0V12a.747.747 0 0 0 .218.529l1.282-.84-1.28.842 2.5 2.5a.75.75 0 1 0 1.06-1.061z&quot;&gt;</path&gt;</svg&gt;')"><img alt="" /></figure>
+				<!-- /wp:image -->
+
+				<!-- wp:heading {"textAlign":"center","level":3,"fontSize":"18"} -->
+				<h3 class="wp-block-heading has-text-align-center has-18-font-size"><?php echo esc_html__( 'Easy Returns', 'aegis' ); ?></h3>
+				<!-- /wp:heading -->
+
+				<!-- wp:paragraph {"align":"center","fontSize":"14"} -->
+				<p class="aligncenter has-text-align-center has-14-font-size aligncenter"><?php echo esc_html__( '30-day hassle-free returns. Items must be unused and in original packaging. Refunds processed within 5 business days.', 'aegis' ); ?></p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|md","bottom":"var:preset|spacing|md","left":"var:preset|spacing|md","right":"var:preset|spacing|md"},"blockGap":"var:preset|spacing|xs"},"border":{"radius":"8px","width":"1px","color":"var:preset|color|neutral-200"}}} -->
+			<div class="wp-block-group has-border-color" style="border-color:var(--wp--preset--color--neutral-200);border-width:1px;border-radius:8px;padding-top:var(--wp--preset--spacing--md);padding-right:var(--wp--preset--spacing--md);padding-bottom:var(--wp--preset--spacing--md);padding-left:var(--wp--preset--spacing--md)"><!-- wp:image {"className":"is-style-icon","iconSet":"wordpress","iconName":"globe","iconSize":"40px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-698f334f7cc63\u0022 data-icon=\u0022wordpress-globe\u0022 width=\u002224\u0022 height=\u002224\u0022 fill=\u0022currentColor\u0022\u003e\u003ctitle id=\u0022icon-698f334f7cc63\u0022\u003eGlobe Icon\u003c/title\u003e\u003cpath d=\u0022M12 3.3c-4.8 0-8.8 3.9-8.8 8.8 0 4.8 3.9 8.8 8.8 8.8 4.8 0 8.8-3.9 8.8-8.8s-4-8.8-8.8-8.8zm6.5 5.5h-2.6C15.4 7.3 14.8 6 14 5c2 .6 3.6 2 4.5 3.8zm.7 3.2c0 .6-.1 1.2-.2 1.8h-2.9c.1-.6.1-1.2.1-1.8s-.1-1.2-.1-1.8H19c.2.6.2 1.2.2 1.8zM12 18.7c-1-.7-1.8-1.9-2.3-3.5h4.6c-.5 1.6-1.3 2.9-2.3 3.5zm-2.6-4.9c-.1-.6-.1-1.1-.1-1.8 0-.6.1-1.2.1-1.8h5.2c.1.6.1 1.1.1 1.8s-.1 1.2-.1 1.8H9.4zM4.8 12c0-.6.1-1.2.2-1.8h2.9c-.1.6-.1 1.2-.1 1.8 0 .6.1 1.2.1 1.8H5c-.2-.6-.2-1.2-.2-1.8zM12 5.3c1 .7 1.8 1.9 2.3 3.5H9.7c.5-1.6 1.3-2.9 2.3-3.5zM10 5c-.8 1-1.4 2.3-1.8 3.8H5.5C6.4 7 8 5.6 10 5zM5.5 15.3h2.6c.4 1.5 1 2.8 1.8 3.7-1.8-.6-3.5-2-4.4-3.7zM14 19c.8-1 1.4-2.2 1.8-3.7h2.6C17.6 17 16 18.4 14 19z\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} -->
+				<figure class="wp-block-image is-style-icon" style="--wp--custom--icon--size:40px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-698f334f7cc63&quot; data-icon=&quot;wordpress-globe&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-698f334f7cc63&quot;&gt;Globe Icon</title&gt;<path d=&quot;M12 3.3c-4.8 0-8.8 3.9-8.8 8.8 0 4.8 3.9 8.8 8.8 8.8 4.8 0 8.8-3.9 8.8-8.8s-4-8.8-8.8-8.8zm6.5 5.5h-2.6C15.4 7.3 14.8 6 14 5c2 .6 3.6 2 4.5 3.8zm.7 3.2c0 .6-.1 1.2-.2 1.8h-2.9c.1-.6.1-1.2.1-1.8s-.1-1.2-.1-1.8H19c.2.6.2 1.2.2 1.8zM12 18.7c-1-.7-1.8-1.9-2.3-3.5h4.6c-.5 1.6-1.3 2.9-2.3 3.5zm-2.6-4.9c-.1-.6-.1-1.1-.1-1.8 0-.6.1-1.2.1-1.8h5.2c.1.6.1 1.1.1 1.8s-.1 1.2-.1 1.8H9.4zM4.8 12c0-.6.1-1.2.2-1.8h2.9c-.1.6-.1 1.2-.1 1.8 0 .6.1 1.2.1 1.8H5c-.2-.6-.2-1.2-.2-1.8zM12 5.3c1 .7 1.8 1.9 2.3 3.5H9.7c.5-1.6 1.3-2.9 2.3-3.5zM10 5c-.8 1-1.4 2.3-1.8 3.8H5.5C6.4 7 8 5.6 10 5zM5.5 15.3h2.6c.4 1.5 1 2.8 1.8 3.7-1.8-.6-3.5-2-4.4-3.7zM14 19c.8-1 1.4-2.2 1.8-3.7h2.6C17.6 17 16 18.4 14 19z&quot;&gt;</path&gt;</svg&gt;')"><img alt="" /></figure>
+				<!-- /wp:image -->
+
+				<!-- wp:heading {"textAlign":"center","level":3,"fontSize":"18"} -->
+				<h3 class="wp-block-heading has-text-align-center has-18-font-size"><?php echo esc_html__( 'International Shipping', 'aegis' ); ?></h3>
+				<!-- /wp:heading -->
+
+				<!-- wp:paragraph {"align":"center","fontSize":"14"} -->
+				<p class="aligncenter has-text-align-center has-14-font-size aligncenter"><?php echo esc_html__( 'We ship to over 50 countries worldwide. International delivery takes 7–14 business days. Customs duties may apply.', 'aegis' ); ?></p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
