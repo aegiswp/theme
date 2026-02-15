9883fe74948eece196f41be9e6048bde0ac64a78
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-02-15 14:37:29 -0500

    Create related-posts-card.php
    - Initial commit
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

---
+++ patterns/blog/related-posts-cards.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: Related Posts: Cards
+ * Slug: related-posts-cards
+ * Categories: blog
+ * Keywords: related, posts, similar, recommended, cards
+ * Description: Boxed cards of related posts with surface background and shadow.
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:aegis/related-posts {"postsPerPage":3,"columns":3,"styleVariant":"cards","heading":"Related Posts","headingTag":"h2","showFeaturedImage":true,"showDate":true,"showExcerpt":true,"showCategory":true,"taxonomySource":"auto","fallbackBehavior":"latest","excerptLength":15,"imageAspectRatio":"4/3","align":"wide"} /-->
