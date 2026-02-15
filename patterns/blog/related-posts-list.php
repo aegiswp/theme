0ecd2cd4a7a6992a5da024875bdd3b37b1622606
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-02-15 16:56:03 -0500

    Create related-posts-list.php
    - Initial commit
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

---
+++ patterns/blog/related-posts-list.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: Related Posts: List
+ * Slug: related-posts-list
+ * Categories: blog
+ * Keywords: related, posts, similar, recommended, list
+ * Description: A horizontal list of related posts with small thumbnails, titles, and dates.
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:aegis/related-posts {"postsPerPage":4,"columns":1,"styleVariant":"list","heading":"You May Also Like","headingTag":"h2","showFeaturedImage":true,"showDate":true,"showExcerpt":false,"showCategory":false,"taxonomySource":"auto","fallbackBehavior":"latest","imageAspectRatio":"1/1","align":"wide"} /-->
