1c3e947db38bcab8d21e30ded25c33dfecc11709
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-02-16 11:28:03 -0500

    Create commerce-newsletter.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

---
+++ patterns/cta/commerce-newsletter.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Title: Commerce Newsletter CTA
+ * Slug: commerce-newsletter
+ * Categories: cta
+ * Keywords: cta, newsletter, email, subscribe, commerce, woocommerce
+ * Description: A newsletter signup CTA with discount incentive for store visitors.
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"metadata":{"categories":["cta"],"patternName":"commerce-newsletter","name":"Commerce Newsletter CTA"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg"}}},"backgroundColor":"neutral-50","layout":{"type":"constrained","contentSize":"640px"},"animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
+<div class="wp-block-group alignfull has-neutral-50-background-color has-background has-animation" style="padding-top:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--lg);animation-iteration-count:"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+	<div class="wp-block-group"><!-- wp:image {"className":"is-style-icon","iconSet":"wordpress","iconName":"mail","iconSize":"45px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 xml:space=\u0022preserve\u0022 viewBox=\u00220 0 24 24\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-6991792814a64\u0022 data-icon=\u0022wordpress-mail\u0022 width=\u002224\u0022 height=\u002224\u0022 fill=\u0022currentColor\u0022\u003e\u003ctitle id=\u0022icon-6991792814a64\u0022\u003eMail Icon\u003c/title\u003e\u003cpath d=\u0022M19 5H5c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zM4.5 7c0-.3.2-.5.5-.5h14c.3 0 .5.2.5.5v1.3l-7.6 4.4-7.4-4.3V7zm15 8.6V17c0 .3-.2.5-.5.5H5c-.3 0-.5-.2-.5-.5v-6.9l5.9 3.4 1.5.9 1.5-.9 6.1-3.5v5.6z\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} -->
+		<figure class="wp-block-image is-style-icon" style="--wp--custom--icon--size:45px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; xml:space=&quot;preserve&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-6991792814a64&quot; data-icon=&quot;wordpress-mail&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-6991792814a64&quot;&gt;Mail Icon</title&gt;<path d=&quot;M19 5H5c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zM4.5 7c0-.3.2-.5.5-.5h14c.3 0 .5.2.5.5v1.3l-7.6 4.4-7.4-4.3V7zm15 8.6V17c0 .3-.2.5-.5.5H5c-.3 0-.5-.2-.5-.5v-6.9l5.9 3.4 1.5.9 1.5-.9 6.1-3.5v5.6z&quot;&gt;</path&gt;</svg&gt;')"><img alt="" /></figure>
+		<!-- /wp:image -->
+
+		<!-- wp:heading {"textAlign":"center","fontSize":"28"} -->
+		<h2 class="wp-block-heading has-text-align-center has-28-font-size"><?php echo esc_html__( 'Get 10% Off Your First Order', 'aegis' ); ?></h2>
+		<!-- /wp:heading -->
+
+		<!-- wp:paragraph {"align":"center","textColor":"neutral-600","fontSize":"16"} -->
+		<p class="aligncenter has-text-align-center has-neutral-600-color has-text-color has-16-font-size aligncenter"><?php echo esc_html__( 'Subscribe to our newsletter for exclusive deals, new arrivals, and insider-only discounts delivered straight to your inbox.', 'aegis' ); ?></p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|sm"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+		<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--sm)"><!-- wp:search {"showLabel":false,"placeholder":"Email address","buttonText":"Subscribe","className":"is-style-newsletter"} /--></div>
+		<!-- /wp:group -->
+
+		<!-- wp:paragraph {"align":"center","textColor":"neutral-400","fontSize":"12"} -->
+		<p class="aligncenter has-text-align-center has-neutral-400-color has-text-color has-12-font-size aligncenter"><?php echo esc_html__( 'No spam, ever. Unsubscribe anytime.', 'aegis' ); ?></p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
