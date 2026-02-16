be90a34639c525d79d04ae4c59a8d66db567352f
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-02-16 07:46:48 -0500

    Update form-map.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/contact/form-map.php
+++ patterns/contact/form-map.php
@@ -9,26 +9,23 @@
  */
 ?>
 
-<!-- wp:group {"metadata":{"categories":["contact"],"patternName":"form-map","name":"Contact Form Map"},"align":"wide","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide">
-    <!-- wp:columns {"verticalAlignment":"center","align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|xl","left":"var:preset|spacing|xl"},"padding":{"top":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg"}}}} -->
-    <div class="wp-block-columns alignwide are-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--lg)">
-        <!-- wp:column {"verticalAlignment":"center"} -->
-        <div class="wp-block-column is-vertically-aligned-center">
-            <!-- wp:heading {"className":"has-text-align-left has-48-font-size"} -->
-            <h2 class="wp-block-heading has-text-align-left has-48-font-size">Contact Us</h2>
+<!-- wp:group {"metadata":{"categories":["contact"],"patternName":"contact-form-map","name":"Contact Form Map"},"align":"wide","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:columns {"verticalAlignment":"center","align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|xl","left":"var:preset|spacing|xl"},"padding":{"top":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg"}}}} -->
+    <div class="wp-block-columns alignwide are-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--lg)"><!-- wp:column {"verticalAlignment":"center"} -->
+        <div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"className":"has-text-align-left has-48-font-size"} -->
+            <h2 class="wp-block-heading has-text-align-left has-48-font-size"><?php echo esc_html__( 'Contact Us', 'aegis' ); ?></h2>
             <!-- /wp:heading -->
 
             <!-- wp:html -->
             <form action="#" method="POST">
-                <label for="name">Name</label>
+                <label for="name"><?php echo esc_html__( 'Name', 'aegis' ); ?></label>
                 <input type="text" id="name" name="name" required>
-                <label for="email">Email</label>
+                <label for="email"><?php echo esc_html__( 'Email', 'aegis' ); ?></label>
                 <input type="email" id="email" name="email" required>
-                <label for="message">Message</label>
+                <label for="message"><?php echo esc_html__( 'Message', 'aegis' ); ?></label>
                 <textarea id="message" name="message" rows="4" required></textarea>
                 <div class="wp-block-buttons is-layout-flex">
-                    <div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link wp-element-button" href="#">Send</a></div>
+                    <div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link wp-element-button" href="#"><?php echo esc_html__( 'Send', 'aegis' ); ?></a></div>
                 </div>
             </form>
             <!-- /wp:html -->
@@ -36,10 +33,7 @@
         <!-- /wp:column -->
 
         <!-- wp:column {"verticalAlignment":"center"} -->
-        <div class="wp-block-column is-vertically-aligned-center"><!-- wp:html -->
-            <iframe width="700" height="500" src="https://www.openstreetmap.org/export/embed.html?bbox=-74.06400114297868%2C4.648583297488079%2C-74.06149059534074%2C4.650419911229698&amp;layer=mapnik&amp;marker=4.64950160495738%2C-74.0627458691597" style="border: 1px solid black"></iframe>
-            <!-- /wp:html -->
-        </div>
+        <div class="wp-block-column is-vertically-aligned-center"><!-- wp:aegis/map {"height":"500px","provider":"openstreetmap"} /--></div>
         <!-- /wp:column -->
     </div>
     <!-- /wp:columns -->
