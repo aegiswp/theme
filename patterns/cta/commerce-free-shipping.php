3d68eec62b8e7800ba3e7555270cdd698c38ea27
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-02-16 11:26:19 -0500

    Create commerce-free-shipping.php
    - Initial commit
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

---
+++ patterns/cta/commerce-free-shipping.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Title: Commerce Free Shipping CTA
+ * Slug: commerce-free-shipping
+ * Categories: cta
+ * Keywords: cta, shipping, free, commerce, banner, woocommerce
+ * Description: A slim announcement bar CTA promoting free shipping with a threshold and shop link.
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"metadata":{"categories":["cta"],"patternName":"commerce-free-shipping","name":"Commerce Free Shipping CTA"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|xs","bottom":"var:preset|spacing|xs"}}},"backgroundColor":"primary-500","textColor":"white","layout":{"type":"constrained"},"animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
+<div class="wp-block-group alignfull has-white-color has-primary-500-background-color has-text-color has-background has-animation" style="padding-top:var(--wp--preset--spacing--xs);padding-bottom:var(--wp--preset--spacing--xs);animation-iteration-count:"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+	<div class="wp-block-group"><!-- wp:image {"className":"is-style-icon","iconSet":"wordpress","iconName":"shipping","iconSize":"40px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-6991792a1969b\u0022 data-icon=\u0022wordpress-shipping\u0022 width=\u002224\u0022 height=\u002224\u0022 fill=\u0022currentColor\u0022\u003e\u003ctitle id=\u0022icon-6991792a1969b\u0022\u003eShipping Icon\u003c/title\u003e\u003cpath d=\u0022M3 6.75C3 5.784 3.784 5 4.75 5H15v2.313l.05.027 5.056 2.73.394.212v3.468a1.75 1.75 0 0 1-1.75 1.75h-.012a2.5 2.5 0 1 1-4.975 0H9.737a2.5 2.5 0 1 1-4.975 0H3V6.75zM13.5 14V6.5H4.75a.25.25 0 0 0-.25.25V14h.965a2.493 2.493 0 0 1 1.785-.75c.7 0 1.332.287 1.785.75H13.5zm4.535 0h.715a.25.25 0 0 0 .25-.25v-2.573l-4-2.16v4.568a2.487 2.487 0 0 1 1.25-.335c.7 0 1.332.287 1.785.75zM6.282 15.5a1.002 1.002 0 0 0 .968 1.25 1 1 0 1 0-.968-1.25zm9 0a1 1 0 1 0 1.937.498 1 1 0 0 0-1.938-.498z\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} -->
+		<figure class="wp-block-image is-style-icon" style="--wp--custom--icon--size:40px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-6991792a1969b&quot; data-icon=&quot;wordpress-shipping&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-6991792a1969b&quot;&gt;Shipping Icon</title&gt;<path d=&quot;M3 6.75C3 5.784 3.784 5 4.75 5H15v2.313l.05.027 5.056 2.73.394.212v3.468a1.75 1.75 0 0 1-1.75 1.75h-.012a2.5 2.5 0 1 1-4.975 0H9.737a2.5 2.5 0 1 1-4.975 0H3V6.75zM13.5 14V6.5H4.75a.25.25 0 0 0-.25.25V14h.965a2.493 2.493 0 0 1 1.785-.75c.7 0 1.332.287 1.785.75H13.5zm4.535 0h.715a.25.25 0 0 0 .25-.25v-2.573l-4-2.16v4.568a2.487 2.487 0 0 1 1.25-.335c.7 0 1.332.287 1.785.75zM6.282 15.5a1.002 1.002 0 0 0 .968 1.25 1 1 0 1 0-.968-1.25zm9 0a1 1 0 1 0 1.937.498 1 1 0 0 0-1.938-.498z&quot;&gt;</path&gt;</svg&gt;')"><img alt="" /></figure>
+		<!-- /wp:image -->
+
+		<!-- wp:paragraph -->
+		<p><?php echo esc_html__( 'Free shipping on all orders over $50!', 'aegis' ); ?></p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:paragraph -->
+		<p><a href="/shop" style="color:inherit;text-decoration:underline"><?php echo esc_html__( 'Shop Now →', 'aegis' ); ?></a></p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
