e1b213339adddca29a73c1906efd96d9f64111d5
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-02-16 09:34:53 -0500

    Update grid-masonry.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/blog/grid-masonry.php
+++ patterns/blog/grid-masonry.php
@@ -9,7 +9,7 @@
  */
 ?>
 
-<!-- wp:group {"metadata":{"categories":["blog"],"patternName":"blog-grid-masonry","name":"Grid Masonry"},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl"}}},"layout":{"type":"constrained"},"animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
+<!-- wp:group {"metadata":{"categories":["blog"],"patternName":"grid-masonry","name":"Grid Masonry"},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl"}}},"layout":{"type":"constrained"},"animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
 <div class="wp-block-group alignwide has-animation" style="padding-top:var(--wp--preset--spacing--xl);padding-bottom:var(--wp--preset--spacing--xl);animation-iteration-count:"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|md","left":"var:preset|spacing|md"}}},"animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
     <div class="wp-block-columns alignwide has-animation" style="animation-iteration-count:"><!-- wp:column {"width":"60%","animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
         <div class="wp-block-column has-animation" style="flex-basis:60%;animation-iteration-count:"><!-- wp:query {"queryId":0,"query":{"perPage":10,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"style":{"spacing":{"blockGap":"0"}}} -->
