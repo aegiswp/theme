35abf86ba5ec059939f3065b43f06eecd261b679
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-02-16 10:29:17 -0500

    Update shipping-info.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/commerce/shipping-info.php
+++ patterns/commerce/shipping-info.php
@@ -10,7 +10,7 @@
  */
 ?>
 
-<!-- wp:group {"metadata":{"categories":["commerce"],"patternName":"commerce-shipping-info","name":"Shipping Information"},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg"}}},"layout":{"type":"constrained"},"animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
+<!-- wp:group {"metadata":{"categories":["commerce"],"patternName":"shipping-info","name":"Shipping Information"},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg"}}},"layout":{"type":"constrained"},"animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
 <div class="wp-block-group alignwide has-animation" style="padding-top:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--lg);animation-iteration-count:"><!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|md"}}},"fontSize":"24"} -->
 	<h2 class="wp-block-heading has-text-align-center has-24-font-size" style="margin-bottom:var(--wp--preset--spacing--md)"><?php echo esc_html__( 'Shipping & Returns', 'aegis' ); ?></h2>
 	<!-- /wp:heading -->
