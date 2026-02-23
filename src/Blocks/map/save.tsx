a8a29a68586dec73fcfa4c5e4975022bd7a8a4eb
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-02-23 05:02:15 -0500

    Create save.tsx
    - Initial commit
    
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

---
+++ src/Blocks/map/save.tsx
@@ -0,0 +1,12 @@
+/**
+ * Map Block - Save Component
+ *
+ * Returns null for server-side rendering via render.php.
+ *
+ * @package Aegis
+ * @since   1.0.0
+ */
+
+export default function save() {
+	return null;
+}
