9873ea13a6e89fdb3ba0edbbc6c2f6421d243eb2
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-03-01 17:26:06 -0500

    Update modal-video.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/modals/modal-video.php
+++ patterns/modals/modal-video.php
@@ -16,8 +16,7 @@ declare(strict_types=1);
 
 <!-- wp:aegis/modal {"triggerText":"Watch Video","animation":"zoom","width":"800px","padding":"0","metadata":{"categories":["modal"],"patternName":"modal-video","name":"Video Modal"}} -->
 <!-- wp:embed {"url":"https://www.youtube.com/watch?v=dQw4w9WgXcQ","type":"video","providerNameSlug":"youtube","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
-<figure
-    class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio">
+<figure class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio">
     <div class="wp-block-embed__wrapper">https://www.youtube.com/watch?v=dQw4w9WgXcQ</div>
 </figure>
 <!-- /wp:embed -->
