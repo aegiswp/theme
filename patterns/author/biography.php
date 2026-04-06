4697984a3fb4a3607c5e0c1abf3ddf5ceb1c592f
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-04-06 15:49:37 -0500

    Update biography.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/author/biography.php
+++ patterns/author/biography.php
@@ -9,14 +9,14 @@
  */
 ?>
 
-<!-- wp:group {"metadata":{"categories":["author"],"patternName":"biography"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|xxl","bottom":"var:preset|spacing|xxl"}}},"backgroundColor":"neutral-50","layout":{"type":"constrained","contentSize":"640px"},"animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
-<div class="wp-block-group alignfull has-neutral-50-background-color has-background has-animation" style="padding-top:var(--wp--preset--spacing--xxl);padding-bottom:var(--wp--preset--spacing--xxl);animation-iteration-count:"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|md"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"},"animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
-    <div class="wp-block-group has-animation" style="animation-iteration-count:"><!-- wp:avatar {"size":120,"style":{"border":{"radius":{"topLeft":"100%","topRight":"100%","bottomLeft":"100%","bottomRight":"100%"}}}} /-->
+<!-- wp:group {"metadata":{"categories":["author"],"patternName":"biography"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|xxl","bottom":"var:preset|spacing|xxl"}}},"backgroundColor":"neutral-50","layout":{"type":"constrained","contentSize":"640px"}} -->
+<div class="wp-block-group alignfull has-neutral-50-background-color has-background" style="padding-top:var(--wp--preset--spacing--xxl);padding-bottom:var(--wp--preset--spacing--xxl)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|md"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+    <div class="wp-block-group"><!-- wp:avatar {"size":120,"style":{"border":{"radius":{"topLeft":"100%","topRight":"100%","bottomLeft":"100%","bottomRight":"100%"}}}} /-->
 
         <!-- wp:post-author-name {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"40"} /-->
 
-        <!-- wp:paragraph {"align":"center","className":"is-style-sub-heading","textColor":"primary-500","animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
-        <p class="aligncenter has-text-align-center is-style-sub-heading has-primary-500-color has-text-color aligncenter has-animation" style="animation-iteration-count:"><?php echo esc_html__( 'Principal Director & Writer', 'aegis' ); ?></p>
+        <!-- wp:paragraph {"align":"center","className":"is-style-sub-heading","textColor":"primary-500"} -->
+        <p class="aligncenter has-text-align-center is-style-sub-heading has-primary-500-color has-text-color aligncenter"><?php echo esc_html__( 'Principal Director & Writer', 'aegis' ); ?></p>
         <!-- /wp:paragraph -->
 
         <!-- wp:separator {"opacity":"css","className":"has-text-color has-neutral-100-color is-style-wide","style":{"border":{"width":"1px"}}} -->
