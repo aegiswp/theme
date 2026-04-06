829d3689d04e83da2180351fb8f5e1bd88c9d220
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-04-06 18:04:10 -0500

    Update two-column-featured.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/blog/two-column-featured.php
+++ patterns/blog/two-column-featured.php
@@ -9,16 +9,16 @@
  */
 ?>
 
-<!-- wp:group {"metadata":{"categories":["blog"],"patternName":"two-column-featured","name":"Two Column Featured"},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl"}}},"layout":{"type":"constrained"},"animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
-<div class="wp-block-group alignwide has-animation" style="padding-top:var(--wp--preset--spacing--xl);padding-bottom:var(--wp--preset--spacing--xl);animation-iteration-count:"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|xl"}}},"animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
-    <div class="wp-block-columns alignwide has-animation" style="animation-iteration-count:"><!-- wp:column {"width":"66.66%","animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
-        <div class="wp-block-column has-animation" style="flex-basis:66.66%;animation-iteration-count:"><!-- wp:query {"queryId":0,"query":{"perPage":10,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"style":{"spacing":{"blockGap":"0"}}} -->
+<!-- wp:group {"metadata":{"categories":["blog"],"patternName":"two-column-featured","name":"Two Column Featured"},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--xl);padding-bottom:var(--wp--preset--spacing--xl)"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|xl"}}}} -->
+    <div class="wp-block-columns alignwide"><!-- wp:column {"width":"66.66%"} -->
+        <div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:query {"queryId":0,"query":{"perPage":10,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"style":{"spacing":{"blockGap":"0"}}} -->
             <div class="wp-block-query"><!-- wp:post-template {"layout":{"type":"default"}} -->
                 <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|sm"}},"layout":{"type":"flex","orientation":"vertical"}} -->
                 <div class="wp-block-group"><!-- wp:post-featured-image {"isLink":true,"aspectRatio":"16/9","style":{"aspectRatio":{"all":"16/9"},"objectFit":{"all":"cover"}}} /-->
 
-                    <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|xxs","margin":{"top":"0","bottom":"var:preset|spacing|xxs"}}},"layout":{"type":"flex","flexWrap":"nowrap"},"animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
-                    <div class="wp-block-group has-animation" style="margin-top:0;margin-bottom:var(--wp--preset--spacing--xxs);animation-iteration-count:"><!-- wp:post-terms {"term":"category","style":{"typography":{"textDecoration":"none"},"spacing":{"margin":{"top":"0","bottom":"0"}}},"fontSize":"14"} /-->
+                    <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|xxs","margin":{"top":"0","bottom":"var:preset|spacing|xxs"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+                    <div class="wp-block-group" style="margin-top:0;margin-bottom:var(--wp--preset--spacing--xxs)"><!-- wp:post-terms {"term":"category","style":{"typography":{"textDecoration":"none"},"spacing":{"margin":{"top":"0","bottom":"0"}}},"fontSize":"14"} /-->
 
                         <!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"textColor":"neutral-200"} -->
                         <p class="has-neutral-200-color has-text-color" style="margin-top:0;margin-bottom:0">|</p>
@@ -41,8 +41,8 @@
         </div>
         <!-- /wp:column -->
 
-        <!-- wp:column {"width":"33.33%","animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
-        <div class="wp-block-column has-animation" style="flex-basis:33.33%;animation-iteration-count:"><!-- wp:paragraph {"className":"is-style-sub-heading","fontSize":"14"} -->
+        <!-- wp:column {"width":"33.33%"} -->
+        <div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:paragraph {"className":"is-style-sub-heading","fontSize":"14"} -->
             <p class="is-style-sub-heading has-14-font-size"><?php echo esc_html__( 'Recent Posts', 'aegis' ); ?></p>
             <!-- /wp:paragraph -->
 
