5cdd40d66ab4634667849957a30f03651695c0c2
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-04-06 15:34:09 -0500

    Update archive.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/author/archive.php
+++ patterns/author/archive.php
@@ -9,14 +9,14 @@
  */
 ?>
 
-<!-- wp:group {"metadata":{"categories":["author"],"patternName":"archive"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl"}}},"layout":{"type":"constrained"},"animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
-<div class="wp-block-group alignfull has-animation" style="padding-top:var(--wp--preset--spacing--xl);padding-bottom:var(--wp--preset--spacing--xl);animation-iteration-count:"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|md","margin":{"bottom":"var:preset|spacing|xl"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center","orientation":"vertical"},"animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
-    <div class="wp-block-group has-animation" style="margin-bottom:var(--wp--preset--spacing--xl);animation-iteration-count:"><!-- wp:avatar {"align":"center","style":{"border":{"radius":"100%"}}} /-->
+<!-- wp:group {"metadata":{"categories":["author"],"patternName":"archive"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|xl","bottom":"var:preset|spacing|xl"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--xl);padding-bottom:var(--wp--preset--spacing--xl)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|md","margin":{"bottom":"var:preset|spacing|xl"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center","orientation":"vertical"}} -->
+    <div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--xl)"><!-- wp:avatar {"align":"center","style":{"border":{"radius":"100%"}}} /-->
 
         <!-- wp:query-title {"type":"archive","textAlign":"center","fontSize":"48"} /-->
 
-        <!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0"}}},"textColor":"neutral-400","fontSize":"18","animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
-        <p class="aligncenter has-text-align-center has-neutral-400-color has-text-color has-18-font-size aligncenter has-animation" style="margin-top:0;animation-iteration-count:"><?php echo esc_html__( 'Exploring ideas, sharing insights, and documenting the journey.', 'aegis' ); ?></p>
+        <!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0"}}},"textColor":"neutral-400","fontSize":"18"} -->
+        <p class="aligncenter has-text-align-center has-neutral-400-color has-text-color has-18-font-size aligncenter" style="margin-top:0"><?php echo esc_html__( 'Exploring ideas, sharing insights, and documenting the journey.', 'aegis' ); ?></p>
         <!-- /wp:paragraph -->
 
         <!-- wp:separator {"opacity":"css","align":"wide","className":"has-text-color has-neutral-100-color is-style-wide"} -->
@@ -27,8 +27,8 @@
 
     <!-- wp:query {"queryId":0,"query":{"perPage":10,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"align":"wide"} -->
     <div class="wp-block-query alignwide"><!-- wp:post-template {"layout":{"type":"grid","columnCount":3}} -->
-        <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|xs"}},"layout":{"type":"flex","orientation":"vertical"},"animation":{"event":"","iterationCount":"","duration":"","delay":""}} -->
-        <div class="wp-block-group has-animation" style="animation-iteration-count:"><!-- wp:post-featured-image {"isLink":true,"aspectRatio":"3/2","style":{"aspectRatio":{"all":""},"objectFit":{"all":""}}} /-->
+        <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|xs"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+        <div class="wp-block-group"><!-- wp:post-featured-image {"isLink":true,"aspectRatio":"3/2","style":{"aspectRatio":{"all":""},"objectFit":{"all":""}}} /-->
 
             <!-- wp:post-terms {"term":"category","className":"is-style-sub-heading"} /-->
 
