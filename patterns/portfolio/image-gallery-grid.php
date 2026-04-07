67d2fc754cf89f7678446a8974ebbf9769a21943
Atmostfear Entertainment <mail@atmostfear-entertainment.com>
2026-04-07 16:11:25 -0500

    Update image-gallery-grid.php
    Co-Authored-By: Alex de Borba <mail@alexdeborba.com>

--- patterns/portfolio/image-gallery-grid.php
+++ patterns/portfolio/image-gallery-grid.php
@@ -16,21 +16,19 @@
     <div class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--xs)">
         <!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|sm"}}},"layout":{"type":"constrained","contentSize":"640px"}} -->
         <div class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--sm)">
-            <!-- wp:group {"metadata":{"name":"Heading"},"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|lg"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"},"animation":{"duration":"1","iterationCount":"1"}} -->
-            <div class="wp-block-group has-animation"
-                style="margin-bottom:var(--wp--preset--spacing--lg);animation-duration:1s;animation-iteration-count:1">
-                <!-- wp:paragraph {"align":"center","className":"is-style-sub-heading","animation":{"duration":"1","iterationCount":"1"}} -->
-                <p class="aligncenter has-text-align-center is-style-sub-heading aligncenter has-animation"
-                    style="animation-duration:1s;animation-iteration-count:1"><?php echo esc_html__( 'Portfolio', 'aegis' ); ?></p>
+            <!-- wp:group {"metadata":{"name":"Heading"},"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|lg"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+            <div class="wp-block-group"
+                style="margin-bottom:var(--wp--preset--spacing--lg)">
+                <!-- wp:paragraph {"align":"center","className":"is-style-sub-heading"} -->
+                <p class="aligncenter has-text-align-center is-style-sub-heading aligncenter"><?php echo esc_html__( 'Portfolio', 'aegis' ); ?></p>
                 <!-- /wp:paragraph -->
 
                 <!-- wp:heading {"textAlign":"center","style":{"display":{"all":"","mobile":""}},"fontSize":"48"} -->
                 <h2 class="wp-block-heading has-text-align-center has-48-font-size"><?php echo esc_html__( 'Latest Projects', 'aegis' ); ?></h2>
                 <!-- /wp:heading -->
 
-                <!-- wp:paragraph {"align":"center","fontSize":"18","animation":{"duration":"1","iterationCount":"1"}} -->
-                <p class="aligncenter has-text-align-center has-18-font-size aligncenter has-animation"
-                    style="animation-duration:1s;animation-iteration-count:1"><?php echo esc_html__( 'Enhance the native experience with lightweight blocks engineered to integrate seamlessly without bloating the core.', 'aegis' ); ?></p>
+                <!-- wp:paragraph {"align":"center","fontSize":"18"} -->
+                <p class="aligncenter has-text-align-center has-18-font-size aligncenter"><?php echo esc_html__( 'Enhance the native experience with lightweight blocks engineered to integrate seamlessly without bloating the core.', 'aegis' ); ?></p>
                 <!-- /wp:paragraph -->
             </div>
             <!-- /wp:group -->
