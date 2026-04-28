741d905eb3f1cc762cfbc052306f1f71677a01ec
Atmostfear Entertainment <mail@alexdeborba.com>
2026-04-28 13:50:12 -0500

    Create notice-under-construction.php
    - Initial commit

---
+++ patterns/notice/notice-under-construction.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: Under Construction
+ * Slug: notice-under-construction
+ * Categories: notice
+ * Keywords: notice, construction, maintenance, coming soon, placeholder, wip
+ * Description: A centered under-construction or maintenance placeholder notice.
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"metadata":{"categories":["notice"],"patternName":"notice-under-construction","name":"Under Construction"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg"}}},"backgroundColor":"warning-100","layout":{"type":"constrained","contentSize":"600px"}} -->
+<div class="wp-block-group alignfull has-warning-100-background-color has-background" style="padding-top:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--lg)"><!-- wp:image {"className":"is-style-icon","iconSet":"wordpress","iconName":"tool","iconSize":"64px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-notice-tool-1\u0022 data-icon=\u0022wordpress-tool\u0022 width=\u002224\u0022 height=\u002224\u0022 fill=\u0022currentColor\u0022\u003e\u003ctitle id=\u0022icon-notice-tool-1\u0022\u003eTool Icon\u003c/title\u003e\u003cpath d=\u0022m14.103 7.128 2.26-2.26a4 4 0 0 0-5.207 4.804L5.828 15a2 2 0 1 0 2.828 2.828l5.329-5.328a4 4 0 0 0 4.804-5.208l-2.261 2.26-1.912-.512-.513-1.912zm-7.214 9.64a.5.5 0 1 1 .707-.707.5.5 0 0 1-.707.707z\u0022\u003e\u003c/path\u003e\u003c/svg\u003e","align":"center"} -->
+	<figure class="wp-block-image aligncenter is-style-icon" style="--wp--custom--icon--size:64px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-notice-tool-1&quot; data-icon=&quot;wordpress-tool&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-notice-tool-1&quot;&gt;Tool Icon</title&gt;<path d=&quot;m14.103 7.128 2.26-2.26a4 4 0 0 0-5.207 4.804L5.828 15a2 2 0 1 0 2.828 2.828l5.329-5.328a4 4 0 0 0 4.804-5.208l-2.261 2.26-1.912-.512-.513-1.912zm-7.214 9.64a.5.5 0 1 1 .707-.707.5.5 0 0 1-.707.707z&quot;&gt;</path&gt;</svg&gt;')"><img alt="" /></figure>
+	<!-- /wp:image -->
+
+	<!-- wp:heading {"textAlign":"center","level":2,"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"32"} -->
+	<h2 class="wp-block-heading has-text-align-center has-32-font-size" style="font-style:normal;font-weight:700"><?php echo esc_html__( 'Under Construction', 'aegis' ); ?></h2>
+	<!-- /wp:heading -->
+
+	<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"16px"}},"textColor":"neutral-600"} -->
+	<p class="aligncenter has-text-align-center has-neutral-600-color has-text-color aligncenter" style="font-size:16px"><?php echo esc_html__( 'We are currently working on this section. Please check back soon for updates. We appreciate your patience as we build something great.', 'aegis' ); ?></p>
+	<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
