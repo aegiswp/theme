7ac1121dc2cef449ba0de5e540f446a2025fb65a
Atmostfear Entertainment <mail@alexdeborba.com>
2026-04-28 11:43:26 -0500

    Create disclaimer-editorial.php
    - Initial commit

---
+++ patterns/notice/disclaimer-editorial.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: Editorial Independence Disclaimer
+ * Slug: disclaimer-editorial
+ * Categories: notice
+ * Keywords: disclaimer, editorial, independence, journalism, opinion, unbiased
+ * Description: A centered editorial independence and journalistic integrity statement.
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"metadata":{"categories":["notice"],"patternName":"disclaimer-editorial","name":"Editorial Independence Disclaimer"},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|md","bottom":"var:preset|spacing|md"}}},"layout":{"type":"constrained","contentSize":"600px"}} -->
+<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--md);padding-bottom:var(--wp--preset--spacing--md)"><!-- wp:image {"className":"is-style-icon","iconSet":"wordpress","iconName":"edit","iconSize":"32px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-notice-edit-1\u0022 data-icon=\u0022wordpress-edit\u0022 width=\u002224\u0022 height=\u002224\u0022 fill=\u0022currentColor\u0022\u003e\u003ctitle id=\u0022icon-notice-edit-1\u0022\u003eEdit Icon\u003c/title\u003e\u003cpath d=\u0022m19 7-3-3-8.5 8.5-1 4 4-1L19 7Zm-7 11.5H5V20h7v-1.5Z\u0022\u003e\u003c/path\u003e\u003c/svg\u003e","align":"center"} -->
+	<figure class="wp-block-image aligncenter is-style-icon" style="--wp--custom--icon--size:32px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-notice-edit-1&quot; data-icon=&quot;wordpress-edit&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-notice-edit-1&quot;&gt;Edit Icon</title&gt;<path d=&quot;m19 7-3-3-8.5 8.5-1 4 4-1L19 7Zm-7 11.5H5V20h7v-1.5Z&quot;&gt;</path&gt;</svg&gt;')"><img alt="" /></figure>
+	<!-- /wp:image -->
+
+	<!-- wp:heading {"textAlign":"center","level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"18"} -->
+	<h4 class="wp-block-heading has-text-align-center has-18-font-size" style="font-style:normal;font-weight:600"><?php echo esc_html__( 'Editorial Independence', 'aegis' ); ?></h4>
+	<!-- /wp:heading -->
+
+	<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"14px"}},"textColor":"neutral-500"} -->
+	<p class="aligncenter has-text-align-center has-neutral-500-color has-text-color aligncenter" style="font-size:14px"><?php echo esc_html__( 'Our editorial content is created independently and is never influenced by advertisers, sponsors, or affiliate partnerships. Our writers and editors maintain full control over all published material. Any sponsored or compensated content is clearly labeled as such.', 'aegis' ); ?></p>
+	<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
