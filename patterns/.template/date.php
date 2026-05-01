16040fe8723bb7edac54be08ffc63b5323a0926d
Atmostfear Entertainment <mail@alexdeborba.com>
2026-04-30 19:46:10 -0500

    Create date.php
    - Initial commit

---
+++ patterns/.template/date.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: Template Date
+ * Slug: date
+ * Categories: template
+ * Template Types: archive
+ * Inserter: false
+ */
+?>
+
+<!-- wp:pattern {"slug":"template-archive"} /-->
