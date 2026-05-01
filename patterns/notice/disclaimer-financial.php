4b85a445243f24e1ff873cb0bc74057bb56f2015
Atmostfear Entertainment <mail@alexdeborba.com>
2026-05-01 10:42:57 -0500

    Create disclaimer-financial.php
    - Initial commit

---
+++ patterns/notice/disclaimer-financial.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Title: Financial Disclaimer
+ * Slug: disclaimer-financial
+ * Categories: notice
+ * Keywords: disclaimer, financial, investment, advice, risk, not financial advice
+ * Description: An investment and financial information disclaimer with a currency icon.
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"metadata":{"categories":["notice"],"patternName":"disclaimer-financial","name":"Financial Disclaimer"},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|sm","bottom":"var:preset|spacing|sm","left":"var:preset|spacing|sm","right":"var:preset|spacing|sm"}},"border":{"bottom":{"width":"2px","color":"var:preset|color|neutral-200"}}},"backgroundColor":"neutral-50","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide has-neutral-50-background-color has-background" style="border-bottom-color:var(--wp--preset--color--neutral-200);border-bottom-width:2px;padding-top:var(--wp--preset--spacing--sm);padding-right:var(--wp--preset--spacing--sm);padding-bottom:var(--wp--preset--spacing--sm);padding-left:var(--wp--preset--spacing--sm)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+	<div class="wp-block-group"><!-- wp:image {"className":"is-style-icon","iconSet":"wordpress","iconName":"currency-dollar","iconSize":"24px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-notice-dollar-1\u0022 data-icon=\u0022wordpress-currency-dollar\u0022 width=\u002224\u0022 height=\u002224\u0022 fill=\u0022currentColor\u0022\u003e\u003ctitle id=\u0022icon-notice-dollar-1\u0022\u003eCurrency Dollar Icon\u003c/title\u003e\u003cpath d=\u0022M3.25 12a8.75 8.75 0 1 1 17.5 0 8.75 8.75 0 0 1-17.5 0zM12 4.75a7.25 7.25 0 1 0 0 14.5 7.25 7.25 0 0 0 0-14.5zm-1.338 4.877c-.314.22-.412.452-.412.623 0 .171.098.403.412.623.312.218.783.377 1.338.377.825 0 1.605.233 2.198.648.59.414 1.052 1.057 1.052 1.852 0 .795-.461 1.438-1.052 1.852-.41.286-.907.486-1.448.582v.316a.75.75 0 0 1-1.5 0v-.316a3.64 3.64 0 0 1-1.448-.582c-.59-.414-1.052-1.057-1.052-1.852a.75.75 0 0 1 1.5 0c0 .171.098.403.412.623.312.218.783.377 1.338.377s1.026-.159 1.338-.377c.314-.22.412-.452.412-.623 0-.171-.098-.403-.412-.623-.312-.218-.783-.377-1.338-.377-.825 0-1.605-.233-2.198-.648-.59-.414-1.052-1.057-1.052-1.852 0-.795.461-1.438 1.052-1.852a3.64 3.64 0 0 1 1.448-.582V7.5a.75.75 0 0 1 1.5 0v.316c.54.096 1.039.296 1.448.582.59.414 1.052 1.057 1.052 1.852a.75.75 0 0 1-1.5 0c0-.171-.098-.403-.412-.623-.312-.218-.783-.377-1.338-.377s-1.026.159-1.338.377z\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} -->
+		<figure class="wp-block-image is-style-icon" style="--wp--custom--icon--size:24px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-notice-dollar-1&quot; data-icon=&quot;wordpress-currency-dollar&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-notice-dollar-1&quot;&gt;Currency Dollar Icon</title&gt;<path d=&quot;M3.25 12a8.75 8.75 0 1 1 17.5 0 8.75 8.75 0 0 1-17.5 0zM12 4.75a7.25 7.25 0 1 0 0 14.5 7.25 7.25 0 0 0 0-14.5zm-1.338 4.877c-.314.22-.412.452-.412.623 0 .171.098.403.412.623.312.218.783.377 1.338.377.825 0 1.605.233 2.198.648.59.414 1.052 1.057 1.052 1.852 0 .795-.461 1.438-1.052 1.852-.41.286-.907.486-1.448.582v.316a.75.75 0 0 1-1.5 0v-.316a3.64 3.64 0 0 1-1.448-.582c-.59-.414-1.052-1.057-1.052-1.852a.75.75 0 0 1 1.5 0c0 .171.098.403.412.623.312.218.783.377 1.338.377s1.026-.159 1.338-.377c.314-.22.412-.452.412-.623 0-.171-.098-.403-.412-.623-.312-.218-.783-.377-1.338-.377-.825 0-1.605-.233-2.198-.648-.59-.414-1.052-1.057-1.052-1.852 0-.795.461-1.438 1.052-1.852a3.64 3.64 0 0 1 1.448-.582V7.5a.75.75 0 0 1 1.5 0v.316c.54.096 1.039.296 1.448.582.59.414 1.052 1.057 1.052 1.852a.75.75 0 0 1-1.5 0c0-.171-.098-.403-.412-.623-.312-.218-.783-.377-1.338-.377s-1.026.159-1.338.377z&quot;&gt;</path&gt;</svg&gt;')"><img alt="" /></figure>
+		<!-- /wp:image -->
+
+		<!-- wp:group {"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"16"} -->
+			<p class="has-16-font-size" style="font-style:normal;font-weight:600"><?php echo esc_html__( 'Financial Disclaimer', 'aegis' ); ?></p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:paragraph {"style":{"typography":{"fontSize":"14px"}},"textColor":"neutral-500"} -->
+			<p class="has-neutral-500-color has-text-color" style="font-size:14px"><?php echo esc_html__( 'The content on this website is for informational purposes only and does not constitute financial, investment, or tax advice. You should consult with a qualified financial advisor before making any investment decisions. Past performance is not indicative of future results, and all investments carry risk, including the potential loss of principal.', 'aegis' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
