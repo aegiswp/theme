9113edb88772590505080fb874875dcd6cc92ece
Atmostfear Entertainment <mail@alexdeborba.com>
2026-05-01 13:02:35 -0500

    Create default.php
    - Initial commit

---
+++ patterns/sidebar/default.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Sidebar Default
+ * Slug: default
+ * Categories: sidebar
+ * Block Types: core/template-part/sidebar
+ */
+?>
+
+<!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group">
+	<!-- wp:heading {"level":2,"fontSize":"large"} -->
+	<h2 class="wp-block-heading has-large-font-size"><?php echo esc_html__( 'Sidebar', 'aegis' ); ?></h2>
+	<!-- /wp:heading -->
+</div>
+<!-- /wp:group -->
