5672d9220ffc95a56e7380f0b6b551763e3e6608
Atmostfear Entertainment <mail@alexdeborba.com>
2026-05-01 08:24:23 -0500

    Create disclaimer-earnings.php
    - Initial commit

---
+++ patterns/notice/disclaimer-earnings.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Title: Earnings Disclaimer
+ * Slug: disclaimer-earnings
+ * Categories: notice
+ * Keywords: disclaimer, earnings, income, results, guarantee, courses, digital
+ * Description: An earnings and income disclaimer for digital products, courses, and business opportunities.
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"metadata":{"categories":["notice"],"patternName":"disclaimer-earnings","name":"Earnings Disclaimer"},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|sm","bottom":"var:preset|spacing|sm","left":"var:preset|spacing|sm","right":"var:preset|spacing|sm"}},"border":{"radius":"4px"}},"backgroundColor":"warning-100","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide has-warning-100-background-color has-background" style="border-radius:4px;padding-top:var(--wp--preset--spacing--sm);padding-right:var(--wp--preset--spacing--sm);padding-bottom:var(--wp--preset--spacing--sm);padding-left:var(--wp--preset--spacing--sm)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+	<div class="wp-block-group"><!-- wp:image {"className":"is-style-icon","iconSet":"wordpress","iconName":"trending-up","iconSize":"24px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-notice-trending-1\u0022 data-icon=\u0022wordpress-trending-up\u0022 width=\u002224\u0022 height=\u002224\u0022 fill=\u0022currentColor\u0022\u003e\u003ctitle id=\u0022icon-notice-trending-1\u0022\u003eTrending Up Icon\u003c/title\u003e\u003cpath d=\u0022M3.445 16.505a.75.75 0 0 0 1.06.05l5.005-4.55 4.024 3.521 4.716-4.715V14h1.5V8.25H14v1.5h3.19l-3.724 3.723L9.49 9.995l-5.995 5.45a.75.75 0 0 0-.05 1.06z\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} -->
+		<figure class="wp-block-image is-style-icon" style="--wp--custom--icon--size:24px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-notice-trending-1&quot; data-icon=&quot;wordpress-trending-up&quot; width=&quot;24&quot; height=&quot;24&quot; fill=&quot;currentColor&quot;&gt;<title id=&quot;icon-notice-trending-1&quot;&gt;Trending Up Icon</title&gt;<path d=&quot;M3.445 16.505a.75.75 0 0 0 1.06.05l5.005-4.55 4.024 3.521 4.716-4.715V14h1.5V8.25H14v1.5h3.19l-3.724 3.723L9.49 9.995l-5.995 5.45a.75.75 0 0 0-.05 1.06z&quot;&gt;</path&gt;</svg&gt;')"><img alt="" /></figure>
+		<!-- /wp:image -->
+
+		<!-- wp:group {"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"16"} -->
+			<p class="has-16-font-size" style="font-style:normal;font-weight:600"><?php echo esc_html__( 'Earnings Disclaimer', 'aegis' ); ?></p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:paragraph {"style":{"typography":{"fontSize":"14px"}},"textColor":"neutral-600"} -->
+			<p class="has-neutral-600-color has-text-color" style="font-size:14px"><?php echo esc_html__( 'Any earnings or income statements are estimates only. There is no guarantee that you will make these levels of income and you accept the risk that the earnings and income statements differ by individual. The use of our products and services should be based on your own due diligence.', 'aegis' ); ?></p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:paragraph {"style":{"typography":{"fontSize":"14px"}},"textColor":"neutral-600"} -->
+			<p class="has-neutral-600-color has-text-color" style="font-size:14px"><?php echo esc_html__( 'We make every effort to ensure accuracy of the information presented, but we are not responsible for errors or omissions. Earnings depend on a number of factors including individual effort, time, and skill.', 'aegis' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
