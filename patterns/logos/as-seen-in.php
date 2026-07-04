67b9de46befe2209bb68a173403e64d0b4d5a699
Atmostfear Entertainment <mail@alexdeborba.com>
2026-07-04 12:19:38 -0500

    Create as-seen-in.php
    - Initial commit

---
+++ patterns/logos/as-seen-in.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Title: As Seen In
+ * Slug: as-seen-in
+ * Categories: logos
+ * Keywords: logos, press, media, featured, as seen in, publications
+ * Description: An "as seen in" press bar with publication logos.
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"metadata":{"categories":["logos"],"patternName":"as-seen-in","name":"As Seen In"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|md","bottom":"var:preset|spacing|md"}},"border":{"top":{"color":"var:preset|color|neutral-200","width":"1px"},"bottom":{"color":"var:preset|color|neutral-200","width":"1px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="border-top-color:var(--wp--preset--color--neutral-200);border-top-width:1px;border-bottom-color:var(--wp--preset--color--neutral-200);border-bottom-width:1px;padding-top:var(--wp--preset--spacing--md);padding-bottom:var(--wp--preset--spacing--md)">
+	<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|lg"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
+	<div class="wp-block-group alignwide">
+		<!-- wp:paragraph {"textColor":"neutral-400","fontSize":"13","style":{"typography":{"fontStyle":"normal","fontWeight":"500","textTransform":"uppercase","letterSpacing":"0.1em"}}} -->
+		<p class="has-neutral-400-color has-text-color has-13-font-size" style="font-style:normal;font-weight:500;text-transform:uppercase;letter-spacing:0.1em"><?php echo esc_html__( 'As seen in', 'aegis' ); ?></p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:image {"width":"100px","sizeSlug":"full","linkDestination":"none","style":{"color":{"duotone":["#cbd5e1","#cbd5e1"]}}} -->
+		<figure class="wp-block-image size-full is-resized"><img alt="" style="width:100px" /></figure>
+		<!-- /wp:image -->
+
+		<!-- wp:image {"width":"100px","sizeSlug":"full","linkDestination":"none","style":{"color":{"duotone":["#cbd5e1","#cbd5e1"]}}} -->
+		<figure class="wp-block-image size-full is-resized"><img alt="" style="width:100px" /></figure>
+		<!-- /wp:image -->
+
+		<!-- wp:image {"width":"100px","sizeSlug":"full","linkDestination":"none","style":{"color":{"duotone":["#cbd5e1","#cbd5e1"]}}} -->
+		<figure class="wp-block-image size-full is-resized"><img alt="" style="width:100px" /></figure>
+		<!-- /wp:image -->
+
+		<!-- wp:image {"width":"100px","sizeSlug":"full","linkDestination":"none","style":{"color":{"duotone":["#cbd5e1","#cbd5e1"]}}} -->
+		<figure class="wp-block-image size-full is-resized"><img alt="" style="width:100px" /></figure>
+		<!-- /wp:image -->
+
+		<!-- wp:image {"width":"100px","sizeSlug":"full","linkDestination":"none","style":{"color":{"duotone":["#cbd5e1","#cbd5e1"]}}} -->
+		<figure class="wp-block-image size-full is-resized"><img alt="" style="width:100px" /></figure>
+		<!-- /wp:image -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
