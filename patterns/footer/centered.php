a8ce64b1a5a6f5fa040c88646dc19f08ff303b1c
Atmostfear Entertainment <mail@alexdeborba.com>
2026-07-04 08:36:20 -0500

    Update centered.php

--- patterns/footer/centered.php
+++ patterns/footer/centered.php
@@ -12,64 +12,55 @@
 
 <!-- wp:group {"metadata":{"categories":["footer"],"patternName":"centered","name":"Centered"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg"}}},"fontSize":"14","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
 <div class="wp-block-group alignfull has-14-font-size"
-    style="padding-top:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--lg)">
-    <!-- wp:group {"style":{"spacing":{"blockGap":"4px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-    <div class="wp-block-group">
-        <!-- wp:image {"className":"is-style-icon","textColor":"primary-500","iconSet":"social","iconName":"aegis","iconSize":"75px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 fill=\u0022currentColor\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-696aa0b58f7c4\u0022 data-icon=\u0022social-aegis\u0022 width=\u002224\u0022 height=\u002224\u0022\u003e\u003ctitle id=\u0022icon-696aa0b58f7c4\u0022\u003eAegis Icon\u003c/title\u003e\u003cpath fill-rule=\u0022evenodd\u0022 d=\u0022M10.06 7.75 L12.02 3.87 L13.95 7.72 L16.65 9.29 L12.03 0 L7.34 9.3 L10.06 7.75 Z M18.37 12.72 L18.2 12.36 L15.5 10.79 L17.02 13.68 L20.05 18.61 L12.02 15.17 L3.98 18.62 L6.96 13.68 L8.39 10.81 L5.67 12.39 L5.5 12.71 L0 22.87 L12.01 16.87 L24 22.94 L18.37 12.72 Z\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} -->
-        <figure class="wp-block-image is-style-icon has-primary-500-color has-text-color"
-            style="--wp--custom--icon--color:var(--wp--preset--color--primary-500,currentColor);--wp--custom--icon--size:75px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; fill=&quot;currentColor&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-696aa0b58f7c4&quot; data-icon=&quot;social-aegis&quot; width=&quot;24&quot; height=&quot;24&quot;&gt;<title id=&quot;icon-696aa0b58f7c4&quot;&gt;Aegis Icon</title&gt;<path fill-rule=&quot;evenodd&quot; d=&quot;M10.06 7.75 L12.02 3.87 L13.95 7.72 L16.65 9.29 L12.03 0 L7.34 9.3 L10.06 7.75 Z M18.37 12.72 L18.2 12.36 L15.5 10.79 L17.02 13.68 L20.05 18.61 L12.02 15.17 L3.98 18.62 L6.96 13.68 L8.39 10.81 L5.67 12.39 L5.5 12.71 L0 22.87 L12.01 16.87 L24 22.94 L18.37 12.72 Z&quot;&gt;</path&gt;</svg&gt;')">
-            <img src="" alt="" />
-        </figure>
-        <!-- /wp:image -->
-    </div>
-    <!-- /wp:group -->
+	style="padding-top:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--lg)">
+	<!-- wp:group {"style":{"spacing":{"blockGap":"4px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+	<div class="wp-block-group">
+		<!-- wp:image {"className":"is-style-icon","iconSet":"social","iconName":"aegis","textColor":"primary-500","iconSize":"75px","iconSvgString":"\u003csvg xmlns=\u0022http://www.w3.org/2000/svg\u0022 viewBox=\u00220 0 24 24\u0022 role=\u0022img\u0022 aria-labelledby=\u0022icon-6a2cc03b31128\u0022 data-icon=\u0022social-aegis\u0022 style=\u0022min-width:75px;height:75px\u0022 fill=\u0022currentColor\u0022\u003e\u003ctitle id=\u0022icon-6a2cc03b31128\u0022\u003eAegis Icon\u003c/title\u003e\u003cpath d=\u0022M10.06 7.75 L12.02 3.87 L13.95 7.72 L16.65 9.29 L12.03 0 L7.34 9.3 L10.06 7.75 Z M18.37 12.72 L18.2 12.36 L15.5 10.79 L17.02 13.68 L20.05 18.61 L12.02 15.17 L3.98 18.62 L6.96 13.68 L8.39 10.81 L5.67 12.39 L5.5 12.71 L0 22.87 L12.01 16.87 L24 22.94 L18.37 12.72 Z\u0022 stroke=\u0022none\u0022 fill-rule=\u0022evenodd\u0022\u003e\u003c/path\u003e\u003c/svg\u003e"} --><figure class="wp-block-image is-style-icon" style="--wp--custom--icon--size:75px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; viewBox=&quot;0 0 24 24&quot; role=&quot;img&quot; aria-labelledby=&quot;icon-6a2cc03b31128&quot; data-icon=&quot;social-aegis&quot; style=&quot;min-width:75px;height:75px&quot; fill=&quot;currentColor&quot;><title id=&quot;icon-6a2cc03b31128&quot;>Aegis Icon</title><path d=&quot;M10.06 7.75 L12.02 3.87 L13.95 7.72 L16.65 9.29 L12.03 0 L7.34 9.3 L10.06 7.75 Z M18.37 12.72 L18.2 12.36 L15.5 10.79 L17.02 13.68 L20.05 18.61 L12.02 15.17 L3.98 18.62 L6.96 13.68 L8.39 10.81 L5.67 12.39 L5.5 12.71 L0 22.87 L12.01 16.87 L24 22.94 L18.37 12.72 Z&quot; stroke=&quot;none&quot; fill-rule=&quot;evenodd&quot;></path></svg>')"><img alt="" /></figure><!-- /wp:image -->
+</div>
+	<!-- /wp:group -->
 
-    <!-- wp:navigation {"overlayMenu":"never","style":{"spacing":{"padding":{"top":"var:preset|spacing|sm","bottom":"var:preset|spacing|sm"}},"typography":{"fontStyle":"normal","fontWeight":"500"}},"layout":{"type":"flex","justifyContent":"center","flexWrap":"wrap"}} /-->
+	<!-- wp:navigation {"overlayMenu":"never","style":{"spacing":{"padding":{"top":"var:preset|spacing|sm","bottom":"var:preset|spacing|sm"}},"typography":{"fontStyle":"normal","fontWeight":"500"}},"layout":{"type":"flex","justifyContent":"center","flexWrap":"wrap"}} /-->
 
-    <!-- wp:social-links {"iconColor":"neutral-400","size":"has-small-icon-size","className":"is-style-logos-only","style":{"spacing":{"blockGap":{"top":"20px","left":"20px"},"padding":{"top":"var:preset|spacing|xxs","bottom":"var:preset|spacing|md"}}},"layout":{"type":"flex","justifyContent":"left","orientation":"horizontal"}} -->
-    <ul class="wp-block-social-links has-small-icon-size has-icon-color is-style-logos-only"
-        style="padding-top:var(--wp--preset--spacing--xxs);padding-bottom:var(--wp--preset--spacing--md)">
-        <!-- wp:social-link {"url":"#","service":"facebook"} /-->
+	<!-- wp:social-links {"iconColor":"neutral-400","size":"has-small-icon-size","className":"is-style-logos-only","style":{"spacing":{"blockGap":{"top":"20px","left":"20px"},"padding":{"top":"var:preset|spacing|xxs","bottom":"var:preset|spacing|md"}}},"layout":{"type":"flex","justifyContent":"left","orientation":"horizontal"}} -->
+	<ul class="wp-block-social-links has-small-icon-size has-icon-color is-style-logos-only"
+		style="padding-top:var(--wp--preset--spacing--xxs);padding-bottom:var(--wp--preset--spacing--md)">
+		<!-- wp:social-link {"url":"#","service":"facebook"} /-->
 
-        <!-- wp:social-link {"url":"#","service":"linkedin"} /-->
+		<!-- wp:social-link {"url":"#","service":"linkedin"} /-->
 
-        <!-- wp:social-link {"url":"#","service":"bluesky"} /-->
+		<!-- wp:social-link {"url":"#","service":"bluesky"} /-->
 
-        <!-- wp:social-link {"url":"#","service":"instagram"} /-->
-    </ul>
-    <!-- /wp:social-links -->
+		<!-- wp:social-link {"url":"#","service":"instagram"} /-->
+	</ul>
+	<!-- /wp:social-links -->
 
-    <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"0","top":"var:preset|spacing|xs"},"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-    <div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--xs);padding-bottom:0">
-        <!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"4px","margin":{"top":"0","bottom":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
-        <div class="wp-block-group alignwide" style="margin-top:0;margin-bottom:0">
-            <!-- wp:paragraph {"align":"center"} -->
-            <p class="aligncenter has-text-align-center aligncenter"><?php echo esc_html__( '© Copyright {year}', 'aegis' ); ?></p>
-            <!-- /wp:paragraph -->
+	<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"0","top":"var:preset|spacing|xs"},"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+	<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--xs);padding-bottom:0">
+		<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"4px","margin":{"top":"0","bottom":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+		<div class="wp-block-group alignwide" style="margin-top:0;margin-bottom:0">
+			<!-- wp:paragraph {"align":"center"} -->
+			<p class="aligncenter has-text-align-center aligncenter"><?php echo esc_html__( '© Copyright {year}', 'aegis' ); ?></p>
+			<!-- /wp:paragraph -->
 
-            <!-- wp:site-title {"level":0} /-->
+			<!-- wp:site-title {"level":0} /-->
 
-            <!-- wp:paragraph {"align":"center"} -->
-            <p class="aligncenter has-text-align-center aligncenter"><?php echo esc_html__( '· All rights reserved.', 'aegis' ); ?></p>
-            <!-- /wp:paragraph -->
-        </div>
-        <!-- /wp:group -->
+			<!-- wp:paragraph {"align":"center"} -->
+			<p class="aligncenter has-text-align-center aligncenter"><?php echo esc_html__( '· All rights reserved.', 'aegis' ); ?></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:group -->
 
-        <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-        <div class="wp-block-group"><!-- wp:buttons -->
-            <div class="wp-block-buttons">
-                <!-- wp:button {"backgroundColor":"transparent","textColor":"current","className":"is-style-fill","style":{"spacing":{"padding":{"top":"0","right":"var:preset|spacing|xxs","bottom":"0","left":"var:preset|spacing|xxs"}}},"onclick":"window.scrollTo({ top: 0, behavior: 'smooth' })","shadowPreset":"none","shadowPresetHover":"sm","iconSet":"wordpress","iconName":"chevron-up","iconSize":"20px","iconPosition":"end","iconSvgString":"\u003csvg viewBox=\u00220 0 24 24\u0022 xmlns=\u0022http://www.w3.org/2000/svg\u0022\u003e\u003cpath d=\u0022M6.5 12.4 12 8l5.5 4.4-.9 1.2L12 10l-4.5 3.6-1-1.2z\u0022/\u003e\u003c/svg\u003e"} -->
-                <div class="wp-block-button is-style-fill has-shadow has-none-shadow has-sm-shadow-hover"
-                    style="padding-top:0;padding-right:var(--wp--preset--spacing--xxs);padding-bottom:0;padding-left:var(--wp--preset--spacing--xxs);--wp--custom--icon--background:var(--wp--preset--color--transparent, currentColor);--wp--custom--icon--color:var(--wp--preset--color--current,currentColor);--wp--custom--icon--padding:0 var(--wp--preset--spacing--xxs) 0 var(--wp--preset--spacing--xxs);--wp--custom--icon--size:20px;--wp--custom--icon--url:url('data:image/svg+xml;utf8,<svg viewBox=&quot;0 0 24 24&quot; xmlns=&quot;http://www.w3.org/2000/svg&quot;&gt;<path d=&quot;M6.5 12.4 12 8l5.5 4.4-.9 1.2L12 10l-4.5 3.6-1-1.2z&quot;/&gt;</svg&gt;')">
-                    <a class="wp-block-button__link has-current-color has-transparent-background-color has-text-color has-background wp-element-button"
-                        style="padding-top:0;padding-right:var(--wp--preset--spacing--xxs);padding-bottom:0;padding-left:var(--wp--preset--spacing--xxs)"><?php echo esc_html__( 'Back to Top', 'aegis' ); ?></a>
-                </div>
-                <!-- /wp:button -->
-            </div>
-            <!-- /wp:buttons -->
-        </div>
-        <!-- /wp:group -->
-    </div>
-    <!-- /wp:group -->
+		<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+		<div class="wp-block-group"><!-- wp:buttons -->
+			<div class="wp-block-buttons">
+				<!-- wp:button {"backgroundColor":"transparent","textColor":"current","className":"is-style-fill","style":{"spacing":{"padding":{"top":"0","right":"var:preset|spacing|xxs","bottom":"0","left":"var:preset|spacing|xxs"}}},"onclick":"window.scrollTo({ top: 0, behavior: 'smooth' })","shadowPreset":"none","shadowPresetHover":"sm","iconSet":"WordPress","iconName":"chevron-up","iconSize":"20px","iconPosition":"end","iconSvgString":"\u003csvg viewBox=\u00220 0 24 24\u0022 xmlns=\u0022http://www.w3.org/2000/svg\u0022\u003e\u003cpath d=\u0022M6.5 12.4 12 8l5.5 4.4-.9 1.2L12 10l-4.5 3.6-1-1.2z\u0022/\u003e\u003c/svg\u003e"} -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button has-transparent-background-color has-background has-current-color"><?php echo esc_html__( 'Back to Top', 'aegis' ); ?></a></div>
+<!-- /wp:button -->
+			</div>
+			<!-- /wp:buttons -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:group -->
 </div>
 <!-- /wp:group -->
