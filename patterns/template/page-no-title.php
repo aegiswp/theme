38984640b38bd02e320e1c5d9f8c089366752a15
Atmostfear Entertainment <mail@alexdeborba.com>
2026-07-05 15:19:37 -0500

    Create page-no-title.php
    - Initial comment

---
+++ patterns/template/page-no-title.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: Page No Title
+ * Slug: page-no-title
+ * Categories: template
+ * Template Types: page, page-no-title
+ * Inserter: false
+ */
+?>
+<!-- wp:template-part {"slug":"header","tagName":"header","className":"site-header"} /-->
+
+<!-- wp:group {"tagName":"main","anchor":"main","align":"full","className":"site-main","layout":{"type":"constrained"},"metadata":{"name":"Main"}} -->
+<main id="main" class="wp-block-group alignfull site-main">
+	<!-- wp:post-content {"layout":{"type":"constrained"}} /-->
+</main>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","tagName":"footer","className":"site-footer"} /-->
