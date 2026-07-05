11935398394b7a923835f891568b75d7403165f3
Atmostfear Entertainment <mail@alexdeborba.com>
2026-07-05 15:31:37 -0500

    Update page.php
    - Initial commit

--- patterns/template/page.php
+++ patterns/template/page.php
@@ -9,12 +9,12 @@
 ?>
 <!-- wp:template-part {"slug":"header","tagName":"header","className":"site-header"} /-->
 
-<!-- wp:group {"tagName":"main","align":"full","className":"site-main","layout":{"type":"constrained"},"metadata":{"name":"Main"}} -->
-<main class="wp-block-group alignfull site-main"><!-- wp:pattern {"slug":"hero-minimal","preview":true} /-->
-    <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|xl"}}},"layout":{"type":"constrained"}} -->
-    <div class="wp-block-group alignfull" style="padding-bottom:var(--wp--preset--spacing--xl)">
-        <!-- wp:post-content /--></div>
-    <!-- /wp:group -->
+<!-- wp:group {"tagName":"main","anchor":"main","align":"full","className":"site-main","layout":{"type":"constrained"},"metadata":{"name":"Main"}} -->
+<main id="main" class="wp-block-group alignfull site-main"><!-- wp:pattern {"slug":"hero-minimal","preview":true} /-->
+	<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|xl"}}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group alignfull" style="padding-bottom:var(--wp--preset--spacing--xl)">
+		<!-- wp:post-content /--></div>
+	<!-- /wp:group -->
 </main>
 <!-- /wp:group -->
 
