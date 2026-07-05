1a646462f523bcb1dada53b2ee23df559b8e52c5
Atmostfear Entertainment <mail@alexdeborba.com>
2026-07-05 14:41:19 -0500

    Update full-width.php

--- patterns/template/full-width.php
+++ patterns/template/full-width.php
@@ -10,8 +10,8 @@
 
 <!-- wp:template-part {"slug":"header","tagName":"header","className":"site-header"} /-->
 
-<!-- wp:group {"tagName":"main","align":"full","className":"site-main","layout":{"type":"constrained"},"metadata":{"name":"Main"}} -->
-<main class="wp-block-group alignfull site-main">
+<!-- wp:group {"tagName":"main","anchor":"main","align":"full","className":"site-main","layout":{"type":"constrained"},"metadata":{"name":"Main"}} -->
+<main id="main" class="wp-block-group alignfull site-main">
 	<!-- wp:post-content {"align":"wide","layout":{"type":"default"}} /-->
 </main>
 <!-- /wp:group -->
