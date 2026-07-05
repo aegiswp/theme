e342eec6cecad06c1d7d09bd9957c56530720580
Atmostfear Entertainment <mail@alexdeborba.com>
2026-07-05 11:38:42 -0500

    Update image-gallery-grid.php

--- patterns/portfolio/image-gallery-grid.php
+++ patterns/portfolio/image-gallery-grid.php
@@ -11,58 +11,58 @@
 
 <!-- wp:group {"metadata":{"categories":["portfolio"],"patternName":"image-gallery-grid","name":"Portfolio Image Gallery Grid"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|lg","bottom":"var:preset|spacing|lg"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull"
-    style="padding-top:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--lg)">
-    <!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|xs"}}},"layout":{"type":"constrained"}} -->
-    <div class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--xs)">
-        <!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|sm"}}},"layout":{"type":"constrained","contentSize":"640px"}} -->
-        <div class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--sm)">
-            <!-- wp:group {"metadata":{"name":"Heading"},"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|lg"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
-            <div class="wp-block-group"
-                style="margin-bottom:var(--wp--preset--spacing--lg)">
-                <!-- wp:paragraph {"align":"center","className":"is-style-sub-heading"} -->
-                <p class="aligncenter has-text-align-center is-style-sub-heading aligncenter"><?php echo esc_html__( 'Portfolio', 'aegis' ); ?></p>
-                <!-- /wp:paragraph -->
+	style="padding-top:var(--wp--preset--spacing--lg);padding-bottom:var(--wp--preset--spacing--lg)">
+	<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|xs"}}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--xs)">
+		<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|sm"}}},"layout":{"type":"constrained","contentSize":"640px"}} -->
+		<div class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--sm)">
+			<!-- wp:group {"metadata":{"name":"Heading"},"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|lg"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+			<div class="wp-block-group"
+				style="margin-bottom:var(--wp--preset--spacing--lg)">
+				<!-- wp:paragraph {"align":"center","className":"is-style-sub-heading"} -->
+				<p class="aligncenter has-text-align-center is-style-sub-heading aligncenter"><?php echo esc_html__( 'Portfolio', 'aegis' ); ?></p>
+				<!-- /wp:paragraph -->
 
-                <!-- wp:heading {"textAlign":"center","style":{"display":{"all":"","mobile":""}},"fontSize":"48"} -->
-                <h2 class="wp-block-heading has-text-align-center has-48-font-size"><?php echo esc_html__( 'Latest Projects', 'aegis' ); ?></h2>
-                <!-- /wp:heading -->
+				<!-- wp:heading {"textAlign":"center","style":{"display":{"all":"","mobile":""}},"fontSize":"48"} -->
+				<h2 class="wp-block-heading has-text-align-center has-48-font-size"><?php echo esc_html__( 'Latest Projects', 'aegis' ); ?></h2>
+				<!-- /wp:heading -->
 
-                <!-- wp:paragraph {"align":"center","fontSize":"18"} -->
-                <p class="aligncenter has-text-align-center has-18-font-size aligncenter"><?php echo esc_html__( 'Enhance the native experience with lightweight blocks engineered to integrate seamlessly without bloating the core.', 'aegis' ); ?></p>
-                <!-- /wp:paragraph -->
-            </div>
-            <!-- /wp:group -->
-        </div>
-        <!-- /wp:group -->
-    </div>
-    <!-- /wp:group -->
+				<!-- wp:paragraph {"align":"center","fontSize":"18"} -->
+				<p class="aligncenter has-text-align-center has-18-font-size aligncenter"><?php echo esc_html__( 'Enhance the native experience with lightweight blocks engineered to integrate seamlessly without bloating the core.', 'aegis' ); ?></p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:group -->
 
-    <!-- wp:group {"align":"wide","style":{"display":{"all":"grid"},"gridTemplateColumns":{"all":"repeat(4,minmax(0,1fr))","mobile":"repeat(1,minmax(0,1fr))"},"gridTemplateRows":{"all":"repeat(2,minmax(0,1fr))","mobile":"repeat(6,minmax(0,1fr))"},"gridColumnStart":"","gridColumnEnd":"","gridRowStart":"","gridRowEnd":""},"layout":{"type":"flex","orientation":"grid","flexWrap":"nowrap"}} -->
-    <div class="wp-block-group alignwide">
-        <!-- wp:image {"scale":"cover","className":"","style":{"aspectRatio":{"all":""},"display":"","order":"","width":"","maxWidth":"","gridColumnEnd":{"desktop":"3"},"gridColumnStart":{"desktop":"1"},"gridRowStart":{"desktop":"1"},"gridRowEnd":{"desktop":"2"},"gridTemplateColumns":"","gridTemplateRows":""}} -->
-        <figure class="wp-block-image"><img alt="" style="object-fit:cover" /></figure>
-        <!-- /wp:image -->
+	<!-- wp:group {"align":"wide","style":{"display":{"all":"grid"},"gridTemplateColumns":{"all":"repeat(4,minmax(0,1fr))","mobile":"repeat(1,minmax(0,1fr))"},"gridTemplateRows":{"all":"repeat(2,minmax(0,1fr))","mobile":"repeat(6,minmax(0,1fr))"},"gridColumnStart":"","gridColumnEnd":"","gridRowStart":"","gridRowEnd":""},"layout":{"type":"flex","orientation":"grid","flexWrap":"nowrap"}} -->
+	<div class="wp-block-group alignwide">
+		<!-- wp:image {"scale":"cover","className":"","style":{"aspectRatio":{"all":""},"display":"","order":"","width":"","maxWidth":"","gridColumnEnd":{"desktop":"3"},"gridColumnStart":{"desktop":"1"},"gridRowStart":{"desktop":"1"},"gridRowEnd":{"desktop":"2"},"gridTemplateColumns":"","gridTemplateRows":""}} -->
+		<figure class="wp-block-image"><img alt="" style="object-fit:cover" /></figure>
+		<!-- /wp:image -->
 
-        <!-- wp:image {"scale":"cover","className":"","style":{"aspectRatio":{"all":""},"display":"","order":"","width":"","maxWidth":"","gridColumnEnd":{"desktop":"5"},"gridColumnStart":{"desktop":"4"},"gridRowStart":{"desktop":"1"},"gridRowEnd":{"desktop":"2"},"gridTemplateColumns":"","gridTemplateRows":""}} -->
-        <figure class="wp-block-image"><img alt="" style="object-fit:cover" /></figure>
-        <!-- /wp:image -->
+		<!-- wp:image {"scale":"cover","className":"","style":{"aspectRatio":{"all":""},"display":"","order":"","width":"","maxWidth":"","gridColumnEnd":{"desktop":"5"},"gridColumnStart":{"desktop":"4"},"gridRowStart":{"desktop":"1"},"gridRowEnd":{"desktop":"2"},"gridTemplateColumns":"","gridTemplateRows":""}} -->
+		<figure class="wp-block-image"><img alt="" style="object-fit:cover" /></figure>
+		<!-- /wp:image -->
 
-        <!-- wp:image {"scale":"cover","className":"","style":{"aspectRatio":{"all":""},"display":"","order":"","width":"","maxWidth":"","gridColumnEnd":{"desktop":"4"},"gridColumnStart":{"desktop":"3"},"gridRowStart":{"desktop":"1"},"gridRowEnd":{"desktop":"2"},"gridTemplateColumns":"","gridTemplateRows":""}} -->
-        <figure class="wp-block-image"><img alt="" style="object-fit:cover" /></figure>
-        <!-- /wp:image -->
+		<!-- wp:image {"scale":"cover","className":"","style":{"aspectRatio":{"all":""},"display":"","order":"","width":"","maxWidth":"","gridColumnEnd":{"desktop":"4"},"gridColumnStart":{"desktop":"3"},"gridRowStart":{"desktop":"1"},"gridRowEnd":{"desktop":"2"},"gridTemplateColumns":"","gridTemplateRows":""}} -->
+		<figure class="wp-block-image"><img alt="" style="object-fit:cover" /></figure>
+		<!-- /wp:image -->
 
-        <!-- wp:image {"scale":"cover","className":"","style":{"aspectRatio":{"all":""},"display":"","order":"","width":"","maxWidth":"","gridColumnEnd":{"desktop":"2"},"gridColumnStart":{"desktop":"1"},"gridRowStart":"","gridRowEnd":"","gridTemplateColumns":"","gridTemplateRows":""}} -->
-        <figure class="wp-block-image"><img alt="" style="object-fit:cover" /></figure>
-        <!-- /wp:image -->
+		<!-- wp:image {"scale":"cover","className":"","style":{"aspectRatio":{"all":""},"display":"","order":"","width":"","maxWidth":"","gridColumnEnd":{"desktop":"2"},"gridColumnStart":{"desktop":"1"},"gridRowStart":"","gridRowEnd":"","gridTemplateColumns":"","gridTemplateRows":""}} -->
+		<figure class="wp-block-image"><img alt="" style="object-fit:cover" /></figure>
+		<!-- /wp:image -->
 
-        <!-- wp:image {"scale":"cover","className":"","style":{"aspectRatio":{"all":""},"display":"","order":"","width":"","maxWidth":"","gridColumnEnd":{"desktop":"4"},"gridColumnStart":{"desktop":"2"},"gridRowStart":"","gridRowEnd":"","gridTemplateColumns":"","gridTemplateRows":""}} -->
-        <figure class="wp-block-image"><img alt="" style="object-fit:cover" /></figure>
-        <!-- /wp:image -->
+		<!-- wp:image {"scale":"cover","className":"","style":{"aspectRatio":{"all":""},"display":"","order":"","width":"","maxWidth":"","gridColumnEnd":{"desktop":"4"},"gridColumnStart":{"desktop":"2"},"gridRowStart":"","gridRowEnd":"","gridTemplateColumns":"","gridTemplateRows":""}} -->
+		<figure class="wp-block-image"><img alt="" style="object-fit:cover" /></figure>
+		<!-- /wp:image -->
 
-        <!-- wp:image {"scale":"cover","className":"","style":{"aspectRatio":{"all":""},"display":"","order":"","width":"","maxWidth":"","gridColumnEnd":{"desktop":"5"},"gridColumnStart":{"desktop":"4","all":""},"gridRowStart":"","gridRowEnd":"","gridTemplateColumns":"","gridTemplateRows":""}} -->
-        <figure class="wp-block-image"><img alt="" style="object-fit:cover" /></figure>
-        <!-- /wp:image -->
-    </div>
-    <!-- /wp:group -->
+		<!-- wp:image {"scale":"cover","className":"","style":{"aspectRatio":{"all":""},"display":"","order":"","width":"","maxWidth":"","gridColumnEnd":{"desktop":"5"},"gridColumnStart":{"desktop":"4","all":""},"gridRowStart":"","gridRowEnd":"","gridTemplateColumns":"","gridTemplateRows":""}} -->
+		<figure class="wp-block-image"><img alt="" style="object-fit:cover" /></figure>
+		<!-- /wp:image -->
+	</div>
+	<!-- /wp:group -->
 </div>
 <!-- /wp:group -->
