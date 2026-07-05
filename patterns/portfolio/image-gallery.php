500f04de42fa62489aaed85bc4e1123937b5ded1
Atmostfear Entertainment <mail@alexdeborba.com>
2026-07-05 11:40:30 -0500

    Update image-gallery.php

--- patterns/portfolio/image-gallery.php
+++ patterns/portfolio/image-gallery.php
@@ -11,93 +11,93 @@
 
 <!-- wp:group {"metadata":{"categories":["portfolio"],"patternName":"image-gallery","name":"Portfolio Image Gallery"},"align":"full","style":{"spacing":{"blockGap":"2em","padding":{"top":"2em","bottom":"2em"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull" style="padding-top:2em;padding-bottom:2em">
-    <!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|xs"}}},"layout":{"type":"constrained"}} -->
-    <div class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--xs)">
-        <!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|sm"}}},"layout":{"type":"constrained","contentSize":"640px"}} -->
-        <div class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--sm)">
-            <!-- wp:group {"metadata":{"name":"Heading"},"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|lg"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
-            <div class="wp-block-group"
-                style="margin-bottom:var(--wp--preset--spacing--lg)">
-                <!-- wp:paragraph {"align":"center","className":"is-style-sub-heading"} -->
-                <p class="aligncenter has-text-align-center is-style-sub-heading aligncenter"><?php echo esc_html__( 'Portfolio', 'aegis' ); ?></p>
-                <!-- /wp:paragraph -->
+	<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|xs"}}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--xs)">
+		<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|sm"}}},"layout":{"type":"constrained","contentSize":"640px"}} -->
+		<div class="wp-block-group" style="padding-bottom:var(--wp--preset--spacing--sm)">
+			<!-- wp:group {"metadata":{"name":"Heading"},"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|lg"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+			<div class="wp-block-group"
+				style="margin-bottom:var(--wp--preset--spacing--lg)">
+				<!-- wp:paragraph {"align":"center","className":"is-style-sub-heading"} -->
+				<p class="aligncenter has-text-align-center is-style-sub-heading aligncenter"><?php echo esc_html__( 'Portfolio', 'aegis' ); ?></p>
+				<!-- /wp:paragraph -->
 
-                <!-- wp:heading {"textAlign":"center","style":{"display":{"all":"","mobile":""}},"fontSize":"48"} -->
-                <h2 class="wp-block-heading has-text-align-center has-48-font-size"><?php echo esc_html__( 'Latest Projects', 'aegis' ); ?></h2>
-                <!-- /wp:heading -->
+				<!-- wp:heading {"textAlign":"center","style":{"display":{"all":"","mobile":""}},"fontSize":"48"} -->
+				<h2 class="wp-block-heading has-text-align-center has-48-font-size"><?php echo esc_html__( 'Latest Projects', 'aegis' ); ?></h2>
+				<!-- /wp:heading -->
 
-                <!-- wp:paragraph {"align":"center","fontSize":"18"} -->
-                <p class="aligncenter has-text-align-center has-18-font-size aligncenter"><?php echo esc_html__( 'Manage your visual language from a single source of truth. By defining typography, colors, and spacing globally, you ensure instant consistency across the entire framework without repetitive manual adjustments.', 'aegis' ); ?></p>
-                <!-- /wp:paragraph -->
-            </div>
-            <!-- /wp:group -->
-        </div>
-        <!-- /wp:group -->
-    </div>
-    <!-- /wp:group -->
+				<!-- wp:paragraph {"align":"center","fontSize":"18"} -->
+				<p class="aligncenter has-text-align-center has-18-font-size aligncenter"><?php echo esc_html__( 'Manage your visual language from a single source of truth. By defining typography, colors, and spacing globally, you ensure instant consistency across the entire framework without repetitive manual adjustments.', 'aegis' ); ?></p>
+				<!-- /wp:paragraph -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:group -->
 
-    <!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|sm","left":"var:preset|spacing|sm"}}}} -->
-    <div class="wp-block-columns alignwide"><!-- wp:column -->
-        <div class="wp-block-column"><!-- wp:image -->
-            <figure class="wp-block-image"><img alt="" /></figure>
-            <!-- /wp:image -->
-        </div>
-        <!-- /wp:column -->
+	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|sm","left":"var:preset|spacing|sm"}}}} -->
+	<div class="wp-block-columns alignwide"><!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:image -->
+			<figure class="wp-block-image"><img alt="" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
 
-        <!-- wp:column -->
-        <div class="wp-block-column"><!-- wp:image -->
-            <figure class="wp-block-image"><img alt="" /></figure>
-            <!-- /wp:image -->
-        </div>
-        <!-- /wp:column -->
+		<!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:image -->
+			<figure class="wp-block-image"><img alt="" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
 
-        <!-- wp:column -->
-        <div class="wp-block-column"><!-- wp:image -->
-            <figure class="wp-block-image"><img alt="" /></figure>
-            <!-- /wp:image -->
-        </div>
-        <!-- /wp:column -->
+		<!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:image -->
+			<figure class="wp-block-image"><img alt="" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
 
-        <!-- wp:column -->
-        <div class="wp-block-column"><!-- wp:image -->
-            <figure class="wp-block-image"><img alt="" /></figure>
-            <!-- /wp:image -->
-        </div>
-        <!-- /wp:column -->
-    </div>
-    <!-- /wp:columns -->
+		<!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:image -->
+			<figure class="wp-block-image"><img alt="" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
 
-    <!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|sm","left":"var:preset|spacing|sm"},"margin":{"top":"var:preset|spacing|sm","bottom":"var:preset|spacing|lg"}}}} -->
-    <div class="wp-block-columns alignwide"
-        style="margin-top:var(--wp--preset--spacing--sm);margin-bottom:var(--wp--preset--spacing--lg)">
-        <!-- wp:column -->
-        <div class="wp-block-column"><!-- wp:image -->
-            <figure class="wp-block-image"><img alt="" /></figure>
-            <!-- /wp:image -->
-        </div>
-        <!-- /wp:column -->
+	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|sm","left":"var:preset|spacing|sm"},"margin":{"top":"var:preset|spacing|sm","bottom":"var:preset|spacing|lg"}}}} -->
+	<div class="wp-block-columns alignwide"
+		style="margin-top:var(--wp--preset--spacing--sm);margin-bottom:var(--wp--preset--spacing--lg)">
+		<!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:image -->
+			<figure class="wp-block-image"><img alt="" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
 
-        <!-- wp:column -->
-        <div class="wp-block-column"><!-- wp:image -->
-            <figure class="wp-block-image"><img alt="" /></figure>
-            <!-- /wp:image -->
-        </div>
-        <!-- /wp:column -->
+		<!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:image -->
+			<figure class="wp-block-image"><img alt="" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
 
-        <!-- wp:column -->
-        <div class="wp-block-column"><!-- wp:image -->
-            <figure class="wp-block-image"><img alt="" /></figure>
-            <!-- /wp:image -->
-        </div>
-        <!-- /wp:column -->
+		<!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:image -->
+			<figure class="wp-block-image"><img alt="" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
 
-        <!-- wp:column -->
-        <div class="wp-block-column"><!-- wp:image -->
-            <figure class="wp-block-image"><img alt="" /></figure>
-            <!-- /wp:image -->
-        </div>
-        <!-- /wp:column -->
-    </div>
-    <!-- /wp:columns -->
+		<!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:image -->
+			<figure class="wp-block-image"><img alt="" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
 </div>
 <!-- /wp:group -->
