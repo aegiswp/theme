586a2baed5da13481becef8e1402d3322d476069
Atmostfear Entertainment <mail@alexdeborba.com>
2026-07-05 15:12:24 -0500

    Create page-contact.php
    - Initial commit

---
+++ patterns/template/page-contact.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Page Contact
+ * Slug: page-contact
+ * Categories: template
+ * Template Types: page-contact
+ * Inserter: false
+ */
+?>
+<!-- wp:template-part {"slug":"header","tagName":"header","className":"site-header"} /-->
+<!-- wp:group {"tagName":"main","anchor":"main","metadata":{"name":"Main"},"align":"full","className":"site-main","layout":{"type":"constrained"}} -->
+<main id="main" class="wp-block-group alignfull site-main">
+	<!-- wp:pattern {"slug":"page-contact"} /-->
+</main>
+<!-- /wp:group -->
+<!-- wp:template-part {"slug":"footer","tagName":"footer","className":"site-footer"} /-->
