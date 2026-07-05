70819549f0aed3ce264e6057fdbe339a428805ce
Atmostfear Entertainment <mail@alexdeborba.com>
2026-07-05 15:16:54 -0500

    Create page-landing.php
    - Initial commit

---
+++ patterns/template/page-landing.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Page Landing
+ * Slug: page-landing
+ * Categories: template
+ * Template Types: page-landing
+ * Inserter: false
+ */
+?>
+<!-- wp:template-part {"slug":"header","tagName":"header","className":"site-header"} /-->
+<!-- wp:group {"tagName":"main","anchor":"main","metadata":{"name":"Main"},"align":"full","className":"site-main","layout":{"type":"constrained"}} -->
+<main id="main" class="wp-block-group alignfull site-main">
+	<!-- wp:pattern {"slug":"page-landing"} /-->
+</main>
+<!-- /wp:group -->
+<!-- wp:template-part {"slug":"footer","tagName":"footer","className":"site-footer"} /-->
