4e4820bf732de7f5938c0e3a445257d7b4fcfb07
Atmostfear Entertainment <mail@alexdeborba.com>
2026-07-05 14:30:45 -0500

    Create author-guest.php
    - Initial commit

---
+++ patterns/template/author-guest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Author Guest
+ * Slug: author-guest
+ * Categories: template
+ * Template Types: author-guest
+ * Inserter: false
+ */
+?>
+<!-- wp:template-part {"slug":"header","tagName":"header","className":"site-header"} /-->
+<!-- wp:group {"tagName":"main","anchor":"main","metadata":{"name":"Main"},"align":"full","className":"site-main","layout":{"type":"constrained"}} -->
+<main id="main" class="wp-block-group alignfull site-main">
+	<!-- wp:pattern {"slug":"author-archive-sidebar"} /-->
+</main>
+<!-- /wp:group -->
+<!-- wp:template-part {"slug":"footer","tagName":"footer","className":"site-footer"} /-->
