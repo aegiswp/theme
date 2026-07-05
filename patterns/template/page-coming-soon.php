5230371c991c7f594a98a454509af3c9e4df11d2
Atmostfear Entertainment <mail@alexdeborba.com>
2026-07-05 15:09:41 -0500

    Create page-coming-soon.php
    - Initial commit

---
+++ patterns/template/page-coming-soon.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Title: Page Coming Soon
+ * Slug: page-coming-soon
+ * Categories: template
+ * Template Types: page-coming-soon
+ * Inserter: false
+ */
+?>
+
+<!-- wp:template-part {"slug":"header","tagName":"header","className":"site-header"} /-->
+
+<!-- wp:group {"tagName":"main","anchor":"main","metadata":{"name":"Main"},"className":"site-main","layout":{"type":"constrained"}} -->
+<main id="main" class="wp-block-group site-main">
+	<!-- wp:woocommerce/coming-soon /-->
+</main>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","tagName":"footer","className":"site-footer"} /-->
