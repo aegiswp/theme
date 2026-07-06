4e8d21ef1d0ee88dba4fd55f256473477b5657cc
Atmostfear Entertainment <mail@alexdeborba.com>
2026-07-06 11:46:24 -0500

    Create index.tsx
    - Initial commit

---
+++ src/Blocks/slide/index.tsx
@@ -0,0 +1,32 @@
+/**
+ * Slide Block
+ *
+ * @package
+ * @since   1.0.0
+ */
+
+import { registerBlockType } from '@wordpress/blocks';
+import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
+
+import metadata from './block.json';
+
+function Edit() {
+	const blockProps = useBlockProps( {
+		className: 'aegis-slide splide__slide',
+	} );
+
+	return (
+		<div { ...blockProps }>
+			<InnerBlocks template={ [ [ 'core/image', {} ] ] } />
+		</div>
+	);
+}
+
+function save() {
+	return <InnerBlocks.Content />;
+}
+
+registerBlockType( metadata.name, {
+	edit: Edit,
+	save,
+} );
