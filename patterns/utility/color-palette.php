b2bfb6b1cf136000d8350c3b49758104ea5cecdc
Atmostfear Entertainment <mail@alexdeborba.com>
2026-07-06 08:16:08 -0500

    Update color-pallete.php

--- patterns/utility/color-palette.php
+++ patterns/utility/color-palette.php
@@ -9,17 +9,24 @@ use Aegis\Utilities\Color;
 use Aegis\Utilities\Str;
 
 $global_settings = wp_get_global_settings();
-$color_palette = $global_settings['color']['palette']['theme'] ?? [];
-$color_slugs = wp_list_pluck($color_palette, 'slug');
-$colors = [];
-$system_colors = Color::SYSTEM_COLORS;
+$color_palette   = $global_settings['color']['palette']['theme'] ?? array();
+$color_slugs     = wp_list_pluck( $color_palette, 'slug' );
+$colors          = array();
+$system_colors   = Color::SYSTEM_COLORS;
 
-if (!function_exists('color_palette_grid')) {
-	function color_palette_grid(string $name, array $shades): string
-	{
-		$count = count($shades);
-		$columns = $count >= 6 ? 6 : 3;
-		$rows = $count > 6 ? 2 : 1;
+if ( ! function_exists( 'aegis_color_palette_grid' ) ) {
+	/**
+	 * Build the block markup for a grid of color shades.
+	 *
+	 * @param string $name   Color name (palette slug prefix).
+	 * @param array  $shades Map of shade => hex value.
+	 *
+	 * @return string Generated block markup.
+	 */
+	function aegis_color_palette_grid( string $name, array $shades ): string {
+		$count       = count( $shades );
+		$columns     = $count >= 6 ? 6 : 3;
+		$rows        = $count > 6 ? 2 : 1;
 		$rows_mobile = $count > 6 ? 4 : 1;
 
 		$html = <<<HTML
@@ -27,7 +34,7 @@ if (!function_exists('color_palette_grid')) {
 <div class="wp-block-group">
 HTML;
 
-		$on_click = <<<JS
+		$on_click = <<<'JS'
 navigator.clipboard.writeText( '{hex}' );
 const copied = this.getElementsByClassName('has-display-none')[0];
 copied.classList.remove('has-display-none');
@@ -38,14 +45,14 @@ setTimeout(()=>{
 }, 1000);
 JS;
 
-		foreach ($shades as $shade => $hex) {
+		foreach ( $shades as $shade => $hex ) {
 			$color = $name . '-' . $shade;
 
-			if (in_array($shade, Color::SYSTEM_COLORS, true)) {
+			if ( in_array( $shade, Color::SYSTEM_COLORS, true ) ) {
 				$color = $shade;
 			}
 
-			$on_click_value = Str::reduce_whitespace(Str::remove_line_breaks(str_replace('{hex}', $hex, $on_click)));
+			$on_click_value = Str::reduce_whitespace( Str::remove_line_breaks( str_replace( '{hex}', $hex, $on_click ) ) );
 
 			$html .= <<<HTML
 <!-- wp:group {"style":{"dimensions":{"minHeight":"80px"},"border":{"radius":"10px"},"position":{"all":"relative"},"overflow":{"all":"hidden"}},"gradient":"checkerboard","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"},"shadowPreset":"xl","onclick":"$on_click_value"} -->
@@ -57,14 +64,14 @@ JS;
 	<p class="screen-reader-text">$color: $hex</p>
 	<!-- /wp:paragraph -->
 	<!-- wp:paragraph {"className":"has-display-none","style":{"position":{"all":"relative"}},"zIndex":{"all":"1"}} -->
-	<p class="has-display-none">Copied!</p>
+	<p class="has-display-none"><?php echo esc_html__( 'Copied!', 'aegis' ); ?></p>
 	<!-- /wp:paragraph -->
 </div>
 <!-- /wp:group -->
 HTML;
 		}
 
-		$html .= <<<HTML
+		$html .= <<<'HTML'
 </div>
 <!-- /wp:group -->
 HTML;
@@ -73,58 +80,58 @@ HTML;
 	}
 }
 
-foreach ($color_palette as $color) {
-	$exploded = explode('-', $color['slug']);
-	$name = $exploded[0] ?? '';
-	$shade = $exploded[1] ?? '';
+foreach ( $color_palette as $color ) {
+	$exploded = explode( '-', $color['slug'] );
+	$name     = $exploded[0] ?? '';
+	$shade    = $exploded[1] ?? '';
 
-	if (in_array($name, $system_colors, true)) {
+	if ( in_array( $name, $system_colors, true ) ) {
 		continue;
 	}
 
-	if (!isset($colors[$name])) {
-		$colors[$name] = [];
+	if ( ! isset( $colors[ $name ] ) ) {
+		$colors[ $name ] = array();
 	}
 
-	$colors[$name][$shade] = $color['color'];
+	$colors[ $name ][ $shade ] = $color['color'];
 }
 
 $width_50 = 'calc(50% - (var(--wp--style--block-gap) / 2))';
-$system = [
-	'current' => 'currentColor',
-	'inherit' => 'inherit',
+$system   = array(
+	'current'     => 'currentColor',
+	'inherit'     => 'inherit',
 	'transparent' => 'transparent',
-];
+);
 
 ?>
 <!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group">
 
-	<?php foreach ($colors as $color_name => $shades): ?>
-		<?php $width = count($shades) < 6 ? $width_50 : '100%'; ?>
+	<?php foreach ( $colors as $color_name => $shades ) : ?>
+		<?php $width = count( $shades ) < 6 ? $width_50 : '100%'; ?>
 
-		<!-- wp:group {"style":{"width":{"mobile":"100%","desktop":"<?php echo esc_attr($width); ?>"}},"layout":{"type":"default"},"fontSize":"14"} -->
+		<!-- wp:group {"style":{"width":{"mobile":"100%","desktop":"<?php echo esc_attr( $width ); ?>"}},"layout":{"type":"default"},"fontSize":"14"} -->
 		<div class="wp-block-group has-14-font-size">
 			<!-- wp:heading {"level":3,"textColor":"gray-400","fontSize":"16"} -->
 			<h3 class="wp-block-heading has-gray-400-color has-text-color has-16-font-size">
-				<?php echo esc_html(Str::title_case($color_name)); ?>
+				<?php echo esc_html( Str::title_case( $color_name ) ); ?>
 			</h3>
 			<!-- /wp:heading -->
 
-			<?php echo color_palette_grid($color_name, $shades); ?>
+			<?php echo aegis_color_palette_grid( $color_name, $shades ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Generated block markup; inner values are escaped. ?>
 		</div>
 		<!-- /wp:group -->
 	<?php endforeach; ?>
 
-	<!-- wp:group {"style":{"width":{"mobile":"100%","desktop":"<?php echo esc_attr($width_50); ?>"}},"layout":{"type":"default"},"fontSize":"14"} -->
+	<!-- wp:group {"style":{"width":{"mobile":"100%","desktop":"<?php echo esc_attr( $width_50 ); ?>"}},"layout":{"type":"default"},"fontSize":"14"} -->
 	<div class="wp-block-group has-14-font-size">
 		<!-- wp:heading {"level":3,"textColor":"gray-400","fontSize":"16"} -->
 		<h3 class="wp-block-heading has-gray-400-color has-text-color has-16-font-size">
-			<?php echo esc_html(Str::title_case('system')); ?>
+			<?php echo esc_html( Str::title_case( 'system' ) ); ?>
 		</h3>
 		<!-- /wp:heading -->
 
-		<?php echo color_palette_grid('system', $system); ?>
+		<?php echo aegis_color_palette_grid( 'system', $system ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Generated block markup; inner values are escaped. ?>
 	</div>
 	<!-- /wp:group -->
 
