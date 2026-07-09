68e1278776506ba61fd5ff580ed5037aacf5d914
Atmostfear Entertainment <mail@alexdeborba.com>
2026-07-09 11:18:21 -0500

    Update NotFoundException.php

--- vendor/aegis/container/src/Exceptions/NotFoundException.php
+++ vendor/aegis/container/src/Exceptions/NotFoundException.php
@@ -2,74 +2,58 @@
 /**
  * Not Found Exception
  *
- * Defines the exception for missing dependencies in the dependency injection
- * container of the Aegis Framework.
+ * This file defines an exception that is thrown when a service or dependency
+ * cannot be found in the container. It adheres to the PSR-11 standard by
+ * implementing the `NotFoundExceptionInterface`.
  *
  * Responsibilities:
- * - Handles errors when requested dependencies are not found in the Aegis
- *   Framework container
- * - Ensures interoperability with other PSR-11 compatible containers and
- *   libraries by implementing NotFoundExceptionInterface
+ * - Provides a specific exception for "not found" errors in the container.
+ * - Implements `Psr\Container\NotFoundExceptionInterface` for interoperability.
  *
  * @package    Aegis\Container\Exceptions
- * @since      1.0.0
+ * @since      0.1.0
  * @author     Atmostfear Entertainment
  * @link       https://github.com/aegiswp/theme
- *
- * For developer documentation and onboarding. No logic changes in this
- * documentation update.
  */
 
-// Enforces strict type checking for all code in this file, ensuring type safety.
-declare(strict_types=1);
+// Enforces strict type checking for all code in this file, ensuring type safety for not found exception.
+declare( strict_types=1 );
 
-// Defines the namespace for the not found exception functionality within the Aegis Framework.
+// Declares the namespace for the not found exception.
 namespace Aegis\Container\Exceptions;
 
-// Imports the base Exception class and the PSR-11 NotFoundExceptionInterface for use in the custom exception.
+// Imports classes, interfaces, and functions used by the not found exception.
 use Exception;
 use Psr\Container\NotFoundExceptionInterface;
 use function __;
 use function sprintf;
 
 /**
- * Not Found Exception for the Aegis Dependency Injection Container.
- *
- * This exception is thrown when a requested dependency (service or parameter)
- * could not be found in the container. It implements the
- * {@link https://www.php-fig.org/psr/psr-11/ PSR-11 NotFoundExceptionInterface},
- * ensuring compatibility with other PSR-11 compliant containers.
- *
- * @see Exception
- * @see Psr\Container\NotFoundExceptionInterface
- *
- * @since 1.0.0
+ * Exception thrown when no entry was found in the container.
+ * This exception complies with the PSR-11 standard for "not found" errors.
  *
  * @package Aegis\Container\Exceptions
+ * @since 0.1.0
  */
-// Declares the NotFoundException class, which extends the base Exception and implements the PSR-11 NotFoundExceptionInterface.
-class NotFoundException extends Exception implements NotFoundExceptionInterface
-{
+class NotFoundException extends Exception implements NotFoundExceptionInterface {
 
 	/**
-	 * Constructor for the NotFoundException.
+	 * NotFoundException constructor.
 	 *
-	 * Initializes the exception with a formatted, translatable message indicating
-	 * which dependency was not found. It leverages WordPress's internationalization
-	 * functions to make the error message translation-ready.
+	 * @since 0.1.0
 	 *
-	 * @since 1.0.0
-	 *
-	 * @param string $id The unique identifier of the dependency that could not be found in the container.
-	 *
-	 * @return void
+	 * @param string $id The identifier of the service that was not found.
 	 */
-	public function __construct(string $id)
-	{
+	public function __construct( string $id ) {
 
-		/* translators: %s: Dependency ID */
-		$message = __('%s not found.', 'aegis');
+		if ( function_exists( '__' ) ) {
+			/* translators: %s: Dependency ID */
+			$message = __( '%s not found.', 'aegis' );
+		} else {
+			$message = '%s not found.';
+		}
 
-		parent::__construct(sprintf($message, $id));
+		parent::__construct( sprintf( $message, $id ) );
 	}
+
 }
