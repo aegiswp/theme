6a8e155f1e147a9d757fe3747c2eb74c9c13124c
Atmostfear Entertainment <mail@alexdeborba.com>
2026-07-09 18:22:36 -0500

    Update JS.php

--- vendor/aegis/dom/src/JS.php
+++ vendor/aegis/dom/src/JS.php
@@ -1,70 +1,57 @@
 <?php
-/**
- * JS Utility Class
- *
- * Provides helper methods for formatting and manipulating JavaScript code
- * within the Aegis Framework.
- *
- * Responsibilities:
- * - Formats inline JavaScript for safer and cleaner output
- * - Assists with JavaScript string and code manipulations for theme and plugin development
- *
- * @package    Aegis\Dom
- * @since      1.0.0
- * @author     Atmostfear Entertainment
- * @link       https://github.com/aegiswp/theme
- *
- * For developer documentation and onboarding. No logic changes in this
- * documentation update.
- */
 
-// Ensures strict type checking for all code in this file.
-declare(strict_types=1);
+// Enforces strict type checking for all code in this file, ensuring type safety for javascript utility helpers.
+declare( strict_types=1 );
 
-// Declares the namespace for JS utilities within the Aegis Framework.
+// Declares the namespace for the javascript utility helpers.
 namespace Aegis\Dom;
 
-// Imports PHP built-in functions and WordPress helpers used by the JS utility class.
+// Imports classes, interfaces, and functions used by the javascript utility helpers.
 use function apply_filters;
 use function preg_replace;
 use function rtrim;
 use function str_replace;
 use function trim;
 
-// Implements a utility class for JavaScript manipulation and formatting.
-
-class JS
-{
+/**
+ * JS utility class for handling inline JavaScript.
+ *
+ * @since 1.0.0
+ */
+class JS {
 
     /**
-     * Formats inline JS.
+     * Formats an inline JavaScript string.
+     *
+     * This function performs several operations to clean up and minify the JS string:
+     * - Converts double quotes to single quotes.
+     * - Trims trailing semicolons.
+     * - Collapses whitespace.
+     * - Removes invisible characters.
+     * - Removes line breaks.
      *
      * @since 1.0.0
      *
-     * @param string $js JS.
+     * @param string $js The JavaScript string to format.
      *
-     * @return string
+     * @return string The formatted JavaScript string.
      */
-    public static function format_inline_js(string $js): string
-    {
+    public static function format_inline_js( string $js ): string {
 
         // Correct double quotes to single quotes.
-        $js = str_replace('"', "'", $js);
+        $js = str_replace( '"', "'", $js );
 
         // Trim trailing semicolon.
-        $js = trim(rtrim($js, ';'));
+        $js = trim( rtrim( $js, ';' ) );
 
         // Remove whitespace.
-        $js = preg_replace('/\s+/', ' ', $js);
+        $js = preg_replace( '/\s+/', ' ', $js );
 
         // Remove zero width spaces and other invisible characters.
-        $js = preg_replace('/^[\pZ\pC]+|[\pZ\pC]+$/u', '', $js);
+        $js = preg_replace( '/^[\pZ\pC]+|[\pZ\pC]+$/u', '', $js );
 
         // Replace line breaks.
-        $js = str_replace(
-            ["\r", "\n", PHP_EOL,],
-            '',
-            $js
+        $js = str_replace( [ "\r", "\n", PHP_EOL, ], '', $js
         );
 
         /**
@@ -72,7 +59,7 @@ class JS
          *
          * @var string $js Formatted JS.
          */
-        $js = apply_filters('aegis_format_inline_js', $js);
+        $js = apply_filters( 'aegis_format_inline_js', $js );
 
         return $js;
     }
