7b8c0931ce42a2d909d77c1af15e36c04a4b0172
Atmostfear Entertainment <mail@alexdeborba.com>
2026-08-26 10:31:20 -0500

    Create icon-block-editor.asset.php
    - Initial commit

---
+++ vendor/aegis/framework/public/js/icon-block-editor.asset.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+	'dependencies' => array(
+		'wp-blocks',
+		'wp-components',
+		'wp-compose',
+		'wp-data',
+		'wp-dom-ready',
+		'wp-element',
+		'wp-hooks',
+		'wp-i18n',
+		'wp-block-editor',
+	),
+	'version'      => '20260709070300',
+);
