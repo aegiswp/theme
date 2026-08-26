a4d92863815aa2d9722b5ca6a9f94264ff698a86
Atmostfear Entertainment <mail@alexdeborba.com>
2026-08-26 16:01:07 -0500

    Update visibility-toggles.asset.php

--- vendor/aegis/framework/public/js/visibility-toggles.asset.php
+++ vendor/aegis/framework/public/js/visibility-toggles.asset.php
@@ -1,1 +1,23 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-block-editor'), 'version' => '20250413180000');
+<?php
+/**
+ * Visibility toggles editor script asset file.
+ *
+ * @package Aegis\Framework
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+return array(
+	'dependencies' => array(
+		'wp-blocks',
+		'wp-components',
+		'wp-compose',
+		'wp-element',
+		'wp-hooks',
+		'wp-i18n',
+		'wp-block-editor',
+	),
+	'version'      => '20260709070300',
+);
