9d01f3281b9fc0229c92e2b99f76d84744da85f7
Atmostfear Entertainment <mail@alexdeborba.com>
2026-09-17 15:10:41 -0500

    Update video.php

--- patterns/hero/video.php
+++ patterns/hero/video.php
@@ -23,13 +23,15 @@
 
 			<!-- wp:paragraph {"align":"center","textColor":"neutral-200","fontSize":"20"} -->
 			<p class="has-text-align-center has-neutral-200-color has-text-color has-20-font-size"><?php echo esc_html__( 'We blend creativity with technology to deliver solutions that drive growth and leave lasting impressions.', 'aegis' ); ?></p>
+			<!-- /wp:paragraph -->
+
 			<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
-			<div class="wp-block-buttons"><!-- wp:button [] -->
+			<div class="wp-block-buttons"><!-- wp:button -->
 <div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#"><?php echo esc_html__( 'Explore Our Work', 'aegis' ); ?></a></div>
 <!-- /wp:button -->
 
 				<!-- wp:button {"className":"is-style-ghost"} -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#"><?php echo esc_html__( 'Contact Us', 'aegis' ); ?></a></div>
+<div class="wp-block-button is-style-ghost"><a class="wp-block-button__link wp-element-button" href="#"><?php echo esc_html__( 'Contact Us', 'aegis' ); ?></a></div>
 <!-- /wp:button -->
 			</div>
 			<!-- /wp:buttons -->
