e82d821d1faece45298dcd5b54313889440e15d8
Atmostfear Entertainment <mail@alexdeborba.com>
2026-09-17 15:05:57 -0500

    Update image-below.php

--- patterns/hero/image-below.php
+++ patterns/hero/image-below.php
@@ -31,7 +31,7 @@
 <!-- /wp:button -->
 
 		<!-- wp:button {"className":"is-style-ghost","onclick":"","size":"large","iconSize":"20px","iconPosition":"end"} -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#features"><?php echo esc_html__( 'Learn More', 'aegis' ); ?></a></div>
+<div class="wp-block-button is-style-ghost"><a class="wp-block-button__link wp-element-button" href="#features"><?php echo esc_html__( 'Learn More', 'aegis' ); ?></a></div>
 <!-- /wp:button -->
 	</div>
 	<!-- /wp:buttons -->
