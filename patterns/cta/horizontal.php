e6f5e51cdcb72880f7287f3ab8187bdc425a937e
Atmostfear Entertainment <mail@alexdeborba.com>
2026-09-17 10:58:04 -0500

    Update horizontal.php

--- patterns/cta/horizontal.php
+++ patterns/cta/horizontal.php
@@ -25,11 +25,11 @@
 		<!-- wp:column {"verticalAlignment":"center"} -->
 		<div class="wp-block-column is-vertically-aligned-center"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right","orientation":"horizontal"}} -->
 			<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-fill","onclick":"","size":"large","iconSet":"WordPress","iconName":"external","iconSize":"20px","iconPosition":"end","iconSvgString":"<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" role=\"img\" aria-labelledby=\"icon-697bcb78d4ff5\" data-icon=\"wordpress-external\" width=\"24\" height=\"24\" fill=\"currentColor\"><title id=\"icon-697bcb78d4ff5\">External Icon</title><path d=\"M19.5 4.5h-7V6h4.44l-5.97 5.97 1.06 1.06L18 7.06v4.44h1.5v-7Zm-13 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-3H17v3a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h3V5.5h-3Z\"></path></svg>"} -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#"><?php echo esc_html__( 'Get Started', 'aegis' ); ?></a></div>
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link wp-element-button" href="#"><?php echo esc_html__( 'Get Started', 'aegis' ); ?></a></div>
 <!-- /wp:button -->
 
 				<!-- wp:button {"className":"is-style-ghost","onclick":"","size":"large","iconSet":"WordPress","iconName":"arrow-right","iconSize":"20px","iconPosition":"end","iconSvgString":"<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" role=\"img\" aria-labelledby=\"icon-690364284e77a\" data-icon=\"wordpress-arrow-right\" width=\"24\" height=\"24\" fill=\"currentColor\"><title id=\"icon-690364284e77a\">Arrow Right Icon</title><path d=\"m14.5 6.5-1 1 3.7 3.7H4v1.6h13.2l-3.7 3.7 1 1 5.6-5.5z\"></path></svg>"} -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#"><?php echo esc_html__( 'Learn More', 'aegis' ); ?></a></div>
+<div class="wp-block-button is-style-ghost"><a class="wp-block-button__link wp-element-button" href="#"><?php echo esc_html__( 'Learn More', 'aegis' ); ?></a></div>
 <!-- /wp:button -->
 			</div>
 			<!-- /wp:buttons -->
