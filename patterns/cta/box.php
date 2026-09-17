a4e16b9f6f78549ee8fd15e9dcbbb721c86c43b7
Atmostfear Entertainment <mail@alexdeborba.com>
2026-09-17 10:48:20 -0500

    Update box.php

--- patterns/cta/box.php
+++ patterns/cta/box.php
@@ -21,7 +21,7 @@
 
 		<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
 		<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-transparent","iconSet":"WordPress","iconName":"calendar","iconSize":"20px","iconPosition":"end","iconSvgString":"<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" role=\"img\" aria-labelledby=\"icon-696a6a1c63217\" data-icon=\"wordpress-calendar\" width=\"24\" height=\"24\" fill=\"currentColor\"><title id=\"icon-696a6a1c63217\">Calendar Icon</title><path d=\"M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm.5 16c0 .3-.2.5-.5.5H5c-.3 0-.5-.2-.5-.5V7h15v12zM9 10H7v2h2v-2zm0 4H7v2h2v-2zm4-4h-2v2h2v-2zm4 0h-2v2h2v-2zm-4 4h-2v2h2v-2zm4 0h-2v2h2v-2z\"></path></svg>"} -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#"><?php echo esc_html__( 'Book Consultation', 'aegis' ); ?></a></div>
+<div class="wp-block-button is-style-transparent"><a class="wp-block-button__link wp-element-button" href="#"><?php echo esc_html__( 'Book Consultation', 'aegis' ); ?></a></div>
 <!-- /wp:button -->
 		</div>
 		<!-- /wp:buttons -->
