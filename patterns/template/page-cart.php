31b264978ced9d9bd92ffeea63ab18742e898605
Atmostfear Entertainment <mail@alexdeborba.com>
2026-09-19 21:05:17 -0500

    Update page-cart.php

--- patterns/template/page-cart.php
+++ patterns/template/page-cart.php
@@ -83,7 +83,7 @@
 
 			<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
 			<div class="wp-block-buttons">
-				<!-- wp:button [] -->
+				<!-- wp:button -->
 <div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#"><?php echo esc_html__( 'Browse Products', 'aegis' ); ?></a></div>
 <!-- /wp:button -->
 			</div>
